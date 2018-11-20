246691e8b8834d2a14a4d927e71de592a3e635a1
Jose Galaz <jdgalaz@uc.cl>
2018-11-20 17:54:13 -0300

    show telemetry on html table

--- love/src/components/TelemetryLog/TelemetryLog.jsx
+++ love/src/components/TelemetryLog/TelemetryLog.jsx
@@ -2,23 +2,36 @@ import React, { Component } from 'react'
 
 export default class TelemetryLog extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            log: ['a','b','1'],
-        };
-    }
-
-
-
     render() {
-        // console.log(Object.keys(this.props.telemetry))
         return (
-            <div>
-                {/* {this.props.data.map((x) => <p key={x}>{x}</p>)} */}
-                {Object.keys(this.props.telemetry).map((x) => <p key={x}>{x}: {this.props.telemetry[x]}</p>)}
-                {this.props.scale}
-            </div>
+            <React.Fragment>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th> Component</th>
+                            <th>Telemetry</th>
+                            <th>Parameter</th>
+                            <th>Value</th>
+                        </tr>
+                        <tr>
+                            <td> Comp name</td>
+                            <td> Tel name</td>
+                            <td>{Object.keys(this.props.telemetry).map( 
+                                (key)=> <p key={key}>{key}</p>
+                            )}</td>
+                            <td>
+                                {Object.keys(this.props.telemetry).map( 
+                                    (key)=> <p key={key}>{this.props.telemetry[key]}</p>
+                                )}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div>
+                    
+                </div>
+            </React.Fragment>
+
         )
     }
 }
