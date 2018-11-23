e0ed9476429b268fb2247a48e84f152579835558
José  Galaz <jdgalazm@gmail.com>
2018-11-23 18:03:42 -0300

    mv fakeData from RawTelTable to separate file

--- love/src/App.js
+++ love/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import sockette from 'sockette';
 import TelemetryLog from './components/TelemetryLog/TelemetryLog';
 import RawTelemetryTable from './components/HealthStatusSummary/RawTelemetryTable/RawTelemetryTable';
-
+import fakeData from './components/HealthStatusSummary/RawTelemetryTable/fakeData';
 class App extends Component {
 
   constructor() {
@@ -11,57 +11,7 @@ class App extends Component {
     this.state ={
       telemetryName: "interestedProposal"
 		}
-		this.data = {
-			'scheduler': {
-				'domeConfig': {
-					'timestamp': '2022/04/25 20:03:10',
-					'nParams': 3,
-					'parameters': [
-						{
-							'name': 'Altitude max speed',
-							'param_name': 'altitude_maxspeed',
-							'data_type': 'double',
-							'value': 1,
-							'units': 'm/s'
-						},
-						{
-							'name': 'Altitude acceleration',
-							'param_name': 'altitude_accel',
-							'data_type': 'double',
-							'value': 2,
-							'units': 'm/s²'
-						},
-						{
-							'name': 'Altitude deceleration',
-							'param_name': 'altitude_decel',
-							'data_type': 'double',
-							'value': 3,
-							'units': 'm/s²'
-						},
-					]
-				},
-				'cloud': {
-					'timestamp': '2022/04/25 20:04:10',
-					'nParams': 2,
-					'parameters': [
-						{
-							'name': 'timestamp',
-							'param_name': 'timestamp',
-							'data_type': 'timestamp',
-							'value': '2022/04/25 10:04:10',
-							'units': 'YYYY/MM/DD HH:MM:SS'
-						},
-						{
-							'name': 'cloud',
-							'param_name': 'cloud',
-							'data_type': 'double',
-							'value': 2.0,
-							'units': '?'
-						},
-					]
-				},
-			}
-		}
+		
 
     const socket = sockette('ws://localhost:8000/ws/subscription/', {
       onopen: e => socket.json({ "option": "subscribe", "data": this.state.telemetryName }),
@@ -82,7 +32,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <RawTelemetryTable data={this.data}></RawTelemetryTable>
+        <RawTelemetryTable data={fakeData}></RawTelemetryTable>
 				<TelemetryLog telemetry={{...this.state}} 
 											telemetryName={this.state.telemetryName}></TelemetryLog>
       </div>
