400150b613fddc407e043e493ad847c28dc8ef6b
José  Galaz <jdgalazm@gmail.com>
2018-11-23 18:02:21 -0300

    lift data from RawTelemetryTable

--- love/src/App.js
+++ love/src/App.js
@@ -10,7 +10,59 @@ class App extends Component {
     super();
     this.state ={
       telemetryName: "interestedProposal"
-    }
+		}
+		this.data = {
+			'scheduler': {
+				'domeConfig': {
+					'timestamp': '2022/04/25 20:03:10',
+					'nParams': 3,
+					'parameters': [
+						{
+							'name': 'Altitude max speed',
+							'param_name': 'altitude_maxspeed',
+							'data_type': 'double',
+							'value': 1,
+							'units': 'm/s'
+						},
+						{
+							'name': 'Altitude acceleration',
+							'param_name': 'altitude_accel',
+							'data_type': 'double',
+							'value': 2,
+							'units': 'm/s²'
+						},
+						{
+							'name': 'Altitude deceleration',
+							'param_name': 'altitude_decel',
+							'data_type': 'double',
+							'value': 3,
+							'units': 'm/s²'
+						},
+					]
+				},
+				'cloud': {
+					'timestamp': '2022/04/25 20:04:10',
+					'nParams': 2,
+					'parameters': [
+						{
+							'name': 'timestamp',
+							'param_name': 'timestamp',
+							'data_type': 'timestamp',
+							'value': '2022/04/25 10:04:10',
+							'units': 'YYYY/MM/DD HH:MM:SS'
+						},
+						{
+							'name': 'cloud',
+							'param_name': 'cloud',
+							'data_type': 'double',
+							'value': 2.0,
+							'units': '?'
+						},
+					]
+				},
+			}
+		}
+
     const socket = sockette('ws://localhost:8000/ws/subscription/', {
       onopen: e => socket.json({ "option": "subscribe", "data": this.state.telemetryName }),
       onmessage: this.receiveMsg,
@@ -30,8 +82,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <RawTelemetryTable></RawTelemetryTable>
-        <TelemetryLog telemetry={{...this.state}} telemetryName={this.state.telemetryName}></TelemetryLog>
+        <RawTelemetryTable data={this.data}></RawTelemetryTable>
+				<TelemetryLog telemetry={{...this.state}} 
+											telemetryName={this.state.telemetryName}></TelemetryLog>
       </div>
     );
   }
--- love/src/components/HealthStatusSummary/RawTelemetryTable/RawTelemetryTable.jsx
+++ love/src/components/HealthStatusSummary/RawTelemetryTable/RawTelemetryTable.jsx
@@ -4,57 +4,6 @@ import styles from './RawTelemetryTable.module.css';
 export default class RawTelemetryTable extends PureComponent {
     constructor() {
         super();
-        let data = {
-            'scheduler': {
-                'domeConfig': {
-                    'timestamp': '2022/04/25 20:03:10',
-                    'nParams': 3,
-                    'parameters': [
-                        {
-                            'name': 'Altitude max speed',
-                            'param_name': 'altitude_maxspeed',
-                            'data_type': 'double',
-                            'value': 1,
-                            'units': 'm/s'
-                        },
-                        {
-                            'name': 'Altitude acceleration',
-                            'param_name': 'altitude_accel',
-                            'data_type': 'double',
-                            'value': 2,
-                            'units': 'm/s²'
-                        },
-                        {
-                            'name': 'Altitude deceleration',
-                            'param_name': 'altitude_decel',
-                            'data_type': 'double',
-                            'value': 3,
-                            'units': 'm/s²'
-                        },
-                    ]
-                },
-                'cloud': {
-                    'timestamp': '2022/04/25 20:04:10',
-                    'nParams': 2,
-                    'parameters': [
-                        {
-                            'name': 'timestamp',
-                            'param_name': 'timestamp',
-                            'data_type': 'timestamp',
-                            'value': '2022/04/25 10:04:10',
-                            'units': 'YYYY/MM/DD HH:MM:SS'
-                        },
-                        {
-                            'name': 'cloud',
-                            'param_name': 'cloud',
-                            'data_type': 'double',
-                            'value': 2.0,
-                            'units': '?'
-                        },
-                    ]
-                },
-            }
-        }
 
         let filters = {
             'component': { 'type': 'regexp', 'value': (new RegExp('(?:)')) },
@@ -74,15 +23,12 @@ export default class RawTelemetryTable extends PureComponent {
             'altitude_accel': 'return 0;',
         }
 
-        let parsedData = this.convertData(data);
 
         this.state = {
-            data: parsedData,
             filters: filters,
             healthFunctions: healthFunctions,
         };
 
-        console.log(parsedData);
     }
 
     convertData = (data) => {
@@ -164,7 +110,8 @@ export default class RawTelemetryTable extends PureComponent {
     }
 
     render() {
-        const { data } = this.state;
+        let data = this.convertData(this.props.data);
+
         return (
             <table className={styles.rawTelemetryTable}>
                 <tbody>
