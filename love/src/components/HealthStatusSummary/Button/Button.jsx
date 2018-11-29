991d556a7e799a864752389856660d253297aa0a
spereirag <sebastian.pereira@inria.cl>
2018-11-29 16:10:09 -0300

    Update Button to accept secondary prop

--- love/src/components/HealthStatusSummary/Button/Button.jsx
+++ love/src/components/HealthStatusSummary/Button/Button.jsx
@@ -3,8 +3,11 @@ import styles from './Button.module.css'
 
 export default class Button extends Component {
   render() {
+    let {secondary, props} = {...this.props.secondary, ...this.props}
+    let secondaryClass = secondary ? styles.secondary : '';
+    let classnames = [styles.button, secondaryClass, this.props.className];
     return (
-      <button {...this.props} className={[styles.button, this.props.className].join(' ')}>
+      <button {...props} className={classnames.join(' ')}>
           {this.props.children}
       </button>
     )
