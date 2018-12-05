7f2bce4d7731d1d93b462d775fe5b6e98f0c273c
José  Galaz <jdgalazm@gmail.com>
2018-12-05 08:14:31 -0300

    sort when clicking on text in FilterDialog

--- love/src/components/HealthStatusSummary/RawTelemetryTable/FilterDialog/FilterDialog.jsx
+++ love/src/components/HealthStatusSummary/RawTelemetryTable/FilterDialog/FilterDialog.jsx
@@ -17,14 +17,22 @@ export default class FilterDialog extends Component {
         if (this.props.show && !this.container.current.contains(e.target) ) {
           this.props.closeFilterDialogs();
         }
-      };
+    };
+
+    sortAscending = ()=>{
+        this.props.changeSortDirection('ascending',this.props.columnName);
+    }
+
+    sortDescending = ()=>{
+        this.props.changeSortDirection('descending',this.props.columnName)
+    }
     
     render(){
         return(
             <div className={styles.superContainer} >
                 <div ref={this.container} className={styles.filterContainer + ' '+ (this.props.show ? styles.show: styles.hide) }>
-                    <p>Sort A - Z</p>
-                    <p>Sort Z - A</p>
+                    <p onClick={this.sortAscending}>Sort A - Z</p>
+                    <p onClick={this.sortDescending}>Sort Z - A</p>
                     <input type="text" className={styles.filterInput} onChange={this.props.changeFilter} />
                 </div>
             </div>
