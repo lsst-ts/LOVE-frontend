0e11fd07684a162568bb2c8892d184c3aaf3339d
José  Galaz <jdgalazm@gmail.com>
2018-12-11 06:48:50 -0300

    refactor FilterIcon>FilterButton

---
+++ love/src/components/HealthStatusSummary/RawTelemetryTable/ColumnHeader/FilterButton/FilterButton.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import styles from './FilterButton.module.css';
+import DefaultFilterIcon from '../../../../icons/FilterIcon/DefaultFilterIcon';
+
+export default class FilterButton extends Component {
+
+    
+    render(){
+        let statusStyle = this.props.selected ? styles.selected : styles.default;
+        
+        return (<div className={[styles.filterIconWrapper, statusStyle].join(' ')} 
+                    onClick={(e) => this.props.columnOnClick(e, this.props.filterName)}>
+            <DefaultFilterIcon style={styles.filterIcon}/>
+        </div>);
+    }
+
+}
+
+
+
+// export default class FilterIcon extends Component {
+//     render() {
+//         // active = 
+//         let statusStyle = this.props.selected ? styles.selected : styles.default;
+
+//         const icons = {
+//             default: <g>
+//                     <line x1="6.29" y1="13.7" x2="11.58" y2="13.7" />
+//                     <line x1="4.53" y1="9.29" x2="13.34" y2="9.29" />
+//                     <line x1="1" y1="4" x2="16.87" y2="4" />
+//                 </g>,
+//             filtered: <path d="M16.94,1.44A.68.68,0,0,0,16.27,1H1.73a.68.68,0,0,0-.67.44.67.67,0,0,0,.16.8l5.6,5.6V13a.7.7,0,0,0,.22.52l2.9,3.26a.67.67,0,0,0,.51.22.79.79,0,0,0,.29-.06.68.68,0,0,0,.44-.67V7.84l5.6-5.6A.67.67,0,0,0,16.94,1.44Z" />
+            
+//         }
+        
+//         let icon = icons.default;
+//         if(this.props.isFiltered){
+//             icon = icons.filtered;;
+//             statusStyle = styles.active;
+//         }
+        
+//         return (
+//             <svg className={[styles.filterIcon, statusStyle].join(' ')} viewBox='0 0 18 18'>
+//                 {icon}
+//             </svg>
+//         )
+//     }
+// }
---
+++ love/src/components/icons/FilterIcon/DefaultFilterIcon.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class DefaultFilterIcon extends Component {
+    render() {       
+        return (
+            <svg className={this.props.style} viewBox='0 0 18 18'>
+                <g>
+                    <line x1="6.29" y1="13.7" x2="11.58" y2="13.7" />
+                    <line x1="4.53" y1="9.29" x2="13.34" y2="9.29" />
+                    <line x1="1" y1="4" x2="16.87" y2="4" />
+                </g>
+            </svg>
+        )
+    }
+}
