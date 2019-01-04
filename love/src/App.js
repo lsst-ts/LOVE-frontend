30643057aa4aaaf9de9c456b36a35a98ef78dcac
spereirag <sebastian.pereira@inria.cl>
2019-01-04 11:04:30 -0300

    Add app router

--- love/src/App.js
+++ love/src/App.js
@@ -2,89 +2,101 @@ import React, { Component } from 'react';
 import './App.css';
 import sockette from 'sockette';
 import HealthStatusSummary from './components/HealthStatusSummary/HealthStatusSummary';
+import DataManagementFlow from './components/DataManagementFlow/DataManagementFlow';
+
+import { BrowserRouter } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 class App extends Component {
 
   constructor() {
     super();
-    this.state ={
-			telemetries: {
-				'interestedProposal':{
+    this.state = {
+      telemetries: {
+        'interestedProposal': {
           parameters: {},
           receptionTimeStamp: "2018/11/23 21:12:24."
         },
         "bulkCloud": {
           parameters: {
             "bulkCloud": 0.6713680575252166,
-          "timestamp": 0.5309269973966433
+            "timestamp": 0.5309269973966433
           },
           receptionTimeStamp: "2018/11/25 12:21:12"
         }
-			}
-		}
-		
+      }
+    }
+
     this.subscribeToTelemetry('all', this.receiveAllMsg);
   }
 
-  subscribeToTelemetry = (name, callback) =>{
-    const socket = sockette('ws://'+process.env.REACT_APP_WEBSOCKET_HOST+'/ws/subscription/', {
+  subscribeToTelemetry = (name, callback) => {
+    const socket = sockette('ws://' + process.env.REACT_APP_WEBSOCKET_HOST + '/ws/subscription/', {
       onopen: e => socket.json({ "option": "subscribe", "data": name }),
       onmessage: callback,
     });
   }
 
   receiveAllMsg = (msg) => {
-    let data = JSON.parse(msg.data);		
-		if(typeof data.data === 'object'){
-      
+    let data = JSON.parse(msg.data);
+    if (typeof data.data === 'object') {
+
       let newTelemetries = Object.assign({}, this.state.telemetries);
       let timestamp = new Date();
-      timestamp = timestamp.toISOString().slice(0,19).replace(/-/g,"/").replace("T", " ");
-      Object.entries(data.data).forEach((entry)=>{
+      timestamp = timestamp.toISOString().slice(0, 19).replace(/-/g, "/").replace("T", " ");
+      Object.entries(data.data).forEach((entry) => {
         let [telemetryName, parameters] = entry;
         let telemetry = {};
         telemetry[telemetryName] = {
-          parameters:  parameters,
+          parameters: parameters,
           receptionTimestamp: timestamp
         }
         Object.assign(newTelemetries, telemetry);
       }, this);
-      
+
       newTelemetries = JSON.parse(JSON.stringify(newTelemetries));
-      this.setState({telemetries: newTelemetries});
+      this.setState({ telemetries: newTelemetries });
     }
   }
 
   receiveMsg = (msg) => {
-    let data = JSON.parse(msg.data);		
-		if(typeof data.data === 'object'){
-			let timestamp = new Date();
-			timestamp = timestamp.toISOString().slice(0,20).replace("-","/").replace("T", " ");
-			let telemetry = {
-				"interestedProposal":{
-          parameters: {...data.data},
+    let data = JSON.parse(msg.data);
+    if (typeof data.data === 'object') {
+      let timestamp = new Date();
+      timestamp = timestamp.toISOString().slice(0, 20).replace("-", "/").replace("T", " ");
+      let telemetry = {
+        "interestedProposal": {
+          parameters: { ...data.data },
           receptionTimestamp: timestamp
         }
       }
       let newTelemetries = Object.assign({}, this.state.telemetries, telemetry);
       newTelemetries = JSON.parse(JSON.stringify(newTelemetries));
-      this.setState({telemetries: newTelemetries});
+      this.setState({ telemetries: newTelemetries });
     }
   }
 
   render() {
-    
+
     return (
       <div className="App">
-        <HealthStatusSummary telemetries={this.state.telemetries}> </HealthStatusSummary>
-	   </div>
+
+
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' render={() => (
+              <Redirect from='/' to='/health-status-summary' />
+            )} />
+            <Route path='/health-status-summary' 
+              render={() => <HealthStatusSummary telemetries={this.state.telemetries}> </HealthStatusSummary>} />
+            <Route path='/dm-flow' 
+              component={DataManagementFlow} />
+          </Switch>
+        </BrowserRouter>
+      </div>
     );
   }
 }
 
 export default App;
 
-
-			// <TelemetryLog telemetry={{...this.state.telemetry.parameters}} 
-				// 							telemetryName={this.state.telemetry.name}></TelemetryLog>
-   
