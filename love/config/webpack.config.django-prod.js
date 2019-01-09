c64e99767eaa9cdb8c7be33d8a5a121ca19e8f7f
spereirag <sebastian.pereira@inria.cl>
2019-01-09 12:47:22 -0300

    Remove unnecessary rule

--- love/config/webpack.config.django-prod.js
+++ love/config/webpack.config.django-prod.js
@@ -276,7 +276,7 @@ module.exports = {
           // "url" loader works just like "file" loader but it also embeds
           // assets smaller than specified size as data URLs to avoid requests.
           {
-            test: [/\.bmp$/, /\.gif$/, /\.otf$/, /\.jpe?g$/, /\.png$/],
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
             loader: require.resolve('url-loader'),
             options: {
               limit: 10000,
