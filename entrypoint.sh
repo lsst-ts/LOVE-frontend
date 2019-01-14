89e4166f84edcba42565916c1e9ce49283629929
spereirag <sebastian.pereira@inria.cl>
2019-01-14 12:36:15 -0300

    Update dockerfile to work with docker-compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 echo "Copying webpack-stats.prod.json"
-mkdir -p /home/LOVE/manager && cp /home/docker/webpack-stats.prod.json /home/LOVE/manager/webpack-stats.prod.json
+mkdir -p /home/LOVE/manager/manager
+cp /home/docker/webpack-stats.prod.json /home/LOVE/manager/manager/webpack-stats.prod.json
+echo "Copying build files"
+mkdir -p /home/LOVE/manager/manager/assets
+cp -r /home/docker/love/build /home/LOVE/manager/manager/assets/bundles
+echo "Done"
