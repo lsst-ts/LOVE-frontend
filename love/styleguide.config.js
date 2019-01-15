684edfc1704cb81d13f39819ffcfb51bf96d79a6
José  Galaz <jdgalazm@gmail.com>
2019-01-15 18:56:16 -0300

    example of how to modify style, theme of styleguide

--- love/styleguide.config.js
+++ love/styleguide.config.js
@@ -1,3 +1,30 @@
 module.exports = { 
-    webpackConfig: require( './config/webpack.config.dev.js' )
+    webpackConfig: require( './config/webpack.config.dev.js' ),
+    theme: {
+        color: {
+          link: 'red',
+          linkHover: 'salmon'
+        },
+        fontFamily: {
+          base: '"Comic Sans MS", "Comic Sans", cursive'
+        },
+        fontSize: {
+          base: 25
+        }
+      },
+      styles: {
+        Logo: {
+          logo: {
+            animation: 'blink ease-in-out 300ms infinite'
+          },
+          '@keyframes blink': {
+            to: { opacity: 0 }
+          }
+        },
+        StyleGuide:{
+          root:{
+            "background-color":'gray'
+          }
+        }
+      }
 }
