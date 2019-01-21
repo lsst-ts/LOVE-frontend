c8ff9791c8ddab76b1397952f6e129d597cec7a1
José  Galaz <jdgalazm@gmail.com>
2019-01-21 16:35:30 -0300

    add cross to tag and styles

--- love/src/components/HealthStatusSummary/RawTelemetryTable/TelemetrySelectionTag/TelemetrySelectionTag.jsx
+++ love/src/components/HealthStatusSummary/RawTelemetryTable/TelemetrySelectionTag/TelemetrySelectionTag.jsx
@@ -5,7 +5,8 @@ import styles from './TelemetrySelectionTag.module.css';
 export default class TelemetrySelectionTag extends Component {
     render(){
         return <span className={styles.tagContainer} title={this.props.telemetryKey}> 
-                {this.props.telemetryName}
+                    {this.props.telemetryName}
+                    <span className={styles.close}>x</span>
                 </span>
     }
 }
