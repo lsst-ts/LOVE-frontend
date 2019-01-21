ea1c3115fcffb661084c95b2c30b266c3c596267
José  Galaz <jdgalazm@gmail.com>
2019-01-21 17:40:15 -0300

    add button to remove telemetry from list (+test)

--- love/src/components/HealthStatusSummary/RawTelemetryTable/TelemetrySelectionTag/TelemetrySelectionTag.jsx
+++ love/src/components/HealthStatusSummary/RawTelemetryTable/TelemetrySelectionTag/TelemetrySelectionTag.jsx
@@ -3,10 +3,17 @@ import styles from './TelemetrySelectionTag.module.css';
 
 
 export default class TelemetrySelectionTag extends Component {
+
+    remove = ()=>{
+        this.props.remove(this.props.telemetryKey);
+    }
     render(){
         return <span className={styles.tagContainer} title={this.props.telemetryKey}> 
                     {this.props.telemetryName}
-                    <span className={styles.close}>x</span>
+                    <span 
+                        className={styles.remove} 
+                        title={'Remove '+this.props.telemetryKey} 
+                        onClick={this.remove}>x</span>
                 </span>
     }
 }
