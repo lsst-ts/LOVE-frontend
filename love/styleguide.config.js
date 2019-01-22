8dcbc7f4aabe289f4b230fdbb480b2e7e0183e10
José  Galaz <jdgalazm@gmail.com>
2019-01-22 13:00:46 -0300

    example of styleguide ignore

--- love/styleguide.config.js
+++ love/styleguide.config.js
@@ -5,5 +5,10 @@ module.exports = {
     webpackConfig: require( './config/webpack.config.dev.js' ),
     require: [
       path.join(__dirname, 'src/index.css')
-    ]
+    ],
+    ignore: ['**/__tests__/**', 
+    '**/*.test.{js,jsx,ts,tsx}', 
+    '**/*.spec.{js,jsx,ts,tsx}', 
+    '**/*.d.ts', 
+    "**/components/HealthStatusSummary/Button/Button.jsx"]
 }
