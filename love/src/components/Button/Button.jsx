1775ee825134e4137d2ba79cb43147ab4a11c5ee
José  Galaz <jdgalazm@gmail.com>
2019-01-24 16:42:29 -0300

    create general button with styles for its status

---
+++ love/src/components/Button/Button.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import styles from './Button.module.css';
+
+export default class Button extends Component {
+  static propTypes = {
+    status: PropTypes.string
+  }
+
+  static defaultProps = {
+      status: 'default',
+      size: 'default'
+  }
+
+  render() {
+    const statusStyleDict = {
+      default: styles['btnDefault'],
+      primary: styles['btnPrimary'],
+      success: styles['btnSuccess'],
+      info: styles['btnInfo'],
+      warning: styles['btnWarning'],
+      danger: styles['btnDanger'],
+      link: styles['btnLink']
+    }
+
+    const sizeStyleDict = {
+      large: 'btnLg',
+      default: ''
+    }
+
+
+    let {btn} = styles;
+    const statusStyle = statusStyleDict[this.props.status];
+    const sizeStyle = sizeStyleDict[this.props.size];
+
+    const style = [btn, statusStyle, statusStyle].join(' ');
+    console.log(this.props.status);
+    
+    return (
+      <button className={style} type="button">{this.props.children}</button>
+    )
+  }
+}
