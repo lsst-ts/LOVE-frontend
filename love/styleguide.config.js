2d8d03ec882105fe91916abbc93ee8a1167e5fb8
José  Galaz <jdgalazm@gmail.com>
2019-01-25 13:37:53 -0300

    add theme color dict to styleguide config

--- love/styleguide.config.js
+++ love/styleguide.config.js
@@ -18,5 +18,38 @@ module.exports = {
     '**/components/HealthStatusSummary/RawTelemetryTable/TelemetrySelectionTag/TelemetrySelectionTag.jsx',
     '**/components/HealthStatusSummary/RawTelemetryTable/fakeData.jsx',
     '**/components/icons/**'
-    ]
+    ],
+    theme: {
+      color: {
+        base: '#aabeca', //default font color, color of headers and paragraphs
+        light: 'red', // color of urls, "props&methods" "view code" "add examples to this components" and corner squares
+        lightest: '#yellow',
+        link: '#fcefe8', // --link-font-color
+        linkHover: '#667986', //  --link-hover-font-color
+        focus: 'rgba(22, 115, 177, 0.25)',//??
+        border: '#e8e8e8',
+        name: '#690',
+        type: '#905',
+        error: '#c00',
+        baseBackground: '#071925', // right panel, color taken from uiguide
+        codeBackground: '#f5f5f5',
+        sidebarBackground: 'black', // of the list of components 
+        ribbonBackground: '#e90',
+        ribbonText: '#fff',
+        // Based on default Prism theme
+        codeComment: '#6d6d6d',
+        codePunctuation: '#999',
+        codeProperty: '#905',
+        codeDeleted: '#905',
+        codeString: '#690',
+        codeInserted: '#690',
+        codeOperator: '#9a6e3a',
+        codeKeyword: '#1673b1',
+        codeFunction: '#DD4A68',
+        codeVariable: '#e90',
+      },
+      fontFamily: {
+        base: '"Comic Sans MS", "Comic Sans", cursive'
+      }
+    }
 }
