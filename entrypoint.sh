9ddddbd68787ae048635fbb0421fae26e971e645
Sebastian <sebafehlandt@gmail.com>
2019-01-30 17:54:28 -0300

    Remove unused files

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Copying webpack-stats.prod.json"
-mkdir -p /home/LOVE/manager/manager
-cp /home/docker/webpack-stats.prod.json /home/LOVE/manager/manager/webpack-stats.prod.json
-echo "Copying build files"
-mkdir -p /home/LOVE/manager/manager/assets
-cp -r /home/docker/love/build /home/LOVE/manager/manager/assets/bundles
-echo "Done"
