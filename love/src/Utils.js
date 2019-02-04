296a347c347f93f3251e9e5d17497b259cf3d662
José  Galaz <jdgalazm@gmail.com>
2019-02-04 13:01:03 -0300

    make unsubscribe work with "interestedProposal"

--- love/src/Utils.js
+++ love/src/Utils.js
@@ -1,15 +1,16 @@
 import sockette from 'sockette';
-
+let socket;
 export function subscribeToTelemetry(name, callback) {
-    const socket = sockette('ws://' + process.env.REACT_APP_WEBSOCKET_HOST + '/ws/subscription/', {
+    socket = sockette('ws://' + process.env.REACT_APP_WEBSOCKET_HOST + '/ws/subscription/', {
         onopen: e => socket.json({ "option": "subscribe", "data": name }),
         onmessage: callback,
     });
 };
 
 export function unsubscribeToTelemetry(name, callback) {
-    const socket = sockette('ws://' + process.env.REACT_APP_WEBSOCKET_HOST + '/ws/subscription/', {
-        onopen: e => socket.json({ "option": "unsubscribe", "data": name }),
-        onmessage: callback,
-    });
+    // socket = sockette('ws://' + process.env.REACT_APP_WEBSOCKET_HOST + '/ws/subscription/', {
+    //     onopen: e => socket.json({ "option": "unsubscribe", "data": name }),
+    //     onmessage: callback,
+    // });
+    socket.json({ "option": "unsubscribe", "data": name })
 };
