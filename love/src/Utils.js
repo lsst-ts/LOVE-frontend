a761a1560b71180d31cc3f1f85d6803e6ddf09ca
José  Galaz <jdgalazm@gmail.com>
2019-02-04 13:11:02 -0300

    create ManagerInterface and dummy subs/unsubs deprecated functions

--- love/src/Utils.js
+++ love/src/Utils.js
@@ -1,16 +1,22 @@
 import sockette from 'sockette';
-let socket;
-export function subscribeToTelemetry(name, callback) {
-    socket = sockette('ws://' + process.env.REACT_APP_WEBSOCKET_HOST + '/ws/subscription/', {
-        onopen: e => socket.json({ "option": "subscribe", "data": name }),
-        onmessage: callback,
-    });
-};
 
-export function unsubscribeToTelemetry(name, callback) {
-    // socket = sockette('ws://' + process.env.REACT_APP_WEBSOCKET_HOST + '/ws/subscription/', {
-    //     onopen: e => socket.json({ "option": "unsubscribe", "data": name }),
-    //     onmessage: callback,
-    // });
-    socket.json({ "option": "unsubscribe", "data": name })
-};
+export default class ManagerInterface{
+    constructor(name,callback){
+        this.socket = sockette('ws://' + process.env.REACT_APP_WEBSOCKET_HOST + '/ws/subscription/', {
+            onopen: e => this.socket.json({ "option": "subscribe", "data": name }),
+            onmessage: callback,
+        });
+    }    
+
+    unsubscribeToTelemetry = (name, callback) =>{
+        this.socket.json({ "option": "unsubscribe", "data": name })
+    }
+}
+
+export function subscribeToTelemetry(){
+    console.log('adsfafds')
+}
+
+export function unsubscribeToTelemetry(){
+    console.log('adsfafds')
+}
