77042951db688eee7b8e8a1559f7c510e207d845
José  Galaz <jdgalazm@gmail.com>
2019-02-05 18:04:34 -0300

    example mock ws server

--- love/src/components/TimeSeries/TimeSeries.test.jsx
+++ love/src/components/TimeSeries/TimeSeries.test.jsx
@@ -9,9 +9,10 @@ import {
 import React from 'react';
 import 'jest-dom/extend-expect'
 import TimeSeries from './TimeSeries';
+import {Server} from 'mock-socket';
+import sockette from 'sockette';
 
-
-jest.mock('../../Utils', () => {
+/* jest.mock('../../Utils', () => {
     return jest.fn().mockImplementation(() => {
         return {
             subscribeToTelemetry: (name, callback) => {
@@ -29,17 +30,17 @@ jest.mock('../../Utils', () => {
                     })
                 };
                 setTimeout(()=>{
-                    /*
-                        The callback is called exactly after setState({selectedRows})
-                        so this timeout gives it some time  until the state is updated 
-                    */
+                    
+                    //   The callback is called exactly after setState({selectedRows})
+                    //    so this timeout gives it some time  until the state is updated 
+                    
                     callback(message);
 
                 },1000)
             }
         }
     });
-});
+});*/
 
 
 
@@ -89,7 +90,6 @@ test('plot works', async () => {
 
     await waitForElement(() => getByText("filterChangeTime"));
 
-    debugger;
     setTimeout(()=>{
         const vegaElement = getByText("filterChangeTime");
         expect(vegaElement).toBeTruthy();
@@ -99,3 +99,37 @@ test('plot works', async () => {
 
 
 
+
+
+
+jest.useFakeTimers();
+
+describe('my ws test', () => {
+  it.only('connects', () => {
+    const msgs = [];
+    const url = 'ws://localhost:8080';
+    const mockServer = new Server(url);
+    mockServer.on('connection', server => {
+        server.send('you are connected');
+        debugger;
+    });
+
+    debugger;
+    const socket = sockette(url,{
+        onopen: e => {
+            console.log('open');
+            debugger;
+        },
+        onmessage: msg => {
+            console.log('callback');
+            msgs.push(msg);
+            debugger;
+        }
+    })
+
+
+    jest.runOnlyPendingTimers();
+
+    expect(msgs.length).toBe(1);
+  });
+});
