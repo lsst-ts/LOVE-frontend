e53e084ebed4964da72e6efb3eb15349e2bce2a5
Sebastian <sebafehlandt@gmail.com>
2019-02-11 17:29:51 -0300

    Add Html and styles to LoginComponent

--- love/src/components/Login/Login.jsx
+++ love/src/components/Login/Login.jsx
@@ -5,7 +5,37 @@ export default class Login extends Component {
   render() {
     return (
       <div className={styles.login}>
-        LOGIN
+        {/* <div className="col-md-4"> */}
+          <div className={styles.panel}>
+            <div className={styles.panelHeading}>
+              <h3 className={styles.panelTitle}>Login</h3>
+            </div>
+            <div className={styles.panelBody}>
+              <form method="post">
+                <form method="post">
+                  {/* {% csrf_token %}
+                  {% if form.errors %} */}
+                  <div className={styles.incorrectCredentialsDiv}>
+                    <p className={styles.incorrectCredentials}>
+                      Your username and password didn't match.
+                      Please try again.
+                    </p>
+                  </div>
+                  {/* {% endif %} */}
+                  <p>
+                    <label>Username</label>
+                    <input type="text" name="username" autofocus="" required="" id="id_username"></input>
+                  </p>
+                  <p>
+                    <label>Password</label>
+                    <input type="password" name="password" required="" id="id_password"></input>
+                  </p>
+                  <button type="submit" className={[styles.btn, styles.btnPrimary].join(" ")}>Login</button>
+                </form>
+              </form>
+            </div>
+          </div>
+        {/* </div> */}
       </div>
     )
   }
