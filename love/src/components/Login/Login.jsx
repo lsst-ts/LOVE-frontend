0d7eb863d3b35418bfd13258698ebfa04e19defa
Sebastian <sebafehlandt@gmail.com>
2019-02-11 15:27:07 -0300

    Add dummy LoginComponent

---
+++ love/src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import styles from './Login.module.css'
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div className={styles.login}>
+        LOGIN
+      </div>
+    )
+  }
+}
