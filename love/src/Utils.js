a28b69f3047e3ec1cfc36ab0e5d191feae51bcd9
José  Galaz <jdgalazm@gmail.com>
2019-02-11 17:18:44 -0300

    apply linter rules to Utils.js

--- love/src/Utils.js
+++ love/src/Utils.js
@@ -11,28 +11,34 @@ export default class ManagerInterface {
     this.callback = callback;
     if (this.socketPromise === null && this.socket === null) {
       this.socketPromise = new Promise((resolve) => {
-        this.socket = sockette(
-          `ws://${process.env.REACT_APP_WEBSOCKET_HOST}/ws/subscription/`,
-          {
-            onopen: () => {
-              this.socket.json({ option: 'subscribe', data: name });
-            },
-            onmessage: (msg) => {
-              this.callback(msg);
-              resolve();
-            },
+        this.socket = sockette(`ws://${process.env.REACT_APP_WEBSOCKET_HOST}/ws/subscription/`, {
+          onopen: () => {
+            this.socket.json({
+              option: 'subscribe',
+              data: name,
+            });
           },
-        );
+          onmessage: (msg) => {
+            this.callback(msg);
+            resolve();
+          },
+        });
       });
     } else {
       this.socketPromise.then(() => {
-        this.socket.json({ option: 'subscribe', data: name });
+        this.socket.json({
+          option: 'subscribe',
+          data: name,
+        });
       });
     }
   };
 
   unsubscribeToTelemetry = (name, callback) => {
-    this.socket.json({ option: 'unsubscribe', data: name });
+    this.socket.json({
+      option: 'unsubscribe',
+      data: name,
+    });
     this.callback = callback;
   };
 }
@@ -45,11 +51,7 @@ export default class ManagerInterface {
  * @param {Array} parametersNames
  * @param {date} timestamp
  */
-export const telemetryObjectToVegaList = (
-  telemetries,
-  parametersNames,
-  timestamp,
-) => {
+export const telemetryObjectToVegaList = (telemetries, parametersNames, timestamp) => {
   const newEntries = [];
 
   Object.keys(telemetries).forEach((stream) => {
@@ -57,9 +59,7 @@ export const telemetryObjectToVegaList = (
       const key = ['scheduler', stream, entry[0]].join('-');
       if (parametersNames.map((r) => r.key).includes(key)) {
         const newEntry = {
-          value: Array.isArray(entry[1].value)
-            ? entry[1].value[0]
-            : entry[1].value,
+          value: Array.isArray(entry[1].value) ? entry[1].value[0] : entry[1].value,
           date: timestamp,
           source: key.split('-')[2],
           dataType: entry[1].dataType,
@@ -69,71 +69,64 @@ export const telemetryObjectToVegaList = (
     });
   });
 
-    return newEntries;
-
+  return newEntries;
 };
 
-export const tableRowListToTimeSeriesObject = (selectedRows) =>{
-    let telemetries = {};
+export const tableRowListToTimeSeriesObject = (selectedRows) => {
+  const telemetries = {};
 
-    selectedRows.forEach((row)=>{
-        const [component, stream, parameter] = row.key.split('-');
+  selectedRows.forEach((row) => {
+    const [, stream, parameter] = row.key.split('-');
 
-        if(telemetries[stream] === undefined){
-            telemetries[stream] = {}
-        }
+    if (telemetries[stream] === undefined) {
+      telemetries[stream] = {};
+    }
 
+    telemetries[stream][parameter] = {
+      value: row.value.value,
+      dataType: row.value.dataType,
+    };
+  });
 
-        telemetries[stream][parameter] = {
-            value: row.value.value, 
-            dataType: row.value.dataType
-        };
-    });
-
-    return telemetries;
-}
+  return telemetries;
+};
 
 /**
  * Returns the value for a fake unit name, based on the name of a telemetry
  * @param {string} name
  */
 export const getFakeUnits = (name) => {
-    const fakeUnits = ['unit1', 'unit2', 'unit3', 'unit4'];
-    return fakeUnits[name.charCodeAt(0) % 4];
-}
-export const getFakeHistoricalTimeSeries = (selectedRows, dateStart, dateEnd) =>{
-
-    dateStart = new Date(dateStart);
-    dateEnd = new Date(dateEnd);
-
-    const telemetries = tableRowListToTimeSeriesObject(selectedRows);
-
-    const arraySize = (dateEnd.getTime()-dateStart.getTime())/2000;
-    const time = new Array(arraySize);
-    const tStart = dateStart.getTime();
-    const dateOffset = (new Date()).getTimezoneOffset()/60;
-    for(let i = 0; i<arraySize; i++){
-        const currentDate = new Date(tStart+i*2000);
-        let dateString = [currentDate.getFullYear(),
-            (currentDate.getMonth()+1),
-        currentDate.getUTCDate() ].join('/');
-
-        const hours = currentDate.getHours()+dateOffset;
-        const minutes = currentDate.getMinutes();
-        const seconds = currentDate.getSeconds();
-        dateString += ' '+hours+':'+minutes+':'+seconds;
-        time[i] = dateString;
-    }
-
-    return time.map((t) =>{
-        let currentValue = telemetryObjectToVegaList(telemetries, selectedRows, t);
-        const dateValue = (new Date(t)).getTime();
-
-        currentValue.forEach( (value)=>{
-            value.value = (Math.cos(dateValue/45*Math.PI/360)+1)/2-0.3+Math.random()*0.3;
-        });
-        return currentValue;
-    }).flat();
+  const fakeUnits = ['unit1', 'unit2', 'unit3', 'unit4'];
+  return fakeUnits[name.charCodeAt(0) % 4];
 };
+export const getFakeHistoricalTimeSeries = (selectedRows, dateStart, dateEnd) => {
+  const telemetries = tableRowListToTimeSeriesObject(selectedRows);
 
+  const arraySize = (new Date(dateEnd).getTime() - new Date(dateStart).getTime()) / 2000;
+  const time = new Array(arraySize);
+  const tStart = dateStart.getTime();
+  const dateOffset = new Date().getTimezoneOffset() / 60;
+  for (let i = 0; i < arraySize; i += 1) {
+    const currentDate = new Date(tStart + i * 2000);
+    let dateString = [currentDate.getFullYear(), currentDate.getMonth() + 1, currentDate.getUTCDate()].join('/');
 
+    const hours = currentDate.getHours() + dateOffset;
+    const minutes = currentDate.getMinutes();
+    const seconds = currentDate.getSeconds();
+    dateString += ` ${hours}:${minutes}:${seconds}`;
+    time[i] = dateString;
+  }
+
+  return time
+    .map((t) => {
+      const currentValue = telemetryObjectToVegaList(telemetries, selectedRows, t);
+      const dateValue = new Date(t).getTime();
+
+      currentValue.forEach((value) => {
+        // eslint-disable-next-line
+        value.value = (Math.cos(((dateValue / 45) * Math.PI) / 360) + 1) / 2 - 0.3 + Math.random() * 0.3;
+      });
+      return currentValue;
+    })
+    .flat();
+};
