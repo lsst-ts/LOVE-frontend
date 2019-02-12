4431ac510e108b7c36b8b3fbe52db1d9115ba6ea
Sebastian <sebafehlandt@gmail.com>
2019-02-12 12:17:09 -0300

    Add LoginComponent form handling

--- love/src/components/Login/Login.jsx
+++ love/src/components/Login/Login.jsx
@@ -4,8 +4,25 @@ import styles from './Login.module.css'
 
 export default class Login extends Component {
 
-  handleSubmit() {
-    console.log('Going to submit');
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
+    console.log('Handling submit, state: ', this.state);
+    event.preventDefault();
   }
 
   render() {
@@ -16,28 +33,41 @@ export default class Login extends Component {
             <h3 className={styles.panelTitle}>Login</h3>
           </div>
           <div className={styles.panelBody}>
-            {/* <form method="post"> */}
-              <form onSubmit={this.handleSubmit}>
-                {/* {% csrf_token %}
-                {% if form.errors %} */}
-                <div className={styles.incorrectCredentialsDiv}>
-                  <p className={styles.incorrectCredentials}>
-                    Your username and password didn't match.
-                    Please try again.
-                  </p>
-                </div>
-                {/* {% endif %} */}
-                <p className={styles.formEntry}>
-                  <label htmlFor="id_username" className={styles.label}>Username</label>
-                  <input type="text" name="username" autofocus="" required="" id="id_username"></input>
+            <form onSubmit={this.handleSubmit}>
+              {/* {% csrf_token %}
+              {% if form.errors %} */}
+              <div className={styles.incorrectCredentialsDiv}>
+                <p className={styles.incorrectCredentials}>
+                  Your username and password didn't match.
+                  Please try again.
                 </p>
-                <p className={styles.formEntry}>
-                  <label htmlFor="id_password"  className={styles.label}>Password</label>
-                  <input type="password" name="password" required="" id="id_password"></input>
-                </p>
-                <Button type="submit" status="primary">Login</Button>
-              </form>
-            {/* </form> */}
+              </div>
+              {/* {% endif %} */}
+              <p className={styles.formEntry}>
+                <label htmlFor="id_username" className={styles.label}>Username</label>
+                <input
+                  type="text"
+                  name="username"
+                  autoFocus=""
+                  required=""
+                  id="id_username"
+                  onChange={this.handleInputChange}
+                  value={this.state.username}
+                />
+              </p>
+              <p className={styles.formEntry}>
+                <label htmlFor="id_password"  className={styles.label}>Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  required=""
+                  id="id_password"
+                  onChange={this.handleInputChange}
+                  value={this.state.password}
+                />
+              </p>
+              <Button type="submit" status="primary">Login</Button>
+            </form>
           </div>
         </div>
       </div>
