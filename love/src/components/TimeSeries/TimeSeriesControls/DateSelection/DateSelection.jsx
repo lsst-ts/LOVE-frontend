2b1e976879b64747f0ba2c175e0187e38cb1f68d
spereirag <sebastian.pereira@inria.cl>
2019-02-12 10:54:45 -0300

    Add date selection to TimeSeriesControls

--- love/src/components/TimeSeries/TimeSeriesControls/DateSelection/DateSelection.jsx
+++ love/src/components/TimeSeries/TimeSeriesControls/DateSelection/DateSelection.jsx
@@ -1,25 +1,51 @@
-import React, { Component } from 'react'
-import Datetime from 'react-datetime'
-import styles from './DateSelection.module.css'
-import './react-datetime.css'
+import React, { PureComponent } from 'react';
+import Datetime from 'react-datetime';
+import styles from './DateSelection.module.css';
+import './react-datetime.css';
 
-export default class DateSelection extends Component {
-    render() {
-        return (
-            <div className={styles.datesContainer}>
-                <div className={styles.fromDateContainer}>
-                    <span className={styles.datetimeDescription}>From:</span>
-                    <div className={styles.datetimeContainer}>
-                        <Datetime onBlur={(momentDate) => console.log(momentDate, 'from')}></Datetime>
-                    </div>
-                </div>
-                <div className={styles.toDateContainer}>
-                    <span className={styles.datetimeDescription}>To:</span>
-                    <div className={styles.datetimeContainer}>
-                        <Datetime onBlur={(momentDate) => console.log(momentDate, 'to')}></Datetime>
-                    </div>
-                </div>
-            </div>
-        )
+export default class DateSelection extends PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      startDate: null,
+      endDate: null,
+    };
+  }
+
+  onDateSelected = (date, isStartDate) => {
+    if (isStartDate)
+      this.setState({
+        startDate: date,
+      });
+    else {
+      this.setState({
+        endDate: date,
+      });
     }
+  };
+
+  componentDidUpdate = () => {
+    if (this.state.startDate !== null && this.state.endDate !== null) {
+      this.props.setHistoricalData(this.state.startDate, this.state.endDate);
+    }
+  };
+
+  render() {
+    return (
+      <div className={styles.datesContainer}>
+        <div className={styles.fromDateContainer}>
+          <span className={styles.datetimeDescription}>From:</span>
+          <div className={styles.datetimeContainer}>
+            <Datetime onBlur={(momentDate) => this.onDateSelected(momentDate, true)} />
+          </div>
+        </div>
+        <div className={styles.toDateContainer}>
+          <span className={styles.datetimeDescription}>To:</span>
+          <div className={styles.datetimeContainer}>
+            <Datetime onBlur={(momentDate) => this.onDateSelected(momentDate, false)} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- love/src/components/TimeSeries/TimeSeriesControls/TimeSeriesControls.jsx
+++ love/src/components/TimeSeries/TimeSeriesControls/TimeSeriesControls.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import DateSelection from './DateSelection/DateSelection';
 import TimeWindow from './TimeWindow/TimeWindow';
 import PropTypes from 'prop-types';
+import styles from './TimeSeriesControls.module.css'
 
 export default class TimeSeriesControls extends Component {
     static propTypes = {
@@ -14,14 +15,34 @@ export default class TimeSeriesControls extends Component {
         liveMode: false,
     }
 
+    constructor() {
+        super();
+    }
+
+    handleChangeChk = () => {
+        if(this.props.isLive)
+            this.props.setLiveMode(false);
+        else
+            this.props.setLiveMode(true);
+    }
+
     render() {
         return (
             <div>
+                <div className={'switchContainer'}>
+                    <span className={[styles.modeSelection, this.props.isLive ? styles.highlightText : ''].join(' ')} onClick={() => this.props.setLiveMode(true)} >Live</span>
+                    <label className={styles.switch}>
+                        <input type="checkbox" checked={!this.props.isLive} onChange={this.handleChangeChk}/>
+                        <span className={[styles.slider, styles.round].join(' ')}></span>
+                    </label>
+                    <span className={[styles.modeSelection, !this.props.isLive ? styles.highlightText : ''].join(' ')} onClick={() => this.props.setLiveMode(false)} >Query</span>
+                </div>
+
                 {
-                    this.props.liveMode ?
+                    this.props.isLive ?
                         <TimeWindow setTimeWindow={this.props.setTimeWindow} timeWindow={this.props.timeWindow}></TimeWindow>
                         :
-                        <DateSelection></DateSelection>
+                        <DateSelection setHistoricalData={this.props.setHistoricalData}></DateSelection>
                 }
             </div>
         )
