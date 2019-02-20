0a89a55354fa50840cd407f099bbf5825f42996f
spereirag <sebastian.pereira@inria.cl>
2019-02-20 12:19:28 -0300

    TelemetryLog can subscribe/unsubscribe to telemetry streams

--- love/src/components/TelemetryLog/TelemetryLog.jsx
+++ love/src/components/TelemetryLog/TelemetryLog.jsx
@@ -4,22 +4,20 @@ import JSONPretty from 'react-json-pretty';
 import 'react-json-pretty/themes/monikai.css';
 
 export default class TelemetryLog extends Component {
-  constructor() {
+  constructor(props) {
     super();
     this.state = {
       msg: '',
       msgList: [],
+      msgNumber: 0,
+      category: props.category,
+      csc: props.csc,
+      stream: props.stream,
     };
     this.managerInterface = new ManagerInterface();
-    // this.managerInterface.subscribeToTelemetry('all', 'all', this.receiveMessage);
   }
 
-  componentDidMount = () => {
-    this.managerInterface.subscribeToStream(this.props.category, this.props.csc, this.props.stream, this.receiveMessage);
-  };
-
   receiveMessage = (msg) => {
-    console.log('subscribed', msg);
     this.setState({
       msg: msg.data,
     });
@@ -28,28 +26,83 @@ export default class TelemetryLog extends Component {
 
   updateMessageList = (msg) => {
     let newMsgList = this.state.msgList;
-    if(this.state.msgList.length > 10) {
-        newMsgList.shift();
-        this.setState({
-            msgList: [...newMsgList, msg]
-        });
+    if (this.state.msgList.length > 10) {
+      newMsgList.shift();
+      this.setState({
+        msgList: [...newMsgList, msg],
+        msgNumber: this.state.msgNumber + 1,
+      });
+    } else {
+      this.setState({
+        msgList: [...this.state.msgList, msg],
+        msgNumber: this.state.msgNumber + 1,
+      });
     }
-    else {
-        this.setState({
-            msgList: [...this.state.msgList, msg]
-        });
-    }
-  }
+  };
+
+  categoryChange = (e) => {
+    this.setState({
+      category: e.target.value,
+    });
+  };
+
+  cscChange = (e) => {
+    this.setState({
+      csc: e.target.value,
+    });
+  };
+
+  streamChange = (e) => {
+    this.setState({
+      stream: e.target.value,
+    });
+  };
+
+  subscribeToStream = () => {
+    this.managerInterface.subscribeToStream(
+      this.state.category,
+      this.state.csc,
+      this.state.stream,
+      this.receiveMessage,
+    );
+  };
+
+  unsubscribeToStream = () => {
+    this.managerInterface.unsubscribeToStream(
+      this.state.category,
+      this.state.csc,
+      this.state.stream,
+      this.receiveMessage,
+    );
+  };
 
   render() {
     return (
-      <div style={{textAlign: 'left'}}>
+      <div style={{ textAlign: 'left' }}>
         <h1>Telemetry log</h1>
-        {
-            this.state.msgList.slice().reverse().map((msg) => {
-                return <JSONPretty data={msg}></JSONPretty>
-            })
-        }
+        <div>
+          <div>
+            <span>Category </span>
+            <input onChange={this.categoryChange} value={this.state.category} />
+          </div>
+          <div>
+            <span>CSC </span>
+            <input onChange={this.cscChange} value={this.state.csc} />
+          </div>
+          <div>
+            <span>Stream </span>
+            <input onChange={this.streamChange} value={this.state.stream} />
+          </div>
+          <button onClick={this.subscribeToStream}>Subscribe</button>
+          <button onClick={this.unsubscribeToStream}>Unsubscribe</button>
+        </div>
+
+        {this.state.msgList
+          .slice()
+          .reverse()
+          .map((msg, index) => {
+            return <JSONPretty key={this.state.msgNumber - index} data={msg} />;
+          })}
       </div>
     );
   }
