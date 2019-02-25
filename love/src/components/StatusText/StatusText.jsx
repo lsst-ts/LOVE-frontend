45fc4ceb53ac976429706b9a0705bc20e2a0c579
spereirag <sebastian.pereira@inria.cl>
2019-02-25 11:02:18 -0300

    Refactor StatusText to render generic text

--- love/src/components/StatusText/StatusText.jsx
+++ love/src/components/StatusText/StatusText.jsx
@@ -1,31 +1,15 @@
-import React, { Component } from 'react'
-import styles from './StatusText.module.css'
+import React, { Component } from 'react';
+import styles from './StatusText.module.css';
 
 export default class StatusText extends Component {
-    // {this.props.statusCode}
+  render() {
+    const status = this.props.status;
+    let statusStyle = styles.undefined;
+    if (status === 'ok') statusStyle = styles.ok;
+    if (status === 'warning') statusStyle = styles.warning;
+    if (status === 'alert') statusStyle = styles.alert;
+    if (status === 'invalid') statusStyle = styles.invalid;
 
-    getRenderValue = (statusCode) => {
-        // console.log('satautscode', statusCode, this.props.getHealthText(statusCode))
-
-        switch (statusCode) {
-            case 0:
-                return <span className={[styles.status, styles.undefined].join(' ')}>{this.props.getHealthText(statusCode)}</span>
-            case -1:
-                return <span className={[styles.status, styles.invalid].join(' ')}>{this.props.getHealthText(statusCode)}</span>
-            case 1:
-                return <span className={[styles.status, styles.ok].join(' ')}>{this.props.getHealthText(statusCode)}</span>
-            case 2:
-                return <span className={[styles.status, styles.warning].join(' ')}>{this.props.getHealthText(statusCode)}</span>
-            case 3:
-                return <span className={[styles.status, styles.alert].join(' ')}>{this.props.getHealthText(statusCode)}</span>
-            default:
-                return <span className={[styles.status, styles.invalid].join(' ')}>{this.props.getHealthText(statusCode)}</span>
-        }
-    }
-
-    render() {
-        return (
-            this.getRenderValue(this.props.statusCode)
-        )
-    }
+    return <span className={[styles.status, statusStyle].join(' ')}>{this.props.children}</span>;
+  }
 }
