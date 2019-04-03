43a275775218dec599ccf0d153a6af1538ffd177
spereirag <sebastian.pereira@inria.cl>
2019-04-03 15:35:16 -0300

    App now remembers token

--- love/src/App.js
+++ love/src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      token: null,
+      token: ManagerInterface.getToken(),
       showSessionExpired: false,
       telemetries: {
         scheduler: {
