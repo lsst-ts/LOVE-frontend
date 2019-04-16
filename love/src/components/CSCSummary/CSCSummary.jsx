3e8589a8e2d3ed5200e76c4c734d8fa1aaeeaa3a
spereirag <sebastian.pereira@inria.cl>
2019-04-16 17:49:11 -0400

    Add CSCSummary tests

--- love/src/components/CSCSummary/CSCSummary.jsx
+++ love/src/components/CSCSummary/CSCSummary.jsx
@@ -3,64 +3,13 @@ import styles from './CSCSummary.module.css';
 import CSCRealm from './CSCRealm/CSCRealm';
 import Panel from '../Panel/Panel';
 import ManagerInterface, { hasFakeData } from '../../Utils';
+import {CSCSummaryHierarchy} from '../../Config';
 
 export default class CSCSummary extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      hierarchy: {
-        'Aux Telescope': {
-          'CSC Group 1': [
-            'ScriptQueue',
-            'scheduler',
-            'ATMonochromator',
-            'FiberSpectrograph',
-            'ATCamera',
-            'Scheduler3',
-            'ScriptQueue3',
-            'Scheduler4',
-            'ScriptQueue4',
-            'Scheduler5',
-            'ScriptQueue5',
-            'Scheduler6',
-            'ScriptQueue6',
-            'Scheduler7',
-            'ScriptQueue7',
-            'Scheduler8',
-            'ScriptQueue8',
-            'Scheduler9',
-            'ScriptQueue9',
-            'Scheduler10',
-            'ScriptQueue10',
-            'Scheduler11',
-            'ScriptQueue11',
-            'Scheduler12',
-            'ScriptQueue12',
-            'Scheduler13',
-            'ScriptQueue13',
-            'Scheduler14',
-            'ScriptQueue14',
-            'Scheduler15',
-            'ScriptQueue15',
-            'Scheduler16',
-            'ScriptQueue16',
-            'Scheduler17',
-            'ScriptQueue17',
-            'Scheduler18',
-            'ScriptQueue18',
-            'Scheduler19',
-            'ScriptQueue19',
-            'Scheduler20',
-          ],
-        },
-        'Main Telescope': {
-          'CSC Group 1': ['CSC4'],
-          'CSC Group 2': [],
-        },
-        Observatory: {
-          'CSC Group 1': [],
-        },
-      },
+      hierarchy: CSCSummaryHierarchy,
       data: {
         ScriptQueue: {
           summaryState: { priority: 1, summaryState: 2 },
