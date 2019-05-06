4d07f6fdf69d32865011ca45c687aa19ff927918
José Galaz <jose.galaz@inria.cl>
2019-05-06 16:47:45 -0400

    add store with dummy reducer

--- love/src/index.js
+++ love/src/index.js
@@ -5,11 +5,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
