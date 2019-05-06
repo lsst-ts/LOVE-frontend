8bf46cae60c8790c792f879b452d11706206fd54
spereirag <sebastian.pereira@inria.cl>
2019-05-06 17:15:54 -0400

    Add camera readout details

--- love/src/components/AuxTel/Camera/Camera.jsx
+++ love/src/components/AuxTel/Camera/Camera.jsx
@@ -24,26 +24,23 @@ export default class Camera extends Component {
         imagesInSequence: 3,
         images: [
           {
-            timestamp: Date.now(),
+            timestamp: new Date()
+              .toISOString()
+              .slice(0, 19)
+              .replace(/-/g, '/')
+              .replace('T', ' '),
             imageName: 'Image A',
             imageIndex: 0,
             exposureTime: 3,
             state: 'INTEGRATING',
-            readoutParameters: {
-              ccdNames: 'RnnSnn',
-              ccdType: 'E2V',
-              overRows: 1,
-              overCols: 1,
-              readRows: 1,
-              readCols: 1,
-              readCols2: 1,
-              preCols: 1,
-              preRows: 1,
-              postCols: 1,
-            },
+            readoutParameters: {},
           },
           {
-            timestamp: Date.now(),
+            timestamp: new Date()
+              .toISOString()
+              .slice(0, 19)
+              .replace(/-/g, '/')
+              .replace('T', ' '),
             imageName: 'Image B',
             imageIndex: 1,
             exposureTime: 3,
@@ -62,7 +59,11 @@ export default class Camera extends Component {
             },
           },
           {
-            timestamp: Date.now(),
+            timestamp: new Date()
+              .toISOString()
+              .slice(0, 19)
+              .replace(/-/g, '/')
+              .replace('T', ' '),
             imageName: 'Image C',
             imageIndex: 2,
             exposureTime: 3,
@@ -81,7 +82,11 @@ export default class Camera extends Component {
             },
           },
           {
-            timestamp: Date.now(),
+            timestamp: new Date()
+              .toISOString()
+              .slice(0, 19)
+              .replace(/-/g, '/')
+              .replace('T', ' '),
             imageName: 'Image D',
             imageIndex: 3,
             exposureTime: 3,
@@ -101,11 +106,20 @@ export default class Camera extends Component {
           },
         ],
       },
+      expandedRows: {},
     };
   }
 
-  clickGearIcon = (key) => {
-    console.log(key);
+  clickGearIcon = (rowId) => {
+    let { expandedRows } = this.state;
+    if (expandedRows[rowId]) expandedRows[rowId] = false;
+    else {
+      expandedRows = {};
+      expandedRows[rowId] = true;
+    }
+    this.setState({
+      expandedRows: { ...expandedRows },
+    });
   };
 
   render() {
@@ -137,28 +151,59 @@ export default class Camera extends Component {
                 <tr>
                   <th>Timestamp</th>
                   <th>Image name</th>
-                  <th>Exposure time</th>
+                  <th className={styles.narrowCol}>Exposure time</th>
                   <th className={styles.mediumCol}>State</th>
                 </tr>
               </thead>
               <tbody>
                 {this.state.imageSequence.images.map((image) => {
                   return (
-                    <tr key={image.imageIndex}>
-                      <td>{image.timestamp}</td>
-                      <td>{image.imageName}</td>
-                      <td>{image.exposureTime} s</td>
-                      <td className={[styles.statusColumn, styles.mediumCol].join(' ')}>
-                        <div className={styles.imageStatusWrapper}>
-                          <div className={styles.statusTextWrapper}>
-                            <StatusText status={getCameraStatusStyle(image.state)}>{image.state}</StatusText>
+                    <React.Fragment key={image.imageIndex}>
+                      <tr>
+                        <td className={styles.string}>{image.timestamp}</td>
+                        <td className={styles.string}>{image.imageName}</td>
+                        <td className={[styles.narrowCol, styles.number].join(' ')}>{image.exposureTime} s</td>
+                        <td
+                          className={[
+                            styles.statusColumn,
+                            styles.mediumCol,
+                            this.state.expandedRows[image.imageName] ? styles.selectedRow : '',
+                          ].join(' ')}
+                        >
+                          <div className={styles.imageStatusWrapper}>
+                            <div className={styles.statusTextWrapper}>
+                              <StatusText status={getCameraStatusStyle(image.state)}>{image.state}</StatusText>
+                            </div>
+                            <div onClick={() => this.clickGearIcon(image.imageName)} className={styles.gearIconWrapper}>
+                              <GearIcon active={true} />
+                            </div>
                           </div>
-                          <div onClick={() => this.clickGearIcon(image)} className={styles.gearIconWrapper}>
-                            <GearIcon active={true} />
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
+                        </td>
+                      </tr>
+                      {this.state.expandedRows[image.imageName] ? (
+                        <tr className={styles.selectedRow} key={`expanded-${image.imageName}`}>
+                          <td className={styles.imageDetailsCell}>
+                            <div className={styles.readoutParametersTitle}>Readout parameters</div>
+                            <div className={styles.readoutParameters}>
+                              {Object.keys(image.readoutParameters).length > 0 ? (
+                                Object.keys(image.readoutParameters).map((key) => {
+                                  return (
+                                    <React.Fragment key={key}>
+                                      <span className={styles.readoutParameterKey}>{key}</span>{' '}
+                                      <span className={styles.readoutParameterValue}>
+                                        {image.readoutParameters[key]}
+                                      </span>
+                                    </React.Fragment>
+                                  );
+                                })
+                              ) : (
+                                <span>None</span>
+                              )}
+                            </div>
+                          </td>
+                        </tr>
+                      ) : null}
+                    </React.Fragment>
                   );
                 })}
               </tbody>
