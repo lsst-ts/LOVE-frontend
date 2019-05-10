fb4361f6540fc1d800802213d133d78208de231e
José Galaz <jose.galaz@inria.cl>
2019-05-10 11:30:16 -0400

    use combineReducers

--- love/src/redux/reducers/index.js
+++ love/src/redux/reducers/index.js
@@ -1,20 +1,6 @@
-import { REQUEST_TOKEN, RECEIVE_TOKEN } from '../actionTypes';
-import { requestToken, receiveToken } from '../actions';
-import ManagerInterface from '../../Utils';
-export const defaultState = {
-  username:'',
-  token: ''
-}
+import { combineReducers } from "redux";
+import auth from './auth';
 
-export default function(state=defaultState, action) {
-  switch (action.type) {
-    case REQUEST_TOKEN: {
-      return Object.assign({}, state, { username: action.username });
-    }
-    case RECEIVE_TOKEN: {
-      return Object.assign({}, state, { token: action.token });
-    }
-    default:
-      return state;
-  }
-}
+export default combineReducers({
+  auth
+});
