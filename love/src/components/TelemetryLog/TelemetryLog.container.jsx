ea8e5b3c25ce0492e58c1edaacd75e5ce6ee91cf
José Galaz <jose.galaz@inria.cl>
2019-05-13 21:16:10 -0400

    unsubscribe onclick with redux

--- love/src/components/TelemetryLog/TelemetryLog.container.jsx
+++ love/src/components/TelemetryLog/TelemetryLog.container.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import TelemetryLog from './TelemetryLog';
-import {requestGroupSubscription} from '../../redux/actions/ws';
+import { requestGroupSubscription, requestGroupSubscriptionRemoval } from '../../redux/actions/ws';
 
-const TelemetryLogContainer = ({ data, subscribeToStream }) => {
-  return <TelemetryLog data={data} subscribeToStream={subscribeToStream}/>;
+const TelemetryLogContainer = ({ data, subscribeToStream, unsubscribeToStream }) => {
+  return <TelemetryLog data={data} subscribeToStream={subscribeToStream} unsubscribeToStream={unsubscribeToStream} />;
 };
 
 TelemetryLogContainer.defaultProps = {
   token: 'asd',
 };
 const mapStateToProps = (state) => {
-  const scriptqueue = state.ws.subscriptions.filter(
-    s => s.groupName === 'event-ScriptQueue-all'
-  );
+  const scriptqueue = state.ws.subscriptions.filter((s) => s.groupName === 'event-ScriptQueue-all');
 
-  if( scriptqueue.length === 0)  return {}
-  if(! scriptqueue[0].data) return {}
-  return {data: scriptqueue[0].data};
+  if (scriptqueue.length === 0) return {};
+  if (!scriptqueue[0].data) return {};
+  return { data: scriptqueue[0].data };
 };
 
 const mapDispatchToProps = (dispatch) => {
@@ -25,8 +23,12 @@ const mapDispatchToProps = (dispatch) => {
     subscribeToStream: (category, csc, stream) => {
       const groupName = [category, csc, stream].join('-');
       dispatch(requestGroupSubscription(groupName));
-    }
-  }
+    },
+    unsubscribeToStream: (category, csc, stream) => {
+      const groupName = [category, csc, stream].join('-');
+      dispatch(requestGroupSubscriptionRemoval(groupName));
+    },
+  };
 };
 
 export default connect(
