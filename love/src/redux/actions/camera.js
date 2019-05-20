1ee4e7e7a1ea12a41c74d179c9aacfd82347e7d9
spereirag <sebastian.pereira@inria.cl>
2019-05-20 16:59:39 -0400

    Add image sequence events

--- love/src/redux/actions/camera.js
+++ love/src/redux/actions/camera.js
@@ -1,8 +1,27 @@
-import { RECEIVE_IMAGE_DATA } from './actionTypes';
+import { RECEIVE_IMAGE_SEQUENCE_DATA } from './actionTypes';
+import { imageStatus } from '../../Config';
 
-export const receiveCameraData = (data) => {
+export const receiveImageSequenceData = (data) => {
+  let imageState, imageData;
+  if (data.ATCamera.startIntegration) {
+    imageData = data.ATCamera.startIntegration;
+    imageState = imageStatus.INTEGRATING;
+  }
+  if (data.ATCamera.startReadout) {
+    imageData = data.ATCamera.startReadout;
+    imageState = imageStatus.READING_OUT;
+  }
+  if (data.ATCamera.endReadout) {
+    imageData = data.ATCamera.endReadout;
+    imageState = imageStatus.END_READOUT;
+  }
+  if (data.ATCamera.endOfImageTelemetry) {
+    imageData = data.ATCamera.endOfImageTelemetry;
+    imageState = imageStatus.END_TELEMETRY;
+  }
   return {
-    type: RECEIVE_IMAGE_DATA,
-    data: data.ATCamera,
+    type: RECEIVE_IMAGE_SEQUENCE_DATA,
+    data: imageData,
+    imageState,
   };
 };
