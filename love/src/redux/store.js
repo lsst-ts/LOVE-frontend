0ea227c69dbba1044f11f98181edbb9ad24b6674
spereirag <sebastian.pereira@inria.cl>
2019-05-22 18:05:20 -0400

    Add logger and remove comments

--- love/src/redux/store.js
+++ love/src/redux/store.js
@@ -2,14 +2,14 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 import thunkMiddleware from 'redux-thunk';
 import {receiveToken } from './actions/auth';
+import logger from 'redux-logger'
 
-const middleWares = [thunkMiddleware];
+const middleWares = [thunkMiddleware, logger];
 const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
 const storageToken = localStorage.getItem('LOVE-TOKEN');
 if (storageToken && storageToken.length > 0) {
     store.dispatch(receiveToken(storageToken))
 }
-// store.dispatch(fetchToken('test','test')).then(() => console.log('token',store.getState()));
-// store.dispatch(openWebsocketConnection());
+
 export default store;
