00c1b6202a381c6a5a5385a32e0f20bdd065726b
spereirag <sebastian.pereira@inria.cl>
2019-05-28 12:15:05 -0400

    Selector sets to undefined missing data

--- love/src/redux/selectors.js
+++ love/src/redux/selectors.js
@@ -12,15 +12,35 @@ export const getCameraState = (state) => {
 
 export const getDomeState = (state) => {
   return {
-    dropoutDoorOpeningPercentage: state.ws.subscriptions['telemetry-ATDome-dropoutDoorOpeningPercentage'] ? state.ws.subscriptions['telemetry-ATDome-dropoutDoorOpeningPercentage'].data: {},
-    mainDoorOpeningPercentage: state.ws.subscriptions['telemetry-ATDome-mainDoorOpeningPercentage'] ? state.ws.subscriptions['telemetry-ATDome-mainDoorOpeningPercentage'].data: {},
-    azimuthPosition: state.ws.subscriptions['telemetry-ATDome-azimuthPosition'] ? state.ws.subscriptions['telemetry-ATDome-azimuthPosition'].data: {},
-    azimuthState: state.ws.subscriptions['event-ATDome-azimuthState'] ? state.ws.subscriptions['event-ATDome-azimuthState'].data: {},
-    dropoutDoorState: state.ws.subscriptions['event-ATDome-dropoutDoorState'] ? state.ws.subscriptions['event-ATDome-dropoutDoorState'].data: {},
-    mainDoorState: state.ws.subscriptions['event-ATDome-mainDoorState'] ? state.ws.subscriptions['event-ATDome-mainDoorState'].data: {},
-    ATMCS_mountEncoders: state.ws.subscriptions['telemetry-ATMCS-ATMCS_mountEncoders'] ? state.ws.subscriptions['telemetry-ATMCS-ATMCS_mountEncoders'].data: {},
-    detailedState: state.ws.subscriptions['event-ATMCS-detailedState'] ? state.ws.subscriptions['event-ATMCS-detailedState'].data: {},
-    atMountState: state.ws.subscriptions['event-ATMCS-atMountState'] ? state.ws.subscriptions['event-ATMCS-atMountState'].data: {},
-    target: state.ws.subscriptions['event-ATMCS-target'] ? state.ws.subscriptions['event-ATMCS-target'].data: {},
-  }
-}
+    dropoutDoorOpeningPercentage: state.ws.subscriptions['telemetry-ATDome-dropoutDoorOpeningPercentage']
+      ? state.ws.subscriptions['telemetry-ATDome-dropoutDoorOpeningPercentage'].data
+      : undefined,
+    mainDoorOpeningPercentage: state.ws.subscriptions['telemetry-ATDome-mainDoorOpeningPercentage']
+      ? state.ws.subscriptions['telemetry-ATDome-mainDoorOpeningPercentage'].data
+      : undefined,
+    azimuthPosition: state.ws.subscriptions['telemetry-ATDome-azimuthPosition']
+      ? state.ws.subscriptions['telemetry-ATDome-azimuthPosition'].data
+      : undefined,
+    azimuthState: state.ws.subscriptions['event-ATDome-azimuthState']
+      ? state.ws.subscriptions['event-ATDome-azimuthState'].data
+      : undefined,
+    dropoutDoorState: state.ws.subscriptions['event-ATDome-dropoutDoorState']
+      ? state.ws.subscriptions['event-ATDome-dropoutDoorState'].data
+      : undefined,
+    mainDoorState: state.ws.subscriptions['event-ATDome-mainDoorState']
+      ? state.ws.subscriptions['event-ATDome-mainDoorState'].data
+      : undefined,
+    ATMCS_mountEncoders: state.ws.subscriptions['telemetry-ATMCS-ATMCS_mountEncoders']
+      ? state.ws.subscriptions['telemetry-ATMCS-ATMCS_mountEncoders'].data
+      : undefined,
+    detailedState: state.ws.subscriptions['event-ATMCS-detailedState']
+      ? state.ws.subscriptions['event-ATMCS-detailedState'].data
+      : undefined,
+    atMountState: state.ws.subscriptions['event-ATMCS-atMountState']
+      ? state.ws.subscriptions['event-ATMCS-atMountState'].data
+      : undefined,
+    target: state.ws.subscriptions['event-ATMCS-target']
+      ? state.ws.subscriptions['event-ATMCS-target'].data
+      : undefined,
+  };
+};
