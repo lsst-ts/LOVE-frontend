bdf9324efcda53b803198bf8006e837727b1ebdb
spereirag <sebastian.pereira@inria.cl>
2019-06-03 17:02:24 -0400

    Fix Dome pointing position

--- love/src/components/AuxTel/Dome/DomePointing.jsx
+++ love/src/components/AuxTel/Dome/DomePointing.jsx
@@ -46,32 +46,47 @@ export default class DomePointing extends Component {
 
   render() {
     const { width, height } = this.props;
-    const zenithPixels = this.azelToPixel({az: 0, el:90}, false);
+    const zenithPixels = this.azelToPixel({ az: 0, el: 90 }, false);
     const currentPixels = this.azelToPixel(this.props.currentPointing, true);
     const targetPixels = this.azelToPixel(this.props.targetPointing, true);
     const el = this.props.currentPointing.el;
     const az = this.props.currentPointing.az;
+    const r0 = 350;
+    const r1 = (90 - el) / 90;
+    const r2 = (90 + Math.abs(el - this.props.targetPointing.el)) / 90;
+    const pathR = r0 * r1 * r2;
     return (
-      <svg
-        className={styles.svgOverlay}
-        height={height}
-        width={width}
-        viewBox="0 0 596 596"
-        
-      >
-        <line
-          x1={currentPixels.x}
-          y1={currentPixels.y}
-          x2={targetPixels.x}
-          y2={targetPixels.y}
-          stroke="white"
+      <svg className={styles.svgOverlay} height={height} width={width} viewBox="0 0 596 596">
+
+        <circle
+          className={styles.targetPointing}
+          r={32}
+          strokeWidth={2}
+          cx={zenithPixels.x}
+          cy={zenithPixels.y}
+          fill="none"
           strokeDasharray="4"
           strokeOpacity="0.3"
+          stroke="white"
+          strokeWidth="2"
+          style={{
+            transform: `rotateZ(${this.props.targetPointing.az}deg) rotateX(${this.props.targetPointing.el-90}deg)`,
+            transformOrigin: `50% 50% ${280}px`,
+          }}
         />
-        <circle r={64} stroke="white" strokeWidth={2} cx={zenithPixels.x} cy={zenithPixels.y} fill="red" style={{
-          transform: `rotateY(${-90+el}deg) rotate3d(${Math.cos(el*Math.PI/180)}, 0, ${Math.sin(el*Math.PI/180)},${-90+az}deg) scale(0.5)`,
-          transformOrigin: `50% 50% ${280}px`,
-        }} />
+        <circle
+          className={styles.currentPointing}
+          r={32}
+          stroke="white"
+          strokeWidth={2}
+          cx={zenithPixels.x}
+          cy={zenithPixels.y}
+          fill="red"
+          style={{
+            transform: `rotateZ(${az}deg) rotateX(${el-90}deg)`,
+            transformOrigin: `50% 50% ${280}px`,
+          }}
+        />
         {/* <circle r={4} cx={targetPixels.x} cy={targetPixels.y} fill="gray" /> */}
       </svg>
     );
