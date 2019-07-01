f5cc07df37faaf7ce6ea224a3b4dfbca5d2588ba
José  Galaz <jdgalazm@gmail.com>
2019-07-01 13:20:58 -0400

    use cypress to add+test new mockup fields for commands

--- love/src/components/TelemetryLog/TelemetryLog.jsx
+++ love/src/components/TelemetryLog/TelemetryLog.jsx
@@ -19,6 +19,9 @@ export default class TelemetryLog extends Component {
       category: props.category,
       csc: props.csc,
       stream: props.stream,
+      cmdComponent: undefined,
+      command: undefined,
+      cmdParams: undefined
     };
     this.managerInterface = new ManagerInterface();
   }
@@ -64,6 +67,19 @@ export default class TelemetryLog extends Component {
     });
   };
 
+
+
+
+  cmdComponentChange = (e) => {
+    console.log(e);
+  }
+  cmdOnChange = (e) => {
+    console.log(e);
+  }
+  cmdParamsOnChange = (e) => {
+    console.log(e);
+  }
+
   subscribeToStream = () => {
     this.props.subscribeToStream([this.state.category, this.state.csc, this.state.stream].join('-'));
   };
@@ -81,19 +97,37 @@ export default class TelemetryLog extends Component {
   render() {
     return (
       <div style={{ textAlign: 'left' }}>
-        <h1>Telemetry log</h1>
+        <h1>Test log and cmd launcher</h1>
+        
+        <h2> Command Launcher</h2>
+        <div>
+          <div>
+            <span>Component </span>
+            <input id="id_commands_csc" onChange={this.cmdComponentChange} value={this.state.cmdComponent} />
+          </div>
+          <div>
+            <span>cmd_ </span>
+            <input id="id_commands" onChange={this.cmdOnChange} value={this.state.command} />
+          </div>
+          <div>
+            <span>params </span>
+            <input id="id_parameters" onChange={this.cmdParamsOnChange} value={this.state.cmdParams} />
+          </div>
+        </div>
+        
+        <h2>Telemetry and events</h2>
         <div>
           <div>
             <span>Category </span>
-            <input onChange={this.categoryChange} value={this.state.category} />
+            <input id="id_category" onChange={this.categoryChange} value={this.state.category} />
           </div>
           <div>
             <span>CSC </span>
-            <input onChange={this.cscChange} value={this.state.csc} />
+            <input id="id_csc" onChange={this.cscChange} value={this.state.csc} />
           </div>
           <div>
             <span>Stream </span>
-            <input onChange={this.streamChange} value={this.state.stream} />
+            <input id="id_stream" onChange={this.streamChange} value={this.state.stream} />
           </div>
           <button onClick={this.subscribeToStream}>Subscribe</button>
           <button onClick={this.unsubscribeToStream}>Unsubscribe</button>
