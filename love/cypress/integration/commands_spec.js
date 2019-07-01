d5499684f21984217e0f5788248c9dff67296dc3
José  Galaz <jdgalazm@gmail.com>
2019-07-01 16:36:53 -0400

    clean command_spec and improve description

--- love/cypress/integration/commands_spec.js
+++ love/cypress/integration/commands_spec.js
@@ -1,4 +1,8 @@
-it('Sending commands causes event messages to be received', function() {
+it(`GIVEN the user is on the test window AND subscribed to the azimuthCommandedState event
+WHEN it sends a command to update the ATDome azimuth on the simulator
+THEN it should receive the updated data inside the message of the azimuthCommandedState event`, function() {
+
+  //---Arrange---
   cy.visit('http://localhost');
   cy.url().should('include', '/login');
   cy.get('#id_username').type('test');
@@ -8,24 +12,22 @@ it('Sending commands causes event messages to be received', function() {
     .click();
   cy.url().should('not.include', 'login');
   cy.root().should('contain', 'Component index');
-
   cy.visit('http://localhost/test')
-
-  // subscribe to atdome-sim
+  // subscribe to the ATDome sim  
   cy.get('#id_category').clear().type('event');
   cy.get('#id_csc').clear().type('ATDome');
   cy.get('#id_stream').clear().type('azimuthCommandedState');
   cy.get('button').contains('Subscribe').click();
 
 
-  // change commanded state
+  //---Act---
+  // change the azimuth
   cy.get('#id_commands_csc').clear().type('ATDome')
   cy.get('#id_commands').clear().type('moveAzimuth')
   cy.get('#id_parameters').clear().type('{{}"azimuth": 123.456}')
-
   cy.get('button').contains('Launch').click();
 
-  cy.wait(2000);
+  //---Assert---
   cy.get('body').contains((`"value": 123.456`));
 
 });
