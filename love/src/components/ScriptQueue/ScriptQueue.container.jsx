32e8a795d8ba0ff7dc701a0f0b87c2c400f712a1
José  Galaz <jdgalazm@gmail.com>
2019-07-02 11:30:40 -0400

    use (missing yet) selector in container

--- love/src/components/ScriptQueue/ScriptQueue.container.jsx
+++ love/src/components/ScriptQueue/ScriptQueue.container.jsx
@@ -1,26 +1,38 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { requestGroupSubscription, requestGroupSubscriptionRemoval, requestSALCommand } from '../../redux/actions/ws';
+import {getSummaryStateValue} from '../../redux/selectors';
 
 import ScriptQueue from './ScriptQueue';
 
-const ScriptQueueContainer = ({ subscribeToStreams, unsubscribeToStreams }) => {
-  return <ScriptQueue subscribeToStreams={subscribeToStreams} unsubscribeToStreams={unsubscribeToStreams} />;
+const ScriptQueueContainer = ({ subscribeToStreams, unsubscribeToStreams, summaryStateValue }) => {
+  return (
+    <ScriptQueue
+      subscribeToStreams={subscribeToStreams}
+      unsubscribeToStreams={unsubscribeToStreams}
+      summaryStateValue={summaryStateValue}
+    />
+  );
 };
 
-const mapStateToProps = () => {};
+const mapStateToProps = (state) => {
+  const summaryStateValue = getSummaryStateValue(state);
+  return {
+    summaryStateValue
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    subscribeToStreams : () =>{
+    subscribeToStreams: () => {
       dispatch(requestGroupSubscription('event-ScriptQueueState-stream'));
       dispatch(requestGroupSubscription('event-ScriptQueueState-summaryState'));
     },
-    unsubscribeToStreams : () =>{
+    unsubscribeToStreams: () => {
       dispatch(requestGroupSubscriptionRemoval('event-ScriptQueueState-stream'));
       dispatch(requestGroupSubscriptionRemoval('event-ScriptQueueState-summaryState'));
-    }
-  }
+    },
+  };
 };
 export default connect(
   mapStateToProps,
