c68b9ad9b0cfcb92a184117749f94bd23318446e
José  Galaz <jdgalazm@gmail.com>
2019-07-17 15:54:26 -0400

    use cscsummaryhierarchy from configfile

--- love/src/components/CSCSummary/CSCSummary.container.jsx
+++ love/src/components/CSCSummary/CSCSummary.container.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import CSCSummary from './CSCSummary';
+import { hasFakeData, CSCSummaryHierarchy } from '../../Config';
 
 const CSCSummaryContainer = () => {
   return <CSCSummary />;
