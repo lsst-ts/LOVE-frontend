286b50a76f7e255696062f4e931e34a2608c1946
Sebastian <sebafehlandt@gmail.com>
2019-07-24 12:41:54 -0400

    Remove redux store logger and clean some prints

--- love/src/redux/store.js
+++ love/src/redux/store.js
@@ -3,8 +3,8 @@ import rootReducer from './reducers';
 import thunkMiddleware from 'redux-thunk';
 import logger from 'redux-logger'
 
-// const middleWares = [thunkMiddleware];
-const middleWares = [thunkMiddleware, logger];
+const middleWares = [thunkMiddleware];
+// const middleWares = [thunkMiddleware, logger];
 const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
 export default store;
