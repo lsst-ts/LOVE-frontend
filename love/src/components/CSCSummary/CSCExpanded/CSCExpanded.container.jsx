487c92aa50304d6a2a65fb97d8f5875be6b567ec
José  Galaz <jdgalazm@gmail.com>
2019-07-31 10:07:59 -0400

    Wrap CSCExpanded in container, based on CSCDetail.container

---
+++ love/src/components/CSCSummary/CSCExpanded/CSCExpanded.container.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import CSCExpanded from './CSCExpanded';
+import { requestGroupSubscription } from '../../../redux/actions/ws';
+import { getStreamData, getCSCHeartbeat } from '../../../redux/selectors';
+
+const CSCExpandedContainer = ({
+  name,
+  salindex,
+  group,
+  realm,
+  onCSCClick,
+  clearCSCErrorCodes,
+  clearCSCLogMessages,
+  summaryStateData,
+  logMessageData,
+  errorCodeData,
+  subscribeToStreams,
+  heartbeatData,
+}) => {
+  return (
+    <CSCExpanded
+      name={name}
+      salindex={salindex}
+      group={group}
+      realm={realm}
+      onCSCClick={onCSCClick}
+      clearCSCErrorCodes={clearCSCErrorCodes}
+      clearCSCLogMessages={clearCSCLogMessages}
+      summaryStateData={summaryStateData}
+      logMessageData={logMessageData}
+      errorCodeData={errorCodeData}
+      subscribeToStreams={subscribeToStreams}
+      heartbeatData={heartbeatData}
+    />
+  );
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    subscribeToStreams: (cscName, index) => {
+      dispatch(requestGroupSubscription('event-Heartbeat-0-stream'));
+      dispatch(requestGroupSubscription(`event-${cscName}-${index}-summaryState`));
+      dispatch(requestGroupSubscription(`event-${cscName}-${index}-logMessage`));
+      dispatch(requestGroupSubscription(`event-${cscName}-${index}-errorCode`));
+      dispatch(requestGroupSubscription('event-Heartbeat-0-stream'));
+    },
+  };
+};
+
+const mapStateToProps = (state, ownProps) => {
+  let summaryStateData = getStreamData(state, `event-${ownProps.name}-${ownProps.salindex}-summaryState`);
+  let heartbeatData = getCSCHeartbeat(state, ownProps.name, ownProps.salindex);
+  if (!summaryStateData) {
+    summaryStateData = {};
+  }
+
+  return {
+    summaryStateData: summaryStateData[0],
+    heartbeatData,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(CSCExpandedContainer);
