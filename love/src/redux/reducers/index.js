373de87a83e3465b6d87239e33b4a89cfaa9c4e2
José  Galaz <jdgalazm@gmail.com>
2019-07-31 10:33:55 -0400

    store logMessageEvents in summaryData branch (dummy for now)

--- love/src/redux/reducers/index.js
+++ love/src/redux/reducers/index.js
@@ -3,9 +3,12 @@ import auth from './auth';
 import ws from './ws';
 import camera from './camera';
 import heartbeats from './heartbeats';
+import summaryData from './summaryData';
+
 export default combineReducers({
   auth,
   ws,
   camera,
   heartbeats,
+  summaryData,
 });
