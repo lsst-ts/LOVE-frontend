aa6777cad9c1c7296a4aade46008a11306d489ec
José  Galaz <jdgalazm@gmail.com>
2019-08-12 18:30:14 -0400

    get GroupLog errorCode data from redux

---
+++ love/src/components/CSCSummary/CSCGroupLog/CSCGroupLog.container.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import CSCGroupLog from './CSCGroupLog';
+import { requestGroupSubscription } from '../../../redux/actions/ws';
+import { getGroupSortedErrorCodeData } from '../../../redux/selectors';
+
+const CSCGroupLogContainer = ({
+  realm,
+  group,
+  csc,
+  data,
+  onCSCClick,
+  hierarchy,
+  clearCSCErrorCodes,
+  subscribeToStream,
+  errorCodeData,
+}) => {
+  return (
+    <CSCGroupLog
+      realm={realm}
+      group={group}
+      name={csc}
+      data={data}
+      onCSCClick={onCSCClick}
+      hierarchy={hierarchy}
+      clearCSCErrorCodes={clearCSCErrorCodes}
+      subscribeToStream={subscribeToStream}
+      errorCodeData={errorCodeData}
+    />
+  );
+};
+
+const mapDispatchtoProps = (dispatch) => {
+  return {
+    subscribeToStream: (cscName, index) => {
+      dispatch(requestGroupSubscription(`event-${cscName}-${index}-errorCode`));
+    },
+  };
+};
+
+const mapStateToProps = (state, ownProps) => {
+  const errorCodeData = getGroupSortedErrorCodeData(state, ownProps.hierarchy[ownProps.realm][ownProps.group]);
+  return {
+    errorCodeData: errorCodeData,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchtoProps,
+)(CSCGroupLogContainer);
