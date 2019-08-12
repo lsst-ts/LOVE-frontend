7bb1905b7369ff0c625d70a31f397becbf0bb099
José  Galaz <jdgalazm@gmail.com>
2019-08-12 15:38:52 -0400

    action to remove log messages of a csc,salindex from state

--- love/src/redux/actions/summaryData.js
+++ love/src/redux/actions/summaryData.js
@@ -1,4 +1,4 @@
-import { UPDATE_LOG_MESSAGE_DATA} from './actionTypes';
+import { UPDATE_LOG_MESSAGE_DATA, REMOVE_CSC_LOG_MESSAGES} from './actionTypes';
 
 export const receiveLogMessageData = (csc, salindex, messages) => {
   return {
@@ -8,3 +8,11 @@ export const receiveLogMessageData = (csc, salindex, messages) => {
     messages,
   };
 };
+
+export const removeCSCLogMessages = (csc, salindex) => {
+  return {
+    type: REMOVE_CSC_LOG_MESSAGES,
+    csc,
+    salindex
+  }
+}
--- love/src/redux/reducers/summaryData.js
+++ love/src/redux/reducers/summaryData.js
@@ -1,4 +1,4 @@
-import { UPDATE_LOG_MESSAGE_DATA } from '../actions/actionTypes';
+import { UPDATE_LOG_MESSAGE_DATA, REMOVE_CSC_LOG_MESSAGES } from '../actions/actionTypes';
 
 const initialState = {
   logMessageData: [],
@@ -7,7 +7,7 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
-    case UPDATE_LOG_MESSAGE_DATA:
+    case UPDATE_LOG_MESSAGE_DATA: {
       let pastMessages = [];
       if (state.logMessageData.length > 0) {
         pastMessages = state.logMessageData;
@@ -36,15 +36,35 @@ export default function(state = initialState, action) {
       newLogMessageData[cscDataIndex] = {
         csc: action.csc,
         salindex: action.salindex,
-        messages: [...cscData.messages, ...action.messages]
-      } 
-
+        messages: [...cscData.messages, ...action.messages],
+      };
 
       return {
         errorCodeData: state.errorCodeData,
-        logMessageData: newLogMessageData
-      }
+        logMessageData: newLogMessageData,
+      };
+    }
+    case REMOVE_CSC_LOG_MESSAGES: {
+      console.log(REMOVE_CSC_LOG_MESSAGES)
+      const cscDataIndex = state.logMessageData.findIndex(
+        (CSCData) => CSCData.salindex === action.salindex && CSCData.csc === action.csc,
+      );
 
+      if (cscDataIndex === -1) return state;
+
+      const newLogMessageData = [...state.logMessageData];
+      const cscData = newLogMessageData[cscDataIndex];
+      newLogMessageData[cscDataIndex] = {
+        csc: action.csc,
+        salindex: action.salindex,
+        messages: [],
+      };
+
+      return {
+        errorCodeData: state.errorCodeData,
+        logMessageData: newLogMessageData,
+      };
+    }
     default:
       return state;
   }
