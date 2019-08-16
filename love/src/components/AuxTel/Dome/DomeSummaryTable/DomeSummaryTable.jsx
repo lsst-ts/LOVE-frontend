f502179e8132c912082e439b08278a1d1389deff
spereirag <sebastian.pereira@inria.cl>
2019-08-16 11:38:17 -0400

    Add rotator limits

--- love/src/components/AuxTel/Dome/DomeSummaryTable/DomeSummaryTable.jsx
+++ love/src/components/AuxTel/Dome/DomeSummaryTable/DomeSummaryTable.jsx
@@ -46,6 +46,19 @@ export default class DomeSummaryTable extends Component {
     const domeInPositionValue = this.props.domeInPosition ? this.props.domeInPosition[0].inPosition.value : 0;
     const mountInPositionValue = this.props.mountInPosition ? this.props.mountInPosition[0].inPosition.value : 0;
     const mountTrackingStateValue = mountTrackingStateMap[this.props.mountTrackingState];
+    const m3State = this.props.m3State;
+    const mountRotator =
+      m3State === 1
+        ? {
+            name: 'Nasmyth1',
+            current: this.props.currentPointing.nasmyth1,
+            target: this.props.targetPointing.nasmyth1,
+          }
+        : {
+            name: 'Nasmyth2',
+            current: this.props.currentPointing.nasmyth2,
+            target: this.props.targetPointing.nasmyth2,
+          };
 
     return (
       <div className={styles.summaryTable}>
@@ -58,7 +71,7 @@ export default class DomeSummaryTable extends Component {
             {domeInPositionValue ? 'In Position' : 'Not in Position'}
           </StatusText>
         </span>
-        
+
         <span className={styles.label}>Azimuth</span>
         <span className={styles.value}>
           <StatusText title={azimuthStateValue} status={stateToStyleDomeAndMount[azimuthStateValue]}>
@@ -116,18 +129,18 @@ export default class DomeSummaryTable extends Component {
         <span className={styles.wide}>
           <Limits lowerLimit={15} upperLimit={90} currentValue={mountEl.current} targetValue={mountEl.target} />
         </span>
-        {/* <span className={styles.label}>M3 rotator (R1)</span>
+
+        <span className={styles.label}>{mountRotator.name}</span>
         <span className={styles.value}>
           <CurrentTargetValue
-            currentValue={mountEl.current.toFixed(2)}
-            targetValue={mountEl.target.toFixed(2)}
+            currentValue={mountRotator.current.toFixed(2)}
+            targetValue={mountRotator.target.toFixed(2)}
             isChanging={true}
           />
         </span>
         <span className={styles.wide}>
-          <Limits lowerLimit={15} upperLimit={90} currentValue={mountEl.current} targetValue={mountEl.target} />
-        </span> */}
-        
+          <Limits lowerLimit={-175} upperLimit={175} currentValue={mountRotator.current} targetValue={mountRotator.target} />
+        </span>
       </div>
     );
   }
