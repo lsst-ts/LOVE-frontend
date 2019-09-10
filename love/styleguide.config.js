1300b1d668f939e86ba3e3c983cb82b450bb8f24
José  Galaz <jdgalazm@gmail.com>
2019-09-10 12:07:01 -0300

    add styleguidist and use port

--- love/styleguide.config.js
+++ love/styleguide.config.js
@@ -2,7 +2,7 @@
 const path = require('path')
 
 module.exports = { 
-    webpackConfig: require( './config/webpack.config.dev.js' ),
+    serverPort: 3001,
     require: [
       path.join(__dirname, 'src/index.css')
     ],
