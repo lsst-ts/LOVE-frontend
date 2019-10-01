8079c309f23f0febe417bcec63c3290c6a6ea6d8
spereirag <sebastian.pereira@inria.cl>
2019-10-01 16:51:13 -0300

    Add Hoverable component

---
+++ love/src/components/GeneralPurpose/Hoverable/Trigger.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const getCursorPos = (e, props) => {
+  let { x, y, height, width } = e.target.getBoundingClientRect();
+  let vOff = 0;
+  if (props.middle) vOff = height / 2;
+  if (props.bottom) vOff = height;
+  let hOff = 0;
+  if (props.center) hOff = width / 2;
+  if (props.right) hOff = width;
+  return [x + hOff, y + vOff];
+};
+
+const Trigger = (props) => {
+  let newProps = {
+    onMouseEnter: (e) => {
+      props.show(getCursorPos(e, props));
+    },
+    onMouseLeave: () => props.hide(),
+  };
+  const trigger = props.children[0];
+  const float = props.children[1];
+  if (float) {
+    if (trigger.props.children) {
+      newProps.children = [trigger.props.children, float];
+    } else newProps.children = float;
+  }
+  return React.cloneElement(trigger, newProps);
+};
+
+export default Trigger;
