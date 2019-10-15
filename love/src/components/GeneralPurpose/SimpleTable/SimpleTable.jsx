01d36e02f4dc114e223c9efb32b032447bb24093
spereirag <sebastian.pereira@inria.cl>
2019-10-15 15:14:23 -0300

    Add simple table generic component

---
+++ love/src/components/GeneralPurpose/SimpleTable/SimpleTable.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './SimpleTable.module.css';
+
+export function Table({ children, className }) {
+  return <table className={[styles.common, styles.table, className].join(' ')}>{children}</table>;
+}
+
+export function Thead({ children, className }) {
+  return <thead className={[styles.common, styles.thead, className].join(' ')}>{children}</thead>;
+}
+
+export function Tbody({ children, className }) {
+  return <tbody className={[styles.common, styles.tbody, className].join(' ')}>{children}</tbody>;
+}
+
+export function Td({ children, className, isNumber }) {
+  return (
+    <td className={[styles.common, styles.td, className, isNumber ? styles.number : styles.string].join(' ')}>
+      {children}
+    </td>
+  );
+}
+
+export function Th({ children, className }) {
+  return <th className={[styles.common, styles.th, className].join(' ')}>{children}</th>;
+}
+
+export function Tr({ children, className }) {
+  return <tr className={[styles.common, styles.tr, className].join(' ')}>{children}</tr>;
+}
