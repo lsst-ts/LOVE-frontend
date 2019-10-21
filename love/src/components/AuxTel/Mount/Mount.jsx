fe7588fa7fdd89b0d7114d968902f2d8a2e4c554
spereirag <sebastian.pereira@inria.cl>
2019-10-21 12:00:50 -0300

    Add motor table

--- love/src/components/AuxTel/Mount/Mount.jsx
+++ love/src/components/AuxTel/Mount/Mount.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import LightPathContainer from './LightPath.container';
 import SummaryPanelContainer from './SummaryPanel/SummaryPanel.container';
 import styles from './Mount.module.css';
+import MotorTableContainer from './MotorTable/MotorTable.container';
 
 export default class Mount extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class Mount extends Component {
       <div className={styles.container}>
         <LightPathContainer></LightPathContainer>
         <SummaryPanelContainer></SummaryPanelContainer>
+        <MotorTableContainer></MotorTableContainer>
       </div>
     );
   }
