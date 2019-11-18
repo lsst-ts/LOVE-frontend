d45a5f0ccca76787d58e8cf7bfa4761aa7858839
sfehlandt <sebafehlandt@gmail.com>
2019-11-18 16:09:54 -0300

    WIP

--- love/src/redux/tests/uif.test.js
+++ love/src/redux/tests/uif.test.js
@@ -19,11 +19,53 @@ const mockWorkspaces = [
     "creation_timestamp": "2019-11-13T21:14:03.441Z",
     "update_timestamp": "2019-11-13T21:14:03.441Z",
     "name": "My-workspace",
-    "views": [0, 1, 2]
-  }
+    "views": [
+      {
+        "id": 0,
+        "name": "My-view-0",
+      },
+      {
+        "id": 1,
+        "name": "My-view-1",
+      },
+      {
+        "id": 2,
+        "name": "My-view-2",
+      },
+    ]
+  },
+  {
+    "id": 1,
+    "creation_timestamp": "2019-11-13T21:14:03.441Z",
+    "update_timestamp": "2019-11-13T21:14:03.441Z",
+    "name": "My-workspace",
+    "views": [
+      {
+        "id": 3,
+        "name": "My-view-3",
+      },
+      {
+        "id": 4,
+        "name": "My-view-2",
+      },
+    ]
+  },
 ];
 
-describe('GIVEN there are no workspaces in the store', () => {
+const mockViews = [
+  {
+    "id": 0,
+    "creation_timestamp": "2019-11-18T18:36:54.570Z",
+    "update_timestamp": "2019-11-18T18:36:54.570Z",
+    "name": "My-view-0",
+    "data": {
+      "key1": "value1",
+      "key2": "value2",
+    }
+  }
+]
+
+describe('GIVEN the store is empty', () => {
 
   beforeEach(async () => {
     const token = '"love-token"';
@@ -46,4 +88,15 @@ describe('GIVEN there are no workspaces in the store', () => {
     const retrievedWorkspaces = getWorkspaces(store.getState());
     expect(retrievedWorkspaces).toEqual(mockWorkspaces);
   });
+
+  // it('WHEN a full workspace is requested, THEN the state should contain the workspace', async () => {
+  //   // Arrange:
+  //   const url = `${ManagerInterface.getUifBaseUrl()}workspaces/`;
+  //   fetchMock.mock(url, mockWorkspaces, ManagerInterface.getHeaders());
+  //   // Act:
+  //   await store.dispatch(requestWorkspaces());
+  //   // Assert:
+  //   const retrievedWorkspaces = getWorkspaces(store.getState());
+  //   expect(retrievedWorkspaces).toEqual(mockWorkspaces);
+  // });
 });
