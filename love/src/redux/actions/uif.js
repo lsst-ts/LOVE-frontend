69d1d3fd60f98e2c52bccb7d3fe7f80783e9e8bd
sfehlandt <sebafehlandt@gmail.com>
2019-11-19 12:28:00 -0300

    Receive the current workspace

--- love/src/redux/actions/uif.js
+++ love/src/redux/actions/uif.js
@@ -1,6 +1,10 @@
-import { RECEIVE_WORKSPACES } from './actionTypes';
+import { RECEIVE_WORKSPACES, RECEIVE_WORKSPACE } from './actionTypes';
 import ManagerInterface from '../../Utils';
 
+
+/**
+ * Action to receive a list of workspaces
+ */
 export const receiveWorkspaces = (workspaces) => {
   return {
     type: RECEIVE_WORKSPACES,
@@ -10,19 +14,53 @@ export const receiveWorkspaces = (workspaces) => {
 
 
 /**
- * Validates the token with the server.
- * Nothing changes if everything is ok. Other cases are handled individually.
+ * Action to receive a particular workspace
+ */
+export const receiveWorkspace = (workspace) => {
+  return {
+    type: RECEIVE_WORKSPACE,
+    workspace,
+  };
+};
+
+
+/**
+ * requestWorkspaces - Action to request the list of workspaces
+ *
+ * @return {object}  the dispatched action
  */
 export function requestWorkspaces() {
   return async (dispatch, getState) => {
-
-    const url = `${ManagerInterface.getUifBaseUrl()}workspaces/`;
+    const url = `${ManagerInterface.getUifBaseUrl()}workspaces/with_view_name`;
     return fetch(url, {
       method: 'GET',
       headers: ManagerInterface.getHeaders(),
     }).then((response) => {
       return response.json().then((workspaces) => {
         dispatch(receiveWorkspaces(workspaces));
+        return Promise.resolve();
+      });
+    }).catch((e) => console.error(e));
+  };
+}
+
+
+
+/**
+ * requestWorkspace - Action to request a full workspace by its given id
+ *
+ * @param  {number} id the id of the QWorkspace to retrieve
+ * @return {object}    the dispatched action
+ */
+export function requestWorkspace(id) {
+  return async (dispatch, getState) => {
+    const url = `${ManagerInterface.getUifBaseUrl()}workspaces/${id}/full`;
+    return fetch(url, {
+      method: 'GET',
+      headers: ManagerInterface.getHeaders(),
+    }).then((response) => {
+      return response.json().then((workspace) => {
+        dispatch(receiveWorkspace(workspace));
         return Promise.resolve();
       });
     }).catch((e) => console.error(e));
--- love/src/redux/tests/uif.test.js
+++ love/src/redux/tests/uif.test.js
@@ -5,8 +5,8 @@ import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers';
 import ManagerInterface from '../../Utils';
 import { getTokenFromStorage } from '../actions/auth';
-import { requestWorkspaces } from '../actions/uif';
-import { getWorkspaces } from '../selectors';
+import { requestWorkspaces, requestWorkspace } from '../actions/uif';
+import { getWorkspaces, getCurrentWorkspace } from '../selectors';
 
 let store;
 beforeEach(() => {
@@ -28,10 +28,6 @@ const mockWorkspaces = [
         "id": 1,
         "name": "My-view-1",
       },
-      {
-        "id": 2,
-        "name": "My-view-2",
-      },
     ]
   },
   {
@@ -41,12 +37,12 @@ const mockWorkspaces = [
     "name": "My-workspace",
     "views": [
       {
-        "id": 3,
-        "name": "My-view-3",
+        "id": 2,
+        "name": "My-view-2",
       },
       {
-        "id": 4,
-        "name": "My-view-2",
+        "id": 3,
+        "name": "My-view-3",
       },
     ]
   },
@@ -62,7 +58,17 @@ const mockViews = [
       "key1": "value1",
       "key2": "value2",
     }
-  }
+  },
+  {
+    "id": 1,
+    "creation_timestamp": "2019-11-18T18:36:54.570Z",
+    "update_timestamp": "2019-11-18T18:36:54.570Z",
+    "name": "My-view-1",
+    "data": {
+      "key1": "value1",
+      "key2": "value2",
+    }
+  },
 ]
 
 describe('GIVEN the store is empty', () => {
@@ -80,7 +86,7 @@ describe('GIVEN the store is empty', () => {
 
   it('WHEN the workspaces are requested, THEN the state should contain the workspaces', async () => {
     // Arrange:
-    const url = `${ManagerInterface.getUifBaseUrl()}workspaces/`;
+    const url = `${ManagerInterface.getUifBaseUrl()}workspaces/with_view_name`;
     fetchMock.mock(url, mockWorkspaces, ManagerInterface.getHeaders());
     // Act:
     await store.dispatch(requestWorkspaces());
@@ -89,14 +95,18 @@ describe('GIVEN the store is empty', () => {
     expect(retrievedWorkspaces).toEqual(mockWorkspaces);
   });
 
-  // it('WHEN a full workspace is requested, THEN the state should contain the workspace', async () => {
-  //   // Arrange:
-  //   const url = `${ManagerInterface.getUifBaseUrl()}workspaces/`;
-  //   fetchMock.mock(url, mockWorkspaces, ManagerInterface.getHeaders());
-  //   // Act:
-  //   await store.dispatch(requestWorkspaces());
-  //   // Assert:
-  //   const retrievedWorkspaces = getWorkspaces(store.getState());
-  //   expect(retrievedWorkspaces).toEqual(mockWorkspaces);
-  // });
+  it('WHEN a full workspace is requested, THEN the state should contain the workspace', async () => {
+    // Arrange:
+    const url = `${ManagerInterface.getUifBaseUrl()}workspaces/0/full`;
+    const mockFullWorkspace = {
+      ...mockWorkspaces[0],
+      views: mockViews,
+    };
+    fetchMock.mock(url, mockFullWorkspace, ManagerInterface.getHeaders());
+    // Act:
+    await store.dispatch(requestWorkspace(0));
+    // Assert:
+    const retrievedWorkspace = getCurrentWorkspace(store.getState());
+    expect(retrievedWorkspace).toEqual(mockFullWorkspace);
+  });
 });
