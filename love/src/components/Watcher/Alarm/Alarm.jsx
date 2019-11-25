999f1e660c186d6c8e243edec71d4179f416ff58
sfehlandt <sebafehlandt@gmail.com>
2019-11-25 18:12:10 -0300

    Remove "NEW" Label and just use the "ACK" button

--- love/src/components/Watcher/Alarm/Alarm.jsx
+++ love/src/components/Watcher/Alarm/Alarm.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styles from './Alarm.module.css';
 import StatusText from '../../GeneralPurpose/StatusText/StatusText';
 import Button from '../../GeneralPurpose/Button/Button';
@@ -12,30 +12,21 @@ export const severityToStatus = {
 };
 
 export default function Alarm({ severity, statusOnly, acknowledged, muted, ackAlarm }) {
-  const [showButton, setShowButton] = useState(false);
   const status = severityToStatus[severity];
   return (
     <div
       className={[styles.alarmContainer, statusOnly ? styles.statusOnly : ''].join(' ')}
-      onMouseEnter={() => {setShowButton(true)}}
-      onMouseLeave={() => {setShowButton(false)}}
     >
       <StatusText status={status}>{status}</StatusText>
       {statusOnly || acknowledged ? null : (
-        !showButton ? (
-          <div className={styles.newLabel}>
-            NEW
-          </div>
-        ) : (
-          <Button
-            title='ack'
-            status='success'
-            disabled={acknowledged}
-            onClick={(event) => {ackAlarm(event)}}
-          >
-            ACK
-          </Button>
-        )
+        <Button
+          title='ack'
+          status='info'
+          disabled={acknowledged}
+          onClick={(event) => {ackAlarm(event)}}
+        >
+          ACK
+        </Button>
       )}
     </div>
   );
