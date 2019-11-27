13e0a8a7f6988a84c352315625bde28ff38e31e5
spereirag <sebastian.pereira@inria.cl>
2019-11-27 13:11:45 -0300

    Add component index with available UIF elements

---
+++ love/src/components/GeneralPurpose/UIF/ComponentIndex.jsx
@@ -0,0 +1,5 @@
+export default {
+    SummaryPanel: require('../../AuxTel/Mount/SummaryPanel/SummaryPanel.container').default,
+    LightPath: require('../../AuxTel/Mount/LightPath.container').default,
+    MotorTable: require('../../AuxTel/Mount/MotorTable/MotorTable.container').default,
+}
