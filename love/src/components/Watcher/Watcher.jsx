e3661de2f8ac63e35aaa5823283887e23a286b1f
Sebastián Fehlandt <sfehlandt@Inria-MacBook-Pro-SF.local>
2019-12-03 13:53:05 -0300

    Add mute/unmute icons to tabs

--- love/src/components/Watcher/Watcher.jsx
+++ love/src/components/Watcher/Watcher.jsx
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
+import MuteIcon from '../icons/MuteIcon/MuteIcon';
 import Panel from '../GeneralPurpose/Panel/Panel';
-import styles from './Watcher.module.css';
 import AlarmsTableContainer from './AlarmsTable/AlarmsTable.container';
+import styles from './Watcher.module.css';
 
 export default class Watcher extends Component {
   render() {
     this.test = null;
     return (
       <Panel title="Watcher" className={styles.panel}>
+
         <div className={styles.tabsWrapper}>
           <div className={styles.tabsRow}>
             <div className={[styles.tab, styles.selected].join(' ')}>
+              <div className={styles.iconWrapper}>
+                <MuteIcon unmuted/>
+              </div>
               UNMUTED ALARMS
             </div>
             <div className={styles.tab}>
+              <div className={styles.iconWrapper}>
+                <MuteIcon/>
+              </div>
               MUTED ALARMS
             </div>
           </div>
+
           <div className={styles.alarmsTableWrapper}>
             <AlarmsTableContainer/>
           </div>
