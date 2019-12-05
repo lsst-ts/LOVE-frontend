6f5789ea0f157391d7b89e2083a08e77a0f68616
Sebastián Fehlandt <sfehlandt@Inria-MacBook-Pro-SF.local>
2019-12-05 13:05:41 -0300

    Add missing fields

--- love/src/components/Watcher/AlarmsTable/DetailsPanel/DetailsPanel.jsx
+++ love/src/components/Watcher/AlarmsTable/DetailsPanel/DetailsPanel.jsx
@@ -28,20 +28,41 @@ export default function DetailsPanel({ alarm, muteAlarm, unmuteAlarm }) {
 
   const [timeout, setTimeout] = useState(initialState.timeout);
   const [muteSeverity, setMuteSeverity] = useState(initialState.muteSeverity);
-  // const currentTime = new Date().getTime();
 
-  const acknowledgedBy = alarm.acknowledgedBy ? alarm.acknowledgedBy : 'Not acknowledged';
+  const sevUpdate = relativeTime(alarm.timestampSeverityOldest * 1000);
+  const maxSevUpdate = relativeTime(alarm.timestampMaxSeverity * 1000);
+  const lastUpdate = relativeTime(alarm.timestampSeverityNewest * 1000);
+
+  const acked = alarm.acknowledged;
+  const acknowledgedBy = acked ? alarm.acknowledgedBy : 'Not acknowledged';
+  const ackTimeTitle = acked ? 'Acknowledged at:' : 'Un-acknowledged at:';
+  const ackTime = relativeTime(alarm.timestampAcknowledged * 1000);
+  const willAutoAckTime = acked? relativeTime(alarm.timestampAutoAcknowledge * 1000) : 'Already acknowledged';
+
+  const escalated = alarm.escalated;
+  const escalatedToTitle = escalated ? 'Escalated to:' : 'Will escalate to:';
+  const escalatedTo = alarm.escalatedTo ? alarm.escalatedTo : 'Nobody';
+  const escalatedTimeTitle = escalated ? 'Escalated at:' : 'Will escalate at:';
+  const escalatedTime = relativeTime(alarm.timestampEscalate * 1000);
+
+  const muted = alarm.mutedBy !== '';
   const mutedBy = alarm.mutedBy ? alarm.mutedBy : 'Not muted';
   const mutedSeverity = alarm.mutedSeverity ? severityToStatus[alarm.mutedSeverity].toUpperCase() : 'Not muted';
-  const timeRemaining = relativeTime(alarm.timestampUnmute * 1000)
-  const muted = alarm.mutedBy !== '';
+  const willUnmuteTime = relativeTime(alarm.timestampUnmute * 1000);
 
   return (
     <div className={styles.expandedColumn}>
+
       <div>
-        <div className={styles.title}>Acknowledged by:</div>
-        <div>
-          <p>{acknowledgedBy}</p>
+        <div className={styles.dataTable}>
+          <div className={styles.title}> Severity update: </div>
+          <div className={styles.dataCell}> {acknowledgedBy} </div>
+
+          <div className={styles.title}> Max sev. update: </div>
+          <div className={styles.dataCell}> {maxSevUpdate} </div>
+
+          <div className={styles.title}> Last update: </div>
+          <div className={styles.dataCell}> {lastUpdate} </div>
         </div>
 
         <div className={styles.title}>Alarm reason:</div>
@@ -50,20 +71,50 @@ export default function DetailsPanel({ alarm, muteAlarm, unmuteAlarm }) {
         </div>
       </div>
 
+      <div>
+        <div className={styles.dataTable}>
+          <div className={styles.title}> Acknowledged by: </div>
+          <div className={styles.dataCell}> {sevUpdate} </div>
+
+          <div className={styles.title}> {ackTimeTitle} </div>
+          <div className={styles.dataCell}> {ackTime} </div>
+
+          <div className={styles.title}> Will auto-ack at: </div>
+          <div className={styles.dataCell}> {willUnmuteTime} </div>
+        </div>
+
+        <div className={styles.dataTable}>
+          <div className={styles.title}> {escalatedToTitle} </div>
+          <div className={styles.dataCell}> {escalatedTo} </div>
+
+          <div className={styles.title}> {escalatedTimeTitle} </div>
+          <div className={styles.dataCell}> {escalatedTime} </div>
+        </div>
+      </div>
+
       { muted ? (
         <div>
-          <div className={styles.title}> Will unmute: </div>
           <div>
-            <p>{timeRemaining}</p>
+            <div className={styles.dataTable}>
+              <div className={styles.title}> Muted by: </div>
+              <div className={styles.dataCell}> {mutedBy} </div>
+
+              <div className={styles.title}> Will unmute at: </div>
+              <div className={styles.dataCell}> {willUnmuteTime} </div>
+
+              <div className={styles.title}> Muted severity: </div>
+              <div className={styles.dataCell}> {mutedSeverity} </div>
+            </div>
+
+            <div className={styles.dataTable}>
+              <div className={styles.title}> {escalatedToTitle} </div>
+              <div className={styles.dataCell}> {escalatedTo} </div>
+
+              <div className={styles.title}> {escalatedTimeTitle} </div>
+              <div className={styles.dataCell}> {escalatedTime} </div>
+            </div>
           </div>
-          <div className={styles.title}> Muted Severity: </div>
-          <div>
-            <p>{mutedSeverity}</p>
-          </div>
-          <div className={styles.title}> Muted by: </div>
-          <div>
-            <p>{mutedBy}</p>
-          </div>
+
           <Button
             title='unmute'
             status='primary'
