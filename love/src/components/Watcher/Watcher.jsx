5777cfbcf58fa67e297244178280c5342d761257
Sebastián Fehlandt <sfehlandt@Inria-MacBook-Pro-SF.local>
2019-12-11 16:13:08 -0300

    Show number of alarms in each tab

--- love/src/components/Watcher/Watcher.jsx
+++ love/src/components/Watcher/Watcher.jsx
@@ -42,26 +42,54 @@ export default class Watcher extends Component {
 
   changeTab(tab) {
     this.setState({ selectedTab: tab });
-  }
+  };
+
+  sortFunctions = {
+    default: (row) => (row['acknowledged'] ? '0-' : '1-') + row['severity'],
+    severity: (row) => row['severity'] + (row['acknowledged'] ? '-0' : '-1'),
+    maxSeverity: (row) => row['maxSeverity'] + (row['acknowledged'] ? '-0' : '-1'),
+    name: (row) => row['name'] + (row['acknowledged'] ? '-0' : '-1'),
+    timestampSeverityOldest: (row) => row['timestampSeverityOldest'] + (row['acknowledged'] ? '-0' : '-1'),
+  };
+
+  mutedSortFunctions = {
+    ...this.sortFunctions,
+    default: this.sortFunctions['severity'],
+  };
 
   render() {
-    const sortFunctions = {
-      default: row => (row['acknowledged'] ? '0-' : '1-') + row['severity'],
-      severity: row => (row['severity'] + (row['acknowledged'] ? '-0' : '-1')),
-      maxSeverity: row => (row['maxSeverity'] + (row['acknowledged'] ? '-0' : '-1')),
-      name: row => (row['name'] + (row['acknowledged'] ? '-0' : '-1')),
-      timestampSeverityOldest: row => (row['timestampSeverityOldest'] + (row['acknowledged'] ? '-0' : '-1')),
-    };
 
     const preFilter = (alarm) =>
       (this.state.selectedTab === 'unmuted' ? alarm['mutedBy'] === '' : alarm['mutedBy'] !== '') &&
       !(alarm['severity'] <= 1 && alarm['maxSeverity'] <= 1 && alarm['acknowledged']);
 
-    const mutedSortFunctions = {
-      ...sortFunctions,
-      default: sortFunctions['severity'],
-    };
+    let alarmsToShow = [];
+    let mutedAlarmsCount = 0;
+    let unmutedAlarmsCount = 0;
+    let unackMutedAlarmsCount = 0;
+    let unackUnmutedAlarmsCount = 0;
+
+    for (let alarm of this.props.alarms) {
+      if (alarm['severity'] <= 1 && alarm['maxSeverity'] <= 1 && alarm['acknowledged']) continue;
+
+      if (alarm['mutedBy'] === '') {
+        unmutedAlarmsCount += 1;
+        unackUnmutedAlarmsCount += alarm['acknowledged'] ? 0 : 1;
+        if (this.state.selectedTab === 'unmuted') {
+          alarmsToShow.push(alarm);
+        }
+      }
+      else {
+        mutedAlarmsCount += 1;
+        unackMutedAlarmsCount += alarm['acknowledged'] ? 0 : 1;
+        if (this.state.selectedTab === 'muted') {
+          alarmsToShow.push(alarm);
+        }
+      }
+    }
+
     this.test = null;
+
     return (
       <Panel title="Watcher" className={styles.panel}>
         <div className={styles.tabsWrapper}>
@@ -73,7 +101,7 @@ export default class Watcher extends Component {
               <div className={styles.iconWrapper}>
                 <MuteIcon unmuted style={this.state.selectedTab === 'unmuted' ? styles.selectedIcon : ''} />
               </div>
-              UNMUTED ALARMS
+              UNMUTED ALARMS ({unmutedAlarmsCount})
             </div>
 
             <div
@@ -83,17 +111,17 @@ export default class Watcher extends Component {
               <div className={styles.iconWrapper}>
                 <MuteIcon style={this.state.selectedTab === 'muted' ? styles.selectedIcon : ''} />
               </div>
-              MUTED ALARMS
+              MUTED ALARMS ({mutedAlarmsCount})
             </div>
           </div>
 
           <div className={styles.alarmsTableWrapper}>
             <AlarmsTable
-              alarms={this.props.alarms.filter(preFilter)}
+              alarms={alarmsToShow}
               ackAlarm={this.props.ackAlarm}
               muteAlarm={this.props.muteAlarm}
               unmuteAlarm={this.props.unmuteAlarm}
-              sortFunctions={this.state.selectedTab === 'unmuted' ? sortFunctions : mutedSortFunctions }
+              sortFunctions={this.state.selectedTab === 'unmuted' ? this.sortFunctions : this.mutedSortFunctions}
             />
           </div>
         </div>
