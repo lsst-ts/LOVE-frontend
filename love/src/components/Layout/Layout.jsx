2f71b2c792bec17d53e7d3f09236dc416322bab3
Sebastián Fehlandt <sfehlandt@Inria-MacBook-Pro-SF.local>
2019-12-27 15:04:12 -0300

    Customize messages

--- love/src/components/Layout/Layout.jsx
+++ love/src/components/Layout/Layout.jsx
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { ToastContainer, toast, Slide } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
-
+import { SALCommandStatus } from '../../redux/actions/ws';
 
 export default class Layout extends Component {
-
   static propTypes = {
     /** Children components */
     children: PropTypes.node,
@@ -17,12 +16,59 @@ export default class Layout extends Component {
     lastSALCommand: undefined,
   };
 
+  static watcherSuccessfulCmds = {
+    'cmd_acknowledge': 'acknowledged',
+    'cmd_mute': 'muted',
+    'cmd_unmute': 'unmuted',
+  }
+
+  static watcherErrorCmds = {
+    'cmd_acknowledge': 'acknowledging',
+    'cmd_mute': 'muting',
+    'cmd_unmute': 'unmuting',
+  }
+
+  componentDidUpdate = (prevProps, _prevState) => {
+    /* Check command ack for toast*/
+    console.log('this.props.lastSALCommand: ', this.props.lastSALCommand);
+    if (
+      prevProps.lastSALCommand.status === SALCommandStatus.REQUESTED &&
+      this.props.lastSALCommand.status === SALCommandStatus.ACK
+    ) {
+      const [message, result] = this.getNotificationMessage(this.props.lastSALCommand);
+      if (result === 'Done') {
+        toast.success(message);
+      } else {
+        toast.info(message);
+      }
+    }
+  };
+
+  getNotificationMessage = (salCommand) => {
+    const cmd = salCommand.cmd;
+    const result = salCommand.result;
+    const component = salCommand.component;
+
+    if (component === 'Watcher') {
+      const alarm = salCommand.params.name;
+      if (result === 'Done') {
+        return [`Alarm '${alarm}' ${Layout.watcherSuccessfulCmds[cmd]} successfully`, result];
+      } else {
+        return [`Error ${Layout.watcherErrorCmds[cmd]} alarm '${alarm}', returned ${result}`, result];
+      }
+    }
+
+    if (result === 'Done') {
+      return [`Command '${cmd}' ran successfully`, result];
+    } else {
+      return [`Command '${cmd}' returned ${result}`, result];
+    }
+  };
+
   render() {
-    toast.info('blah');
-    console.log('props: ', this.props);
     return (
       <>
-        <ToastContainer position={toast.POSITION.BOTTOM_CENTER} transition={Slide} />
+        <ToastContainer position={toast.POSITION.BOTTOM_CENTER} transition={Slide} hideProgressBar />
         {this.props.children}
       </>
     );
