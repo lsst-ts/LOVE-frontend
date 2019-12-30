57b7d2142b82cf195f3199b8cce42d22dd49e9d3
Sebastián Fehlandt <sfehlandt@Inria-MacBook-Pro-SF.local>
2019-12-30 12:19:24 -0300

    Filter ack alarms only if they are 10 or more seconds old

--- love/src/components/Watcher/Watcher.jsx
+++ love/src/components/Watcher/Watcher.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import MuteIcon from '../icons/MuteIcon/MuteIcon';
 import Panel from '../GeneralPurpose/Panel/Panel';
 import Badge from '../GeneralPurpose/Badge/Badge';
 import AlarmsTable from './AlarmsTable/AlarmsTable';
 import styles from './Watcher.module.css';
+
 
 export default class Watcher extends Component {
   static propTypes = {
@@ -70,9 +72,17 @@ export default class Watcher extends Component {
     let unmutedAlarmsCount = 0;
     let unackMutedAlarmsCount = 0;
     let unackUnmutedAlarmsCount = 0;
+    const now = moment().unix() - this.props.taiToUtc;
 
     for (let alarm of this.props.alarms) {
-      if (alarm['severity'] <= 1 && alarm['maxSeverity'] <= 1 && alarm['acknowledged']) continue;
+      if (
+        alarm['severity'] <= 1 &&
+        alarm['maxSeverity'] <= 1 &&
+        alarm['acknowledged'] &&
+        now - alarm['timestampSeverityOldest'] >= 10
+      ) {
+        continue;
+      }
 
       if (alarm['mutedBy'] === '') {
         unmutedAlarmsCount += 1;
