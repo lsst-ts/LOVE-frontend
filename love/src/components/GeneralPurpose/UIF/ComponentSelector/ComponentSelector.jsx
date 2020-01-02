6b03caca2c10ca8997a6990a5faa23f5a26463f9
Sebastián Fehlandt <sfehlandt@Inria-MacBook-Pro-SF.local>
2020-01-02 15:33:09 -0300

    Add dummy ComponentSelector component

---
+++ love/src/components/GeneralPurpose/UIF/ComponentSelector/ComponentSelector.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './ComponentSelector.module.css';
+
+
+
+export default class ComponentSelector extends Component {
+  static propTypes = {
+    /** Children components */
+    children: PropTypes.node,
+    /** Last SAL command that has been sent */
+    lastSALCommand: PropTypes.object,
+  };
+
+  static defaultProps = {
+    lastSALCommand: undefined,
+  };
+
+  render() {
+    return (
+      <>
+        <div className={styles.container}>
+          Blah
+        </div>
+      </>
+    );
+  }
+}
