cd146617df8956582b800d057e23a4a376310dac
Sebastián Fehlandt <sfehlandt@Inria-MacBook-Pro-SF.local>
2020-01-02 17:44:42 -0300

    Add close button, add "transparent" status option to Button

--- love/src/components/GeneralPurpose/Modal/Modal.jsx
+++ love/src/components/GeneralPurpose/Modal/Modal.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import ReactModal from 'react-modal';
+import Button from '../Button/Button';
+
 import styles from './Modal.module.css';
 
 
 export default function Modal(props) {
+
+  console.log('props: ', props);
+
   ReactModal.setAppElement('#root');
   const { children, ...other } = props;
+
   return (
     <ReactModal {...other} className={styles.modal} overlayClassName={styles.overlay}>
-      {children}
+      <div className={styles.topbar}>
+        <Button title='Close' status='transparent' onClick={props.onRequestClose}>
+          &#10005;
+        </Button>
+      </div>
+      <div className={styles.content}>
+        {children}
+      </div>
     </ReactModal>
   );
 };
