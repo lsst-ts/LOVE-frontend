48eb3cd79bbe2cba609bbc864d2be3aa94c4cc41
Sebastián Fehlandt <sfehlandt@Inria-MacBook-Pro-SF.local>
2020-01-02 18:26:13 -0300

    Refctor UIF ComponentIndex

--- love/src/components/GeneralPurpose/UIF/ComponentIndex.jsx
+++ love/src/components/GeneralPurpose/UIF/ComponentIndex.jsx
@@ -1,11 +1,5 @@
-export default {
-    MountSummaryPanel: require('../../AuxTel/Mount/SummaryPanel/SummaryPanel.container').default,
-    LightPath: require('../../AuxTel/Mount/LightPath.container').default,
-    MotorTable: require('../../AuxTel/Mount/MotorTable/MotorTable.container').default,
+export const uifIndex = {
     ScriptQueue: require('../../ScriptQueue/ScriptQueue.container').default,
-    Camera: require('../../AuxTel/Camera/Camera.container').default,
-    Dome: require('../../AuxTel/Dome/Dome.container').default,
-    LATISS: require('../../AuxTel/LATISS/LATISS.container').default,
     CSCDetail: require('../../CSCSummary/CSCDetail/CSCDetail.container').default,
     CSCExpanded: require('../../CSCSummary/CSCExpanded/CSCExpanded.container').default,
     CSCGroupLog: require('../../CSCSummary/CSCGroupLog/CSCGroupLog.container').default,
@@ -15,4 +9,22 @@ export default {
     TimeSeriesPlot: require('../../GeneralPurpose/TimeSeriesPlot/TimeSeriesPlot.container').default,
     TelemetryLog: require('../../TelemetryLog/TelemetryLog.container').default,
     Watcher: require('../../Watcher/Watcher.container').default,
-}
+};
+
+export const auxtelIndex = {
+    MountSummaryPanel: require('../../AuxTel/Mount/SummaryPanel/SummaryPanel.container').default,
+    LightPath: require('../../AuxTel/Mount/LightPath.container').default,
+    MotorTable: require('../../AuxTel/Mount/MotorTable/MotorTable.container').default,
+    Camera: require('../../AuxTel/Camera/Camera.container').default,
+    Dome: require('../../AuxTel/Dome/Dome.container').default,
+    LATISS: require('../../AuxTel/LATISS/LATISS.container').default,
+};
+
+export const mainIndex = {
+};
+
+export default {
+  ...uifIndex,
+  ...auxtelIndex,
+  ...mainIndex,
+};
