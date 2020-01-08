a9e2ce3769e145d0b5f11fd8cd4f9618412fdf53
Sebastián Fehlandt <sfehlandt@Inria-MacBook-Pro-SF.local>
2020-01-08 12:06:05 -0300

    Use descriptions from schemas

--- love/src/components/GeneralPurpose/UIF/ComponentSelector/ComponentSelector.jsx
+++ love/src/components/GeneralPurpose/UIF/ComponentSelector/ComponentSelector.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './ComponentSelector.module.css';
-import {uifIndex, auxtelIndex, mainIndex, indexes} from '../ComponentIndex';
+import {indexes} from '../ComponentIndex';
 
 
 export default class ComponentSelector extends Component {
@@ -22,22 +22,23 @@ export default class ComponentSelector extends Component {
         <div className={styles.container}>
           <h2> Select Components to add to the view </h2>
 
-          {indexes.map((index) => (
-            <>
-              <h3> {index['name']} </h3>
-              <div className={styles.gallery}>
-                {Object.keys(index['index']).map((name) => (
-                  <>
-                    <div className={styles.component}>
-                      <h4> {name} </h4>
-                      <p> Component used to display Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur leo at interdum aliquam. Integer at ante quis risus scelerisque dictum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In egestas sodales volutpat. Praesent congue est quis diam porta placerat. In iaculis fermentum arcu. Donec ultricies elementum velit quis tincidunt. Nulla risus neque, posuere sit amet velit ac, iaculis tempor diam. Nulla lacinia in massa vel maximus. Nulla facilisi.  </p>
+          {indexes.map((index) => {
+            const category = index['name'];
+            const componentsMap = index['index'];
+            return (
+              <div key={category}>
+                <h3> {category} </h3>
+                <div className={styles.gallery}>
+                  {Object.keys(componentsMap).map((component) => (
+                    <div key={component} className={styles.component}>
+                      <h4> {component} </h4>
+                      <p> { componentsMap[component]['schema']['description'] }</p>
                     </div>
-                  </>
-                ))}
+                  ))}
+                </div>
               </div>
-            </>
-          ))}
-
+            )
+          })}
         </div>
       </>
     );
