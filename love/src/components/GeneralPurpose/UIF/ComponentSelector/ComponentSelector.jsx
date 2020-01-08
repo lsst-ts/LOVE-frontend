84b9ae26cdb4814519c2b89076de0d660df4c490
Sebastián Fehlandt <sfehlandt@Inria-MacBook-Pro-SF.local>
2020-01-08 15:22:45 -0300

    Enable selection

--- love/src/components/GeneralPurpose/UIF/ComponentSelector/ComponentSelector.jsx
+++ love/src/components/GeneralPurpose/UIF/ComponentSelector/ComponentSelector.jsx
@@ -16,10 +16,29 @@ export default class ComponentSelector extends Component {
     lastSALCommand: undefined,
   };
 
+  constructor() {
+    super();
+    this.state = {
+      selected: [],
+    };
+  }
+
+  addOrRemoveFromSelection = (component) => {
+    if (!this.state.selected.includes(component)) {
+      this.setState({
+        selected:  [...this.state.selected, component],
+      });
+    } else {
+      this.setState({
+        selected:  this.state.selected.filter(comp => comp !== component),
+      });
+    }
+  }
+
   render() {
     return (
-      <>
-        <div className={styles.container}>
+      <div className={styles.container}>
+        <div className={styles.content}>
           <h2> Select Components to add to the view </h2>
 
           {indexes.map((index) => {
@@ -29,18 +48,35 @@ export default class ComponentSelector extends Component {
               <div key={category}>
                 <h3> {category} </h3>
                 <div className={styles.gallery}>
-                  {Object.keys(componentsMap).map((component) => (
-                    <div key={component} className={styles.component}>
-                      <h4> {component} </h4>
-                      <p> { componentsMap[component]['schema']['description'] }</p>
-                    </div>
-                  ))}
+                  {Object.keys(componentsMap).map((component) => {
+                    const selected = this.state.selected.includes(component)
+                    const checkboxId = 'checkbox-' + component;
+                    return (
+                      <div
+                        key={component}
+                        className={[styles.card, selected ? styles.selected : null].join(' ')}
+                        onClick={() => this.addOrRemoveFromSelection(component)}
+                      >
+                        <div className={styles.cardHeader}>
+                          <h4> {component} </h4>
+                          <div className={styles.customCheckbox} id={checkboxId}>
+                            <input type="checkbox" className={styles.checkbox} checked={selected}/>
+                            <label for={checkboxId} />
+                          </div>
+                        </div>
+                        <p> { componentsMap[component]['schema']['description'] }</p>
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
             )
           })}
         </div>
-      </>
+        <div className={styles.footer}>
+          
+        </div>
+      </div>
     );
   }
 }
