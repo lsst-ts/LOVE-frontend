51a712b62a5574c0e42df5339f822c19dbd729e1
Sebastián Fehlandt <sfehlandt@Inria-MacBook-Pro-SF.local>
2020-01-08 16:58:01 -0300

    Define callback for insert button

--- love/src/components/GeneralPurpose/UIF/ComponentSelector/ComponentSelector.jsx
+++ love/src/components/GeneralPurpose/UIF/ComponentSelector/ComponentSelector.jsx
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Button from '../../Button/Button';
 import styles from './ComponentSelector.module.css';
-import {indexes} from '../ComponentIndex';
-
+import { indexes } from '../ComponentIndex';
 
 export default class ComponentSelector extends Component {
   static propTypes = {
     /** Children components */
     children: PropTypes.node,
-    /** Last SAL command that has been sent */
-    lastSALCommand: PropTypes.object,
+    /** Function to call when the "insert" button is clicked */
+    selectCallback: PropTypes.func,
   };
 
   static defaultProps = {
@@ -27,20 +26,20 @@ export default class ComponentSelector extends Component {
   addOrRemoveFromSelection = (component) => {
     if (!this.state.selected.includes(component)) {
       this.setState({
-        selected:  [...this.state.selected, component],
+        selected: [...this.state.selected, component],
       });
     } else {
       this.setState({
-        selected:  this.state.selected.filter(comp => comp !== component),
+        selected: this.state.selected.filter((comp) => comp !== component),
       });
     }
-  }
+  };
 
   clearSelection = () => {
     this.setState({
-      selected:  [],
+      selected: [],
     });
-  }
+  };
 
   render() {
     const buttonsDisabled = this.state.selected.length === 0;
@@ -57,7 +56,7 @@ export default class ComponentSelector extends Component {
                 <h3> {category} </h3>
                 <div className={styles.gallery}>
                   {Object.keys(componentsMap).map((component) => {
-                    const selected = this.state.selected.includes(component)
+                    const selected = this.state.selected.includes(component);
                     const checkboxId = 'checkbox-' + component;
                     return (
                       <div
@@ -68,25 +67,33 @@ export default class ComponentSelector extends Component {
                         <div className={styles.cardHeader}>
                           <h4> {component} </h4>
                           <div className={styles.customCheckbox} id={checkboxId}>
-                            <input type="checkbox" className={styles.checkbox} checked={selected}/>
-                            <label for={checkboxId} />
+                            <input type="checkbox" readOnly className={styles.checkbox} checked={selected} />
+                            <label htmlFor={checkboxId} />
                           </div>
                         </div>
-                        <p> { componentsMap[component]['schema']['description'] }</p>
+                        <p> {componentsMap[component]['schema']['description']}</p>
                       </div>
                     );
                   })}
                 </div>
               </div>
-            )
+            );
           })}
         </div>
         <div className={styles.footer}>
-          <span/>
-          <Button status='default' onClick={this.clearSelection} disabled={buttonsDisabled}>
+          <span />
+          <Button
+            status="default"
+            disabled={buttonsDisabled}
+            onClick={this.clearSelection}
+          >
             Clear Selection
           </Button>
-          <Button status='primary' disabled={buttonsDisabled}>
+          <Button
+            status="primary"
+            disabled={buttonsDisabled}
+            onClick={() => this.props.selectCallback(this.state.selected)}
+          >
             Insert
           </Button>
         </div>
