2b9eac1cd4ca9727985f06b252c1ed1c564d9786
José  Galaz <jdgalazm@gmail.com>
2020-01-13 16:13:20 -0300

    read header and body of image stream

--- love/src/components/GenericCamera/GenericCamera.jsx
+++ love/src/components/GenericCamera/GenericCamera.jsx
@@ -1,4 +1,40 @@
 import React, { useEffect } from 'react';
+const decoder = new TextDecoder('utf-8');
+const encoder = new TextEncoder('utf-8');
+
+const getHeaderInfo = (arrayBuffer) => {
+  let headerCandidate = new Uint8Array(arrayBuffer.slice(0, 50));
+  const footerCandidate = new Uint8Array(arrayBuffer.slice(arrayBuffer.byteLength - 100, arrayBuffer.byteLength - 1));
+  const decoded = decoder.decode(headerCandidate);
+  const split = decoded.split('\r\n');
+  const START = split[0];
+  const widthString = split[1];
+  const heightString = split[2];
+  const isPNGString = split[3];
+  const lengthString = split[4];
+  const matrixStartPosition = decoded.search(`${lengthString}\r\n`) + `${lengthString}\r\n`.length;
+  const headerString = decoded.slice(0, matrixStartPosition);
+
+  const headerByteLength = encoder.encode(headerString).length;
+  const ENDLabelPosition = decoder.decode(arrayBuffer).indexOf('[END]\r\n');
+
+  const body = arrayBuffer.slice(headerByteLength, headerByteLength + 1024 * 1024);
+
+  return {
+    headerByteLength,
+    _decoded: decoded,
+    _START: START,
+    width: parseInt(widthString),
+    height: parseInt(heightString),
+    isPNG: isPNGString === '1',
+    length: parseInt(lengthString),
+    body: body,
+  };
+};
+
+const bufferIncludesString = (buffer, string) => {
+  return decoder.decode(buffer).includes(string);
+};
 
 /**
  * Based on https://developer.mozilla.org/en-US/docs/Web/API/Streams_API/Using_readable_streams
@@ -19,9 +55,11 @@ export default function() {
             function pump() {
               return reader.read().then(({ done, value }) => {
                 count = count + 1;
-                console.log('reading', count, done);
+                // console.log('\nSTART?:', bufferIncludesString(value, 'START'))
+                // console.log('END?', bufferIncludesString(value, 'END'))
                 // When no more data needs to be consumed, close the stream
-                if (count > 2) {
+                if (count > 3) {
+                  controller.enqueue(value);
                   controller.close();
                   return;
                 }
@@ -40,77 +78,29 @@ export default function() {
       })
       .then((response) => response.blob())
       .then((blob) => {
-        const fileReader = new FileReader();
+        return new Promise((resolve, reject) => {
+          const fileReader = new FileReader();
 
-        fileReader.onload = (event) => {
-          /**
-          
-          # https://github.com/lsst-ts/ts_GenericCamera/blob/3eb5d5e104413cecb7950e2880a1154ea9846482/python/lsst/ts/GenericCamera/liveview/liveview.py#L277
+          fileReader.onloadend = (event) => {
+            const arrayBuffer = event.target.result;
+            console.log(arrayBuffer);
+            const headerInfo = getHeaderInfo(arrayBuffer);
+            const { headerByteLength, length, body } = headerInfo;
 
-          read_bytes = await self.reader.readline()
+            const newHeader = decoder.decode(new Uint8Array(arrayBuffer.slice(0, headerByteLength)));
+            const newHeaderPlusONe = decoder.decode(new Uint8Array(arrayBuffer.slice(0, headerByteLength + 1)));
+            resolve(headerInfo);
+          };
 
-          if read_bytes.rstrip().endswith(b'[START]'):
+          fileReader.readAsArrayBuffer(blob);
+        });
 
-              self.log.debug(f"Image started... Got {read_bytes}")
-
-              read_bytes = await asyncio.wait_for(self.reader.readline(), timeout=2.)
-              width = int(read_bytes.decode().rstrip())
-
-              read_bytes = await asyncio.wait_for(self.reader.readline(), timeout=2.)
-              height = int(read_bytes.decode().rstrip())
-
-              read_bytes = await asyncio.wait_for(self.reader.readline(), timeout=2.)
-              isJPEG = bool(int(read_bytes.decode().rstrip()))
-
-              read_bytes = await asyncio.wait_for(self.reader.readline(), timeout=2.)
-              length = int(read_bytes.decode().rstrip())
-
-              read_bytes = await asyncio.wait_for(self.reader.readline(), timeout=2.)
-              buffer = read_bytes
-
-              while len(buffer) < length:
-                  read_bytes = await asyncio.wait_for(self.reader.readline(), timeout=2.)
-                  buffer += read_bytes
-
-              dtype = np.uint8 if isJPEG else np.uint16
-
-              return exposure.Exposure(np.frombuffer(buffer.rstrip()[:-5], dtype=dtype),
-                                       width, height, {}, isJPEG)
-
-           */
-          const decoder = new TextDecoder('utf-8');
-          const encoder = new TextEncoder('utf-8');
-
-          const arrayBuffer = event.target.result;
-          
-          let headerCandidate = new Uint8Array(arrayBuffer.slice(0,50));
-          const decoded = decoder.decode(headerCandidate);
-          const split = decoded.split('\r\n');
-          const START = split[0];
-          const widthString = split[1];
-          const heightString = split[2];
-          const isPNGString = split[3];
-          const lengthString = split[4];
-          const matrixStartPosition = decoded.search(`${lengthString}\r\n`) + `${lengthString}\r\n`.length;
-          const headerString = decoded.slice(0, matrixStartPosition);
-
-          const headerByteCount = encoder.encode(headerString).length;
-
-          const newHeader = decoder.decode(new Uint8Array(arrayBuffer.slice(0,headerByteCount)));
-          const newHeaderPlusONe = decoder.decode(new Uint8Array(arrayBuffer.slice(0,headerByteCount+1)))          
-
-          console.log('decoded', decoded)
-          console.log('newHeader', newHeader);
-          console.log('newHeaderPlusONe', newHeaderPlusONe);
-          debugger;
-        };
-
-        fileReader.readAsArrayBuffer(blob);
-
-        return URL.createObjectURL(blob);
       })
-      .then((url) => console.log(url))
+      .then((r) => {
+        console.log(r);
+
+      })
       .catch((err) => console.error(err));
   }, []);
-  return <div>adsfsaf</div>;
+  return <div><img id="photo"></img></div>;
 }
