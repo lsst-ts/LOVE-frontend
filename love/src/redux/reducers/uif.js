fcf74d9b1e448e5e64be7dd3d60b41c485b5feec
spereirag <sebastian.pereira@inria.cl>
2020-01-23 11:59:17 -0300

    Add view initial state and save views into views array

--- love/src/redux/reducers/uif.js
+++ love/src/redux/reducers/uif.js
@@ -4,7 +4,6 @@ import {
   LOADING_VIEWS,
   RECEIVE_VIEWS,
   RECEIVE_CURRENT_WORKSPACE,
-  RECEIVE_VIEW,
   UPDATE_EDITED_VIEW,
   CLEAR_EDITED_VIEW,
   LOAD_EDITED_VIEW,
@@ -42,7 +41,7 @@ export const initialState = {
         h: 2,
         i: 0,
         allowOverflow: true,
-        cols: 100
+        cols: 100,
       },
       content: {},
     },
@@ -57,7 +56,6 @@ export const initialState = {
   workspaces: [],
 };
 
-
 /**
  * export default - Modifies the state of the UI Framework
  *
@@ -67,96 +65,92 @@ export const initialState = {
  */
 export default function(state = initialState, action) {
   switch (action.type) {
-    case RECEIVE_WORKSPACES:
-      {
-        return Object.assign({}, state, {
-          workspaces: action.workspaces,
-        });
+    case RECEIVE_WORKSPACES: {
+      return Object.assign({}, state, {
+        workspaces: action.workspaces,
+      });
+    }
+    case RECEIVE_VIEWS: {
+      return Object.assign({}, state, {
+        views: action.views,
+        viewsStatus: viewsStates.LOADED,
+      });
+    }
+    case LOADING_VIEWS: {
+      return Object.assign({}, state, {
+        viewsStatus: viewsStates.LOADING,
+      });
+    }
+    case RECEIVE_CURRENT_WORKSPACE: {
+      return Object.assign({}, state, {
+        currentWorkspace: action.workspace.id,
+        views: action.workspace.views,
+      });
+    }
+    case UPDATE_EDITED_VIEW: {
+      return Object.assign({}, state, {
+        editedViewCurrent: rfdc()(action.view),
+        editedViewStatus: {
+          code: editViewStates.UNSAVED,
+          details: null,
+        },
+      });
+    }
+    case LOAD_EDITED_VIEW: {
+      let view = state.views.find((v) => v.id === action.id);
+      if (view === undefined) {
+        view = rfdc()(initialState.editedViewCurrent);
       }
-    case RECEIVE_VIEWS:
-      {
-        return Object.assign({}, state, {
-          views: action.views,
-          viewsStatus: viewsStates.LOADED,
-        });
+      return Object.assign({}, state, {
+        editedViewCurrent: rfdc()(view),
+        editedViewSaved: rfdc()(view),
+        editedViewStatus: {
+          code: editViewStates.SAVED,
+          details: null,
+        },
+      });
+    }
+    case CLEAR_EDITED_VIEW: {
+      return Object.assign({}, state, {
+        editedViewCurrent: rfdc()(initialState.editedViewCurrent),
+        editedViewSaved: rfdc()(initialState.editedViewSaved),
+        editedViewStatus: rfdc()(initialState.editedViewStatus),
+      });
+    }
+    case SAVING_EDITED_VIEW: {
+      return Object.assign({}, state, {
+        editedViewStatus: {
+          code: editViewStates.SAVING,
+          details: null,
+        },
+      });
+    }
+    case SAVE_ERROR: {
+      return Object.assign({}, state, {
+        editedViewStatus: {
+          code: editViewStates.SAVE_ERROR,
+          details: action.response,
+        },
+      });
+    }
+    case SAVED_EDITED_VIEW: {
+      const newView = rfdc()(action.view);
+      const index = state.views.findIndex((view) => view.id === newView.id);
+      const newViews = [...state.views];
+      if (index !== -1) {
+        newViews[index] = newView;
+      } else {
+        newViews.push(newView);
       }
-    case LOADING_VIEWS:
-      {
-        return Object.assign({}, state, {
-          viewsStatus: viewsStates.LOADING,
-        });
-      }
-    case RECEIVE_CURRENT_WORKSPACE:
-      {
-        return Object.assign({}, state, {
-          currentWorkspace: action.workspace.id,
-          views: action.workspace.views,
-        });
-      }
-    case UPDATE_EDITED_VIEW:
-      {
-        return Object.assign({}, state, {
-          editedViewCurrent: rfdc()(action.view),
-          editedViewStatus: {
-            code: editViewStates.UNSAVED,
-            details: null,
-          },
-        });
-      }
-    case LOAD_EDITED_VIEW:
-      {
-        const view = state.views.find(view => view.id === action.id);
-        return Object.assign({}, state, {
-          editedViewCurrent: rfdc()(view),
-          editedViewSaved: rfdc()(view),
-          editedViewStatus: {
-            code: editViewStates.SAVED,
-            details: null,
-          },
-        });
-      }
-    case CLEAR_EDITED_VIEW:
-      {
-        return Object.assign({}, state, {
-          editedViewCurrent: rfdc()(initialState.editedViewCurrent),
-          editedViewSaved: rfdc()(initialState.editedViewSaved),
-          editedViewStatus: rfdc()(initialState.editedViewStatus),
-        });
-      }
-    case SAVING_EDITED_VIEW:
-      {
-        return Object.assign({}, state, {
-          editedViewStatus: {
-            code: editViewStates.SAVING,
-            details: null,
-          },
-        });
-      }
-    case SAVE_ERROR:
-      {
-        return Object.assign({}, state, {
-          editedViewStatus: {
-            code: editViewStates.SAVE_ERROR,
-            details: action.response,
-          },
-        });
-      }
-    case SAVED_EDITED_VIEW:
-      {
-        const newView = rfdc()(action.view);
-        const index = state.views.findIndex(view => view.id === newView.id);
-        if (index !== -1) {
-          state.views[index] = newView;
-        }
-        return Object.assign({}, state, {
-          editedViewStatus: {
-            code: editViewStates.SAVED,
-            details: null,
-          },
-          editedViewSaved: newView,
-          views: [...state.views],
-        });
-      }
+      return Object.assign({}, state, {
+        editedViewStatus: {
+          code: editViewStates.SAVED,
+          details: null,
+        },
+        editedViewSaved: newView,
+        views: newViews,
+      });
+    }
     default:
       return state;
   }
