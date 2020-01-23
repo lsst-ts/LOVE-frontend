1d721da45ec8ffcf664ca51786b830c70dd958ac
spereirag <sebastian.pereira@inria.cl>
2020-01-23 16:08:43 -0300

    Update uif reducer to use redux undo

--- love/src/redux/reducers/index.js
+++ love/src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
+import undoable, { includeAction } from 'redux-undo';
 import auth from './auth';
 import ws from './ws';
 import camera from './camera';
 import heartbeats from './heartbeats';
 import summaryData from './summaryData';
 import uif from './uif';
+import { UPDATE_EDITED_VIEW } from '../actions/actionTypes';
 
 export default combineReducers({
   auth,
@@ -12,5 +14,5 @@ export default combineReducers({
   camera,
   heartbeats,
   summaryData,
-  uif,
+  uif: undoable(uif, { filter: includeAction(UPDATE_EDITED_VIEW) }),
 });
