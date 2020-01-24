976ed9a56e76af793ed890abf2ceb3a9a8a55d36
José  Galaz <jdgalazm@gmail.com>
2020-01-24 10:13:10 -0300

    style-resize canvas when receiving image

--- love/src/components/GenericCamera/GenericCamera.jsx
+++ love/src/components/GenericCamera/GenericCamera.jsx
@@ -4,7 +4,7 @@ import * as CameraUtils from './CameraUtils';
 export const schema = {
   description: 'Renders the exposures streamed by the GenericCamera live view server into an HTML5 canvas',
   defaultSize: [10, 10],
-  props: {}
+  props: {},
 };
 /**
  * Draws a canvas in grayscale representing colors coming from
@@ -20,6 +20,20 @@ export default function() {
         canvasRef.current.width = image.width;
         canvasRef.current.height = image.height;
         CameraUtils.draw(new Uint8Array(image.body), canvasRef.current);
+
+        const containerWidth = parseInt(canvasRef.current.parentNode.style.width.slice(0, -2));
+        const containerHeight = parseInt(canvasRef.current.parentNode.style.height.slice(0, -2));
+        const imageAspectRatio = image.width / image.height;
+
+        if (containerWidth / containerHeight <= imageAspectRatio) {
+          // image width does not fit container
+          canvasRef.current.style.width = `${containerWidth}px`;
+          canvasRef.current.style.height = `${containerWidth / imageAspectRatio}px`;
+        } else {
+          // image height does not fit in container
+          canvasRef.current.style.height = `${containerHeight}px`;
+          canvasRef.current.style.width = `${containerHeight * imageAspectRatio}px`;
+        }
       }
     }, signal);
 
