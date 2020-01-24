6afcea281bfaf597f168bdc07e7d91752bb9a9d3
José  Galaz <jdgalazm@gmail.com>
2020-01-24 10:44:07 -0300

    auto-resize canvas with container resize

--- love/src/components/GenericCamera/GenericCamera.jsx
+++ love/src/components/GenericCamera/GenericCamera.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as CameraUtils from './CameraUtils';
 
 export const schema = {
@@ -11,29 +11,22 @@ export const schema = {
  * the Generic Camera images
  */
 export default function() {
+  const [imageWidth, setImageWidth] = useState(1024);
+  const [imageHeight, setImageHeight] = useState(1024);
+  const [containerWidth, setContainerWidth] = useState(1);
+  const [containerHeight, setContainerHeight] = useState(1);
+
   const canvasRef = useRef(null);
   useEffect(() => {
+    /** Start the stream once and update image size on every receive */
     const controller = new AbortController();
     const signal = controller.signal;
     CameraUtils.fetchImageFromStream((image) => {
       if (canvasRef.current) {
-        canvasRef.current.width = image.width;
-        canvasRef.current.height = image.height;
         CameraUtils.draw(new Uint8Array(image.body), canvasRef.current);
 
-        const containerWidth = parseInt(canvasRef.current.parentNode.style.width.slice(0, -2));
-        const containerHeight = parseInt(canvasRef.current.parentNode.style.height.slice(0, -2));
-        const imageAspectRatio = image.width / image.height;
-
-        if (containerWidth / containerHeight <= imageAspectRatio) {
-          // image width does not fit container
-          canvasRef.current.style.width = `${containerWidth}px`;
-          canvasRef.current.style.height = `${containerWidth / imageAspectRatio}px`;
-        } else {
-          // image height does not fit in container
-          canvasRef.current.style.height = `${containerHeight}px`;
-          canvasRef.current.style.width = `${containerHeight * imageAspectRatio}px`;
-        }
+        setImageWidth(image.width);
+        setImageHeight(image.height);
       }
     }, signal);
 
@@ -41,5 +34,46 @@ export default function() {
       controller.abort();
     };
   }, []);
+
+  useEffect(() => {
+    /** Listen to size changes of the canvas parent element*/
+    if (!canvasRef.current) return;
+    const observer = new ResizeObserver((entries) => {
+      const container = entries[0];
+      setContainerWidth(container.contentRect.width);
+      setContainerHeight(container.contentRect.height);
+    });
+
+    observer.observe(canvasRef.current.parentNode);
+
+    return ()=>{
+      observer.disconnect();
+    }
+  }, []);
+
+  useEffect(() => {
+    /** Sync canvas size with its container and stream  */
+    const imageAspectRatio = imageWidth / imageHeight;
+    const containerAspectRatio = containerWidth / containerHeight;
+
+    if (containerAspectRatio <= imageAspectRatio) {
+      // image width does not fit container
+      canvasRef.current.style.width = `${containerWidth}px`;
+      canvasRef.current.style.height = `${containerWidth / imageAspectRatio}px`;
+    } else {
+      // image height does not fit in container
+      canvasRef.current.style.height = `${containerHeight}px`;
+      canvasRef.current.style.width = `${containerHeight * imageAspectRatio}px`;
+    }
+
+    /**
+     * previous block is equivalent to:
+     * width = Math.min( containerWidth, imageAspectRatio * containerHeight );
+     * height = Math.min(containerHeight, 1/imageAspectRatio * containerWidth);
+     * but this might be less readable
+     */
+    //
+  }, [imageWidth, imageHeight, containerWidth, containerHeight]);
+
   return <canvas ref={canvasRef}></canvas>;
 }
