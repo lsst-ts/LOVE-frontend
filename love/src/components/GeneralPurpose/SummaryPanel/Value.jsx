1642d2c69585c9561ba74415501f34232f9d4575
spereirag <sebastian.pereira@inria.cl>
2020-02-10 12:29:25 -0300

    Make SummaryPanel render more robust

--- love/src/components/GeneralPurpose/SummaryPanel/Value.jsx
+++ love/src/components/GeneralPurpose/SummaryPanel/Value.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from './SummaryPanel.module.css';
 
-const Value = ({children}) => {
-  return <span className={styles.value}>{children}</span>;
+const Value = ({ children }) => {
+  const child = typeof children === 'object' && !React.isValidElement(children) ? JSON.stringify(children) : children;
+  return <span className={styles.value}>{child}</span>;
 };
 
 export default Value;
