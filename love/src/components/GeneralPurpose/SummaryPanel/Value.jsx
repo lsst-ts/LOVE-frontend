0fa42e8e1527023b14261fd19aa5dd4dc15cbb8c
spereirag <sebastian.pereira@inria.cl>
2020-02-12 14:55:10 -0300

    Fix SummaryPanel bug

--- love/src/components/GeneralPurpose/SummaryPanel/Value.jsx
+++ love/src/components/GeneralPurpose/SummaryPanel/Value.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import styles from './SummaryPanel.module.css';
 
 const Value = ({ children }) => {
-  const child = typeof children === 'object' && !React.isValidElement(children) ? JSON.stringify(children) : children;
+  let child = children;
+  if (typeof children === 'object' && !React.isValidElement(children)) {
+    if (children.value !== undefined) child = children.value;
+    else child = JSON.stringify(children);
+  }
   return <span className={styles.value}>{child}</span>;
 };
 
