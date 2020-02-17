6d98a9031db18beb98d636f24a45724829279c8b
spereirag <sebastian.pereira@inria.cl>
2020-02-17 10:49:47 -0300

    Fix notch and LATISS view in Safari

--- love/src/components/Layout/NotchCurve/NotchCurve.jsx
+++ love/src/components/Layout/NotchCurve/NotchCurve.jsx
@@ -4,7 +4,7 @@ import styles from './NotchCurve.module.css';
 function NotchCurve(props) {
   const style = [styles, props.className].join(' ');
   return (
-    <svg viewBox="0 0 76.3 60.5" {...props} transform={props.flip ? 'scale(-1,1)' : ''}>
+    <svg viewBox="0 0 76.3 60.5" {...props} style={{transform: (props.flip ? 'scale(-1,1)' : '')}}>
       <path className={style.path} d="M0,60.5h15.8c9.4,0,18.3-4.3,24.2-11.6L76.3,0H0V60.5z"/>
     </svg>
   );
