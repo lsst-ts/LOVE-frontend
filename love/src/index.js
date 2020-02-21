4f848cdd543352af63a458023d0c998584eeac3d
José  Galaz <jdgalazm@gmail.com>
2020-02-21 17:21:10 -0300

    use custom dialog for unsaved changes confirmation

--- love/src/index.js
+++ love/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import AppContainer from './App.container';
+import ConfirmationModal from './components/GeneralPurpose/ConfirmationModal/ConfirmationModal';
 import * as serviceWorker from './serviceWorker';
 import { doGetTokenFromStorage } from './redux/actions/auth';
 import { requestGroupSubscription } from './redux/actions/ws';
@@ -12,9 +13,39 @@ import store from './redux/store';
 store.dispatch(doGetTokenFromStorage());
 store.dispatch(requestGroupSubscription('heartbeat-manager-0-stream'));
 
+/**
+ * Replaces the behavior of window.confirm(message) whenever
+ * there is an URL change in BrowserRouter
+ * @param {string} message to be displayed
+ * @param {func} callback, used by BrowserRouter
+ *  calling with true will confirm the url change
+ *  calling with false will block it
+ *  must be used as `callback(window.confirm(message))`
+ */
+const getUserConfirmation = (message, callback) => {
+  const container = document.createElement('div');
+  document.body.appendChild(container);
+
+  const closeModal = (allowUrlChange) => {
+    ReactDOM.unmountComponentAtNode(container);
+    callback(allowUrlChange);
+  };
+
+  ReactDOM.render(
+    <ConfirmationModal
+      isOpen={true}
+      message={message}
+      confirmText="Accept"
+      cancelText="Cancel"
+      confirmCallback={() => closeModal(true)}
+      cancelCallback={() => closeModal(false)}
+    />,
+    container,
+  );
+};
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter getUserConfirmation={getUserConfirmation}>
       <AppContainer />
     </BrowserRouter>
   </Provider>,
