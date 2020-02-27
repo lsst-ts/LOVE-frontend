82d2141692aca4c77c4493920f18da07876c9b7e
Sebastián Fehlandt <sfehlandt@Inria-MacBook-Pro-SF.local>
2020-02-27 17:24:56 -0300

    WIP

--- love/src/redux/selectors/selectors.js
+++ love/src/redux/selectors/selectors.js
@@ -10,6 +10,8 @@ export const getPermCmdExec = (state) => state.auth.permissions.cmd_exec;
 
 export const getTokenStatus = (state) => state.auth.status;
 
+export const getConnectionStatus = (state) => state.ws.connectionState;
+
 export const getStreamsData = (state, groupNames) => {
   if (state.ws === undefined) return undefined;
 
