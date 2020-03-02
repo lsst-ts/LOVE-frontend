dd553469785bfe8a73bcb3f782cb1831ea3d00cb
Sebastián Fehlandt <sfehlandt@Inria-MacBook-Pro-SF.local>
2020-03-02 15:37:33 -0300

    Remove unnecessary promise

--- love/src/redux/actions/ws.js
+++ love/src/redux/actions/ws.js
@@ -88,103 +88,98 @@ export const openWebsocketConnection = () => {
     console.log('Opening ws connection, token: ', token);
     dispatch(changeConnectionState(connectionStates.OPENING));
 
-    const wsPromise = new Promise((resolve) => {
-      socket = sockette(connectionPath, {
-        onopen: () => {
-          dispatch(changeConnectionState(connectionStates.OPEN));
-          resolve();
-        },
-        onclose: (event) => {
-          const status = event.code === 4000 || event.code === 1000 ?
-            connectionStates.CLOSED : connectionStates.RETRYING;
-          dispatch(changeConnectionState(status));
-          resolve();
-        },
-        onerror: () => {
-          dispatch(changeConnectionState(connectionStates.RETRYING));
-          resolve();
-        },
-        onmessage: (msg) => {
-          if (!msg.data) return;
+    socket = sockette(connectionPath, {
+      onopen: () => {
+        dispatch(changeConnectionState(connectionStates.OPEN));
+      },
+      onclose: (event) => {
+        const status = event.code === 4000 || event.code === 1000 ?
+          connectionStates.CLOSED : connectionStates.RETRYING;
+        dispatch(changeConnectionState(status));
+      },
+      onerror: () => {
+        dispatch(changeConnectionState(connectionStates.RETRYING));
+      },
+      onmessage: (msg) => {
+        if (!msg.data) return;
 
-          const data = JSON.parse(msg.data);
-          if (!data.category) {
-            dispatch(receiveGroupConfirmationMessage(data.data));
-            return;
+        const data = JSON.parse(msg.data);
+        if (!data.category) {
+          dispatch(receiveGroupConfirmationMessage(data.data));
+          return;
+        }
+        if (data.category === 'heartbeat') {
+          dispatch(receiveManagerHeartbeat(data.data[0]));
+        }
+        if (data.category === 'event') {
+          const stream = data.data[0].data;
+          if (data.data[0].csc === 'ATCamera') {
+            if (stream.startIntegration || stream.endReadout || stream.startReadout || stream.endOfImageTelemetry) {
+              dispatch(receiveImageSequenceData(stream));
+            } else if (stream.imageReadoutParameters) {
+              dispatch(receiveReadoutData(stream));
+            } else if (
+              stream.startIntegration ||
+              stream.raftsDetailedState ||
+              stream.shutterDetailedState ||
+              stream.imageReadinessDetailedState ||
+              stream.calibrationDetailedState
+            ) {
+              dispatch(receiveCameraStateData(stream));
+            }
           }
-          if (data.category === 'heartbeat') {
-            dispatch(receiveManagerHeartbeat(data.data[0]));
-          }
-          if (data.category === 'event') {
-            const stream = data.data[0].data;
-            if (data.data[0].csc === 'ATCamera') {
-              if (stream.startIntegration || stream.endReadout || stream.startReadout || stream.endOfImageTelemetry) {
-                dispatch(receiveImageSequenceData(stream));
-              } else if (stream.imageReadoutParameters) {
-                dispatch(receiveReadoutData(stream));
-              } else if (
-                stream.startIntegration ||
-                stream.raftsDetailedState ||
-                stream.shutterDetailedState ||
-                stream.imageReadinessDetailedState ||
-                stream.calibrationDetailedState
-              ) {
-                dispatch(receiveCameraStateData(stream));
-              }
-            }
-            if (data.data[0].csc === 'ScriptHeartbeats') {
-              if (
-                stream.stream.script_heartbeat.salindex ||
-                stream.stream.script_heartbeat.lost ||
-                stream.stream.script_heartbeat.last_heartbeat_timestamp
-              ) {
-                const queueSalindex = data.data[0].salindex;
-                dispatch(receiveScriptHeartbeat(stream.stream.script_heartbeat, queueSalindex));
-              }
-            }
-
-            if (data.data[0].csc === 'ScriptQueueState') {
-              if (stream.stream.finished_scripts) {
-                const finishedIndices = stream.stream.finished_scripts.map((script) => script.index);
-                dispatch(removeScriptsHeartbeats(finishedIndices));
-              }
-            }
-
-            if (data.data[0].csc === 'Heartbeat') {
-              dispatch(receiveCSCHeartbeat(stream.stream));
-            }
-
-            if (data.data[0].csc === 'Watcher') {
-              dispatch(receiveAlarms(stream.alarm));
-            }
-
-            if (data.data[0].data.logMessage) {
-              dispatch(receiveLogMessageData(data.data[0].csc, data.data[0].salindex, data.data[0].data.logMessage));
-            }
-
-            if (data.data[0].data.errorCode) {
-              dispatch(receiveErrorCodeData(data.data[0].csc, data.data[0].salindex, data.data[0].data.errorCode));
+          if (data.data[0].csc === 'ScriptHeartbeats') {
+            if (
+              stream.stream.script_heartbeat.salindex ||
+              stream.stream.script_heartbeat.lost ||
+              stream.stream.script_heartbeat.last_heartbeat_timestamp
+            ) {
+              const queueSalindex = data.data[0].salindex;
+              dispatch(receiveScriptHeartbeat(stream.stream.script_heartbeat, queueSalindex));
             }
           }
 
-          if (data.category === 'ack') {
-            dispatch(updateLastSALCommandStatus(SALCommandStatus.ACK, data.data[0].data.stream.result));
+          if (data.data[0].csc === 'ScriptQueueState') {
+            if (stream.stream.finished_scripts) {
+              const finishedIndices = stream.stream.finished_scripts.map((script) => script.index);
+              dispatch(removeScriptsHeartbeats(finishedIndices));
+            }
           }
 
-          if (data.data[0].data.observingLog) {
-            dispatch(receiveObservingLog(data.data[0].data.observingLog));
+          if (data.data[0].csc === 'Heartbeat') {
+            dispatch(receiveCSCHeartbeat(stream.stream));
           }
 
-          data.data.forEach((stream) => {
-            dispatch(
-              receiveGroupSubscriptionData({
-                category: data.category,
-                ...stream,
-              }),
-            );
-          });
-        },
-      });
+          if (data.data[0].csc === 'Watcher') {
+            dispatch(receiveAlarms(stream.alarm));
+          }
+
+          if (data.data[0].data.logMessage) {
+            dispatch(receiveLogMessageData(data.data[0].csc, data.data[0].salindex, data.data[0].data.logMessage));
+          }
+
+          if (data.data[0].data.errorCode) {
+            dispatch(receiveErrorCodeData(data.data[0].csc, data.data[0].salindex, data.data[0].data.errorCode));
+          }
+        }
+
+        if (data.category === 'ack') {
+          dispatch(updateLastSALCommandStatus(SALCommandStatus.ACK, data.data[0].data.stream.result));
+        }
+
+        if (data.data[0].data.observingLog) {
+          dispatch(receiveObservingLog(data.data[0].data.observingLog));
+        }
+
+        data.data.forEach((stream) => {
+          dispatch(
+            receiveGroupSubscriptionData({
+              category: data.category,
+              ...stream,
+            }),
+          );
+        });
+      },
     });
   };
 };
