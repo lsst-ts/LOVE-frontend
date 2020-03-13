2c06c645d5431c13ec732cc444e6ac11c055b07b
Sebastián Fehlandt <sfehlandt@Inria-MacBook-Pro-SF.local>
2020-03-13 13:19:28 -0300

    Clean linter errors

--- love/src/components/GenericCamera/GenericCamera.jsx
+++ love/src/components/GenericCamera/GenericCamera.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as CameraUtils from './CameraUtils';
 import styles from './GenericCamera.module.css';
 
