c66d1ee25c055138dab6943b3d8e801e125f2973
José  Galaz <jdgalazm@gmail.com>
2020-03-17 19:10:11 -0300

    add unformatted textfield

--- love/src/components/ObservingLog/ObservingLogMessages.jsx
+++ love/src/components/ObservingLog/ObservingLogMessages.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Panel from '../GeneralPurpose/Panel/Panel';
 import styles from './ObservingLogMessages.module.css';
+import TextField from '../TextField/TextField';
 
 export default class ObservingLogInput extends Component {
   static propTypes = {
@@ -19,7 +20,9 @@ export default class ObservingLogInput extends Component {
 
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      filter: '',
+    };
   }
 
   componentDidMount = () => {
@@ -30,17 +33,26 @@ export default class ObservingLogInput extends Component {
     this.props.unsubscribeToStreams();
   };
 
+  changeFilter = (event) => {
+    this.setState({
+      filter: event.target.value,
+    });
+  };
+
   render() {
     return (
       <Panel title="Observing Log" className={styles.panel}>
         <div className={styles.container}>
+          <div className={styles.filterContainer}>
+            <TextField type="text" value={this.state.filter} onChange={this.changeFilter}/>
+          </div>
           {this.props.logMessages.map((msg) => {
             return (
               <div key={Math.random()} className={styles.logMessageWrapper}>
                 <div className={styles.logMessage}>
                   <div className={styles.topSection}>
                     <span>{msg.user.value}</span>
-                    <span>{new Date(msg.private_rcvStamp.value*1000).toLocaleString()}</span>
+                    <span>{new Date(msg.private_rcvStamp.value * 1000).toLocaleString()}</span>
                   </div>
                   <div className={styles.messageSection}>
                     <span>{msg.message.value}</span>
