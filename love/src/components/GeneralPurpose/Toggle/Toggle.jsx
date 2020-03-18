956ba78550baaf2d6e0c451783f6a9858595669d
José  Galaz <jdgalazm@gmail.com>
2020-03-18 17:15:00 -0300

    add small toggle component

---
+++ love/src/components/GeneralPurpose/Toggle/Toggle.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './Toggle.module.css';
+const Toggle = () => {
+  const [isLive, setLiveMode] = React.useState(false);
+  const   handleChangeChk = () => {
+    setLiveMode(!isLive);
+  };
+  return (
+    <div className={styles.switchContainer}>
+      <span
+        className={[styles.modeSelection, isLive ? styles.highlightText : ''].join(' ')}
+        onClick={() => setLiveMode(true)}
+      >
+        Live
+      </span>
+      <label className={styles.switch}>
+        <input type="checkbox" alt="Live/query mode toggle" checked={!isLive} onChange={handleChangeChk} />
+        <span className={[styles.slider, styles.round].join(' ')} />
+      </label>
+      <span
+        className={[styles.modeSelection, !isLive ? styles.highlightText : ''].join(' ')}
+        onClick={() => setLiveMode(false)}
+      >
+        Query
+      </span>
+    </div>
+  );
+};
+
+export default Toggle;
