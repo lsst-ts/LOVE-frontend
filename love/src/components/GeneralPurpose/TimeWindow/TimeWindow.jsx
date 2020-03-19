0f3637081c4fa030d187e0e7c4919d1d1a4cc234
José  Galaz <jdgalazm@gmail.com>
2020-03-19 15:04:26 -0300

    timewindow options as props and fix its filter

--- love/src/components/GeneralPurpose/TimeWindow/TimeWindow.jsx
+++ love/src/components/GeneralPurpose/TimeWindow/TimeWindow.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import styles from './TimeWindow.module.css';
 
-const options = {
-  '10s': 10,
-  '1min': 60,
-  '10min': 600,
-  '1d': 24 * 60 * 60,
-  '10d': 240 * 60 * 60,
-  '1m': 720 * 60 * 60,
-  '6m': 4320 * 60 * 60,
-  '1y': 8640 * 60 * 60,
-  Max: Infinity,
-};
-const TimeWindow = ({ setTimeWindowCallback = () => null, enabledOptions = [] }) => {
-  const [timeWindow, setTimeWindow] = React.useState(options['1d']);
-
+const TimeWindow = ({
+  options = {
+    '10s': 10,
+    '1min': 60,
+    '10min': 600,
+    '1d': 24 * 60 * 60,
+    '10d': 240 * 60 * 60,
+    '1m': 720 * 60 * 60,
+    '6m': 4320 * 60 * 60,
+    '1y': 8640 * 60 * 60,
+    Max: Infinity,
+  },
+  timeWindow = '1d',
+  setTimeWindow = () => null,
+  enabledOptions = [],
+}) => {
   const handleChange = (changeEvent) => {
     console.log(changeEvent.target.value);
     let tw = changeEvent.target.value !== 'Infinity' ? parseInt(changeEvent.target.value, 10) : Infinity;
-    console.log('tw',tw)
     setTimeWindow(tw);
-    setTimeWindowCallback(tw);
   };
   return (
     <div className={styles['time-window']}>
