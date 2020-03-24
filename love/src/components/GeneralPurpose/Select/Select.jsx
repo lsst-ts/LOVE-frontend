e62fc2c7a2b7d87ace0a9520fec2089bb38c09a9
José  Galaz <jdgalazm@gmail.com>
2020-03-24 14:13:37 -0300

    style react-dropdown according to palette

--- love/src/components/GeneralPurpose/Select/Select.jsx
+++ love/src/components/GeneralPurpose/Select/Select.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+import styles from './Select.module.css';
 
+const MOBILE = 'Mobile';
+const DESKTOP = 'Desktop';
+const TABLET = 'Tablet';
 
-const Select = ({children}) => {
+const options = [MOBILE, DESKTOP, TABLET].map((v) => ({ value: v, label: v }));
 
-    return (
-        <select>
-            {children}
-        </select>
-    )
-}
+const Select = ({ children }) => {
+  const [option, setOption] = React.useState(options[0]);
+  const alarm = {};
+  return (
+    <Dropdown
+      className={styles.dropDownClassName}
+      controlClassName={styles.dropDownControlClassName}
+      menuClassName={styles.dropDownMenuClassName}
+      arrowClassName={styles.arrowClassName}
+      options={options}
+      onChange={(option) => console.log(option)}
+      value={option}
+      placeholder="Select an option"
+    />
+  );
+};
 
 export default Select;
