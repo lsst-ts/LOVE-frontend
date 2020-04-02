0e14c7ba9d08b7d693d6c9a001948944ff1edca2
Sebastián Fehlandt <sfehlandt@Inria-MacBook-Pro-SF.local>
2020-04-02 19:10:22 -0300

    Convert seconds to Milliseconds

--- love/src/components/AuxTel/Camera/Camera.jsx
+++ love/src/components/AuxTel/Camera/Camera.jsx
@@ -121,7 +121,7 @@ export default class Camera extends Component {
                     return (
                       <React.Fragment key={imageKey}>
                         <tr>
-                          <td className={styles.string}>{formatTimestamp(image.timeStamp)}</td>
+                          <td className={styles.string}>{formatTimestamp(image.timeStamp * 1000)}</td>
                           <td className={styles.string}>{imageName}</td>
                           <td className={[styles.narrowCol].join(' ')}>
                             <LoadingBar
