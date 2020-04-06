f5b0e8ef6de2358ff707a8d5a05838f0ece37b89
Sebastián Fehlandt <sfehlandt@Sebastians-MacBook-Pro.local>
2020-04-06 16:07:36 -0400

    Improve clock and add movement

--- love/src/components/Time/TimeDisplay.jsx
+++ love/src/components/Time/TimeDisplay.jsx
@@ -6,33 +6,60 @@ import styles from './TimeDisplay.module.css';
 import * as dayjs from 'dayjs';
 
 
-TimeDisplay.propTypes = {
-  /** Number of seconds to add to a TAI timestamp to convert it in UTC */
-  taiToUtc: PropTypes.number,
-}
+export default class TimeDisplay extends React.Component {
 
-export default function TimeDisplay ({taiToUtc}) {
-  const localTime = dayjs();
-  return (
-    <div className={styles.container}>
-      <div className={styles.group}>
-        <ClockWrapper timestamp={localTime} title='Local Time' showAnalog/>
-        <ClockWrapper timestamp={localTime} title='Sidereal Time' showAnalog/>
-      </div>
-      <div className={styles.group}>
-        <div className={styles.column}>
-          <ClockWrapper timestamp={localTime} title='La Serena'/>
-          <ClockWrapper timestamp={localTime} title='Arizona'/>
-          <ClockWrapper timestamp={localTime} title='Illinois'/>
+  static propTypes = {
+    /** Number of seconds to add to a TAI timestamp to convert it in UTC */
+    taiToUtc: PropTypes.number,
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      timestamp: dayjs(),
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      timestamp: dayjs(),
+    });
+  }
+
+  render () {
+    const localTime = this.state.timestamp
+    return (
+      <div className={styles.container}>
+        <div className={styles.group}>
+          <ClockWrapper timestamp={localTime} title='Local Time' showAnalog/>
+          <ClockWrapper timestamp={localTime} title='Sidereal Time' showAnalog/>
         </div>
-        <div className={styles.column}>
-          <ClockWrapper timestamp={localTime} title='Universal Time (UTC)'/>
-          <ClockWrapper timestamp={localTime} title='International Atomic Time (TAI)'/>
-          <ClockWrapper timestamp={localTime} title='Modified JD:'/>
+        <div className={styles.group}>
+          <div className={styles.column}>
+            <ClockWrapper timestamp={localTime} title='La Serena'/>
+            <ClockWrapper timestamp={localTime} title='Arizona'/>
+            <ClockWrapper timestamp={localTime} title='Illinois'/>
+          </div>
+          <div className={styles.column}>
+            <ClockWrapper timestamp={localTime} title='Universal Time (UTC)'/>
+            <ClockWrapper timestamp={localTime} title='International Atomic Time (TAI)'/>
+            <ClockWrapper timestamp={localTime} title='Modified JD:'/>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 function ClockWrapper ({timestamp, title, showAnalog}) {
