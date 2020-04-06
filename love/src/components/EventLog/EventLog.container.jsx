7a06ddbcfdf7da38a4df0788cd3155ae2f8a71c5
spereirag <sebastian.pereira@inria.cl>
2020-04-06 12:49:27 -0400

    Add EventLog base component

---
+++ love/src/components/EventLog/EventLog.container.jsx
@@ -0,0 +1,136 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import EventLog from './EventLog';
+import { addGroupSubscription } from '../../redux/actions/ws';
+import { removeCSCLogMessages, removeCSCErrorCodeData } from '../../redux/actions/summaryData';
+import {
+  getStreamData,
+  getCSCHeartbeat,
+  getCSCLogMessages,
+  getCSCErrorCodeData,
+  getGroupSortedErrorCodeData,
+  getGroupSortedLogMessageData,
+} from '../../redux/selectors';
+import { CSCSummaryHierarchy } from '../../Config';
+
+export const schema = {
+  description: 'Displays the error code and message logs for a single CSC',
+  defaultSize: [24, 29],
+  props: {
+    titleBar: {
+      type: 'boolean',
+      description: 'Whether to display the title bar',
+      isPrivate: false,
+      default: false,
+    },
+    title: {
+      type: 'string',
+      description: 'Name diplayed in the title bar (if visible)',
+      isPrivate: false,
+      default: 'CSC expanded',
+    },
+    margin: {
+      type: 'boolean',
+      description: 'Whether to display component with a margin',
+      isPrivate: false,
+      default: true,
+    },
+    name: {
+      type: 'string',
+      description: 'Name of the CSC to monitor',
+      isPrivate: false,
+      default: 'Test',
+    },
+    salindex: {
+      type: 'number',
+      description: 'Salindex of the CSC',
+      isPrivate: false,
+      default: 1,
+    },
+    _functionProps: {
+      type: 'array',
+      description: 'Array containing the props that are functions',
+      isPrivate: true,
+      default: [],
+    },
+  },
+};
+
+const EventLogContainer = ({
+  name,
+  salindex,
+  group,
+  onCSCClick,
+  clearCSCErrorCodes,
+  clearCSCLogMessages,
+  summaryStateData,
+  logMessageData,
+  errorCodeData,
+  subscribeToStreams,
+  heartbeatData,
+  ...props
+}) => {
+  return (
+    <EventLog
+      name={name}
+      salindex={salindex}
+      group={group}
+      onCSCClick={onCSCClick}
+      clearCSCErrorCodes={clearCSCErrorCodes}
+      errorCodeData={errorCodeData}
+      summaryStateData={summaryStateData}
+      subscribeToStreams={subscribeToStreams}
+      logMessageData={logMessageData}
+      heartbeatData={heartbeatData}
+      clearCSCLogMessages={clearCSCLogMessages}
+      {...props}
+    />
+  );
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    subscribeToStreams: (cscList) => {
+      cscList.forEach((cscNameIndex) => {
+        const nameIndex = Object.values(cscNameIndex).join('-')
+        dispatch(addGroupSubscription(`event-${nameIndex}-logMessage`));
+        dispatch(addGroupSubscription(`event-${nameIndex}-errorCode`));
+      });
+    //   dispatch(addGroupSubscription('event-Heartbeat-0-stream'));
+    },
+    clearCSCLogMessages: (cscList) => {
+      cscList.forEach((cscNameIndex) => {
+        dispatch(removeCSCLogMessages(cscNameIndex));
+      });
+    },
+    clearCSCErrorCodes: (cscList) => {
+      cscList.forEach((cscNameIndex) => {
+        dispatch(removeCSCErrorCodeData(cscNameIndex));
+      });
+    },
+  };
+};
+
+const mapStateToProps = (state, ownProps) => {
+  const cscList = Object.values(CSCSummaryHierarchy).flatMap((realm) => {
+    // console.log(realm);
+    return Object.values(realm).flatMap((cscPairs) => {
+      return cscPairs.flatMap((pair) => {
+        // return Object.values(pair).join('-');
+        return pair;
+      });
+    });
+  });
+  console.log(cscList);
+  const errorCodeData = getGroupSortedErrorCodeData(state, cscList);
+  const logMessageData = getGroupSortedLogMessageData(state, cscList);
+  console.log(logMessageData);
+
+  return {
+    cscList,
+    errorCodeData,
+    logMessageData
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventLogContainer);
