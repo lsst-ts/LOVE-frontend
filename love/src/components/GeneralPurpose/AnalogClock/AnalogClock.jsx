bbe3e8d72430cc0030a0dac75b4d307cff18ef93
Sebastián Fehlandt <sfehlandt@Sebastians-MacBook-Pro.local>
2020-04-07 16:36:34 -0400

    Replace dayjs by luxon

--- love/src/components/GeneralPurpose/AnalogClock/AnalogClock.jsx
+++ love/src/components/GeneralPurpose/AnalogClock/AnalogClock.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './AnalogClock.module.css';
-import * as dayjs from 'dayjs';
 import { DateTime } from 'luxon';
 
 /**
--- love/src/components/GeneralPurpose/DigitalClock/DigitalClock.jsx
+++ love/src/components/GeneralPurpose/DigitalClock/DigitalClock.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './DigitalClock.module.css';
-import * as dayjs from 'dayjs';
 import { DateTime } from 'luxon';
 import { parseTimestamp } from '../../../Utils';
 
