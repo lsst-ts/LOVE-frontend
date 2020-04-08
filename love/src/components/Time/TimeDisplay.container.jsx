a4868ab4360a18e25cc53972a0672200286a5491
Sebastián Fehlandt <sfehlandt@Sebastians-MacBook-Pro.local>
2020-04-08 13:09:33 -0400

    WIP (adding Config to TimeDisplays)

--- love/src/components/Time/TimeDisplay.container.jsx
+++ love/src/components/Time/TimeDisplay.container.jsx
@@ -32,7 +32,31 @@ export const schema = {
         'en-GB' by default (so it is displayed as GMT always). Null or empty to use the browser locale.
         The format for this string must be: <language (2 lowercase chars)>-<COUNTRY (2 uppercase chars)>`,
       isPrivate: false,
-      default: 'en-GB',
+      default: 'en-US',
+    },
+    clocks: {
+      type: 'array',
+      description:
+        `Layout of clocks in Json form.
+        It is a list of horizontalGroups, each of which list of vertically-aligned elements.
+        Each clock has 3 properties:
+        - name: (string) name of the clock, to be displayed above it.
+        - hideAnalog: (boolean = false) flag to hide the analog clock.
+        - hideDate: (boolean = false) flag to hide the date.
+        - hideOffset: (boolean = false) flag to hide the UTC offset, displayed at the right of the name
+        - timezone: timezone string used to configure which UTC offset to use.
+          Null or empty if current should be used. 'UTC' for UTC. Null by default.
+          The format for this string must be: <Continent>/<City> (use camelcase and underscores (_) instead of spaces).
+          For example:
+          - For UTC use UTC
+          - For TAI use TAI
+          - For La Serena use America/Santiago (yes America, not Chile)
+          - For Arizona use America/Phoenix
+          - For Illinois use America/Chicago
+          Note that not every city is available, check the IANA DB documentation for more info: https://www.iana.org/time-zones
+          See the default value as an example`,
+      isPrivate: false,
+      default: [],
     },
   },
 };
