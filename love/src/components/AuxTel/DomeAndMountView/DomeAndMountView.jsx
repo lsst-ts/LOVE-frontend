3862de6cd6cd4a36e965b8d38c303ca6e42b4994
spereirag <sebastian.pereira@inria.cl>
2020-04-20 16:51:47 -0400

    Small unrelated fixes

--- love/src/components/AuxTel/DomeAndMountView/DomeAndMountView.jsx
+++ love/src/components/AuxTel/DomeAndMountView/DomeAndMountView.jsx
@@ -9,24 +9,6 @@ export default class DomeAndMountView extends Component {
     return (
       <div className={styles.mainGrid}>
         <div className={styles.statusGrid}>
-          {/* <Panel title="Status" className={styles.panel}>
-            <div className={styles.statusContainer}>
-              <LabeledStatusTextContainer
-                label={'Azimuth state'}
-                groupName="event-ATDome-azimuthState"
-                accessor={(event) => event.state.value}
-                stateToLabelMap={{ 0: 'STOPPED', 1: 'MOVINGCW', 2: 'MOVINGCCW' }}
-                stateToStyleMap={{ 0: 'ok', 1: 'running', 2: 'running' }}
-              />
-              <LabeledStatusTextContainer
-                label={'Azimuth state'}
-                groupName="event-ATMCS-atMountState"
-                accessor={(event) => event.state.value}
-                stateToLabelMap={{ 0: 'TRACK_DISABLED', 1: 'TRACK_ENABLED', 2: 'STOPPING' }}
-                stateToStyleMap={{ 0: 'ok', 1: 'running', 2: 'running' }}
-              />
-            </div>
-          </Panel> */}
         </div>
         <div className={styles.domeGrid}>
           <Panel title="Dome and Mount view" className={styles.panel}>
