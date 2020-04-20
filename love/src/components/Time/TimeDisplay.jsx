177e473488158ee130043d7970d5a686cb398500
Sebastián Fehlandt <sebafehlandt@gmail.com>
2020-04-20 16:02:51 -0400

    Fix errors syncking clocks

--- love/src/components/Time/TimeDisplay.jsx
+++ love/src/components/Time/TimeDisplay.jsx
@@ -5,13 +5,12 @@ import styles from './TimeDisplay.module.css';
 import { DateTime } from 'luxon';
 import { siderealSecond } from '../../Utils';
 
-
 export default class TimeDisplay extends React.Component {
   static propTypes = {
     /** Time Data from the server */
     timeData: PropTypes.object,
     /** Locale string used to configure how to display the UTC Offset. en-GB by default (so it is displayed as GMT always).
-    * Null or empty to use the browser locale */
+     * Null or empty to use the browser locale */
     locale: PropTypes.string,
     clocks: PropTypes.array,
   };
@@ -28,9 +27,9 @@ export default class TimeDisplay extends React.Component {
         sidereal_summit: 0,
         sidereal_greenwich: 0,
         tai_to_utc: 0,
-      }
-    }
-  }
+      },
+    },
+  };
 
   constructor(props) {
     super(props);
@@ -51,52 +50,60 @@ export default class TimeDisplay extends React.Component {
   }
 
   componentDidUpdate(prevProps) {
-    if (prevProps.timeData.server_time.utc !== this.props.timeData.server_time.utc || this.state.sidereal_greenwich === 0) {
-      const diffSecs = this.props.timeData.server_time.utc - (this.props.timeData.receive_time + this.props.timeData.request_time) / 2;
+    if (
+      prevProps.timeData.server_time.utc !== this.props.timeData.server_time.utc ||
+      this.state.sidereal_greenwich === 0
+    ) {
+      const error = (this.props.timeData.receive_time - this.props.timeData.request_time) / 2;
+      const fixedServer = this.props.timeData.server_time.utc + error;
       this.setState({
-        local: DateTime.local().plus({seconds: diffSecs}),
-        sidereal_greenwich: DateTime.fromSeconds(this.props.timeData.server_time.sidereal_greenwich * 3600 + diffSecs * siderealSecond),
-        sidereal_summit: DateTime.fromSeconds(this.props.timeData.server_time.sidereal_summit * 3600 + diffSecs * siderealSecond),
-        mjd: this.props.timeData.server_time.mjd + diffSecs / (3600*24),
+        local: DateTime.fromSeconds(fixedServer),
+        sidereal_greenwich: DateTime.fromSeconds(
+          this.props.timeData.server_time.sidereal_greenwich * 3600 + error * siderealSecond,
+        ),
+        sidereal_summit: DateTime.fromSeconds(
+          this.props.timeData.server_time.sidereal_summit * 3600 + error * siderealSecond,
+        ),
+        mjd: this.props.timeData.server_time.mjd + error / (3600 * 24),
       });
     }
   }
 
   tick() {
     this.setState({
-      local: this.state.local.plus({seconds: 1}),
-      sidereal_greenwich: this.state.sidereal_greenwich ? this.state.sidereal_greenwich.plus({milliseconds: siderealSecond * 1000}) : 0,
-      sidereal_summit: this.state.sidereal_summit ? this.state.sidereal_summit.plus({milliseconds: siderealSecond * 1000}) : 0,
-      mjd: this.state.mjd + 1 / (3600*24),
+      local: this.state.local.plus({ seconds: 1 }),
+      sidereal_greenwich: this.state.sidereal_greenwich
+        ? this.state.sidereal_greenwich.plus({ milliseconds: siderealSecond * 1000 })
+        : 0,
+      sidereal_summit: this.state.sidereal_summit
+        ? this.state.sidereal_summit.plus({ milliseconds: siderealSecond * 1000 })
+        : 0,
+      mjd: this.state.mjd + 1 / (3600 * 24),
     });
   }
 
   render() {
     return (
       <div className={styles.container}>
-        {this.props.clocks.map( (horizontalGroup, index) => (
+        {this.props.clocks.map((horizontalGroup, index) => (
           <div key={index} className={styles.horizontalGroup}>
-            {horizontalGroup.map( (element, index) => {
+            {horizontalGroup.map((element, index) => {
               const verticalGroup = element instanceof Array ? element : [element];
               return (
                 <div key={index} className={styles.verticalGroup}>
-                  {verticalGroup.map( (element, index) => {
+                  {verticalGroup.map((element, index) => {
                     let timestamp = this.state.local;
                     if (element.timezone === 'sidereal-greenwich') {
                       timestamp = this.state.sidereal_greenwich;
-                    }
-                    else if(element.timezone === 'sidereal-summit') {
+                    } else if (element.timezone === 'sidereal-summit') {
                       timestamp = this.state.sidereal_summit;
-                    }
-                    else if(element.timezone === 'MJD') {
+                    } else if (element.timezone === 'MJD') {
                       timestamp = this.state.mjd;
                     }
-                    return (
-                      <Clock key={index} {...element} timestamp={timestamp} timeData={this.props.timeData}/>
-                    )
+                    return <Clock key={index} {...element} timestamp={timestamp} timeData={this.props.timeData} />;
                   })}
                 </div>
-              )
+              );
             })}
           </div>
         ))}
