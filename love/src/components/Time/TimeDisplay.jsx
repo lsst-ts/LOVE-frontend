5eb80971ade37e05daabaeaea8c738e2f4106bbd
Sebastián Fehlandt <sebafehlandt@gmail.com>
2020-04-21 19:06:03 -0400

    Rename clocks to clocks_layout

--- love/src/components/Time/TimeDisplay.jsx
+++ love/src/components/Time/TimeDisplay.jsx
@@ -14,13 +14,38 @@ export default class TimeDisplay extends React.Component {
     locale: PropTypes.string,
     /** Current time clocks */
     clock: PropTypes.object,
-    clocks: PropTypes.array,
+    /**
+     * Layout of clocks in JSON format.
+        It is a list of horizontalGroups, each of which list of vertically-aligned elements.
+        Each clock has the following properties:
+
+        1. name: (string) name of the clock, to be displayed above it.
+        2. hideAnalog: (boolean = false) flag to hide the analog clock.
+        3. hideDate: (boolean = false) flag to hide the date.
+        4. hideOffset: (boolean = false) flag to hide the UTC offset, displayed at the right of the name
+        5. timezone: timezone string used to configure which UTC offset to use. Null or empty if current should be used. Null by default.
+
+        The format for the timezone string can be a fixed string (for UTC or TAI); a fixed-offset string (e.g. UTC+5);
+        or a location string in the format <Continent>/<City> (use camelcase with underscores instead of spaces, like America/New_York)
+        For example:
+        - For UTC use UTC
+        - For TAI use TAI
+        - For Greenwich Sidereal Time use sidereal-greenwich
+        - For Summit Sidereal Time use sidereal-summit
+        - For a fixed offset (e.g. GMT+5) use <UTC
+        - For La Serena use America/Santiago (yes America, not Chile)
+        - For Arizona use America/Phoenix
+        - For Illinois use America/Chicago
+        Note that not every city is available, check the IANA DB documentation for more info: https://www.iana.org/time-zones
+        See the default value as an example
+    */
+    clocks_layout: PropTypes.array,
   };
 
   static defaultProps = {
     locale: 'en-GB',
     taiToUtc: 0,
-    clocks: [],
+    clocks_layout: [],
     timeData: {
       server_time: {
         utc: 0,
@@ -76,7 +101,7 @@ export default class TimeDisplay extends React.Component {
     console.log('clock: ', this.props.clock);
     return (
       <div className={styles.container}>
-        {this.props.clocks.map((horizontalGroup, index) => (
+        {this.props.clocks_layout.map((horizontalGroup, index) => (
           <div key={index} className={styles.horizontalGroup}>
             {horizontalGroup.map((element, index) => {
               const verticalGroup = element instanceof Array ? element : [element];
