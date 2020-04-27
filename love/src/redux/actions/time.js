e60641542148527ecad1ab8a14a5b3f7ffc87b29
Sebastián Fehlandt <sebafehlandt@gmail.com>
2020-04-27 13:02:10 -0400

    Send and receive a periodic server time request

--- love/src/redux/actions/time.js
+++ love/src/redux/actions/time.js
@@ -5,10 +5,13 @@ import {
   CLOCK_STOP,
   CLOCK_TICK,
 } from './actionTypes';
+import { sendAction } from './ws';
 import { getAllTime } from '../selectors';
 import { siderealSecond } from '../../Utils';
 
-
+/**
+ * Receive server time data
+ */
 export function receiveServerTime(time_data, request_time) {
   return (dispatch) => {
     const receive_time = DateTime.utc().toMillis() / 1000;
@@ -17,6 +20,9 @@ export function receiveServerTime(time_data, request_time) {
   };
 }
 
+/**
+ * Update the internal clock
+ */
 export function tick() {
   return (dispatch, getState) => {
     const time = getAllTime(getState());
@@ -40,19 +46,39 @@ export function tick() {
   }
 };
 
-let timerID = null;
+/**
+ * Send a websockets message to the server requesting a time update
+ */
+export function requestServerTime() {
+  return (dispatch) => {
+    dispatch(sendAction('get_time_data'));
+  }
+}
+
+let tickTimer = null;
+let syncTimer = null;
+
+/**
+ * Start an internal clock, setting it to update every second and requesting a server sync every 10 seconds
+ */
 export function clockStart() {
   return (dispatch) => {
-    clearInterval(timerID);
-    timerID = setInterval(() => dispatch(tick()), 1000);
+    clearInterval(tickTimer);
+    clearInterval(syncTimer);
+    tickTimer = setInterval(() => dispatch(tick()), 1000);
+    syncTimer = setInterval(() => dispatch(requestServerTime()), 10000);
     dispatch({ type: CLOCK_START });
     dispatch(tick());
   }
 }
 
+/**
+ * Stop the internal clock
+ */
 export function clockStop() {
   return (dispatch) => {
-    clearInterval(timerID);
+    clearInterval(tickTimer);
+    clearInterval(syncTimer);
     dispatch({ type: CLOCK_STOP });
   }
 }
