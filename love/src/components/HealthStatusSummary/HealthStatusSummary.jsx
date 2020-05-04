0ce641ed933dcc9d8706836c0ec98b3aeffe0eeb
José  Galaz <jdgalazm@gmail.com>
2020-05-04 15:55:38 -0400

    monitoring view: healthstatussummary, with dummy data

--- love/src/components/HealthStatusSummary/HealthStatusSummary.jsx
+++ love/src/components/HealthStatusSummary/HealthStatusSummary.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import saveAs from 'file-saver';
+import StatusText from '../GeneralPurpose/StatusText/StatusText';
 import TelemetrySelectionTableContainer from './TelemetrySelectionTable/TelemetrySelectionTable.container';
 import Button from './Button/Button';
 import ExportIcon from '../icons/ExportIcon/ExportIcon';
 import styles from './HealthStatusSummary.module.css';
 import UploadButton from './Button/UploadButton';
-import ManagerInterface from '../../Utils';
+import ManagerInterface, { saveGroupSubscriptions } from '../../Utils';
 
 /**
  * Configurable summary displaying the health status of an arbitrary subset
@@ -15,7 +16,19 @@ import ManagerInterface from '../../Utils';
  * status. Then the user launches another YET TO BE IMPLEMENTED "printed" table.
  *
  */
+
+const healthStatusCodes = {
+  0: 'Undefined',
+  1: 'OK',
+  2: 'Warning',
+  3: 'Alert',
+  4: 'Invalid',
+};
 export default class HealthStatusSummary extends Component {
+  static defaultProps = {
+    telemetriesToMonitor: {},
+  };
+
   constructor() {
     super();
     // eslint-disable-next-line
@@ -68,9 +81,46 @@ export default class HealthStatusSummary extends Component {
   });
 
   render() {
+    const { telemetryMetadata } = this.props;
+
     return (
       <div className={styles.container}>
-        <div className={styles.topButtons}>
+        {Object.keys(telemetryMetadata).map((indexedComponentName) => {
+          const [component, salindex] = indexedComponentName.split('-');
+          const componentName = `${component}${parseInt(salindex) === 0 ? '' : `.${salindex}`}`;
+
+          return (
+            <div key={indexedComponentName} className={styles.componentContainer}>
+              <div className={styles.componentName}>{componentName}</div>
+              {Object.keys(telemetryMetadata[indexedComponentName]).map((topic) => {
+                return (
+                  <React.Fragment>
+                    <div className={styles.topic}>
+                      <div className={styles.topicName}>{topic}</div>
+                      <div className={styles.topicTimestamp}>2012-01-01 12:34:12</div>
+                    </div>
+                    <div className={styles.divider}></div>
+                    {telemetryMetadata[indexedComponentName][topic].map((parameter) => {
+                      return (
+                        <div key={`${indexedComponentName}${topic}${parameter}`} className={styles.parameterContainer}>
+                          <div className={styles.parameterName}> {parameter} </div>
+                          {/* <div className={styles.parameterDescription}> */}
+                          {/* </div> */}
+                          <div className={styles.healthStatus}>
+                            <div className={styles.parameterValue}> {(12312123.123123).toFixed(4)}</div>
+                            <div className={styles.parameterUnits}> second</div>
+                            <StatusText status={healthStatusCodes[3].toLowerCase()}>{healthStatusCodes[3]}</StatusText>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </React.Fragment>
+                );
+              })}
+            </div>
+          );
+        })}
+        {/* <div className={styles.topButtons}>
           <div className={styles.buttonWrapper}>
             <UploadButton onLoadFile={this.onLoadFile} />
           </div>
@@ -79,17 +129,16 @@ export default class HealthStatusSummary extends Component {
               <ExportIcon />
               Export
             </Button>
-            {/* </a> */}
           </div>
-        </div>
-        <div className={styles.telemetryTableWrapper}>
+        </div> */}
+        {/* <div className={styles.telemetryTableWrapper}>
           <TelemetrySelectionTableContainer
             {...this.state}
             // eslint-disable-next-line
             onClick={() => console.log('RawTel Click')}
             showSelection={false}
           />
-        </div>
+        </div> */}
       </div>
     );
   }
