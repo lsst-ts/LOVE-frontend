1017e61febfccba0b84756b3f856ac332bdbe22f
José  Galaz <jdgalazm@gmail.com>
2020-05-04 18:22:56 -0400

    add tooltips

--- love/src/components/HealthStatusSummary/HealthStatusSummary.jsx
+++ love/src/components/HealthStatusSummary/HealthStatusSummary.jsx
@@ -48,14 +48,14 @@ export default class HealthStatusSummary extends Component {
 
           return (
             <div key={indexedComponentName} className={styles.componentContainer}>
-              <div className={styles.componentName}>{componentName}</div>
+              <div className={styles.componentName} title={`CSC: ${componentName}`}>{componentName}</div>
               {Object.keys(telemetryConfiguration[indexedComponentName]).map((topic) => {
                 let timestamp = streams[`telemetry-${indexedComponentName}-${topic}`]?.private_rcvStamp;
                 timestamp = timestamp?.value !== undefined ? formatTimestamp(timestamp.value * 1000) : '-';
 
                 return (
                   <React.Fragment key={`${indexedComponentName}${topic}`}>
-                    <div className={styles.topic}>
+                    <div className={styles.topic} title={`Topic: ${componentName}.${topic}`}>
                       <div className={styles.topicName}>{topic}</div>
                       <div className={styles.topicTimestamp}>{timestamp}</div>
                     </div>
@@ -76,6 +76,7 @@ export default class HealthStatusSummary extends Component {
                         <div
                           key={`${indexedComponentName}${topic}${parameterName}`}
                           className={styles.parameterContainer}
+                          title={`Item: ${indexedComponentName}.${topic}.${parameterName}`}
                         >
                           <div className={styles.parameterName}> {parameterName} </div>
                           <div className={styles.healthStatus}>
