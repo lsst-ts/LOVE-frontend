0c245c45994c3a1d3eaec4450fe39cb4f36a7b59
José  Galaz <jdgalazm@gmail.com>
2020-05-04 18:02:23 -0400

    add healthstatusfunctions to hs container

--- love/src/components/HealthStatusSummary/HealthStatusSummary.jsx
+++ love/src/components/HealthStatusSummary/HealthStatusSummary.jsx
@@ -26,59 +26,10 @@ const healthStatusCodes = {
 };
 export default class HealthStatusSummary extends Component {
   static defaultProps = {
-    telemetriesToMonitor: {},
+    telemetryConfiguration: {},
+    streams: undefined
   };
 
-  constructor() {
-    super();
-    // eslint-disable-next-line
-    RegExp.prototype.toJSON = RegExp.prototype.toString;
-
-    let healthFunctions = localStorage.getItem('healthFunctions');
-    if (!healthFunctions) {
-      healthFunctions = {
-        timestamp0: '//asdasdadsa',
-        altitude_decel0: '//dsasdssa\nreturn ALERT;',
-        altitude_accel0: 'return WARNING;',
-        altitude_maxspeed0: 'return OK;',
-      };
-    } else {
-      healthFunctions = JSON.parse(healthFunctions);
-    }
-    this.managerInterface = new ManagerInterface();
-
-    this.state = {
-      healthFunctions,
-      setHealthFunctions: this.setHealthFunctions,
-    };
-  }
-
-  setHealthFunctions = (healthFunctions) => {
-    this.setState({
-      healthFunctions: { ...healthFunctions },
-    });
-  };
-
-  download = (data, filename) => {
-    const jsonBlob = new Blob([JSON.stringify(data)], { type: 'application/javascript;charset=utf-8' });
-    saveAs(jsonBlob, filename);
-  };
-
-  onLoadFile = (data) => {
-    try {
-      const parsedData = JSON.parse(data);
-      // console.log('onLoadFile', parsedData);
-      this.setFilters(parsedData.filters);
-      this.setHealthFunctions(parsedData.healthFunctions);
-    } catch (error) {
-      // console.error(error);
-    }
-  };
-
-  getOutputConfig = () => ({
-    filters: this.state.filters,
-    healthFunctions: this.state.healthFunctions,
-  });
 
   componentDidMount = () => {
     this.props.subscribeToStreams();
@@ -88,19 +39,17 @@ export default class HealthStatusSummary extends Component {
     this.props.unsubscribeToStreams();
   };
   render() {
-    const { telemetryMetadata, streams } = this.props;
-
-    console.log('streams', this.props.streams);
+    const { telemetryConfiguration, streams } = this.props;
     return (
       <div className={styles.container}>
-        {Object.keys(telemetryMetadata).map((indexedComponentName) => {
+        {Object.keys(telemetryConfiguration).map((indexedComponentName) => {
           const [component, salindex] = indexedComponentName.split('-');
           const componentName = `${component}${parseInt(salindex) === 0 ? '' : `.${salindex}`}`;
 
           return (
             <div key={indexedComponentName} className={styles.componentContainer}>
               <div className={styles.componentName}>{componentName}</div>
-              {Object.keys(telemetryMetadata[indexedComponentName]).map((topic) => {
+              {Object.keys(telemetryConfiguration[indexedComponentName]).map((topic) => {
                 let timestamp = streams[`telemetry-${indexedComponentName}-${topic}`]?.private_rcvStamp;
                 timestamp = timestamp?.value !== undefined ? formatTimestamp(timestamp.value * 1000) : '-';
 
@@ -111,7 +60,7 @@ export default class HealthStatusSummary extends Component {
                       <div className={styles.topicTimestamp}>{timestamp}</div>
                     </div>
                     <div className={styles.divider}></div>
-                    {telemetryMetadata[indexedComponentName][topic].map((parameterName) => {
+                    {Object.keys(telemetryConfiguration[indexedComponentName][topic]).map((parameterName) => {
                       const parameterValue = streams[`telemetry-${indexedComponentName}-${topic}`]?.[parameterName];
                       let renderedValue = '';
                       if (parameterValue?.value !== undefined) {
@@ -121,6 +70,8 @@ export default class HealthStatusSummary extends Component {
                           renderedValue = parameterValue.value.toFixed(4);
                         }
                       }
+
+                      const healthStatusCode = telemetryConfiguration[indexedComponentName][topic][parameterName]();
                       return (
                         <div
                           key={`${indexedComponentName}${topic}${parameterName}`}
@@ -130,7 +81,7 @@ export default class HealthStatusSummary extends Component {
                           <div className={styles.healthStatus}>
                             <div className={styles.parameterValue}> {renderedValue}</div>
                             <div className={styles.parameterUnits}> {parameterValue?.units ?? ''}</div>
-                            <StatusText status={healthStatusCodes[3].toLowerCase()}>{healthStatusCodes[3]}</StatusText>
+                            <StatusText status={healthStatusCodes[healthStatusCode].toLowerCase()}>{healthStatusCodes[healthStatusCode]}</StatusText>
                           </div>
                         </div>
                       );
