966d492bac7542123fe77e4880a1fd544b7ff381
Sebastián Fehlandt <sebafehlandt@gmail.com>
2020-05-12 17:58:54 -0400

    Use AlarmUtils in other components

--- love/src/components/Watcher/AlarmUtils.js
+++ love/src/components/Watcher/AlarmUtils.js
@@ -1,9 +1,27 @@
 import { severityEnum } from '../../Config';
 
-export const isAcknowledged = (alarm) => alarm.acknowledged?.value;
+/** Auxiliary function to define wether an alarm is acknowledged or not */
+export const isAcknowledged = (alarm) =>
+  alarm.acknowledged?.value || (alarm.severity?.value === 1 && alarm.maxSeverity?.value === 1);
 
+/**
+ * Auxiliary function to define wether an alarm is muted or not.
+ * That is, its severity is lower than its mutedSeverity
+ */
 export const isMuted = (alarm) => alarm.severity?.value <= alarm.mutedSeverity?.value;
 
+/**
+ * Auxiliary function to define wether an alarm has muting configuration or not.
+ * That is, its mutedSeverity > OK
+ * Not that if OK < mutedSeverity < severity we wopuld have muted(a) = false and hasMuteConfig(a) = true,
+ */
+export const hasMuteConfig = (alarm) => alarm.mutedSeverity?.value > severityEnum.ok;
+
+/** Auxiliary function to define wether an alarms maxSeverity is Critical */
 export const isMaxCritical = (alarm) => alarm.maxSeverity?.value === severityEnum.critical;
 
+/** Auxiliary function to define wether an alarm is active or not */
 export const isActive = (alarm) => alarm.severity?.value > severityEnum.ok;
+
+/** Auxiliary function to define wether an alarm can be unacknowledged or not */
+export const canUnack = (alarm) => alarm.acknowledged?.value && alarm.severity?.value !== 1;
