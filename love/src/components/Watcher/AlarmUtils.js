45716332b6dfa00380c1802dfd2945d76236f622
Sebastián Fehlandt <sebafehlandt@gmail.com>
2020-05-12 16:06:36 -0400

    Use AlarmUtils in Layout

--- love/src/components/Watcher/AlarmUtils.js
+++ love/src/components/Watcher/AlarmUtils.js
@@ -5,3 +5,5 @@ export const isAcknowledged = (alarm) => alarm.acknowledged?.value;
 export const isMuted = (alarm) => alarm.severity?.value <= alarm.mutedSeverity?.value;
 
 export const isMaxCritical = (alarm) => alarm.maxSeverity?.value === severityEnum.critical;
+
+export const isActive = (alarm) => alarm.severity?.value > severityEnum.ok;
