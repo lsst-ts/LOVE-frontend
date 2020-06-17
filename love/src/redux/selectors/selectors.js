94eba5e341237a4087bccad44c1274f5fbfa2db8
Sebastián Fehlandt <sebafehlandt@gmail.com>
2020-06-17 13:02:11 -0400

    Remove redux logs and adsd selector for cam_feeds

--- love/src/redux/selectors/selectors.js
+++ love/src/redux/selectors/selectors.js
@@ -14,6 +14,8 @@ export const getServerTime = (state) => ({ ...state.time.server_time });
 
 export const getConfig = (state) => (state.auth.config ? { ...state.auth.config } : null);
 
+export const getCamFeeds = (state) => getConfig(state)?.cam_feeds;
+
 export const getAllTime = (state) => ({ ...state.time });
 
 export const getClock = (state) => ({ ...state.time.clock });
--- love/src/redux/store.js
+++ love/src/redux/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 import thunkMiddleware from 'redux-thunk';
-import logger from 'redux-logger';
-const middleWares = [thunkMiddleware, logger];
+// import logger from 'redux-logger';
+// const middleWares = [thunkMiddleware, logger];
 
-// const middleWares = [thunkMiddleware];
+const middleWares = [thunkMiddleware];
 const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
 export default store;
