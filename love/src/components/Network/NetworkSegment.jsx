36c01578d3ed42e3fe2edad49b8237e7a0a21ebf
spereirag <sebastian.pereira@inria.cl>
2020-07-01 19:13:05 -0400

    Add timestamp and fetching interval

--- love/src/components/Network/NetworkSegment.jsx
+++ love/src/components/Network/NetworkSegment.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Network.module.css';
-
+import { formatTimestamp } from '../../Utils';
 export default class NetworkSegment extends Component {
   static propTypes = {
-      /** One of the archive dictionaries coming  */
+    /** One of the archive dictionaries coming  */
     metadata: PropTypes.string,
   };
 
@@ -16,6 +16,15 @@ export default class NetworkSegment extends Component {
   }
 
   componentDidMount() {
+    this.fetchData();
+    this.interval = setInterval(this.fetchData, 60000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  fetchData = () => {
     const { metadata } = this.props;
     const baseURL = metadata.url.split(metadata.uri)[0];
     const event = metadata['event-types'].filter((e) => {
@@ -32,7 +41,7 @@ export default class NetworkSegment extends Component {
           data: data[data.length - 1],
         });
       });
-  }
+  };
 
   render() {
     const { metadata } = this.props;
@@ -40,9 +49,13 @@ export default class NetworkSegment extends Component {
     const maxValue = data?.val?.maximum ? Math.round(data?.val?.maximum * 100) / 100 : '?';
     const minValue = data?.val?.maximum ? Math.round(data?.val?.minimum * 100) / 100 : '?';
     const meanValue = data?.val?.maximum ? Math.round(data?.val?.mean * 100) / 100 : '?';
+    const ts = data?.ts ? formatTimestamp(data?.ts * 1000) : '-';
     const valid = maxValue !== '?' && minValue !== '?' && meanValue !== '?';
     return (
       <div className={[styles.segmentContainer, valid == false ? styles.error : ''].join(' ')}>
+        <div className={styles.timestampContainer}>
+          <span className={styles.highlight}>{ts}</span>
+        </div>
         <div className={styles.endpointsContainer}>
           <span>Source: </span>
           <span className={[styles.highlight, styles.ip].join(' ')}>{metadata.source} </span>
