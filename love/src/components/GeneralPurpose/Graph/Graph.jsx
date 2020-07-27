30b18c5955eb75cb0b664d0975847581e560e9ba
José  Galaz <jdgalazm@gmail.com>
2020-07-27 18:15:13 -0400

    onSelectedLink callback

--- love/src/components/GeneralPurpose/Graph/Graph.jsx
+++ love/src/components/GeneralPurpose/Graph/Graph.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import createEngine, {
-  DefaultNodeModel,
-  DefaultPortModel,
-  DiagramModel
-} from '@projectstorm/react-diagrams';
+import createEngine, { DefaultNodeModel, DefaultPortModel, DiagramModel } from '@projectstorm/react-diagrams';
 // import the custom models
 import { DiamondNodeModel } from './entities/node/DiamondNodeModel';
 import { DiamondNodeFactory } from './entities/node/DiamondNodeFactory';
@@ -15,14 +11,14 @@ import styles from './Graph.module.css';
 import { EditableLabelModel } from './entities/label/EditableLabelModel';
 import { EditableLabelFactory } from './entities/label/EditableLabelFactory';
 
-const Graph = ({ nodes, links, width = 500, height = 500 }) => {
-
+const Graph = ({ nodes, links, width = 500, height = 500, onSelectedLink }) => {
   const engine = React.useMemo(() => {
     //1) setup the diagram engine
     const engine = createEngine();
 
     // register some other factories as well
-    engine.getPortFactories()
+    engine
+      .getPortFactories()
       .registerFactory(new SimplePortFactory('diamond', (config) => new DiamondPortModel(PortModelAlignment.LEFT)));
     engine.getNodeFactories().registerFactory(new DiamondNodeFactory());
     engine.getLabelFactories().registerFactory(new EditableLabelFactory());
@@ -48,9 +44,18 @@ const Graph = ({ nodes, links, width = 500, height = 500 }) => {
       const linkObject = sourcePort.link(targetPort, link.color, link.width);
       linkObject.addLabel(
         new EditableLabelModel({
-          value: link.tooltip
-        })
+          value: link.tooltip,
+        }),
       );
+
+      linkObject.registerListener({
+        eventDidFire: (event) => {
+          if(event.function === "selectionChanged" && event.isSelected){
+            onSelectedLink(link.id, event)
+          }
+        }
+      });
+
       prevDict[link.id] = linkObject;
 
       return prevDict;
@@ -72,50 +77,59 @@ const Graph = ({ nodes, links, width = 500, height = 500 }) => {
   );
 };
 
-
 Graph.propTypes = {
   /** Width of the diagram canvas, defaults to 500px */
   width: PropTypes.number,
   /** Height of the diagram canvas, defaults to 500px */
   height: PropTypes.number,
   /** Array describing the nodes of the graph */
-  nodes: PropTypes.arrayOf(PropTypes.shape({
-    /** Unique identifier of a node */
-    id: PropTypes.string.isRequired,
-    /** Text or node to be shown inside each node box */
-    label: PropTypes.node,
-    /** Position of the top left corner of the node HTML element in the conainer*/
-    position: PropTypes.shape({
-      /** x coordinate (increasing from the left)*/
-      x: PropTypes.number.isRequired,
-      /** y coordinate (increasing from the top of the container) */
-      y: PropTypes.number.isRequired,
-    }).isRequired
-  })).isRequired,
-
+  nodes: PropTypes.arrayOf(
+    PropTypes.shape({
+      /** Unique identifier of a node */
+      id: PropTypes.string.isRequired,
+      /** Text or node to be shown inside each node box */
+      label: PropTypes.node,
+      /** Position of the top left corner of the node HTML element in the conainer*/
+      position: PropTypes.shape({
+        /** x coordinate (increasing from the left)*/
+        x: PropTypes.number.isRequired,
+        /** y coordinate (increasing from the top of the container) */
+        y: PropTypes.number.isRequired,
+      }).isRequired,
+    }),
+  ).isRequired,
   /** Array describing the links (i.e., connectors, edges, etc) of the graph */
-  links: PropTypes.arrayOf(PropTypes.shape({
-    /** Unique identifier of the link */
-    id: PropTypes.string.isRequired,
-    /** Source point of the link */
-    source: PropTypes.shape({
-      /** Node that the link is attached to */
+  links: PropTypes.arrayOf(
+    PropTypes.shape({
+      /** Unique identifier of the link */
       id: PropTypes.string.isRequired,
-      /** Node's port to which it is attached */
-      port: PropTypes.string.isRequired
-    }).isRequired,
-    /** End point of the link */
-    target: PropTypes.shape({
-      /** Node that the link is attached to */
-      id: PropTypes.string.isRequired,
-      /** Node's port to which it is attached (TODO: make a list of available ports)*/
-      port: PropTypes.string.isRequired
-    }).isRequired,
-    /** Color of the line, defaults to grey */
-    color: PropTypes.string,
-    /** Tooltip to be displayed in the middle of the line */
-    tooltip: PropTypes.node
-  })).isRequired
-}
+      /** Source point of the link */
+      source: PropTypes.shape({
+        /** Node that the link is attached to */
+        id: PropTypes.string.isRequired,
+        /** Node's port to which it is attached */
+        port: PropTypes.string.isRequired,
+      }).isRequired,
+      /** End point of the link */
+      target: PropTypes.shape({
+        /** Node that the link is attached to */
+        id: PropTypes.string.isRequired,
+        /** Node's port to which it is attached (TODO: make a list of available ports)*/
+        port: PropTypes.string.isRequired,
+      }).isRequired,
+      /** Color of the line, defaults to grey */
+      color: PropTypes.string,
+      /** Tooltip to be displayed in the middle of the line */
+      tooltip: PropTypes.node,
+    }),
+  ).isRequired,
+
+  /** 
+   * Callback called when a link is selected.
+   * @param {String} id Link id from the links prop. 
+   * @param {Object} event react-diagrams event object
+   */
+  onSelectedLink: PropTypes.func
+};
 
 export default Graph;
--- love/src/components/GeneralPurpose/Graph/Graph.md
+++ love/src/components/GeneralPurpose/Graph/Graph.md
@@ -1,12 +1,17 @@
 asdfafds
 
 ```jsx
+import React from 'react';
+const [selectedLinkId, setSelectedLinkId] = React.useState(null);
+
 const nodes = [
   { id: 'node1', label: 'This is node 1', position: { x: 50, y: 200 } },
   { id: 'node2', label: 'This is node 2', position: { x: 500, y: 0 } },
   { id: 'node3', label: 'This is node 3', position: { x: 500, y: 200 } },
   { id: 'node4', label: 'This is node 4', position: { x: 500, y: 400 } },
 ];
+
+const onSelectedLink = (id, ev) => setSelectedLinkId(id);
 
 const links = [
   {
@@ -15,19 +20,29 @@ const links = [
     target: { id: 'node2', port: 'left1' },
     color: 'white',
     width: 2,
-    tooltip: 'link1'
+    tooltip: 'link1',
   },
   {
     id: 'link2',
     source: { id: 'node1', port: 'right2' },
     target: { id: 'node3', port: 'left2' },
-    tooltip: 'link2'
+    tooltip: 'link2',
   },
   {
     id: 'link3',
     source: { id: 'node1', port: 'right1' },
     target: { id: 'node4', port: 'left3' },
-    tooltip: (<div style={{maxWidth: 100, padding: '1rem', background: 'grey'}}>Custom node tooltip</div>)
+    tooltip: (
+      <div
+        style={{
+          maxWidth: 100,
+          padding: '1rem',
+          background: 'grey'
+        }}
+      >
+        Custom node tooltip
+      </div>
+    ),
   },
 ];
 
@@ -36,6 +51,6 @@ const links = [
     background: 'black',
   }}
 >
-  <Graph nodes={nodes} links={links} width={800} />
+  <Graph nodes={nodes} links={links} width={800} onSelectedLink={onSelectedLink} />
 </div>;
 ```
