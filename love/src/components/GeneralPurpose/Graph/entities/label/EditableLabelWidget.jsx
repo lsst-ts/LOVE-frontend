de10020de0d399566107703a104ba3fac8a731d1
José  Galaz <jdgalazm@gmail.com>
2020-07-27 12:57:29 -0400

    add custom tooltip

--- love/src/components/GeneralPurpose/Graph/entities/label/EditableLabelWidget.jsx
+++ love/src/components/GeneralPurpose/Graph/entities/label/EditableLabelWidget.jsx
@@ -2,41 +2,18 @@ import * as React from 'react';
 
 import { EditableLabelModel } from './EditableLabelModel';
 import styled from '@emotion/styled';
+import EventLogContainer from 'components/EventLog/EventLog.container';
+import styles from './EditableLabelWidget.module.css';
 
-// export interface FlowAliasLabelWidgetProps {
-//     model: EditableLabelModel;
-// }
 
-const Label = ({ children }) => {
-// 	// NOTE: this CSS rules allows to interact with elements in label
-    return (<div style={{
-        userSelect: 'none',
-        pointerEvents: 'auto'
-    }}>
-        {children}
-    </div>)
-}
-
-// now we can render all what we want in the label
 export const EditableLabelWidget = (props) => {
     const [str, setStr] = React.useState(props.model.value);
 
     console.log('widget')
     return (
-        <Label>
-            <input
-                value={str}
-                onChange={(event) => {
-                    const newVal = event.target.value;
 
-                    // update value both in internal component state
-                    setStr(newVal);
-                    // and in model object
-                    props.model.value = newVal;
-                }}
-            />
-
-            <button onClick={() => console.log('model eventDidFire')}>Click me!</button>
-        </Label>
+        <div className={styles.tooltipContainer}>
+            custom tooltip
+        </div>
     );
 };
