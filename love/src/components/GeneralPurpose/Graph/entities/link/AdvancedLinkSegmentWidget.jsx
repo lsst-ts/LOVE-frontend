f2783400568b8a0a3e27370b5717c137460a05db
José  Galaz <jdgalazm@gmail.com>
2020-07-29 11:05:51 -0400

    refactor linksegment into function

--- love/src/components/GeneralPurpose/Graph/entities/link/AdvancedLinkSegmentWidget.jsx
+++ love/src/components/GeneralPurpose/Graph/entities/link/AdvancedLinkSegmentWidget.jsx
@@ -1,64 +1,60 @@
 import * as React from 'react';
 
-export class AdvancedLinkSegmentWidget extends React.Component {
-  highlightedColor = getComputedStyle(document.body).getPropertyValue('--second-quinary-background-color');
+export const AdvancedLinkSegmentWidget = (props) => {
+  const highlightedColor = React.useMemo(
+    () => getComputedStyle(document.body).getPropertyValue('--second-quinary-background-color'),
+    [],
+  );
 
-  topCircle = React.createRef();
-  bottomCircle = React.createRef();
+  const topCircle = React.useRef();
+  const bottomCircle = React.useRef();
 
-  state = {
-    highlighted: false,
-  };
+  const [highlighted, setHighlighted] = React.useState(false);
 
-  componentDidMount() {
-    const path = this.props.forwardRef.current;
+  React.useEffect(() => {
+    const path = props.forwardRef.current;
     const midPoint = path.getPointAtLength(path.getTotalLength() * 0.5);
 
-    this.topCircle.current.setAttribute('cx', '' + midPoint.x);
-    this.topCircle.current.setAttribute('cy', '' + midPoint.y);
-    this.bottomCircle.current.setAttribute('cx', '' + midPoint.x);
-    this.bottomCircle.current.setAttribute('cy', '' + midPoint.y);
-  }
-  render() {
-    const selected = this.props.selected || this.props.link.isSelected();
+    topCircle.current.setAttribute('cx', '' + midPoint.x);
+    topCircle.current.setAttribute('cy', '' + midPoint.y);
+    bottomCircle.current.setAttribute('cx', '' + midPoint.x);
+    bottomCircle.current.setAttribute('cy', '' + midPoint.y);
+  });
 
-    const commonProps = {
-      pointerEvents: 'all',
-      style: { cursor: 'pointer' },
-      onMouseEnter: () => {
-        this.setState({
-          highlighted: true,
-        });
-      },
-      onMouseLeave: () => {
-        this.setState({
-          highlighted: false,
-        });
-      },
-    };
+  const selected = props.selected || props.link.isSelected();
 
-    const color = selected ? this.props.link.getOptions().selectedColor : this.props.link.getOptions().color;
-    return (
-      <g>
-        <path
-          strokeLinecap="round"
-          stroke={this.highlightedColor}
-          opacity={this.state.highlighted ? 0.7 : 0}
-          strokeWidth={this.props.link.getOptions().width + 10}
-          d={this.props.path}
-          {...commonProps}
-        />
-        <path
-          ref={this.props.forwardRef}
-          stroke={color}
-          strokeWidth={this.props.link.getOptions().width}
-          d={this.props.path}
-          {...commonProps}
-        />
+  const commonProps = {
+    pointerEvents: 'all',
+    style: { cursor: 'pointer' },
+    onMouseEnter: () => {
+      setHighlighted(true);
+    },
+    onMouseLeave: () => {
+      setHighlighted(false);
+    },
+  };
 
-        <circle ref={this.bottomCircle} r={6.4} stroke={color} fill={color} opacity={0.5} {...commonProps} />
-        <circle ref={this.topCircle} r={3.2} stroke={color} fill={color} {...commonProps} />
-      </g>
-    );
-  }
-}
+  const color = selected ? props.link.getOptions().selectedColor : props.link.getOptions().color;
+  return (
+    <g>
+      <path
+        strokeLinecap="round"
+        stroke={highlightedColor}
+        opacity={highlighted ? 0.7 : 0}
+        strokeWidth={props.link.getOptions().width + 10}
+        d={props.path}
+        {...commonProps}
+      />
+      <path
+        ref={props.forwardRef}
+        stroke={color}
+        strokeWidth={props.link.getOptions().width}
+        d={props.path}
+        {...commonProps}
+      />
+
+      <circle ref={bottomCircle} r={6.4} stroke={color} fill={color} opacity={0.5} {...commonProps} />
+      <circle ref={topCircle} r={3.2} stroke={color} fill={color} {...commonProps} />
+    </g>
+  );
+};
