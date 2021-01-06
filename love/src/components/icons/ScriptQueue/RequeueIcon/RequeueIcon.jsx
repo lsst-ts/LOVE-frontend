194a92cbd8884a5f441adabd7ef8bb2db01497b2
spereirag <sebastian.pereira@inria.cl>
2021-01-06 11:17:57 -0300

    Add optional title to RequeueIcon

--- love/src/components/icons/ScriptQueue/RequeueIcon/RequeueIcon.jsx
+++ love/src/components/icons/ScriptQueue/RequeueIcon/RequeueIcon.jsx
@@ -4,12 +4,13 @@ import styles from './RequeueIcon.module.css';
 export default class RequeueIcon extends Component {
   render() {
   const status = this.props.active !== undefined && this.props.active === false ? styles.inactive : styles.active;
+  const title = this.props.title ? this.props.title : 'Requeue script';
   return (
       <svg
       className={[styles.svg, this.props.className].join(' ')} 
       viewBox="0 0 22 27.25"
     >
-      <title>{'Requeue script'}</title>
+      <title>{title}</title>
       <path
         className={[styles.st0, status].join(' ')}
         d="M18.36 22.49v-14L14.74 5H3a1.34 1.34 0 00-1.35 1.36v19.28A1.34 1.34 0 003 27h14a1.34 1.34 0 001.34-1.34"
