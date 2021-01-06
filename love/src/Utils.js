2f98e367294951f44ae55a1f6be7a96316ada558
spereirag <sebastian.pereira@inria.cl>
2021-01-06 14:25:35 -0300

    Add emergency contact list endpoint

--- love/src/Utils.js
+++ love/src/Utils.js
@@ -266,6 +266,32 @@ export default class ManagerInterface {
       });
     });
   }
+
+  static getEmergencyContactList(index) {
+    const token = ManagerInterface.getToken();
+    if (token === null) {
+      // console.log('Token not found during validation');
+      return new Promise((resolve) => resolve(false));
+    }
+    const url = `${this.getApiBaseUrl()}emergencycontact`;
+    return fetch(url, {
+      method: 'GET',
+      headers: this.getHeaders(),
+    }).then((response) => {
+      if (response.status >= 500) {
+        // console.error('Error communicating with the server.);
+        return false;
+      }
+      if (response.status === 401 || response.status === 403) {
+        // console.log('Session expired. Logging out');
+        ManagerInterface.removeToken();
+        return false;
+      }
+      return response.json().then((resp) => {
+        return resp;
+      });
+    });
+  }
 }
 
 /**
