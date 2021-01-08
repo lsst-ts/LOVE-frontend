50bde015f4dee23ddd0d21f2cb0dfc5b315ed37b
spereirag <sebastian.pereira@inria.cl>
2021-01-08 17:31:21 -0300

    Update emergency contacts panel style

--- love/src/components/Layout/EmergencyContactsPanel/EmergencyContactsPanel.jsx
+++ love/src/components/Layout/EmergencyContactsPanel/EmergencyContactsPanel.jsx
@@ -7,26 +7,59 @@ import Select from 'components/GeneralPurpose/Select/Select';
 import styles from './EmergencyContactsPanel.module.css';
 import ManagerInterface, { formatTimestamp } from 'Utils';
 import RequeueIcon from 'components/icons/ScriptQueue/RequeueIcon/RequeueIcon';
+import Input from 'components/GeneralPurpose/Input/Input';
 
 EmergencyContactsPanel.propTypes = {};
 
 /** Contents of the Config File view Panel, displayed in a modal */
 function EmergencyContactsPanel() {
   const [contactList, setContactList] = useState([]);
-
+  const [displayedList, setDisplayedList] = useState([]);
+  const [filterString, setFilterString] = useState('');
   useEffect(() => {
     ManagerInterface.getEmergencyContactList().then((list) => {
       setContactList(list);
     });
   }, []);
 
+  const ECCard = (ECData) => {
+    return (
+      <div className={styles.cardContainer}>
+        <div className={styles.subsystem}>{ECData?.subsystem}</div>
+        <div className={[styles.contactItem, styles.highlighted].join(' ')}>{ECData?.name}</div>
+        <div className={styles.contactItem}>
+          <span>Contact info: </span>
+          <span>{ECData?.contact_info}</span>
+        </div>
+        <div className={styles.contactItem}>
+          <span>Email: </span>
+          <span>{ECData?.email}</span>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.title}>Emergency contacts</div>
-      <div className={styles.subTitle}>Subtitle</div>
-      {contactList.map((contact) => {
-        return <div>{JSON.stringify(contact)}</div>;
-      })}
+      <div className={styles.filterContainer}>
+        <span className={styles.subTitle}>Filter:</span>
+        <Input onChange={(e) => setFilterString(e.target.value)} className={styles.availableScriptsInput} />
+      </div>
+      {contactList
+        .filter((contact) => {
+          const lowerCaseFilter = filterString?.toLowerCase();
+          return (
+            filterString === '' ||
+            (contact?.subsystem?.toLowerCase() ?? '').includes(lowerCaseFilter) ||
+            (contact?.name?.toLowerCase() ?? '').includes(lowerCaseFilter) ||
+            (contact?.contact_info?.toLowerCase() ?? '').includes(lowerCaseFilter) ||
+            (contact?.email?.toLowerCase() ?? '').includes(lowerCaseFilter)
+          );
+        })
+        .map((contact) => {
+          return ECCard(contact);
+        })}
     </div>
   );
 }
