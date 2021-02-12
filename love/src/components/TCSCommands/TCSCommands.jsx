13eaf76b40c1244b3ba293ae8f0798bf9a86d22d
spereirag <sebastian.pereira@inria.cl>
2021-02-12 15:03:51 -0300

    Add incorrect angle format warning

--- love/src/components/TCSCommands/TCSCommands.jsx
+++ love/src/components/TCSCommands/TCSCommands.jsx
@@ -5,20 +5,35 @@ import Input from 'components/GeneralPurpose/Input/Input';
 import Button from 'components/GeneralPurpose/Button/Button';
 import { TCSCommands } from 'Config.js';
 
+const angleRegExp = new RegExp(/(\d\d(:| )\d\d(:| )\d\d)+(\.\d{1,10})?$/);
+const floatRegExp = new RegExp(/^-?\d*(\.\d+)?$/);
+
 export default class CommandPanel extends Component {
   constructor(props) {
     super(props);
     this.state = {
       selectedCommand: null,
       paramValues: {},
+      paramWarnings: {},
     };
   }
 
-  updateParamValue = (name, value) => {
+  updateParamValue = (name, value, paramType) => {
     this.setState({
       paramValues: {
         ...this.state.paramValues,
         [name]: value,
+      },
+    });
+  };
+
+  checkInvalidAngle = (name, value) => {
+    console.log(value, angleRegExp.test(value), isNaN(parseFloat(value, 10)));
+    const testValue = value ?? '';
+    this.setState({
+      paramWarnings: {
+        ...this.state.paramWarnings,
+        [name]: isNaN(parseFloat(testValue.match(floatRegExp))) && !angleRegExp.test(testValue),
       },
     });
   };
@@ -30,26 +45,37 @@ export default class CommandPanel extends Component {
       <div className={[styles.paramContainer, paramType == 'boolean' ? styles.checkboxParam : ''].join(' ')}>
         <div className={styles.paramLabel}>{name}</div>
         {paramType == 'string' && (
-          <Input value={paramValues[name]} onChange={(e) => this.updateParamValue(name, e.target.value)} />
+          <Input value={paramValues[name]} onChange={(e) => this.updateParamValue(name, e.target.value, paramType)} />
         )}
         {paramType == 'number' && (
-          <Input value={paramValues[name]} onChange={(e) => this.updateParamValue(name, e.target.value)} />
+          <Input value={paramValues[name]} onChange={(e) => this.updateParamValue(name, e.target.value, paramType)} />
         )}
         {paramType == 'angle' && (
-          <Input value={paramValues[name]} onChange={(e) => this.updateParamValue(name, e.target.value)} />
+          <>
+            <Input
+              value={paramValues[name]}
+              onChange={(e) => this.updateParamValue(name, e.target.value, paramType)}
+              onBlur={() => this.checkInvalidAngle(name, paramValues[name])}
+            />
+            {this.state.paramWarnings[name] && (
+              <div className={styles.paramWarning}>
+                Angle should be a float (deg) or a sexagesimal string (DD:MM:SS.S or DD MM SS.S)
+              </div>
+            )}
+          </>
         )}
         {paramType == 'boolean' && (
           <input
             type="checkbox"
             defaultChecked={defaultValue}
-            onChange={(e) => this.updateParamValue(name, e.target.checked)}
+            onChange={(e) => this.updateParamValue(name, e.target.checked, paramType)}
           />
         )}
         {Array.isArray(paramType) && (
           <Select
             options={paramType}
             option={paramValues[name]}
-            onChange={(selection) => this.updateParamValue(name, selection )}
+            onChange={(selection) => this.updateParamValue(name, selection, paramType)}
           />
         )}
       </div>
@@ -68,6 +94,7 @@ export default class CommandPanel extends Component {
     console.log(TCSCommands);
     console.log(this.state.selectedCommand);
     console.log(this.state.paramValues);
+    console.log(this.state.paramWarnings);
     const paramsDict = TCSCommands[this.state.selectedCommand] ?? {};
 
     return (
