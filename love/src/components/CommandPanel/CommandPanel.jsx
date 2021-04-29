e8dc7b28b87a618b05913f0338c0316b11e93f25
sebastian-aranda <aranda.sebastian@gmail.com>
2021-04-29 17:37:59 -0400

    Comment EmergencyClose

--- love/src/components/CommandPanel/CommandPanel.jsx
+++ love/src/components/CommandPanel/CommandPanel.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import DomeCloseButton from './DomeCloseButton/DomeCloseButton';
+// import DomeCloseButton from './DomeCloseButton/DomeCloseButton';
 import StopAllTSCButton from './StopAllTSCButton/StopAllTSCButton';
 import styles from './CommandPanel.module.css';
 
@@ -7,7 +7,7 @@ export default class CommandPanel extends Component {
   render() {
     return (
       <div className={styles.container}>
-        <DomeCloseButton {...this.props}></DomeCloseButton>
+        {/* <DomeCloseButton {...this.props}></DomeCloseButton> */}
         <StopAllTSCButton {...this.props}></StopAllTSCButton>
       </div>
     );
