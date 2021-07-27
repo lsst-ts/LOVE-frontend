352e9ab2637dcd1805c8699528a0025219f6b161
sebastian-aranda <aranda.sebastian@gmail.com>
2021-07-27 12:29:36 -0400

    Add Vega-lite custom plots implementation

---
+++ love/src/components/GeneralPurpose/Plot/VegaCustomPlot/VegaCustomPlot.container.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { connect } from 'react-redux';
+// import { addGroup, requestGroupRemoval } from 'redux/actions/ws';
+// import { getStreamsData } from 'redux/selectors/selectors';
+import VegaCustomPlot from './VegaCustomPlot';
+
+export const schema = {
+  description: 'Plots Vega-Lite JSON Schemas',
+  defaultSize: [8, 8],
+  props: {
+    titleBar: {
+      type: 'boolean',
+      description: 'Whether to display the title bar',
+      isPrivate: true,
+      default: false,
+    },
+    title: {
+      type: 'string',
+      description: 'Name diplayed in the title bar (if visible)',
+      isPrivate: true,
+      default: 'Time series plot',
+    },
+    hasRawMode: {
+      type: 'boolean',
+      description: 'Whether the component has a raw mode version',
+      isPrivate: true,
+      default: false,
+    },
+    schema: {
+      type: 'object',
+      description: 'Vega-Lite JSON Schema',
+      isPrivate: false,
+      default: {
+        description: 'A simple bar chart with embedded data.',
+        data: {
+          values: [
+            { a: 'A', b: 28 },
+            { a: 'B', b: 55 },
+            { a: 'C', b: 43 },
+            { a: 'D', b: 91 },
+            { a: 'E', b: 81 },
+            { a: 'F', b: 53 },
+            { a: 'G', b: 19 },
+            { a: 'H', b: 87 },
+            { a: 'I', b: 52 },
+          ],
+        },
+        mark: 'bar',
+        encoding: {
+          x: { field: 'a', type: 'nominal', axis: { labelAngle: 0 } },
+          y: { field: 'b', type: 'quantitative' },
+        },
+        width: 400,
+        height: 200,
+      },
+    },
+  },
+};
+
+class PlotContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.state = {};
+    this.containerRef = React.createRef();
+  }
+
+  render() {
+    const { schema, containerNode, width, height } = this.props;
+
+    const plotProps = { schema, width, height };
+    if (!width && !height && !containerNode) {
+      return (
+        <div ref={this.containerRef}>
+          <VegaCustomPlot {...plotProps} containerNode={this.containerRef} />
+        </div>
+      );
+    }
+
+    return <VegaCustomPlot {...plotProps} containerNode={containerNode} />;
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {};
+};
+
+const mapStateToProps = (state, ownProps) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PlotContainer);
---
+++ love/src/components/GeneralPurpose/Plot/VegaCustomPlot/VegaCustomPlot.jsx
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import { VegaLite } from 'react-vega';
+import PropTypes from 'prop-types';
+import styles from './VegaCustomPlot.module.css';
+
+/**
+ * Generates plots using different marks (lines, points, bars, etc) on different layers
+ * on a canvas that is auto-sized to match its parent node size.
+ */
+class VegaCustomPlot extends Component {
+  static propTypes = {
+    /** Width of the plot in pixels */
+    width: PropTypes.number,
+
+    /** Height of the plot in pixels */
+    height: PropTypes.number,
+
+    /** Node to be used to track width and height.
+     *  Use this instead of props.width and props.height for responsive plots.
+     *  Will be ignored if both props.width and props.height are provided */
+    containerNode: PropTypes.node,
+
+    /** Vega-lite JSON schema.
+     *  Some examples can be found here: https://vega.github.io/vega-lite/examples/ */
+    schema: PropTypes.object,
+
+    /** classname to be appended to the default one in <VegaLite ...> */
+    className: PropTypes.string,
+  };
+
+  constructor(props) {
+    super(props);
+    this.resizeObserver = undefined;
+    this.state = {
+      containerWidth: 400,
+      containerHeight: 200,
+      spec: {},
+      data: {},
+    };
+  }
+
+  updateSchema = (customSchema) => {
+    this.setState({
+      spec: {
+        mark: this.props.schema.mark,
+        encoding: this.props.schema.encoding,
+        data: { name: 'values' },
+        width: this.props.schema.width,
+        height: this.props.schema.height,
+        ...customSchema,
+      },
+      data: { ...this.props.schema.data },
+    });
+  };
+
+  setResizeObserver = () => {
+    this.resizeObserver = new ResizeObserver((entries) => {
+      const { width, height } = entries[0].contentRect;
+      this.setState({
+        containerHeight: height,
+        containerWidth: width,
+      });
+    });
+
+    if (this.props.containerNode.current) {
+      this.resizeObserver.observe(this.props.containerNode.current);
+    }
+  };
+
+  componentDidMount = () => {
+    this.updateSchema();
+  };
+
+  componentDidUpdate = (prevProps) => {
+    if (
+      this.props.containerNode.current &&
+      !this.resizeObserver &&
+      this.props.width === undefined &&
+      this.props.height === undefined
+    ) {
+      this.setResizeObserver();
+    }
+
+    if (prevProps.schema !== this.props.schema) {
+      this.updateSchema();
+    }
+  };
+
+  componentWillUnmount = () => {
+    if (this.resizeObserver) {
+      this.resizeObserver.disconnect();
+    }
+  };
+
+  render() {
+    const { spec, data } = this.state;
+    return (
+      <VegaLite
+        renderer="svg"
+        spec={spec}
+        data={data}
+        className={[styles.plotContainer, this.props.className].join(' ')}
+        actions={false}
+      />
+    );
+  }
+}
+
+export default VegaCustomPlot;
