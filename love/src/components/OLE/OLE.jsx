8501848e8990e0e437303c6fc0447ad59757c6bd
Astrid Reyes De La Rosa <astrid.reyes@inria.cl>
2021-10-04 17:46:59 -0300

    First attempt for ole table

--- love/src/components/OLE/OLE.jsx
+++ love/src/components/OLE/OLE.jsx
@@ -1,12 +1,64 @@
 import React, { Component } from 'react';
 import styles from './OLE.module.css';
-// import SimpleTable from 'components/GeneralPurpose/SimpleTable/SimpleTable';
-import SummaryPanel from 'components/GeneralPurpose/SummaryPanel/SummaryPanel';
-import Label from 'components/GeneralPurpose/SummaryPanel/Label';
-// import Value from 'components/GeneralPurpose/SummaryPanel/Value';
-import Title from 'components/GeneralPurpose/SummaryPanel/Title';
+import SimpleTable from 'components/GeneralPurpose/SimpleTable/SimpleTable';
+import Select from 'components/GeneralPurpose/Select/Select';
+import TextArea from 'components/GeneralPurpose/TextArea/TextArea';
 
 class OLE extends Component {
+  static oleTableHeaders() {
+    return [
+      {
+        field: 'status',
+        title: 'Obs. status',
+        type: 'string',
+      },
+      {
+        field: 'id',
+        title: 'Observation ID',
+        type: 'string',
+      },
+      {
+        field: 'instrument',
+        title: 'Instrument',
+        type: 'string',
+      },
+      {
+        field: 'date',
+        title: 'Date & Time (TAI)',
+        type: 'date',
+      },
+      {
+        field: 'msg',
+        title: 'Messages',
+        type: 'number',
+      },
+      {
+        field: 'more',
+        title: 'See more',
+      },
+      {
+        field: 'new_msg',
+        title: 'New Message',
+      },
+    ];
+  }
+
+  getOLETableData = () => {
+    const data = [
+      {
+        status: 'Upcoming',
+        id: 'LC20210424-01',
+        instrument: 'LSST Cam',
+        date: '2021-04-24 22:2000',
+        msg: '1',
+        more: '',
+        new_msg: '+',
+      },
+    ];
+
+    return data;
+  };
+
   componentDidMount() {
     this.props.subscribeToStreams();
   }
@@ -16,26 +68,38 @@ class OLE extends Component {
   }
 
   render() {
+    const OLEData = Object.values(this.getOLETableData());
     return (
-      <div className={styles.oleContainer}>
+      <div>
         <div>
-          <SummaryPanel className={styles.SummaryPanel}>
-            <div>
-              <Title>Observations()</Title>
-            </div>
-            <div className={styles.SummaryPanelLabels}>
-              <Label>Instrument</Label>
-              {/* <Value></Value> */}
+          <div className={styles.oleh3}>
+            <h3>Observations(1)</h3>
+          </div>
 
-              <Label>Date & Time</Label>
-              {/* <Value></Value> */}
-
-              <Label>Obs ID</Label>
-              {/* <Value></Value> */}
+          <div className={styles.oleContainer}>
+            <div className={styles.oleContainerItems}>
+              <span>Instrument</span>
+              <Select></Select>
             </div>
-          </SummaryPanel>
+
+            <div className={styles.oleContainerItems}>
+              <span>Date & Time</span>
+              <Select></Select>
+            </div>
+
+            <div className={styles.oleContainerItems}>
+              <span>Obs ID</span>
+              <Select></Select>
+            </div>
+
+            <div className={styles.oleTextAreaFilter}>
+              <TextArea></TextArea>
+            </div>
+          </div>
         </div>
-        <div>{/* <SimpleTable/> */}</div>
+        <div>
+          <SimpleTable headers={OLE.oleTableHeaders()} data={OLEData} />
+        </div>
       </div>
     );
   }
