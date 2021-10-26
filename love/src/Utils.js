986021fbe310741edaafb6ea25b6a7db75de7892
Astrid Reyes De La Rosa <astrid.reyes@inria.cl>
2021-10-26 13:33:35 -0300

    Toast element was added after successful request

--- love/src/Utils.js
+++ love/src/Utils.js
@@ -472,14 +472,17 @@ export default class ManagerInterface {
     }).then((response) => {
       if (response.status >= 500) {
         // console.error('Error communicating with the server.);
+        toast.error('Error communicating with the server.');
         return false;
       }
       if (response.status === 401 || response.status === 403) {
         // console.log('Session expired. Logging out');
         ManagerInterface.removeToken();
+        toast.error('Session expired. Logging out');
         return false;
       }
       return response.json().then((resp) => {
+        toast.success('Request received');
         return resp;
       });
     });
