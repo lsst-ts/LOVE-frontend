747d549b758fb9b324bf375ee54f1c12fd73bfbc
sebastian-aranda <aranda.sebastian@gmail.com>
2021-11-08 12:19:51 -0300

    Change cscList prop to array of objects

--- love/src/components/AuthList/SummaryAuthList/SummaryAuthList.container.jsx
+++ love/src/components/AuthList/SummaryAuthList/SummaryAuthList.container.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { addGroup, removeGroup } from 'redux/actions/ws';
 import { getAuthlistState, getPermAuthlistAdministrator, getUsername } from 'redux/selectors';
+import { defaultCSCList } from 'Config';
 import SummaryAuthList from './SummaryAuthList';
 import SubscriptionTableContainer from '../../GeneralPurpose/SubscriptionTable/SubscriptionTable.container';
 
@@ -17,9 +18,11 @@ export const schema = {
     },
     cscList: {
       type: 'array',
-      description: 'List of CSCs to display their Auth Lists',
+      description:
+        'List of CSCs to listen their authorization lists, ' +
+        'each element of the array must be an object with the format: {name: <string>, salindex: <number>}',
       isPrivate: false,
-      default: ['ATDome:0', 'ATMCS:0'],
+      default: defaultCSCList,
     },
   },
 };
@@ -33,7 +36,7 @@ const SummaryAuthListContainer = ({ ...props }) => {
 
 const mapStateToProps = (state, ownProps) => {
   const subscriptions = ownProps.cscList.map((csc) => {
-    const [name, salindex] = csc.split(':');
+    const { name, salindex } = csc;
     return `event-${name}-${salindex}-authList`;
   });
   const authlistAdminPermission = getPermAuthlistAdministrator(state);
@@ -44,7 +47,7 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   const subscriptions = ownProps.cscList.map((csc) => {
-    const [name, salindex] = csc.split(':');
+    const { name, salindex } = csc;
     return `event-${name}-${salindex}-authList`;
   });
   return {
