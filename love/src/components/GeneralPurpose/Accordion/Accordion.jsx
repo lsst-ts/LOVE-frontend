a985449f1a645abfbd65798e0e00b79eb165caf3
sebastian-aranda <aranda.sebastian@gmail.com>
2021-11-26 11:49:03 -0300

    Remove commented code

--- love/src/components/GeneralPurpose/Accordion/Accordion.jsx
+++ love/src/components/GeneralPurpose/Accordion/Accordion.jsx
@@ -1,46 +1,5 @@
 import React, { Component } from 'react';
 
-// class Panel extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       height: 0,
-//     };
-//   }
-
-//   componentDidMount() {
-//     window.setTimeout(() => {
-//       const el = ReactDOM.findDOMNode(this);
-//       const height = el.querySelector('.panel__inner').scrollHeight;
-//       this.setState({
-//         height,
-//       });
-//     }, 333);
-//   }
-
-//   render() {
-//     const { label, content, activeTab, index, activateTab } = this.props;
-//     const { height } = this.state;
-//     const isActive = activeTab === index;
-//     const innerStyle = {
-//       height: `${isActive ? height : 0}px`,
-//     };
-
-//     return (
-//       <div className="panel" role="tabpanel" aria-expanded={isActive}>
-//         {/* <button className='panel__label'
-// 					role='tab'
-// 					onClick={ activateTab }>
-// 					{ label }
-// 				</button> */}
-//         <div className="panel__inner" style={innerStyle} aria-hidden={!isActive}>
-//           <p className="panel__content">{content}</p>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
 class Accordion extends React {
   constructor(props) {
     super(props);
