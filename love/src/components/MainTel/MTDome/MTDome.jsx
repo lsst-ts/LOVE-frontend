8bc79bc06685ea095efd652e50ce3360300870de
Astrid Reyes De La Rosa <astrid.reyes@inria.cl>
2022-01-04 14:00:46 -0300

    Initial commit

---
+++ love/src/components/MainTel/MTDome/MTDome.jsx
@@ -0,0 +1,301 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import ManagerInterface, { parseCommanderData } from 'Utils';
+// import SkymapGrid from '../Skymap/SkymapGrid';
+import PlotContainer from 'components/GeneralPurpose/Plot/Plot.container';
+import TimeSeriesControls from 'components/GeneralPurpose/Plot/TimeSeriesControls/TimeSeriesControls';
+import DomeTopView from './MTDomeTopView';
+import DomePointing from './MTDomePointing';
+import DomeShutter from './MTDomeShutter';
+import MountTopView from './MountTopView';
+
+import WindRose from '../../icons/WindRose/WindRose';
+import DomeSummaryTable from './MTDomeSummaryTable/MTDomeSummaryTable';
+
+import styles from './MTDome.module.css';
+
+export default class Dome extends Component {
+  static propTypes = {
+    // raftsDetailedState: PropTypes.string,
+    // imageReadinessDetailedState: PropTypes.string,
+    // calibrationDetailedState: PropTypes.string,
+    // shutterDetailedState: PropTypes.string,
+    // imageSequence: PropTypes.object,
+  };
+
+  static defaultProps = {
+    width: 500,
+    height: 500,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      az: 0,
+      el: 0,
+      timeWindow: 60,
+      isLive: true,
+      historicalData: [],
+    };
+
+    this.azimuthPlotRef = React.createRef();
+    this.elevationPlotRef = React.createRef();
+  }
+
+  componentDidMount = () => {
+    this.props.subscribeToStream();
+  };
+
+  componentWillUnmount = () => {
+    this.props.unsubscribeToStream();
+  };
+
+  elevationPlotInputs = {
+    'Mount elevation': {
+      category: 'telemetry',
+      csc: 'ATMCS',
+      salindex: '0',
+      topic: 'mount_AzEl_Encoders',
+      item: 'elevationCalculatedAngle',
+      type: 'line',
+      accessor: (x) => x[0],
+      color: 'hsl(201, 70%, 40%)',
+    },
+    'Mount target': {
+      category: 'event',
+      csc: 'ATMCS',
+      salindex: '0',
+      topic: 'target',
+      item: 'elevation',
+      type: 'line',
+      accessor: (x) => x,
+      color: 'white',
+      dash: [4, 1],
+    },
+  };
+
+  azimuthPlotInputs = {
+    'Dome Azimuth': {
+      category: 'telemetry',
+      csc: 'ATDome',
+      salindex: 0,
+      topic: 'position',
+      item: 'azimuthPosition',
+      type: 'line',
+      accessor: (x) => x,
+      color: 'hsl(201, 70%, 40%)',
+    },
+    'Dome Target Az': {
+      category: 'event',
+      csc: 'ATDome',
+      salindex: 0,
+      topic: 'azimuthCommandedState',
+      item: 'azimuth',
+      type: 'line',
+      accessor: (x) => x,
+      color: 'hsl(201, 70%, 40%)',
+      dash: [4, 1],
+    },
+    'Mount Azimuth': {
+      category: 'telemetry',
+      csc: 'ATMCS',
+      salindex: 0,
+      topic: 'mount_AzEl_Encoders',
+      item: 'azimuthCalculatedAngle',
+      type: 'line',
+      accessor: (x) => x[0],
+      color: 'hsl(160, 70%, 40%)',
+    },
+    'Mount Target': {
+      category: 'event',
+      csc: 'ATMCS',
+      salindex: 0,
+      topic: 'target',
+      item: 'azimuth',
+      type: 'line',
+      accessor: (x) => x,
+      color: 'hsl(160, 70%, 40%)',
+      dash: [4, 1],
+    },
+  };
+
+  setHistoricalData = (startDate, timeWindow) => {
+    const cscs = {
+      ATDome: {
+        0: {
+          position: ['azimuthPosition'],
+          azimuthCommandedState: ['azimuth'],
+        },
+      },
+      ATMCS: {
+        0: {
+          mount_AzEl_Encoders: ['azimuthCalculatedAngle', 'elevationCalculatedAngle'],
+          target: ['azimuth', 'elevation'],
+        },
+      },
+    };
+    const parsedDate = startDate.format('YYYY-MM-DDTHH:mm:ss');
+    // historicalData
+    ManagerInterface.getEFDTimeseries(parsedDate, timeWindow, cscs, '1min').then((data) => {
+      const parsedPlotData = parseCommanderData(data, 'x', 'y');
+      this.setState({ historicalData: parsedPlotData });
+    });
+  };
+
+  render() {
+    const width = this.props.width;
+    const height = this.props.height;
+    const currentPointing = {
+      az: this.props.azElMountEncoders ? this.props.azElMountEncoders.azimuthCalculatedAngle.value[0] : 0,
+      el: this.props.azElMountEncoders ? this.props.azElMountEncoders.elevationCalculatedAngle.value[0] : 0,
+      nasmyth1: this.props.nasmythMountEncoders ? this.props.nasmythMountEncoders.nasmyth1CalculatedAngle.value[0] : 0,
+      nasmyth2: this.props.nasmythMountEncoders ? this.props.nasmythMountEncoders.nasmyth2CalculatedAngle.value[0] : 0,
+    };
+    const targetPointing = {
+      az: this.props.target ? this.props.target[this.props.target.length - 1].azimuth.value : 0,
+      el: this.props.target ? this.props.target[this.props.target.length - 1].elevation.value : 0,
+      nasmyth1: this.props.target ? this.props.target[this.props.target.length - 1].nasmyth1RotatorAngle.value : 0,
+      nasmyth2: this.props.target ? this.props.target[this.props.target.length - 1].nasmyth2RotatorAngle.value : 0,
+    };
+    const domeAz = this.props.azimuthPosition ? this.props.azimuthPosition.value : 0;
+    const domeTargetAz = this.props.azimuthCommandedState
+      ? this.props.azimuthCommandedState[this.props.azimuthCommandedState.length - 1].azimuth.value
+      : 0;
+    const mountTrackingState = this.props.atMountState
+      ? this.props.atMountState[this.props.atMountState.length - 1].state.value
+      : 0;
+    const azimuthState = this.props.azimuthState
+      ? this.props.azimuthState[this.props.azimuthState.length - 1].state.value
+      : 0;
+    const dropoutDoorState = this.props.dropoutDoorState
+      ? this.props.dropoutDoorState[this.props.dropoutDoorState.length - 1].state.value
+      : 0;
+    const mainDoorState = this.props.mainDoorState
+      ? this.props.mainDoorState[this.props.mainDoorState.length - 1].state.value
+      : 0;
+    const domeInPosition = this.props.domeInPosition;
+    const mountInPosition = this.props.mountInPosition;
+
+    const dropoutDoorOpeningPercentage = this.props.dropoutDoorOpeningPercentage
+      ? this.props.dropoutDoorOpeningPercentage.value
+      : 0;
+    const mainDoorOpeningPercentage = this.props.dropoutDoorOpeningPercentage
+      ? this.props.dropoutDoorOpeningPercentage.value
+      : 0;
+    const trackID = this.props.target ? this.props.target[this.props.target.length - 1].trackId.value : '';
+    const m3State = this.props.m3State ? this.props.m3State[this.props.m3State.length - 1].value : 2;
+    const currentTimesToLimits = this.props.currentTimesToLimits ? this.props.currentTimesToLimits : {};
+    const positionLimits = this.props.positionLimits
+      ? this.props.positionLimits[this.props.positionLimits.length - 1]
+      : {};
+
+    const isProjected = true;
+    let azDiff = Math.abs(domeAz - currentPointing.az);
+    if (azDiff > 180) azDiff = azDiff - 360;
+    const vignettingDistance = (Math.abs(azDiff) * Math.cos((currentPointing.el * Math.PI) / 180)).toFixed(2);
+
+    const timeSeriesControlsProps = {
+      timeWindow: this.state.timeWindow,
+      isLive: this.state.isLive,
+      historicalData: this.state.historicalData,
+    };
+
+    // console.log(currentPointing)
+    return (
+      <div className={styles.domeContainer}>
+        {/* <h2>TOP VIEW</h2> */}
+        <div className={styles.topRow}>
+          <div className={styles.skymapGridContainer}>
+            {/* <SkymapGrid width={width} height={height} isProjected={isProjected} /> */}
+            <div className={styles.windRoseContainer}>
+              <WindRose />
+            </div>
+
+            <DomeTopView width={width} height={height} />
+            <MountTopView currentPointing={currentPointing} />
+            <DomeShutter
+              width={width}
+              height={height}
+              azimuthPosition={domeAz}
+              dropoutDoorOpeningPercentage={dropoutDoorOpeningPercentage}
+              mainDoorOpeningPercentage={mainDoorOpeningPercentage}
+              targetAzimuthPosition={domeTargetAz}
+            />
+            <DomePointing
+              width={width}
+              height={height}
+              currentPointing={currentPointing}
+              targetPointing={targetPointing}
+              isProjected={isProjected}
+            />
+            <div
+              className={styles.vignettingDistanceContainer}
+              title="Difference between telescope and dome azimuth, multiplied by cos(telescope altitude)"
+            >
+              <span>Vignetting distance: </span>
+              <span className={styles.value}>{vignettingDistance}º</span>
+            </div>
+          </div>
+          <DomeSummaryTable
+            currentPointing={currentPointing}
+            targetPointing={targetPointing}
+            domeAz={domeAz}
+            domeTargetAz={domeTargetAz}
+            domeInPosition={domeInPosition}
+            azimuthState={azimuthState}
+            dropoutDoorState={dropoutDoorState}
+            mainDoorState={mainDoorState}
+            mountTrackingState={mountTrackingState}
+            trackID={trackID}
+            mountInPosition={mountInPosition}
+            m3State={m3State}
+            currentTimesToLimits={currentTimesToLimits}
+            positionLimits={positionLimits}
+          />
+        </div>
+        {/* {this.props.controls && (
+          <div>
+            <TimeSeriesControls
+              setTimeWindow={(timeWindow) => this.setState({ timeWindow })}
+              timeWindow={this.state.timeWindow}
+              setLiveMode={(isLive) => this.setState({ isLive })}
+              isLive={this.state.isLive}
+              setHistoricalData={this.setHistoricalData}
+            />
+          </div>
+        )} */}
+        {/* <div className={styles.telemetryTable}>
+          <div className={styles.azimuthSection}>
+            <h2>Azimuth</h2>
+            <div ref={this.azimuthPlotRef} className={styles.azimuthPlot}>
+              <div>
+                <PlotContainer
+                  inputs={this.azimuthPlotInputs}
+                  containerNode={this.azimuthPlotRef?.current}
+                  xAxisTitle="Time"
+                  yAxisTitle="Azimuth"
+                  timeSeriesControlsProps={timeSeriesControlsProps}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.elevationSection}>
+            <h2>Elevation</h2>
+            <div ref={this.elevationPlotRef} className={styles.elevationPlot}>
+              <div>
+                <PlotContainer
+                  inputs={this.elevationPlotInputs}
+                  containerNode={this.elevationPlotRef?.current}
+                  xAxisTitle="Time"
+                  yAxisTitle="Elevation"
+                  timeSeriesControlsProps={timeSeriesControlsProps}
+                />
+              </div>
+            </div>
+          </div>
+        </div> */}
+      </div>
+    );
+  }
+}
---
+++ love/src/components/MainTel/MTDome/MTDomeSummaryTable/MTDomeSummaryTable.jsx
@@ -0,0 +1,256 @@
+import React, { Component } from 'react';
+import styles from './MTDomeSummaryTable.module.css';
+import StatusText from '../../../GeneralPurpose/StatusText/StatusText';
+import CurrentTargetValue from '../../../GeneralPurpose/CurrentTargetValue/CurrentTargetValue';
+import PropTypes from 'prop-types';
+import {
+  domeAzimuthStateMap,
+  dropoutDoorStateMap,
+  mainDoorStateMap,
+  mountTrackingStateMap,
+  stateToStyleDomeAndMount,
+} from '../../../../Config';
+import Limits from '../../../GeneralPurpose/Limits/Limits';
+import SummaryPanel from '../../../GeneralPurpose/SummaryPanel/SummaryPanel';
+import Row from '../../../GeneralPurpose/SummaryPanel/Row';
+import Label from '../../../GeneralPurpose/SummaryPanel/Label';
+import Value from '../../../GeneralPurpose/SummaryPanel/Value';
+import Title from '../../../GeneralPurpose/SummaryPanel/Title';
+import { stateToStyleDome, stateToStyleMount } from '../../../../Config';
+
+export default class DomeSummaryTable extends Component {
+  static propTypes = {
+    currentPointing: PropTypes.object,
+    targetPointing: PropTypes.object,
+    domeAz: PropTypes.number,
+    domeTargetAz: PropTypes.number,
+    azimuthState: PropTypes.number,
+    dropoutDoorState: PropTypes.number,
+    mainDoorState: PropTypes.number,
+    mountTrackingState: PropTypes.number,
+  };
+
+  static defaultProps = {};
+
+  render() {
+    const domeAz = {
+      current: this.props.domeAz,
+      target: this.props.domeTargetAz,
+    };
+    const mountAz = {
+      current: this.props.currentPointing.az,
+      target: this.props.targetPointing.az,
+    };
+    const mountEl = {
+      current: this.props.currentPointing.el,
+      target: this.props.targetPointing.el,
+    };
+
+    const azimuthStateValue = domeAzimuthStateMap[this.props.azimuthState];
+    const dropoutDoorStateValue = dropoutDoorStateMap[this.props.dropoutDoorState];
+    const mainDoorStateValue = mainDoorStateMap[this.props.mainDoorState];
+    const domeInPositionValue = this.props.domeInPosition ? this.props.domeInPosition[0].inPosition.value : 0;
+    const mountInPositionValue = this.props.mountInPosition ? this.props.mountInPosition[0].inPosition.value : 0;
+    const mountTrackingStateValue = mountTrackingStateMap[this.props.mountTrackingState];
+    const m3State = this.props.m3State;
+    const { positionLimits } = this.props;
+    const timesToLimit = this.props.currentTimesToLimits;
+    const timeToAzLimit = timesToLimit.timeToAzlim ? timesToLimit.timeToAzlim.value : 0;
+    const timeToRotLimit = timesToLimit.timeToRotlim ? timesToLimit.timeToRotlim.value : 0;
+    const timeToUnobservable = timesToLimit.timeToUnobservable ? timesToLimit.timeToUnobservable.value : 0;
+    const timeToBlindSpot = timesToLimit.timeToBlindSpot ? timesToLimit.timeToBlindSpot.value : 0;
+    const closestLimit = timeToBlindSpot > timeToUnobservable && timeToBlindSpot > 0 ? 'blind spot' : 'unobservable';
+    const timeToElLimit = closestLimit === 'blind spot' ? timeToBlindSpot : timeToUnobservable;
+    // L1 (software) position limits, in order elevation, azimuth, Nasmyth 1, Nasmyth 2 and M3.
+    const { maximum, minimum } = positionLimits;
+    let [maxEl, maxAz, maxNas1, maxNas2, maxM3] = maximum ? maximum.value : [];
+    let [minEl, minAz, minNas1, minNas2, minM3] = minimum ? minimum.value : [];
+    [maxEl, maxAz, maxNas1, maxNas2, maxM3] = [
+      maxEl ? maxEl : 90,
+      maxAz ? maxAz : 270,
+      maxNas1 ? maxNas1 : 165,
+      maxNas2 ? maxNas2 : 165,
+      maxM3 ? maxM3 : 180,
+    ];
+    [minEl, minAz, minNas1, minNas2, minM3] = [
+      minEl ? minEl : 5,
+      minAz ? minAz : -270,
+      minNas1 ? minNas1 : -165,
+      minNas2 ? minNas2 : -165,
+      minM3 ? minM3 : 0,
+    ];
+    const mountRotator =
+      m3State === 1
+        ? {
+            name: '(1)',
+            current: this.props.currentPointing.nasmyth1,
+            target: this.props.targetPointing.nasmyth1,
+            minRot: minNas1,
+            maxRot: maxNas1,
+          }
+        : {
+            name: '(2)',
+            current: this.props.currentPointing.nasmyth2,
+            target: this.props.targetPointing.nasmyth2,
+            minRot: minNas2,
+            maxRot: maxNas2,
+          };
+    const domeInPositionLabel = domeInPositionValue ? 'IN POSITION' : 'NOT IN POSITION';
+    const mountInPositionLabel = mountInPositionValue ? 'IN POSITION' : 'NOT IN POSITION';
+    return (
+      <SummaryPanel className={styles.summaryTable}>
+        <Title>Track ID</Title>
+        <Value>{this.props.trackID}</Value>
+        {/* Dome */}
+        <Title>Dome</Title>
+        <Value>
+          <StatusText
+            title={domeInPositionValue ? 'true' : 'false'}
+            status={stateToStyleDome[domeInPositionLabel]}
+            small
+          >
+            {domeInPositionLabel}
+          </StatusText>
+        </Value>
+
+        <Label>Azimuth</Label>
+        <Value>
+          <StatusText title={azimuthStateValue} status={stateToStyleDomeAndMount[azimuthStateValue]} small>
+            {azimuthStateValue}
+          </StatusText>
+        </Value>
+        <Label>Dropout door</Label>
+        <Value>
+          <StatusText title={dropoutDoorStateValue} status={stateToStyleDomeAndMount[dropoutDoorStateValue]} small>
+            {dropoutDoorStateValue}
+          </StatusText>
+        </Value>
+        <Label>Main door</Label>
+        <Value>
+          <StatusText title={mainDoorStateValue} status={stateToStyleDomeAndMount[mainDoorStateValue]} small>
+            {mainDoorStateValue}
+          </StatusText>
+        </Value>
+        <Label>Az</Label>
+        <Value>
+          <CurrentTargetValue currentValue={domeAz.current} targetValue={domeAz.target} isChanging={true} />
+        </Value>
+        {/* <span className={[styles.subRow, styles.wide].join(' ')} title={`Time to limit: ${2} min`}>
+          <span>
+            <Limits
+              lowerLimit={0}
+              upperLimit={360}
+              currentValue={domeAz.current}
+              targetValue={domeAz.target}
+              height={30}
+              displayLabels={false}
+            />
+          </span>
+          <span>
+            <span>Time to limit: </span>
+            <span className={styles.highlight}>2 min</span>
+          </span>
+        </span> */}
+        {/* Mount */}
+        <Title>Mount</Title>
+        <Value>
+          <StatusText
+            title={mountInPositionValue ? 'true' : 'false'}
+            status={stateToStyleMount[mountInPositionLabel]}
+            small
+          >
+            {mountInPositionLabel}
+          </StatusText>
+        </Value>
+        <Label>Tracking</Label>
+        <Value>
+          <StatusText title={mountTrackingStateValue} status={stateToStyleDomeAndMount[mountTrackingStateValue]} small>
+            {mountTrackingStateValue}
+          </StatusText>
+        </Value>
+        <Label>Az</Label>
+        <Value>
+          <CurrentTargetValue
+            currentValue={mountAz.current.toFixed(2)}
+            targetValue={mountAz.target.toFixed(2)}
+            isChanging={true}
+          />
+        </Value>
+
+        <Row
+          title={`Current value: ${mountAz.current}\nTarget value: ${mountAz.target}\nLimits: [${minAz}º, ${maxAz}º]`}
+        >
+          <span>
+            <Limits
+              lowerLimit={minAz}
+              upperLimit={maxAz}
+              currentValue={mountAz.current}
+              targetValue={mountAz.target}
+              height={30}
+              displayLabels={false}
+            />
+          </span>
+          <span>
+            <span>Time to limit: </span>
+            <span className={styles.highlight}>{Math.round(timeToAzLimit)} min</span>
+          </span>
+        </Row>
+        <Label>El</Label>
+        <Value>
+          <CurrentTargetValue
+            currentValue={mountEl.current.toFixed(2)}
+            targetValue={mountEl.target.toFixed(2)}
+            isChanging={true}
+          />
+        </Value>
+
+        <Row
+          title={`Current value: ${mountEl.current}\nTarget value: ${mountEl.target}\nLimits: [${minEl}º, ${maxEl}º]`}
+        >
+          <span>
+            <Limits
+              lowerLimit={minEl}
+              upperLimit={maxEl}
+              currentValue={mountEl.current}
+              targetValue={mountEl.target}
+              height={30}
+              displayLabels={false}
+            />
+          </span>
+          <span>
+            <span>{`Time to ${closestLimit}: `}</span>
+            <span className={styles.highlight}>{Math.round(timeToElLimit)} min</span>
+          </span>
+        </Row>
+        <Label>
+          Nasmyth <span className={styles.highlight}>{mountRotator.name}</span>
+        </Label>
+        <Value>
+          <CurrentTargetValue
+            currentValue={mountRotator.current.toFixed(2)}
+            targetValue={mountRotator.target.toFixed(2)}
+            isChanging={true}
+          />
+        </Value>
+        <Row
+          title={`Current value: ${mountRotator.current}\nTarget value: ${mountRotator.target}\nLimits: [${mountRotator.minRot}º, ${mountRotator.maxRot}º]`}
+        >
+          <span>
+            <Limits
+              lowerLimit={mountRotator.minRot}
+              upperLimit={mountRotator.maxRot}
+              currentValue={mountRotator.current}
+              targetValue={mountRotator.target}
+              height={30}
+              displayLabels={false}
+            />
+          </span>
+          <span>
+            <span>Time to limit: </span>
+            <span className={styles.highlight}>{Math.round(timeToRotLimit)} min</span>
+          </span>
+        </Row>
+      </SummaryPanel>
+    );
+  }
+}
