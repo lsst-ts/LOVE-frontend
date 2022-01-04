0bc3dfd1ab691076aedfb81f0e4dcb86d34fa495
Astrid Reyes De La Rosa <astrid.reyes@inria.cl>
2022-01-04 14:00:46 -0300

    Initial commit

---
+++ love/src/components/MainTel/MTDome/MTDome.container.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Dome from './MTDome';
+import { getDomeState } from '../../../redux/selectors';
+import { addGroup, removeGroup } from '../../../redux/actions/ws';
+import SubscriptionTableContainer from '../../GeneralPurpose/SubscriptionTable/SubscriptionTable.container';
+
+export const schema = {
+  description: 'Summary view of the MTDome. Contains general information about the dome and mount state',
+  defaultSize: [51, 45],
+  props: {
+    title: {
+      type: 'string',
+      description: 'Name displayed in the title bar (if visible)',
+      isPrivate: false,
+      default: 'Dome',
+    },
+    controls: {
+      type: 'boolean',
+      description: "Whether to display controls to configure periods of time'",
+      default: true,
+      isPrivate: false,
+    },
+  },
+};
+
+const MTDomeContainer = ({
+  dropoutDoorOpeningPercentage,
+  mainDoorOpeningPercentage,
+  azimuthPosition,
+  azimuthState,
+  domeInPosition,
+  azimuthCommandedState,
+  dropoutDoorState,
+  mainDoorState,
+  azElMountEncoders,
+  nasmythMountEncoders,
+  detailedState,
+  atMountState,
+  target,
+  mountInPosition,
+  currentTimesToLimits,
+  positionLimits,
+  width,
+  height,
+  subscribeToStream,
+  unsubscribeToStream,
+  controls,
+  ...props
+}) => {
+  if (props.isRaw) {
+    return <SubscriptionTableContainer subscriptions={props.subscriptions}></SubscriptionTableContainer>;
+  }
+  return (
+    <Dome
+      dropoutDoorOpeningPercentage={dropoutDoorOpeningPercentage}
+      mainDoorOpeningPercentage={mainDoorOpeningPercentage}
+      domeInPosition={domeInPosition}
+      azimuthPosition={azimuthPosition}
+      azimuthState={azimuthState}
+      azimuthCommandedState={azimuthCommandedState}
+      dropoutDoorState={dropoutDoorState}
+      mainDoorState={mainDoorState}
+      azElMountEncoders={azElMountEncoders}
+      nasmythMountEncoders={nasmythMountEncoders}
+      detailedState={detailedState}
+      atMountState={atMountState}
+      target={target}
+      mountInPosition={mountInPosition}
+      subscribeToStream={subscribeToStream}
+      unsubscribeToStream={unsubscribeToStream}
+      width={width}
+      height={height}
+      currentTimesToLimits={currentTimesToLimits}
+      positionLimits={positionLimits}
+      controls={controls}
+    />
+  );
+};
+
+const mapStateToProps = (state) => {
+  const domeState = getDomeState(state);
+  return domeState;
+};
+
+const mapDispatchToProps = (dispatch) => {
+  const subscriptions = [
+    'telemetry-ATDome-0-dropoutDoorOpeningPercentage',
+    'telemetry-ATDome-0-mainDoorOpeningPercentage',
+    'telemetry-ATDome-0-azimuthPosition',
+    'event-ATDome-0-azimuthState',
+    'event-ATDome-0-azimuthCommandedState',
+    'event-ATDome-0-dropoutDoorState',
+    'event-ATDome-0-mainDoorState',
+    'event-ATDome-0-allAxesInPosition',
+    'telemetry-ATMCS-0-mount_AzEl_Encoders',
+    'telemetry-ATMCS-0-mount_Nasmyth_Encoders',
+    'event-ATMCS-0-detailedState',
+    'event-ATMCS-0-atMountState',
+    'event-ATMCS-0-target',
+    'event-ATMCS-0-allAxesInPosition',
+    'event-ATMCS-0-m3State',
+    'event-ATMCS-0-positionLimits',
+    'telemetry-ATPtg-1-currentTimesToLimits',
+  ];
+  return {
+    subscriptions,
+    subscribeToStream: () => {
+      subscriptions.forEach((stream) => dispatch(addGroup(stream)));
+    },
+    unsubscribeToStream: () => {
+      subscriptions.forEach((stream) => dispatch(removeGroup(stream)));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MTDomeContainer);
