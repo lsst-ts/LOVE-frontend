1e41a005b15b4a47c62a26340a65324b3f825a44
Fernando del Campo Vergara <fdelcampo@gmail.com>
2022-02-24 19:56:06 -0300

    fix comments in Pull Request. Pending about the use of prevState

--- love/src/redux/selectors/selectors.js
+++ love/src/redux/selectors/selectors.js
@@ -1057,7 +1057,7 @@ export const getTMASummary = (state) => {
     connected: summaryData['event-MTMount-0-connected']
       ? summaryData['event-MTMount-0-connected'][0].command.value
       : false,
-    balancing: summaryData['event-MTMount-0-commander']
+    balancing: summaryData['event-MTMount-0-balanceSystemState']
       ? summaryData['event-MTMount-0-balanceSystemState'][0].powerState.value
       : 0,
   };
@@ -1149,7 +1149,7 @@ export const getDrivesAzimuthElevationState = (state) => {
  * Selects the Mirror Covers status for Mirror Covers view
  * @param {object} state
  */
- export const getMirrorCoversMotionState = (state) => {
+export const getMirrorCoversMotionState = (state) => {
   const subscriptions = [
     'event-MTMount-0-mirrorCoversMotionState',
   ];
