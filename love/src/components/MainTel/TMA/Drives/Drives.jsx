3c7692982870ecfd9a06498b6aa57e1620de36d1
Fernando del Campo Vergara <fdelcampo@gmail.com>
2022-02-28 17:47:30 -0300

    add import PropTypes

--- love/src/components/MainTel/TMA/Drives/Drives.jsx
+++ love/src/components/MainTel/TMA/Drives/Drives.jsx
@@ -1,5 +1,6 @@
 import { range } from 'lodash';
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import SimpleTable from '../../../GeneralPurpose/SimpleTable/SimpleTable';
 import styles from './Drives.module.css';
