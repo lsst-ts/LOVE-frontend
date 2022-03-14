a4ec85d5159faa6dba3207dfd2136fd1ef7a3c6c
Fernando del Campo Vergara <fdelcampo@gmail.com>
2022-03-14 17:41:00 -0300

    add component about inclinometer of M2

---
+++ love/src/components/MainTel/M2/Actuators/Inclinometer/Inclinometer.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { fixedFloat } from 'Utils';
+import { inclinationTelemetrySourceStateMap } from 'Config';
+import styles from './Inclinometer.module.css';
+
+export default class Inclinometer extends Component {
+  static propTypes = {
+    /** zenithAngleMeasured */
+    zenithAngleMeasured: PropTypes.number,
+    inclinationTelemetrySource: PropTypes.number,
+  };
+
+  static defaultProps = {
+    zenithAngleMeasured: 0,
+    inclinationTelemetrySource: 1,
+  };
+
+  render() {
+    const { zenithAngleMeasured, inclinationTelemetrySource } = this.props;
+    const inclinationTelemetrySourceValue = inclinationTelemetrySource ? 
+              inclinationTelemetrySourceStateMap[inclinationTelemetrySource] : 
+              inclinationTelemetrySourceStateMap[1];
+
+    return (
+      <div className={styles.container}>
+          <p className={styles.title}>Inclination</p>
+          <svg className={styles.svgContainer} viewBox="0 0 302 165.85">
+            <path className={styles['cls-1']} d="M1,164.85a150,150,0,0,1,300,0" />
+            <line className={styles['cls-1']} x1="151" y1="14.85" x2="151" y2="159.46" />
+            <rect className={styles['cls-1']} x="1" y="155.62" width="300" height="9.23" />
+            <line
+              style={{
+                transform: `rotate(${zenithAngleMeasured}deg)`,
+              }}
+              className={styles.marker}
+              x1="151"
+              y1="3.31"
+              x2="151"
+              y2="164.85"
+            />
+            <path className={styles['cls-3']} d="M111,164.85a40,40,0,0,1,80,0" />
+            <rect
+              style={{
+                transform: `rotate(${zenithAngleMeasured}deg)`,
+              }}
+              className={styles['cls-3']}
+              x="104.85"
+              y="1"
+              width="92.31"
+              height="23.08"
+            />
+            <text className={styles.axisLabel} x="150" y="-5">0°</text>
+          </svg>
+        <div className={styles.inclinometerValues}>
+            <span>Inclination</span>
+            <span className={styles.value}>{fixedFloat(zenithAngleMeasured, 2)}°</span>
+            <span>Source</span>
+            <span className={styles.value}>{inclinationTelemetrySourceValue}</span>
+        </div>
+      </div>
+    );
+  }
+}
