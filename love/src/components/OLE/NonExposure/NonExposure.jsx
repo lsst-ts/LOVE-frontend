be810a14d6a0f06041c0dc8d266954a7b85aefe2
Fernando del Campo Vergara <fdelcampo@gmail.com>
2022-03-29 18:16:54 -0300

    initial definition about OLE with tabs

---
+++ love/src/components/OLE/NonExposure/NonExposure.jsx
@@ -0,0 +1,155 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import SimpleTable from 'components/GeneralPurpose/SimpleTable/SimpleTable';
+import Input from 'components/GeneralPurpose/Input/Input';
+import Label from 'components/GeneralPurpose/SummaryPanel/Label';
+import Value from 'components/GeneralPurpose/SummaryPanel/Value';
+import Button from 'components/GeneralPurpose/Button/Button';
+import { Link } from 'react-router-dom';
+import ArrowIcon from 'components/icons/ArrowIcon/ArrowIcon';
+import EditIcon from 'components/icons/EditIcon/EditIcon';
+import AcknowledgeIcon from 'components/icons/Watcher/AcknowledgeIcon/AcknowledgeIcon';
+import styles from './NonExposure.module.css';
+import { style } from 'd3';
+import { stateToStyleLATISS } from 'Config';
+
+export default class NonExposure extends Component {
+  static propTypes = {};
+
+  static defaultProps = {};
+
+  getHeaders = () => {
+    return [
+      {
+        field: 'id',
+        title: 'Log Id',
+        type: 'number',
+      },
+      {
+        field: 'userId',
+        title: 'User Id',
+        type: 'string',
+      },
+      {
+        field: 'agent',
+        title: 'Agent',
+        type: 'string',
+      },
+      {
+        field: 'timestamp',
+        title: 'Timestamp',
+        type: 'timestamp',
+      },
+      {
+        field: 'timeIncident',
+        title: 'Time of Incident',
+        type: 'string',
+      },
+      {
+        field: 'type',
+        title: 'Type',
+        type: 'string',
+      },
+      {
+        field: 'obsTimeLoss',
+        title: 'Obs. Time Loss',
+        type: 'string',
+      },
+      {
+        field: 'subsystem',
+        title: 'Subsystem',
+        type: 'string',
+      },
+      {
+        field: 'csc',
+        title: 'CSC',
+        type: 'string',
+        render: (cell) => <b>{cell}</b>,
+      },
+      {
+        field: 'cscTopic',
+        title: 'CSC Topic',
+        type: 'string',
+      },
+      {
+        field: 'file',
+        title: 'File',
+        type: 'link',
+        render: (value) => (
+          <Button
+            className={styles.iconBtn}
+            title="File"
+            onClick={() => {}}
+          >
+            <ArrowIcon className={styles.icon}/>
+          </Button>
+        ),
+      },
+      {
+        field: 'jira',
+        title: 'Jira',
+        type: 'link',
+        render: (value) => (
+          <Link to={value}>{value}</Link>
+        ),
+      },
+      {
+        field: 'action',
+        title: 'Action',
+        type: 'string',
+        render: () => {
+          return (
+            <>
+              <span className={styles.margin}>
+                <Button className={styles.iconBtn} title="View" onClick={() => {}} status="transparent">
+                  <AcknowledgeIcon className={styles.icon} nonAcknowledge={false}/>
+                </Button>
+              </span>
+              <span className={styles.margin}>
+                <Button className={styles.iconBtn} title="Edit" onClick={() => {}} status="transparent">
+                  <EditIcon className={styles.icon}/>
+                </Button>
+              </span>
+            </>
+          );
+        },
+      },
+    ];
+  }
+
+  render() {
+    const headers = Object.values(this.getHeaders());
+    const filteredData = [
+      {
+        id: 1,
+        userId: 'MiaElbo',
+        agent: 'Lorem',
+        timestamp: '2022-03-21 11:24:24',
+        timeIncident: '2022-03-21 12:25:25',
+        type: 'Observation',
+        obsTimeLoss: '-',
+        subsystem: 'M. Telescope',
+        csc: 'MTHexapod',
+        cscTopic: 'Actuators',
+        file: 'http://file.org',
+        jira: 'http://lsst.jira.org',
+      },
+    ];
+    const tableData = Object.values(filteredData);
+
+    return (
+      <>
+        <div className={styles.title}>
+          Filter
+        </div>
+        <div className={styles.filters}>
+          <Label>From: </Label>
+          <Value>
+            <Input/>
+          </Value>
+        </div>
+        <SimpleTable headers={headers} data={tableData} />
+      </>
+    );
+  }
+}
