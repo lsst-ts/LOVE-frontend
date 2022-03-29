ac85272cc4977db91f5b9f8e5b409e69a10a5d2b
Fernando del Campo Vergara <fdelcampo@gmail.com>
2022-03-29 18:16:54 -0300

    initial definition about OLE with tabs

---
+++ love/src/components/OLE/Exposure/Exposure.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Exposure extends Component {
+  static propTypes = {};
+
+  static defaultProps = {};
+
+  render() {
+    return (
+      <span>Exposure</span>
+    );
+  }
+}
