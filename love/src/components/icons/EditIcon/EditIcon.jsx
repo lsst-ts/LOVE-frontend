438c8eaa4a28e2b98ea18c7f4a4aa46e430a7edc
Fernando del Campo Vergara <fdelcampo@gmail.com>
2022-03-31 15:31:08 -0300

    add look of view detail of the non-exposure component

--- love/src/components/icons/EditIcon/EditIcon.jsx
+++ love/src/components/icons/EditIcon/EditIcon.jsx
@@ -1,35 +1,16 @@
 import React, { Component } from 'react';
-import styles from './EditIcon.module.css';
 
-const EditIcon = ({ className }) => (
-  <svg viewBox="0 0 55.25 55.25">
-    <g>
-      <rect
-        className={[styles.color, className].join(' ')}
-        x="5.5"
-        y="19.39"
-        transform="matrix(0.7071 -0.7071 0.7071 0.7071 -12.3013 25.9513)"
-        width="39.35"
-        height="16.86"
-      />
-      <polygon
-        className={[styles.color, className].join(' ')}
-        points="0,53 15.9,49.02 3.98,37.1  "
-      />
-      <path
-        className={[styles.color, className].join(' ')}
-        d="M52.06,8.34l-7.4-7.4c-1.25-1.25-3.28-1.25-4.53,0L37.1,3.97L49.03,15.9l3.04-3.04C53.31,11.61,53.31,9.59,52.06,8.34z"
-      />
-      <rect
-        className={[styles.color, className].join(' ')}
-        x="40.14"
-        y="3.49"
-        transform="matrix(0.7071 -0.7071 0.7071 0.7071 3.599 32.5375)"
-        width="1.87"
-        height="16.86"
-      />
-    </g>
-  </svg>
-);
-
-export default EditIcon;
+export default class EditIcon extends Component {
+  render() {
+    return (
+      <svg {...this.props} viewBox="0 0 55.25 55.25">
+        <g>
+          <rect x="5.5" y="19.39" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -12.3013 25.9513)" width="39.35" height="16.86"/>
+          <polygon points="0,53 15.9,49.02 3.98,37.1  "/>
+          <path d="M52.06,8.34l-7.4-7.4c-1.25-1.25-3.28-1.25-4.53,0L37.1,3.97L49.03,15.9l3.04-3.04C53.31,11.61,53.31,9.59,52.06,8.34z"/>
+          <rect x="40.14" y="3.49" transform="matrix(0.7071 -0.7071 0.7071 0.7071 3.599 32.5375)" width="1.87" height="16.86"/>
+        </g>
+      </svg>
+    );
+  }
+}
