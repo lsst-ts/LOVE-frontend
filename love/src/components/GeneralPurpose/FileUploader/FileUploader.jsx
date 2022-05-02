04b9111ffe1ddbba051e589f0df497c20fbe1048
Fernando del Campo Vergara <fdelcampo@gmail.com>
2022-05-02 16:11:28 -0400

    new button FileUploader in GeneralPurpose

---
+++ love/src/components/GeneralPurpose/FileUploader/FileUploader.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Button from '../Button/Button';
+import styles from './FileUploader.module.css';
+
+const FileUploader = props => {
+  const hiddenFileInput = React.useRef(null);
+  
+  const handleClick = event => {
+    hiddenFileInput.current.click();
+  };
+  const handleChange = event => {
+    const fileUploaded = event.target.files[0];
+    if (fileUploaded !== undefined) props.handleFile(fileUploaded);
+  };
+
+  const handleDelete = event => {
+    props.handleDelete();
+  };
+
+  return (
+    <div className={styles.containerFileUpload}>
+      <Button
+        onClick={handleClick}
+        className={styles.inputButton}
+      >
+        Select File
+      </Button>
+      <span
+        className={styles.inputText}
+        onClick={handleClick}
+      >
+        {props.value}
+      </span>
+      <span className={styles.inputClose}
+        onClick={handleDelete}
+      >
+        x
+      </span>
+      <input type="file"
+        ref={hiddenFileInput}
+        onChange={handleChange}
+        style={{display:'none'}}
+      /> 
+    </div>
+  );
+};
+export default FileUploader;
+
+FileUploader.defaultProps = {
+  handleFile: () => {},
+  handleDelete: () => {},
+  value: "",
+}
+
+// {` ${this.state.logEdit.file.name} (${ (parseInt(this.state.logEdit.file.size) / 1024).toFixed(2)} KB) `}
