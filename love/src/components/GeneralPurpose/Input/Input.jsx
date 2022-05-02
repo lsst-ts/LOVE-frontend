9797cf1d70f9b2c539024c5a37166b61f2792ceb
Fernando del Campo Vergara <fdelcampo@gmail.com>
2022-05-02 16:13:23 -0400

    refactor Input. Set defaultProps for type, onChange and onClick to the Input component

--- love/src/components/GeneralPurpose/Input/Input.jsx
+++ love/src/components/GeneralPurpose/Input/Input.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import styles from './Input.module.css';
 
-export default function Input({ defaultValue, value, onChange, className, ...props }) {
+export default function Input({ defaultValue, value, onChange, onClick, className, type, ...props }) {
   return (
     <input
-      type="text"
+      type={type}
       className={[styles.input, className].join(' ')}
       defaultValue={defaultValue}
       value={value}
       onChange={onChange}
+      onClick={onClick}
       {...props}
     />
   );
 }
+
+Input.defaultProps = {
+  type: 'text',
+  onChange: () => {},
+  onClick: () => {},
+};
