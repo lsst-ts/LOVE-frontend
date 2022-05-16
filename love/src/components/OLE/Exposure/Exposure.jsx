d0357726bfd5f98ececb18a5f47ca041c9790d54
Fernando del Campo Vergara <fdelcampo@gmail.com>
2022-05-16 17:13:36 -0400

    add initial of exposure

--- love/src/components/OLE/Exposure/Exposure.jsx
+++ love/src/components/OLE/Exposure/Exposure.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import AddIcon from 'components/icons/AddIcon/AddIcon';
+import FlagIcon from 'components/icons/FlagIcon/FlagIcon';
 import AcknowledgeIcon from 'components/icons/Watcher/AcknowledgeIcon/AcknowledgeIcon';
 import SimpleTable from 'components/GeneralPurpose/SimpleTable/SimpleTable';
 import Label from 'components/GeneralPurpose/SummaryPanel/Label';
 import Value from 'components/GeneralPurpose/SummaryPanel/Value';
 import Button from 'components/GeneralPurpose/Button/Button';
+import ExposureAdd from './ExposureAdd';
+import ExposureDetail from './ExposureDetail';
 import styles from './Exposure.module.css';
 
 export default class Exposure extends Component {
@@ -13,16 +16,40 @@ export default class Exposure extends Component {
 
   static defaultProps = {};
 
-  add(index) {
-    console.log('add', index);
+  constructor(props) {
+    super(props);
+    this.state = {
+      modeView: false,
+      modeAdd: false,
+      selected: {},
+    };
   }
 
   view(index) {
-    console.log('view', index);
+    if (index) {
+      this.setState({
+        modeView: true,
+        selected: index,
+      });
+    }   
   }
 
-  flag(index) {
-    console.log('flag', index);
+  add(index) {
+    if (index) {
+      this.setState({
+        modeAdd: true,
+        selected: index,
+      });
+    }   
+  }
+
+  statusFlag(flag) {
+    const result = {
+      none: 'ok',
+      junk: 'warning',
+      questionable: 'alert'
+    };
+    return result[flag] ? result[flag] : 'unknown';
   }
 
   getHeaders = () => {
@@ -55,12 +82,8 @@ export default class Exposure extends Component {
           const values = String(value).split(',');
           return values.map((val) => {
             return (
-              <span className={styles.margin}>
-                <Button className={styles.iconBtn} title={val}
-                  onClick={() => { this.flag(val) }} status="transparent"
-                >
-                  <AddIcon className={styles.icon}/>
-                </Button>
+              <span>
+                <FlagIcon title={val} status={this.statusFlag(val)} className={styles.iconFlag}/>
               </span>
             );
           });
@@ -74,20 +97,19 @@ export default class Exposure extends Component {
           return (
             <>
               <span className={styles.margin}>
-                <Button className={styles.iconBtn} title="Add"
-                  onClick={() => { this.add(index) }} status="transparent"
-                >
-                  <AddIcon className={styles.icon}/>
-                </Button>
-              </span>
-              <span className={styles.margin}>
                 <Button className={styles.iconBtn} title="View"
                   onClick={() => { this.view(index) }} status="transparent"
                 >
                   <AcknowledgeIcon className={styles.icon} nonAcknowledge={false}/>
                 </Button>
               </span>
-              
+              <span className={styles.margin}>
+                <Button className={styles.iconBtn} title="Add"
+                  onClick={() => { this.add(index) }} status="transparent"
+                >
+                  <AddIcon className={styles.icon}/>
+                </Button>
+              </span>
             </>
           );
         },
@@ -96,7 +118,8 @@ export default class Exposure extends Component {
   }
 
   render() {
-    
+    const modeView = this.state.modeView;
+    const modeAdd = this.state.modeAdd;
     const headers = Object.values(this.getHeaders());
 
     const filteredData = [
@@ -106,7 +129,36 @@ export default class Exposure extends Component {
         obsId: 'LC201204-01',
         timestamp: '2022-03-21 11:24:24',
         instrument: 'LSST Cam',
-        flags: '1,2',
+        flags: 'none,junk',
+        obsType: 'Engtest',
+        obsReason: 'extra',
+        obsDay: undefined,
+        messages: [
+          {
+            id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+            siteId: '',
+            type: undefined,
+            user: 'Mia Elbo',
+            flag: 'none',
+            file: {name: 'file.csv', size: 6078},
+            jira: 'http://lsst.jira.org',
+            description: 'First we have the Logs component, which will display logs created by different  love-operators. This component will have two tabs, one for non-exposure logs and another for exposure logs, viewing these two types of logs is very different.',
+            dateAdded: '2022-05-11 12:34',
+            dateInvalidated: undefined,
+          },
+          {
+            id: '3fa85f64-5717-4562-b3fc',
+            siteId: '',
+            type: undefined,
+            user: 'Mia Elbo',
+            flag: 'junk',
+            file: {name: 'file.csv', size: 6078},
+            jira: 'http://lsst.jira.org',
+            description: undefined,
+            dateAdded: '2022-05-11 12:34',
+            dateInvalidated: undefined,
+          },
+        ]
       },
       {
         id: 2,
@@ -114,7 +166,38 @@ export default class Exposure extends Component {
         obsId: 'LC201204-01',
         timestamp: '2022-03-21 11:24:24',
         instrument: 'LSST Cam',
-        flags: '1,2',
+        flags: 'none,questionable',
+        obsId: 'LC20210224-1',
+        instrument: 'LATISS',
+        obsType: 'Engtest',
+        obsReason: 'extra',
+        obsDay: undefined,
+        messages: [
+          {
+            id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+            siteId: '',
+            type: undefined,
+            user: undefined,
+            flag: 'none',
+            jira: undefined,
+            file: undefined,
+            description: 'First we have the Logs component, which will display logs created by different  love-operators.',
+            dateAdded: undefined,
+            dateInvalidated: undefined,
+          },
+          {
+            id: '4562-b3fc-2c963f66afa6',
+            siteId: '',
+            type: undefined,
+            user: undefined,
+            flag: 'questionable',
+            file: {name: 'file.csv', size: 6078},
+            jira: 'http://lsst.jira.org',
+            description: 'This component will have two tabs, one for non-exposure logs and another for exposure logs, viewing these two types of logs is very different',
+            dateAdded: undefined,
+            dateInvalidated: undefined,
+          },
+        ]
       },
       {
         id: 3,
@@ -122,25 +205,49 @@ export default class Exposure extends Component {
         obsId: 'LC201204-01',
         timestamp: '2022-03-21 11:24:24',
         instrument: 'LSST Cam',
-        flags: '3',
+        flags: 'questionable',
+        obsId: 'LC20210224-1',
+        instrument: 'LATISS',
+        obsType: 'Engtest',
+        obsReason: 'extra',
+        obsDay: undefined,
+        messages: [
+          {
+            id: '4562-b3fc-2c963f66afa6',
+            siteId: '',
+            type: undefined,
+            user: undefined,
+            flag: 'questionable',
+            file: {name: 'file.csv', size: 6078},
+            jira: 'http://lsst.jira.org',
+            description: 'on Image 2 we are looking at the non-exposure logs tab in which users will be able to navigate through the logs listed by their unique id on a table.',
+            dateAdded: undefined,
+            dateInvalidated: undefined,
+          },
+        ]
       },
     ];
     const tableData = Object.values(filteredData);
 
-
     return (
-      <div className={styles.margin10}>
-        <div className={styles.title}>
-          Filter
-        </div>
-        <div className={styles.filters}>
-          <Label>From: </Label>
-          {/* <Value>
-            <Input/>
-          </Value> */}
-        </div>
-        <SimpleTable headers={headers} data={tableData} />
-      </div>
+      modeView && !modeAdd
+      ? <ExposureDetail back={() => { this.setState({ modeView: false });}} logDetail={this.state.selected}/>
+      : modeAdd && !modeView
+        ? <ExposureAdd back={() => { this.setState({ modeEdit: false });}} logEdit={this.state.selected}/>
+        : (
+          <div className={styles.margin10}>
+            <div className={styles.title}>
+              Filter
+            </div>
+            <div className={styles.filters}>
+              <Label>From: </Label>
+              {/* <Value>
+                <Input/>
+              </Value> */}
+            </div>
+            <SimpleTable headers={headers} data={tableData} />
+          </div>
+      )
     );
   }
 }
---
+++ love/src/components/OLE/Exposure/Message/Message.jsx
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import DeleteIcon from 'components/icons/DeleteIcon/DeleteIcon';
+import EditIcon from 'components/icons/EditIcon/EditIcon';
+import DownloadIcon from 'components/icons/DownloadIcon/DownloadIcon';
+import Button from 'components/GeneralPurpose/Button/Button';
+import styles from './Message.module.css';
+import FlagIcon from 'components/icons/FlagIcon/FlagIcon';
+
+
+export default class Message extends Component {
+  static propTypes = {
+    message: PropTypes.object,
+  };
+
+  static defaultProps = {
+    message: {
+      id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+      siteId: '',
+      type: undefined,
+      user: undefined,
+      flag: 'ok',
+      jira: undefined,
+      file: undefined,
+      description: undefined,
+      dateAdded: undefined,
+      dateInvalidated: undefined,
+    },
+  };
+
+  statusFlag(flag) {
+    const result = {
+      none: 'ok',
+      junk: 'warning',
+      questionable: 'alert'
+    };
+    return result[flag] ? result[flag] : 'unknown';
+  }
+
+
+  render() {
+    const message = this.props.message ? this.props.message : this.defaultProps.message;
+
+    return (
+      <div className={styles.message}>
+        <div className={styles.header}>
+          <span className={[styles.floatLeft, styles.title, styles.margin3].join(' ')}>
+            #{message.id} - {message.type}
+            <span>
+              <Button status="link" onClick={() => { link() }}>view Jira ticket</Button>
+              </span>
+          </span>
+          <span className={[styles.floatRight, styles.margin3].join(' ')}>
+            <Button className={styles.iconBtn} title="Delete" onClick={() => {}} status="transparent">
+              <DeleteIcon className={styles.icon}/>
+            </Button>
+          </span>
+          <span className={[styles.floatRight, styles.margin3].join(' ')}>
+            <Button className={styles.iconBtn} title="Edit" onClick={() => {}} status="transparent">
+              <EditIcon className={styles.icon}/>
+            </Button>
+          </span>
+        </div>
+        <div className={styles.description}>
+          <div className={[styles.floatLeft, styles.margin3].join(' ')}>
+            <span>On </span>
+            <span className={styles.bold}>{message.timeIncident} </span>
+            <span>by </span>
+            <span className={styles.bold}>{message.user} </span>
+            <span>wrote:</span>
+          </div>
+          <p className={[styles.textDescription, styles.margin3].join(' ')}>
+            {message.description}
+          </p>
+        </div>
+        <div className={styles.footer}>
+          <span className={[styles.floatLeft, styles.margin3].join(' ')}>
+            <span className={styles.label}>
+              File Attached:
+            </span>
+            <span className={styles.value}>
+              { message.file
+                ? ` ${message.file.name} (${(parseInt(message.file.size) / 1024).toFixed(2)} KB) `
+                : ``
+              }
+              <Button className={styles.iconBtn} title="File" onClick={() => {}} status="transparent">
+                <DownloadIcon className={styles.icon}/>
+              </Button>
+            </span>
+          </span>
+          <span className={[styles.floatRight, styles.margin3].join(' ')}>
+            <FlagIcon title={message.flag} status={this.statusFlag(message.flag)}
+              className={styles.iconFlag}/>
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
