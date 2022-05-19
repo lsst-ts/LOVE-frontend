049d31c5154da1f32005dac4ad191c58dc5706ac
tribeiro <tribeiro@lsst.org>
2022-05-19 15:28:58 -0700

    Add new TCSOffset view to send offset commands.

---
+++ love/src/components/TCSCommands/TCSOffset/TCSOffset.container.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { addGroup, removeGroup, requestSALCommand } from '../../../redux/actions/ws';
+import { getPermCmdExec, getScriptQueueState } from '../../../redux/selectors';
+import TCSOffset from './TCSOffset';
+
+export const schema = {
+    description: 'Panel containing multiple buttons that execute different commands, such as closing the dome',
+    defaultSize: [57, 35],
+    props: {
+        title: {
+            type: 'string',
+            description: 'Name diplayed in the title bar (if visible)',
+            isPrivate: false,
+            default: 'Telescope Offset',
+        },
+        nameTCS: {
+            type: 'string',
+            description: 'Name of the Telescope Control System (TCS) for the call of the commands ("aux" or "main").',
+            isPrivate: false,
+            default: 'aux',
+        },
+        hasRawMode: {
+            type: 'boolean',
+            description: 'Whether the component has a raw mode version',
+            isPrivate: true,
+            default: false,
+        },
+        scriptQueueIndex: {
+            type: 'number',
+            description: 'Salindex of the ScriptQueue to listen events',
+            isPrivate: false,
+            default: 1,
+        },
+    },
+};
+
+const TCSCommandsContainer = ({
+    queueState,
+    commandExecutePermission,
+    subscribeToStreams,
+    unsubscribeToStreams,
+    ...props
+}) => {
+    return (
+        <TCSOffset
+            state={queueState.state}
+            subscribeToStreams={subscribeToStreams}
+            unsubscribeToStreams={unsubscribeToStreams}
+            commandExecutePermission={commandExecutePermission}
+            {...props}
+        />
+    );
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+    const subscriptions = [`event-ScriptQueueState-${ownProps.scriptQueueIndex}-stream`];
+    return {
+        subscriptions,
+        subscribeToStreams: () => {
+            subscriptions.forEach((stream) => dispatch(addGroup(stream)));
+        },
+        unsubscribeToStreams: () => {
+            subscriptions.forEach((stream) => dispatch(removeGroup(stream)));
+        },
+        requestSALCommand: (component, salindex, cmd) => {
+            return dispatch(requestSALCommand({ ...cmd, component, salindex }));
+        },
+    };
+};
+
+const mapStateToProps = (state) => {
+    const commandExecutePermission = getPermCmdExec(state);
+    const queueState = getScriptQueueState(state, 1);
+    return {
+        commandExecutePermission,
+        queueState,
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TCSCommandsContainer);
