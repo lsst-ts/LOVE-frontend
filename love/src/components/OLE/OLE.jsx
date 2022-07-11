72d2410afc9a99282b1979ead60e9ce69e1a38cd
Fernando del Campo <fdelcampo@gmail.com>
2022-07-11 16:11:27 -0400

    Refactor. It's moved the states of the filters to OLE component, for the remember the selected states of the list of Exposure and Narrative Logs

--- love/src/components/OLE/OLE.jsx
+++ love/src/components/OLE/OLE.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Moment from 'moment';
+import { extendMoment } from 'moment-range';
 import Exposure from './Exposure/Exposure';
 import NonExposure from './NonExposure/NonExposure';
 import ExposureAdd from './Exposure/ExposureAdd';
 import NonExposureEdit from './NonExposure/NonExposureEdit';
 import Button from 'components/GeneralPurpose/Button/Button';
+import ManagerInterface from 'Utils';
 import styles from './OLE.module.css';
 
 export default class OLE extends Component {
@@ -21,19 +24,72 @@ export default class OLE extends Component {
     this.state = {
       selectedTab: props.tabs[0].value,
       clickNewLog: false,
+      // Non Exposure filters
+      selectedCommentType: { value: 'all', label: 'All comment types' },
+      selectedSubsystem: 'all',
+      selectedObsTimeLoss: false,
+      selectedDateStartNonExposure: new Date() - 24 * 30 * 60 * 60 * 1000,
+      selectedDateEndNonExposure: new Date(Date.now()),
+      // Exposure filters
+      selectedDateStartExposure: new Date() - 24 * 30 * 60 * 60 * 1000,
+      selectedDateEndExposure: new Date(Date.now()),
+      selectedInstrument: null,
+      instruments: [],
+      selectedExposureType: 'all',
+      
     };
-  }
-
-  changeTab(tab) {
-    this.setState({ selectedTab: tab });
   }
 
   componentDidMount() {
     this.props.subscribeToStreams();
+    ManagerInterface.getListExposureInstruments().then((data) => {
+      const instrumentsArray = Object.values(data).map((arr) => arr[0]);
+      this.setState({ instruments: instrumentsArray, selectedInstrument: instrumentsArray[0] });
+    });
   }
 
   componentWillUnmount() {
     this.props.unsubscribeToStreams();
+  }
+
+  changeCommentTypeSelect(value) {
+    this.setState({ selectedCommentType: value });
+  }
+
+  changeSubsystemSelect(value) {
+    this.setState({ selectedSubsystem: value });
+  }
+
+  changeObsTimeLossSelect(value) {
+    this.setState({ selectedObsTimeLoss: value });
+  }
+
+  changeInstrumentSelect(value) {
+    this.setState({ selectedInstrument: value })
+  }
+
+  changeExposureType(value) {
+    this.setState({ selectedExposureType: value })
+  }
+
+  handleDateTimeRangeNonExposure(date, type) {
+    if (type === 'start') {
+      this.setState({ selectedDateStartNonExposure: date });
+    } else if (type === 'end') {
+      this.setState({ selectedDateEndNonExposure: date });
+    }
+  }
+
+  handleDateTimeRangeExposure(date, type) {
+    if (type === 'start') {
+      this.setState({ selectedDateStartExposure: date });
+    } else if (type === 'end') {
+      this.setState({ selectedDateEndExposure: date });
+    }
+  }
+
+  changeTab(tab) {
+    this.setState({ selectedTab: tab });
   }
 
   getComponent(clickNewLog, tab) {
@@ -60,10 +116,31 @@ export default class OLE extends Component {
       }
     } else {
       if (tab === 'exposure') {
-        return <Exposure props={this.props} />;
+        return <Exposure
+          props={this.props}
+          instruments={this.state.instruments}
+          selectedInstrument={this.state.selectedInstrument}
+          changeInstrumentSelect={(value) => this.changeInstrumentSelect(value)}
+          selectedExposureType={this.state.selectedExposureType}
+          changeExposureType={(value) => this.changeExposureType(value)}
+          selectedDateStart={this.state.selectedDateStartExposure}
+          selectedDateEnd={this.state.selectedDateEndExposure}
+          handleDateTimeRange={(date, type) => this.handleDateTimeRangeExposure(date, type)}
+        />;
       }
       if (tab === 'non-exposure') {
-        return <NonExposure props={this.props} />;
+        return <NonExposure
+          props={this.props}
+          selectedDateStart={this.state.selectedDateStartNonExposure}
+          selectedDateEnd={this.state.selectedDateEndNonExposure}
+          handleDateTimeRange={(date, type) => this.handleDateTimeRangeNonExposure(date, type)}
+          selectedCommentType={this.state.selectedCommentType}
+          changeCommentTypeSelect={(value) => this.changeCommentTypeSelect(value)}
+          selectedSubsystem={this.state.selectedSubsystem}
+          changeSubsystemSelect={(value) => this.changeSubsystemSelect(value)}
+          selectedObsTimeLoss={this.state.selectedObsTimeLoss}
+          changeObsTimeLossSelect={(value) => this.changeObsTimeLossSelect(value)}
+        />;
       }
     }
   }
