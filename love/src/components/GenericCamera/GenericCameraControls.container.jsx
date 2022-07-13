2cbc7f4016c988eabccfcfd8e0521c8f48c8d28a
Michael Reuter <mareuter@lsst.org>
2022-07-13 15:21:35 -0700

    Add schema vars to controls container.

--- love/src/components/GenericCamera/GenericCameraControls.container.jsx
+++ love/src/components/GenericCamera/GenericCameraControls.container.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { addGroup, removeGroup, requestSALCommand } from '../../redux/actions/ws';
 import GenericCameraControls from './GenericCameraControls';
 
 export const schema = {
   description: 'Controls to run live view and image taking for a GenericCamera',
-  defaultSize: [10, 30],
+  defaultSize: [20, 20],
   props: {
+    titleBar: {
+      type: 'boolean',
+      description: 'Whether to display the title bar',
+      isPrivate: false,
+      default: false,
+    },
+    title: {
+      type: 'string',
+      description: 'Name diplayed in the title bar (if visible)',
+      isPrivate: false,
+      default: 'GenericCamera Controls',
+    },
     margin: {
       type: 'boolean',
       description: 'Whether to display component with a margin',
