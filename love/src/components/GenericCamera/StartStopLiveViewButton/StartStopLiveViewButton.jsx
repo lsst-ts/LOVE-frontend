2bbdc9570cf5e1d9d974e1be796a2d10068eb29f
Michael Reuter <mareuter@lsst.org>
2022-07-15 17:22:32 -0700

    Add live view button icons.

--- love/src/components/GenericCamera/StartStopLiveViewButton/StartStopLiveViewButton.jsx
+++ love/src/components/GenericCamera/StartStopLiveViewButton/StartStopLiveViewButton.jsx
@@ -1,6 +1,30 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import ResumeIcon from 'components/icons/GenericCamera/ResumeIcon/ResumeIcon';
+import StopIcon from 'components/icons/GenericCamera/StopIcon/StopIcon';
 import styles from './StartStopLiveViewButton.module.css';
+/**
+ * Handles starting and stopping the live view of the Generic camera
+ */
+export default function StartStopLiveViewButton({ onChange }) {
+  const [isLatched, setLatched] = useState(false);
+
+  const triggerLatch = () => {
+    setLatched(!isLatched);
+    if (typeof onChange === 'function') {
+      onChange(!isLatched);
+    }
+  };
+
+  return (
+    <div className={styles.buttonWrapper}>
+      <button className={isLatched ? styles.buttonLatched : styles.button} onClick={triggerLatch}>
+        {isLatched ? <StopIcon /> : <ResumeIcon />}
+        <span className={styles.buttonLabel}>{isLatched ? 'Stop' : 'Start'} Live View</span>
+      </button>
+    </div>
+  );
+}
 
 StartStopLiveViewButton.propTypes = {
   /**
@@ -12,25 +36,3 @@ StartStopLiveViewButton.propTypes = {
 // StartStopLiveViewButton.defaultProps = {
 //   onChange: (),
 // };
-
-/**
- * Handles starting and stopping the live view of the Generic camera
- */
-export default function StartStopLiveViewButton({ onChange }) {
-  let [isLatched, setLatched] = useState(false);
-
-  const triggerLatch = () => {
-    setLatched(!isLatched);
-    if (typeof onChange === 'function') {
-      onChange(!isLatched);
-    }
-  };
-
-  return (
-    <div className={styles.buttonWrapper}>
-      <button className={styles.button} onClick={triggerLatch}>
-        <span className={styles.buttonLabel}>{isLatched ? 'Stop' : 'Start'} Live View</span>
-      </button>
-    </div>
-  );
-}
