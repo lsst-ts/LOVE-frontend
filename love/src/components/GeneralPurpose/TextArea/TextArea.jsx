cb380c265272d31534266e91607e0598eb4c1127
Sebastian Aranda <aranda.sebastian@gmail.com>
2022-07-28 18:07:39 -0400

    Extend TextArea to receive default value

--- love/src/components/GeneralPurpose/TextArea/TextArea.jsx
+++ love/src/components/GeneralPurpose/TextArea/TextArea.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './TextArea.module.css';
 
-export default function TextArea({ callback, ...otherProps }) {
-  const [content, setContent] = useState('');
+export default function TextArea({ value, callback, ...otherProps }) {
+  const [content, setContent] = useState(value ?? '');
+
+  useEffect(() => {
+    setContent(value ?? '');
+  }, [value]);
 
   const onChange = (e) => {
     setContent(e.target.value);
     callback(e.target.value);
   };
+
   return <textarea className={styles.textarea} onChange={onChange} value={content} {...otherProps}></textarea>;
 }
