f7118bc64359d9c186b494d5b34f2bc651ea992a
Sebastian Aranda <aranda.sebastian@gmail.com>
2022-08-23 11:30:09 -0400

    Fix rebase syntax error

--- love/src/components/UIF/ComponentIndex.jsx
+++ love/src/components/UIF/ComponentIndex.jsx
@@ -321,6 +321,9 @@ export const mainIndex = {
       props: {
         ...defaultSchemaProps,
         ...require('../MainTel/TMA/TMA.container').schema.props,
+      },
+    },
+  },
   GIS: {
     component: require('../GIS/GIS.container').default,
     schema: {
