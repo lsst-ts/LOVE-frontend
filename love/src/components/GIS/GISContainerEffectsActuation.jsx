a04afff52dd507bb14f146c310dd767c7d01dfe9
Sebastian Aranda <aranda.sebastian@gmail.com>
2022-09-08 16:41:22 -0400

    Add proptypes library

--- love/src/components/GIS/GISContainerEffectsActuation.jsx
+++ love/src/components/GIS/GISContainerEffectsActuation.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import styles from './GIS.module.css';
 
 export default class GISContainerEffectsActuation extends Component {
