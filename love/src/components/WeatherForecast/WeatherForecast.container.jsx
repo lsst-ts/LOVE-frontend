d1b21306f7993f54775c1fae6109946dcc514d8b
fdelcampo <fdelcampo@gmail.com>
2022-11-29 13:06:35 -0300

    little progress changes about weatherforecast

--- love/src/components/WeatherForecast/WeatherForecast.container.jsx
+++ love/src/components/WeatherForecast/WeatherForecast.container.jsx
@@ -1,84 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { addGroup, removeGroup } from '../../redux/actions/ws';
-import { getStreamData } from '../../redux/selectors';
-import SubscriptionTableContainer from '../GeneralPurpose/SubscriptionTable/SubscriptionTable.container';
+import { addGroup, removeGroup } from 'redux/actions/ws';
+import { getM1M3State} from 'redux/selectors';
+import SubscriptionTableContainer from 'components/GeneralPurpose/SubscriptionTable/SubscriptionTable.container';
 import WeatherForecast from './WeatherForecast';
 
 export const schema = {
-  description: 'Component that displays the data coming from a weather station',
-  defaultSize: [57, 35],
+  description: 'View of Weather Forecast',
+  defaultSize: [61, 32],
   props: {
     title: {
       type: 'string',
       description: 'Name diplayed in the title bar (if visible)',
       isPrivate: false,
-      default: 'Weather forecast component',
-    },
-    salindex: {
-      type: 'number',
-      description: 'Salindex of the CSC',
-      isPrivate: false,
-      default: 1,
-    },
-    hasRawMode: {
-      type: 'boolean',
-      description: 'Whether the component has a raw mode version',
-      isPrivate: true,
-      default: false,
-    },
-    controls: {
-      type: 'boolean',
-      description: "Whether to display controls to configure periods of time'",
-      default: true,
-      isPrivate: false,
+      default: 'Weather Forecast',
     },
   },
 };
 
-const WeatherForecastContainer = ({ ...props }) => {
+const WeatherForecastContainer = ({ subscribeToStreams, unsubscribeToStreams, ...props }) => {
   if (props.isRaw) {
     return <SubscriptionTableContainer subscriptions={props.subscriptions}></SubscriptionTableContainer>;
   }
-  return <WeatherForecast {...props} />;
+  return <WeatherForecast subscribeToStreams={subscribeToStreams} unsubscribeToStreams={unsubscribeToStreams} {...props} />;
 };
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    weather: getStreamData(state, `telemetry-WeatherForecast-${ownProps.salindex}-weather`),
-    windSpeed: getStreamData(state, `telemetry-WeatherForecast-${ownProps.salindex}-windSpeed`),
-  };
+const mapStateToProps = (state) => {
+  const weatherState = {}; // = getWeatherState(state);
+  return { ...weatherState };
 };
 
-const mapDispatchToProps = (dispatch, ownProps) => {
+const mapDispatchToProps = (dispatch) => {
   const subscriptions = [
-    `telemetry-WeatherForecast-${ownProps.salindex}-weather`,
-    `telemetry-WeatherForecast-${ownProps.salindex}-windDirection`,
-    `telemetry-WeatherForecast-${ownProps.salindex}-windGustDirection`,
-    `telemetry-WeatherForecast-${ownProps.salindex}-windSpeed`,
-    `telemetry-WeatherForecast-${ownProps.salindex}-airTemperature`,
-    `telemetry-WeatherForecast-${ownProps.salindex}-soilTemperature`,
-    `telemetry-WeatherForecast-${ownProps.salindex}-dewPoint`,
-    `telemetry-WeatherForecast-${ownProps.salindex}-relativeHumidity`,
-    `telemetry-WeatherForecast-${ownProps.salindex}-snowDepth`,
-    `telemetry-WeatherForecast-${ownProps.salindex}-precipitation`,
-    `telemetry-WeatherForecast-${ownProps.salindex}-airPressure`,
-    `telemetry-WeatherForecast-${ownProps.salindex}-solarNetRadiation`,
+    'telemetry-WeatherForecast-0-WeatherForecast_dailyTrend',
   ];
   return {
     subscriptions,
     subscribeToStreams: () => {
-      subscriptions.forEach((stream) => dispatch(addGroup(stream)));
+      subscriptions.forEach((s) => dispatch(addGroup(s)));
     },
     unsubscribeToStreams: () => {
-      subscriptions.forEach((stream) => dispatch(removeGroup(stream)));
+      subscriptions.forEach((s) => dispatch(removeGroup(s)));
     },
   };
 };
-const connectedContainer = connect(mapStateToProps, mapDispatchToProps)(WeatherForecastContainer);
-
-connectedContainer.defaultProps = {
-  salindex: 1,
-};
-
-export default connectedContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(WeatherForecastContainer);
