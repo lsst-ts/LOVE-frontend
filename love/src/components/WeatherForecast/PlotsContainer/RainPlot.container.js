32c7c615c63dca6f0dc93f123f822c105959cec1
fdelcampo <fdelcampo@gmail.com>
2022-12-05 13:02:49 -0300

    add plot of wheather forecast to component index, for that can add the plot independently to the view

--- love/src/components/WeatherForecast/PlotsContainer/RainPlot.container.js
+++ love/src/components/WeatherForecast/PlotsContainer/RainPlot.container.js
@@ -7,14 +7,39 @@ import Plot from 'components/GeneralPurpose/Plot/Plot';
 
 
 export const schema = {
-  description: 'View of Weather Forecast',
-  defaultSize: [61, 32],
+  description: 'View of Precipitation Plot of Weather Forecast',
+  defaultSize: [62, 18],
   props: {
     title: {
       type: 'string',
       description: 'Name diplayed in the title bar (if visible)',
       isPrivate: false,
-      default: 'Weather Forecast',
+      default: 'Precipitation Plot Weather Forecast',
+    },
+    hasRawMode: {
+      type: 'boolean',
+      description: 'Whether the component has a raw mode version',
+      isPrivate: false,
+      default: true,
+    },
+    xAxisTitle: {
+      type: 'string',
+      description: 'Title of the horizontal axis of this plot',
+      default: 'Time',
+      isPrivate: false,
+    },
+    yAxisTitle: {
+      type: 'string',
+      description: 'Title of the vertical axis of this plot',
+      default: 'Precipitation',
+      isPrivate: false,
+    },
+    legendPosition: {
+      type: 'string',
+      description:
+        "Whether to display the legend to the right of the plot or at the bottom. One of 'right' or 'bottom'",
+      default: 'bottom',
+      isPrivate: false,
     },
     inputs: {
       type: 'object',
@@ -138,11 +163,11 @@ export const schema = {
             },
             {
               variable: 'deltatime',
-              /* category: 'telemetry',
+              category: 'telemetry',
               csc: 'WeatherForecast',
               salindex: 0,
               topic: 'WeatherForecast_dailyTrend',
-              item: 'timestamp', */
+              item: 'timestamp',
               accessor: '(x) => (30 * 60)',
             },
             {
@@ -273,11 +298,11 @@ export const schema = {
             },
             {
               variable: 'deltatime',
-              /* category: 'telemetry',
+              category: 'telemetry',
               csc: 'ATMCS',
               salindex: 0,
               topic: 'mount_AzEl_Encoders',
-              item: 'elevationCalculatedAngle', */
+              item: 'elevationCalculatedAngle',
               accessor: '(x) => (30 * 60)',
             },
             {
@@ -340,7 +365,7 @@ const getGroupNames = (inputs) => {
       }
     },
   );
-  return inputsMap.flat();
+  return [...new Set(inputsMap.flat())];
 }
 
 const mapStateToProps = (state, ownProps) => {
