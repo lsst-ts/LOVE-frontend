34acc36c6eefb906ebd37bfd545b30332fc54d77
Sebastian Aranda <aranda.sebastian@gmail.com>
2022-12-14 13:11:16 -0300

    Add documentation for DateTimeRange component

--- love/src/components/GeneralPurpose/DateTimeRange/DateTimeRange.jsx
+++ love/src/components/GeneralPurpose/DateTimeRange/DateTimeRange.jsx
@@ -1,30 +1,39 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { DATE_TIME_FORMAT } from 'Config';
 import DateTime from 'components/GeneralPurpose/DateTime/DateTime';
 import styles from './DateTimeRange.module.css';
 
-const DateTimeRange = (props) => {
-  const [dateStart, setDateStart] = useState(props.startDate ?? new Date(Date.now() - 24 * 60 * 60 * 1000));
-  const [dateEnd, setDateEnd] = useState(props.endDate ?? new Date(Date.now() + 37 * 1000)); // Add 37 seconds to comply with TAI
+const DateTimeRange = ({
+  className,
+  startDate,
+  endDate,
+  startDateProps,
+  endDateProps,
+  label = '',
+  onChange = () => {},
+}) => {
+  const [dateStart, setDateStart] = useState(startDate ?? new Date(Date.now() - 24 * 60 * 60 * 1000));
+  const [dateEnd, setDateEnd] = useState(endDate ?? new Date(Date.now() + 37 * 1000)); // Add 37 seconds to comply with TAI
 
   useEffect(() => {
-    props.onChange(dateStart, 'start');
-    props.onChange(dateEnd, 'end');
-  }, []);
+    onChange(dateStart, 'start');
+    onChange(dateEnd, 'end');
+  }, [startDate, endDate]);
 
   const handleChangeStart = (changeEvent) => {
     setDateStart(changeEvent);
-    props.onChange(changeEvent, 'start');
+    onChange(changeEvent, 'start');
   };
 
   const handleChangeEnd = (changeEvent) => {
     setDateEnd(changeEvent);
-    props.onChange(changeEvent, 'end');
+    onChange(changeEvent, 'end');
   };
 
   return (
-    <div className={[styles.horizontalFilter, props.className].join(' ')}>
-      {props.label !== '' && <span className={styles.label}>{props.label}</span>}
+    <div className={[styles.horizontalFilter, className].join(' ')}>
+      {label !== '' && <span className={styles.label}>{label}</span>}
       <DateTime
         viewMode="time"
         inputProps={{ placeholder: 'Initial date' }}
@@ -33,7 +42,7 @@ const DateTimeRange = (props) => {
         dateFormat="YYYY/MM/DD"
         timeFormat="HH:mm:ss"
         closeOnSelect={true}
-        {...props.startDateProps}
+        {...startDateProps}
       />
       <span className={styles.to}>to</span>
       <DateTime
@@ -44,10 +53,27 @@ const DateTimeRange = (props) => {
         dateFormat="YYYY/MM/DD"
         timeFormat="HH:mm:ss"
         closeOnSelect={true}
-        {...props.endDateProps}
+        {...endDateProps}
       />
     </div>
   );
+};
+
+DateTimeRange.propTypes = {
+  /** Classname of the component */
+  className: PropTypes.string,
+  /** Date for the datetime range start */
+  startDate: PropTypes.number,
+  /** Date for the datetime range end */
+  endtDate: PropTypes.instanceOf(Date),
+  /** Properties to add to the start DateTime component */
+  startDateProps: PropTypes.object,
+  /** Properties to add to the end DateTime component */
+  endDateProps: PropTypes.object,
+  /** Label to be appended at the beginning of the datetime range component */
+  label: PropTypes.string,
+  /** Function used to change the values of the dateTimeRange */
+  onChange: PropTypes.func,
 };
 
 export default DateTimeRange;
