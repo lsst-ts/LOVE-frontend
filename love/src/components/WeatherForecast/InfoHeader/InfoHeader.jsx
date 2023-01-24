94891c44cbe88a6aac6c22087320401ac7de2d9c
Fernando del Campo <fdelcampo@gmail.com>
2023-01-24 18:48:25 -0300

    add telemetry/event of weather forecast in selector/redux for the summary state and the info header component

--- love/src/components/WeatherForecast/InfoHeader/InfoHeader.jsx
+++ love/src/components/WeatherForecast/InfoHeader/InfoHeader.jsx
@@ -1,5 +1,7 @@
 import React, { Component }  from 'react';
 import PropTypes from 'prop-types';
+import Moment from 'moment';
+import isEqual from 'lodash/isEqual';
 import {Table, Th, Tr, Td} from 'components/GeneralPurpose/SimpleTable/TableBorder';
 import WeatherForecastIcon from 'components/icons/WeatherForecastIcon/WeatherForecastIcon';
 import styles from './InfoHeader.module.css';
@@ -7,176 +9,167 @@ import styles from './InfoHeader.module.css';
 export default class InfoHeader extends Component {
   static propTypes = {
     frecuency: PropTypes.oneOf(['daily', 'hourly']),
+    daily: PropTypes.objectOf(
+        PropTypes.shape({
+          timestamp: PropTypes.arrayOf(PropTypes.number),
+          pictocode: PropTypes.arrayOf(PropTypes.number),
+          temperatureMax: PropTypes.arrayOf(PropTypes.number),
+          temperatureMin: PropTypes.arrayOf(PropTypes.number),
+          predictability: PropTypes.arrayOf(PropTypes.number),
+          predictabilityClass: PropTypes.arrayOf(PropTypes.oneOf(
+            ['very low', 'low', 'medium', 'high', 'very high'])),
+        }),
+    ),
+    hourly: PropTypes.objectOf(
+      PropTypes.shape({
+        timestamp: PropTypes.arrayOf(PropTypes.number),
+        pictocode: PropTypes.arrayOf(PropTypes.number),
+        temperatureMax: PropTypes.arrayOf(PropTypes.number),
+        temperatureMin: PropTypes.arrayOf(PropTypes.number),
+        predictability: PropTypes.arrayOf(PropTypes.number),
+        predictabilityClass: PropTypes.arrayOf(PropTypes.oneOf(
+          ['very low', 'low', 'medium', 'high', 'very high'])),
+      }),
+    ),
   };
 
   static defaultProps = {
-    frecuency: 'daily'
+    frecuency: 'daily',
+    daily: {
+      timestamp: [],
+      pictocode: [],
+      temperatureMax: [],
+      temperatureMin: [],
+      predictability: [],
+      predictabilityClass: [],
+    },
+    hourly: {
+      timestamp: [],
+      pictocode: [],
+      temperatureMax: [],
+      temperatureMin: [],
+      predictability: [],
+      predictabilityClass: [],
+    },
   };
 
   constructor(props) {
     super(props);
+
+    this.frecuencyOptions = ['daily', 'hourly'];
+    this.sliceSizeOptions = {'daily': 15, 'hourly': 28};
+    this.temporalFormatOptions = {'daily': 'MMM DD', 'hourly': 'HH:mm:ss'};
+
+    this.state = {
+      data: [],
+    };
   }
 
+  getStyle(className) {
+    switch (className) {
+      case 'very low':
+        return styles.veryLow;
+      case 'low':
+        return styles.low;
+      case 'medium':
+        return styles.medium;
+      case 'high':
+        return styles.high;
+      case 'very high':
+        return styles.veryHigh;
+      default:
+        return styles.default;
+    }
+  }
+
+  getStructures(frecuency) {
+    let result = new Array(this.sliceSizeOptions[frecuency]);
+    const object = frecuency === 'daily' ? this.props.daily :  this.props.hourly;
+    for (let i = 0; i < this.sliceSizeOptions[frecuency]; i++) {
+      result[i] = {};
+      for (const [key, values] of Object.entries(object)) {
+        result[i][key] = values[i];
+      }
+    }
+    return result.map((_d) => {
+      return ({
+        dayHour:  Moment(_d.timestamp).format(this.temporalFormatOptions[frecuency]),
+        pictocode: <WeatherForecastIcon pictogramNumber={_d.pictocode}/>,
+        temperatureMax: _d.temperatureMax,
+        temperatureMin: _d.temperatureMin,
+        predictability: _d.predictability,
+        predictabilityClass: _d.predictabilityClass,
+      })
+    });
+  }
+
+  componentDidMount = () => {
+    this.setState({
+      data: this.getStructures(this.props.frecuency),
+    });
+  };
+
+  componentDidUpdate = (prevProps) => {
+    if (!isEqual(prevProps.daily, this.props.daily) ||
+        !isEqual(prevProps.hourly, this.props.hourly) ||
+        prevProps.frecuency !== this.props.frecuency
+    ) {
+      this.setState({
+        data: this.getStructures(this.props.frecuency)
+      });
+    }
+  };
 
   render() {
-    const data = [
-      {
-        dayHour: 'Nov 19',
-        pictocode: <WeatherForecastIcon pictogramNumber={29}/>,
-        temperatureMax: 28,
-        temperatureMin: 16,
-        predictability: 80,
-        predictabilityClass: styles.high,
-      },
-      {
-        dayHour: 'Nov 20',
-        pictocode: <WeatherForecastIcon pictogramNumber={30}/>,
-        temperatureMax: 28,
-        temperatureMin: 16,
-        predictability: 80,
-        predictabilityClass: styles.veryHigh,
-      },
-      {
-        dayHour: 'Nov 21',
-        pictocode: <WeatherForecastIcon pictogramNumber={31}/>,
-        temperatureMax: 28,
-        temperatureMin: 16,
-        predictability: 20,
-        predictabilityClass: styles.veryLow,
-      },
-      {
-        dayHour: 'Nov 22',
-        pictocode: <WeatherForecastIcon pictogramNumber={32}/>,
-        temperatureMax: 28,
-        temperatureMin: 16,
-        predictability: 50,
-        predictabilityClass: styles.low,
-      },
-      {
-        dayHour: 'Nov 23',
-        pictocode: <WeatherForecastIcon pictogramNumber={33}/>,
-        temperatureMax: 28,
-        temperatureMin: 16,
-        predictability: 80,
-        predictabilityClass: styles.medium,
-      },
-      {
-        dayHour: 'Nov 24',
-        pictocode: <WeatherForecastIcon pictogramNumber={34}/>,
-        temperatureMax: 28,
-        temperatureMin: 16,
-        predictability: 60,
-        predictabilityClass: styles.medium,
-      },
-      {
-        dayHour: 'Nov 25',
-        pictocode: <WeatherForecastIcon pictogramNumber={35}/>,
-        temperatureMax: 28,
-        temperatureMin: 16,
-        predictability: 40,
-        predictabilityClass: styles.low,
-      },
-      {
-        dayHour: 'Nov 26',
-        pictocode: <WeatherForecastIcon pictogramNumber={22}/>,
-        temperatureMax: 28,
-        temperatureMin: 16,
-        predictability: 80,
-        predictabilityClass: styles.low,
-      },
-      {
-        dayHour: 'Nov 27',
-        pictocode: <WeatherForecastIcon pictogramNumber={23}/>,
-        temperatureMax: 28,
-        temperatureMin: 16,
-        predictability: 80,
-        predictabilityClass: styles.high,
-      },
-      {
-        dayHour: 'Nov 28',
-        pictocode: <WeatherForecastIcon pictogramNumber={24}/>,
-        temperatureMax: 28,
-        temperatureMin: 16,
-        predictability: 80,
-        predictabilityClass: styles.high,
-      },
-      {
-        dayHour: 'Nov 29',
-        pictocode: <WeatherForecastIcon pictogramNumber={25}/>,
-        temperatureMax: 28,
-        temperatureMin: 16,
-        predictability: 100,
-        predictabilityClass: styles.veryHigh,
-      },
-      {
-        dayHour: 'Nov 30',
-        pictocode: <WeatherForecastIcon pictogramNumber={26}/>,
-        temperatureMax: 28,
-        temperatureMin: 16,
-        predictability: 90,
-        predictabilityClass: styles.veryHigh,
-      },
-      {
-        dayHour: 'Dic 01',
-        pictocode: <WeatherForecastIcon pictogramNumber={27}/>,
-        temperatureMax: 28,
-        temperatureMin: 16,
-        predictability: 70,
-        predictabilityClass: styles.medium,
-      },
-      {
-        dayHour: 'Dic 02',
-        pictocode: <WeatherForecastIcon pictogramNumber={28}/>,
-        temperatureMax: 28,
-        temperatureMin: 16,
-        predictability: 40,
-        predictabilityClass: styles.low,
-      }
-    ];
+    const {data} = this.state;
 
     return (
-      <div className={styles.scrollX}>
+      <div className={styles.infoHeaderTable }>
         <Table>
           <Tr>
-            <Td>Day</Td>
+            <Td className={styles.paddingInfoHeader}>Day</Td>
             {data.map((row) => {
-              return (<Td>{row.dayHour}</Td>);
+              return (<Td className={styles.paddingInfoHeader}>{row.dayHour}</Td>);
             })}
-            <Td>Day</Td>
+            <Td className={styles.paddingInfoHeader}>Day</Td>
           </Tr>
           <Tr>
-            <Td>Pictocode</Td>
+            <Td className={styles.paddingInfoHeader}>Pictocode</Td>
             {data.map((row) => {
-              return (<Td>{row.pictocode}</Td>);
+              return (<Td className={styles.paddingInfoHeader}>{row.pictocode}</Td>);
             })}
-            <Td>Pictocode</Td>
+            <Td className={styles.paddingInfoHeader}>Pictocode</Td>
           </Tr>
           <Tr>
-            <Td>High °C</Td>
+            <Td className={styles.paddingInfoHeader}>High °C</Td>
             {data.map((row) => {
-              return (<Td className={styles.temperatureMax}>{row.temperatureMax}</Td>);
+              return (<Td className={[styles.temperatureMax,styles.paddingInfoHeader].join(' ')}>{row.temperatureMax}</Td>);
             })}
-            <Td>High °C</Td>
+            <Td className={styles.paddingInfoHeader}>High °C</Td>
           </Tr>
           <Tr>
-            <Td>Low °C</Td>
+            <Td className={styles.paddingInfoHeader}>Low °C</Td>
             {data.map((row) => {
-              return (<Td>{row.temperatureMin}</Td>);
+              return (<Td className={styles.paddingInfoHeader}>{row.temperatureMin}</Td>);
             })}
-            <Td>Low °C</Td>
+            <Td className={styles.paddingInfoHeader}>Low °C</Td>
           </Tr>
           <Tr>
-            <Td>Predictability</Td>
+            <Td className={styles.paddingInfoHeader}>Predictability</Td>
             {data.map((row) => {
               return (
                 <Td className={styles.padding0}>
                   <span className={styles.backgroundPredictability} style={{'width': ((100 - row.predictability) / 2) + '%'}}></span>
-                  <span className={[row.predictabilityClass, styles.predictability].join(' ')} style={{'width': row.predictability + '%'}}></span>
+                  <span
+                    className={[this.getStyle(row.predictabilityClass), styles.predictability].join(' ')}
+                    style={{'width': row.predictability + '%'}}
+                    title={row. predictabilityClass + ' -- ' + row.predictability + '%'}
+                  ></span>
                   <span className={styles.backgroundPredictability} style={{'width': ((100 - row.predictability) / 2) + '%'}}></span>
                 </Td>
               );
             })}
-            <Td>Predictability</Td>
+            <Td className={styles.paddingInfoHeader}>Predictability</Td>
           </Tr>
         </Table>
       </div>
