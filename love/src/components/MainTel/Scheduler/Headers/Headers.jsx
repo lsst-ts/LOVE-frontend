ffc0a0b10670435d706d50cd99301ce90fd5ba32
Astrid Reyes <astridreyes@MacBook-Pro-de-Astrid-7.local>
2023-01-26 17:11:07 -0300

    1st iteration on style header component

--- love/src/components/MainTel/Scheduler/Headers/Headers.jsx
+++ love/src/components/MainTel/Scheduler/Headers/Headers.jsx
@@ -4,6 +4,7 @@ import SummaryPanel from 'components/GeneralPurpose/SummaryPanel/SummaryPanel';
 import Label from '../../../GeneralPurpose/SummaryPanel/Label';
 import Value from '../../../GeneralPurpose/SummaryPanel/Value';
 import Title from '../../../GeneralPurpose/SummaryPanel/Title';
+import GearIcon from 'components/icons/ScriptQueue/GearIcon/GearIcon.jsx';
 
 export default class Headers extends Component {
 
@@ -11,16 +12,21 @@ export default class Headers extends Component {
         return (
             <div className={styles.container}>
                 <SummaryPanel className={styles.summaryPanel}>
-                    <Title>Scheduler</Title>
-                    <Value>ENABLED</Value>
-
-                    <Title>Obs. Mode</Title>
-                    <Value>Automatic</Value>
-
-                    <Title>Obs. Type</Title>
-                    <Value>Scheduler Target of Opportunity</Value>
-                    
-                    <Label>Night #13</Label>-<Value>03:21:52 till</Value>  <Label>Sunrise</Label>
+                    <div className={styles.headersLeft}>
+                        <Label>Summary State</Label>
+                        <Value>ENABLED</Value>
+                        <GearIcon className={styles.gearIcon}/>
+                        <Value>RUNNING</Value>
+                    </div>
+                    <div className={styles.headersCenter}>
+                        <Label>Obs. Mode</Label>
+                        <Value>Automatic</Value>
+                        <Label>Obs. Type</Label>
+                        <Value>Scheduler Target of Opportunity</Value>
+                    </div>
+                    <div className={styles.headersRigth}>
+                        <Label>Night #13</Label>-<Value>03:21:52 till</Value>  <Label>Sunrise</Label>
+                    </div>
                 </SummaryPanel>
             </div>
         );
