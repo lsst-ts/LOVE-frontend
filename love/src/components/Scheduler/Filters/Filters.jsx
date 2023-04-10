134081482c7d2418b10887a724863f0d4878a268
Astrid Reyes <astridreyes@MacBook-Pro-de-Astrid-610.local>
2023-04-10 17:02:40 -0400

    Initial improvements on filters subcomponent

--- love/src/components/Scheduler/Filters/Filters.jsx
+++ love/src/components/Scheduler/Filters/Filters.jsx
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import styles from './Filters.module.css';
+// import RightArrowIcon from 'components/icons/RightArrowIcon';
 
 export default class Filters extends Component {
   render() {
     const { needSwap, filterToMount, filterToUnmount } = this.props;
     return (
       <div className={styles.container}>
-        <h3 className={styles.title}>Filter</h3>
-        <div className={styles.separator}></div>
-        <div className={styles.filters}>
-          <div className={styles.filter}>u</div>
-          <div className={styles.filter}>g</div>
-          <div className={styles.filter}>r</div>
-          <div className={styles.filter}>i</div>
-          <div className={styles.filter}>z</div>
-          <div className={styles.filter}>y</div>
-        </div>
+        {needSwap ?
+          (<div>
+            <h3 className={styles.title}>Filter</h3>
+            <div className={styles.separator}></div>
+            <div className={styles.filters}>
+              <div className={styles.filter}>u</div>
+              <div className={styles.filter}>g</div>
+              <div className={styles.filter}>r</div>
+              <div className={styles.filter}>i</div>
+              <div className={styles.filter}>z</div>
+              <div className={styles.filter}>y</div>
+            </div>
+          </div>) :
+          (<div>
+            <div><span>Filter</span> Swap Needed</div>
+            <div></div>
+          </div>)}
       </div>
     );
   }
