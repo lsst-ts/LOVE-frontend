b65f73168a7688a7ca8cae1546ef53ce29fa5147
Fernando del Campo <fdelcampo@gmail.com>
2023-05-23 12:08:59 -0400

    change slider to function and add react.memo for the not repeat render

--- love/src/components/GeneralPurpose/Slider/Slider.jsx
+++ love/src/components/GeneralPurpose/Slider/Slider.jsx
@@ -1,67 +1,73 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Slider.module.css';
 /**
  * Generic slider input implemented on top of the `<Slider>` tag
  */
-export default class Slider extends Component {
-  static propTypes = {
-    /**
-     * Function for the onChange event of the `<input>` element. The actual value, of the event target, is passed as argument.
-     */
-    onChange: PropTypes.func,
-    /**
-     * Aditional names of css classes to be applied after the presets.
-     */
-    className: PropTypes.string,
-    /**
-     * Define wether or not the button is disabled. If it's True, you can't interact with the input, and the value is the min.
-     */
-    disabled: PropTypes.bool,
-    /**
-     * Define the initial value of the range.
-     */
-    value: PropTypes.number,
-    /**
-     * Define the min value of the range.
-     */
-    min: PropTypes.number,
-    /**
-     * Define the max value of the range.
-     */
-    max: PropTypes.number,
-    /**
-     * Define the step of the range.
-     */
-    step: PropTypes.number,
-  };
-
-  static defaultProps = {
-    onChange: (value) => {},
-    className: '',
-    disabled: false,
-    min: 0,
-    max: 1,
-    step: 0.1,
-  };
-
-  disabledFunct = (target) => {
-    target.value = 0;
-  };
-
-  render() {
-    const { min, max, value, step, className, onChange, disabled } = this.props;
-    return (
-      <input
-        onChange={(event) => (disabled ? this.disabledFunct(event.target) : onChange(event.target.value))}
-        className={[className, styles.input, disabled ? styles.opacity : ''].join(' ')}
-        type="range"
-        id="inputRange"
-        min={min}
-        max={max}
-        step={step}
-        value={disabled ? min : value}
-      />
-    );
-  }
+function Slider(props) {
+  const { min, max, value, step, className, onChange, disabled } = props;
+  return (
+    <input
+      onChange={(event) => (disabled ? this.disabledFunct(event.target) : onChange(event.target.value))}
+      className={[className, styles.input, disabled ? styles.opacity : ''].join(' ')}
+      type="range"
+      id="inputRange"
+      min={min}
+      max={max}
+      step={step}
+      value={disabled ? min : value}
+    />
+  );
 }
+
+const comparator = (prevProps, nextProps) => {
+  return (
+    nextProps.min === prevProps.min &&
+    nextProps.max === prevProps.max &&
+    nextProps.value === prevProps.value &&
+    nextProps.disabled === prevProps.disabled &&
+    nextProps.onChange === prevProps.onChange
+  );
+}
+
+Slider.propTypes = {
+  /**
+    * Function for the onChange event of the `<input>` element. The actual value, of the event target, is passed as argument.
+    */
+   onChange: PropTypes.func,
+   /**
+    * Aditional names of css classes to be applied after the presets.
+    */
+   className: PropTypes.string,
+   /**
+    * Define wether or not the button is disabled. If it's True, you can't interact with the input, and the value is the min.
+    */
+   disabled: PropTypes.bool,
+   /**
+    * Define the initial value of the range.
+    */
+   value: PropTypes.number,
+   /**
+    * Define the min value of the range.
+    */
+   min: PropTypes.number,
+   /**
+    * Define the max value of the range.
+    */
+   max: PropTypes.number,
+   /**
+    * Define the step of the range.
+    */
+   step: PropTypes.number,
+};
+
+Slider.defaultProps = {
+  onChange: (value) => {},
+  className: '',
+  disabled: false,
+  min: 0,
+  max: 1,
+  step: 0.1,
+}
+
+export default React.memo(Slider, comparator);
