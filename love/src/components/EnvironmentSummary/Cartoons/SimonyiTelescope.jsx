7855204144de308dad1eb5f5931fe413c44cbad8
Sebastian Aranda <aranda.sebastian@gmail.com>
2023-07-24 15:03:17 -0400

    Comment platform lift section until the CSC is available

--- love/src/components/EnvironmentSummary/Cartoons/SimonyiTelescope.jsx
+++ love/src/components/EnvironmentSummary/Cartoons/SimonyiTelescope.jsx
@@ -94,15 +94,15 @@ function SimonyiTelescope({ className, ...props }) {
           points="33.94 176.07 274.72 176.07 274.72 200.89 385.54 200.89 396.18 230.26 581.34 230.28 701.87 309.3 692.77 309.14 692.8 314.24 694.51 314.24 720.11 346.48 712.52 346.83 712.52 351.39 714.5 351.53 725.27 380.34 712.97 380.34 712.97 385.81 716.87 385.81 714.51 430.74 712.97 430.74 712.97 435.7 1.05 435.7 1 300.02 41.16 300.02 41.32 209.26 33.94 208.77 33.94 176.07"
         />
       </g>
-      <g id="LiftSchute">
+      {/* <g id="LiftSchute">
         <rect className={styles.cls1} x="286.38" y="175.7" width="98.48" height="200" />
         <line className={styles.cls7} x1="287.38" y1="335.7" x2="383.85" y2="335.7" />
         <line className={styles.cls7} x1="287.38" y1="295.7" x2="383.85" y2="295.7" />
         <line className={styles.cls7} x1="287.38" y1="255.7" x2="383.85" y2="255.7" />
         <polyline className={styles.cls7} points="286.38 174.7 286.38 135.7 384.85 135.7 384.85 174.7" />
         <line className={styles.cls7} x1="287.38" y1="215.7" x2="383.85" y2="215.7" />
-      </g>
-      <g id="Floors" className={styles.cls10}>
+      </g> */}
+      {/* <g id="Floors" className={styles.cls10}>
         <text className={styles.cls9} transform="translate(326.84 362.67)">
           <tspan x="0" y="0">
             3
@@ -133,15 +133,15 @@ function SimonyiTelescope({ className, ...props }) {
             8
           </tspan>
         </text>
-      </g>
-      <g id="Platform">
+      </g> */}
+      {/* <g id="Platform">
         <polygon
           className={styles.cls2}
           points="375.61 270.98 375.61 280.98 365.61 290.98 305.61 290.98 295.61 280.98 295.61 270.98 375.61 270.98"
         />
         <rect className={styles.cls3} x="300.62" y="260.98" width="70" height="10" />
         <path className={styles.cls5} d="m358.12,260.98c0,3.31-10.07,6-22.5,6s-22.5-2.69-22.5-6" />
-      </g>
+      </g> */}
       {showSimonyiSummary && (
         <foreignObject className={styles.simonyiHover}>
           <div className={styles.simonyiSummary}>
