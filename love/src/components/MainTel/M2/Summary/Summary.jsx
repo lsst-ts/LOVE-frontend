86379708127932ea48b775866e7890c16649ac60
Mia <MiaRoseElbo@gmail.com>
2023-07-27 12:05:00 -0400

    set significant figures to 2 decimals

--- love/src/components/MainTel/M2/Summary/Summary.jsx
+++ love/src/components/MainTel/M2/Summary/Summary.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
+import { summaryStateMap, summaryStateToStyle } from 'Config';
 import SummaryPanel from 'components/GeneralPurpose/SummaryPanel/SummaryPanel';
 import Title from 'components/GeneralPurpose/SummaryPanel/Title';
-import CSCDetail from 'components/CSCSummary/CSCDetail/CSCDetail';
-import CSCDetailStyles from 'components/CSCSummary/CSCDetail/CSCDetail.module.css';
+import StatusText from 'components/GeneralPurpose/StatusText/StatusText';
 
 import styles from './Summary.module.css';
 
@@ -30,44 +29,39 @@ export default class Summary extends Component {
   };
 
   render() {
-    const summaryStateValue = CSCDetail.states[this.props.summaryState ? this.props.summaryState : 0];
+    const summaryStateName = summaryStateMap[this.props.summaryState];
+    const summaryStateStatus = summaryStateToStyle[summaryStateName];
+
     const commandableByDDSValue = {
       name: this.props.commandableByDDS ? 'CSC' : 'EUI',
-      class: CSCDetailStyles[this.props.commandableByDDS ? 'ok' : 'warning'],
+      class: this.props.commandableByDDS ? 'ok' : 'warning',
     };
     const forceBalanceSystemStatusValue = {
       name: this.props.forceBalanceSystemStatus ? 'ENABLED' : 'DISABLED',
-      class: CSCDetailStyles[this.props.forceBalanceSystemStatus ? 'ok' : 'warning'],
+      class: this.props.forceBalanceSystemStatus ? 'ok' : 'warning',
     };
     const m2AssemblyInPositionValue = {
       name: this.props.m2AssemblyInPosition ? 'InPosition' : 'NotInPosition',
-      class: CSCDetailStyles[this.props.m2AssemblyInPosition ? 'ok' : 'warning'],
+      class: this.props.m2AssemblyInPosition ? 'ok' : 'warning',
     };
-
     return (
       <div className={styles.summaryContainer}>
         <SummaryPanel className={styles.summaryPanelStates}>
           <div className={styles.state}>
             <Title>M2 Status</Title>
-            <span className={[summaryStateValue?.class, styles.summaryState].join(' ')}>{summaryStateValue?.name}</span>
+            <StatusText status={summaryStateStatus}>{summaryStateName}</StatusText>
           </div>
           <div className={styles.state}>
             <Title>Command</Title>
-            <span className={[commandableByDDSValue.class, styles.summaryState].join(' ')}>
-              {commandableByDDSValue.name}
-            </span>
+            <StatusText status={commandableByDDSValue?.class}>{commandableByDDSValue?.name}</StatusText>
           </div>
           <div className={styles.state}>
             <Title>M2 Assembly</Title>
-            <span className={[m2AssemblyInPositionValue.class, styles.summaryState].join(' ')}>
-              {m2AssemblyInPositionValue.name}
-            </span>
+            <StatusText status={m2AssemblyInPositionValue?.class}>{m2AssemblyInPositionValue?.name}</StatusText>
           </div>
           <div className={styles.state}>
             <Title>Force Balance</Title>
-            <span className={[forceBalanceSystemStatusValue.class, styles.summaryState].join(' ')}>
-              {forceBalanceSystemStatusValue.name}
-            </span>
+            <StatusText status={forceBalanceSystemStatusValue?.class}>{forceBalanceSystemStatusValue?.name}</StatusText>
           </div>
         </SummaryPanel>
       </div>
