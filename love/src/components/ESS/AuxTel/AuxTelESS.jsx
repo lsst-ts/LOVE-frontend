fc2ccffc80aee51f9aa6baedd80c43b9882dce22
Fernando del Campo <fdelcampo@gmail.com>
2023-08-02 12:55:33 -0400

    add initial auxtel component of ESS

--- love/src/components/ESS/AuxTel/AuxTelESS.jsx
+++ love/src/components/ESS/AuxTel/AuxTelESS.jsx
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import isEqual from 'lodash/isEqual';
+import * as d3 from 'd3';
+import Scene from './Scene/Scene';
+import Info from '../Common/Info/Info';
+import Gradiant from '../Common/Gradiant/Gradiant';
+
+import PlotContainer from 'components/GeneralPurpose/Plot/Plot.container';
 import styles from './AuxTelESS.module.css';
 
 export default class AuxTelESS extends Component {
@@ -8,23 +15,294 @@ export default class AuxTelESS extends Component {
     subscribeToStreams: PropTypes.func,
     /** Function to unsubscribe to streams to stop receiving */
     unsubscribeToStreams: PropTypes.func,
+    temperature: PropTypes.arrayOf(PropTypes.shape({
+      sensorName: PropTypes.string,
+      value: PropTypes.number,
+      indexArr: PropTypes.number,
+      location: PropTypes.string,
+      numChannels: PropTypes.number,
+      xPosition: PropTypes.number,
+      yPosition: PropTypes.number,
+      zPosition: PropTypes.number,
+    })),
+    relativeHumidity: PropTypes.arrayOf(PropTypes.shape({
+      sensorName: PropTypes.string,
+      value: PropTypes.number,
+      location: PropTypes.string,
+      xPosition: PropTypes.number,
+      yPosition: PropTypes.number,
+      zPosition: PropTypes.number,
+    })),
+    airFlow: PropTypes.arrayOf(PropTypes.shape({
+      sensorName: PropTypes.string,
+      value: PropTypes.number,
+      direction: PropTypes.number,
+      location: PropTypes.string,
+      xPosition: PropTypes.number,
+      yPosition: PropTypes.number,
+      zPosition: PropTypes.number,
+    })),
+    airTurbulence: PropTypes.arrayOf(PropTypes.shape({
+      sensorName: PropTypes.string,
+      value: PropTypes.number,
+      location: PropTypes.string,
+      xPosition: PropTypes.number,
+      yPosition: PropTypes.number,
+      zPosition: PropTypes.number,
+    })),
+    minGradiantLimit: PropTypes.number,
+    maxGradiantLimit: PropTypes.number,
+    option: PropTypes.oneOf(['temperature', 'relativeHumidity', 'airFlow', 'airTurbulence']),
   };
 
-  static defaultProps = {};
+  static defaultProps = {
+    temperature: [{
+      sensorName: '',
+      value: 0,
+      indexArr: 0,
+      numChannels: 0,
+      xPosition: 0,
+      yPosition: 0,
+      zPosition: 0,
+    }],
+    relativeHumidity: [{
+      sensorName: '',
+      value: 0,
+      xPosition: 0,
+      yPosition: 0,
+      zPosition: 0,
+    }],
+    airFlow: [{
+      sensorName: '',
+      value: 0,
+      direction: 0,
+      xPosition: 0,
+      yPosition: 0,
+      zPosition: 0,
+    }],
+    airTurbulence: [{
+      sensorName: '',
+      value: 0,
+      speed: {x: 0, y:0, z: 0},
+      xPosition: 0,
+      yPosition: 0,
+      zPosition: 0,
+    }],
+    minGradiantLimit: -20,
+    maxGradiantLimit: 40,
+    option: 'temperature',
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedSensor: 0,
+      selectedSensorData: {},
+      positions: [],
+      referenceIds: Array.from({length: 16}).fill(0).map((_, index) => index + 1),
+      plot: this.getBasePlot(undefined, undefined, undefined, undefined),
+      louversIds: Array.from({length: 34}).fill(0).map((_, index) => index + 1),
+    };
+    this.plotRef = React.createRef();
+    this.items = {
+      'temperature': 'temperature',
+      'relativeHumidity': 'relativeHumidity',
+      'airFlow': 'speed',
+      'airTurbulence' : 'speedMagnitude',
+    };
+  }
+
+  getBasePlot(salindex, topic, item, indexArr=undefined) {
+    const config = {};
+    const accessor = indexArr !== undefined ? `(x) => x[${indexArr}]` : '(x) => x';
+    config[`${topic}`] = {
+      'type': 'line',
+      // 'color': '#ff7f0e',
+      'dash': [
+        4,
+        0
+      ],
+      'values': [
+        {
+          'variable': 'x',
+          'category': 'telemetry',
+          'csc': 'ESS',
+          'salindex': salindex,
+          'topic': topic,
+          'item': 'timestamp',
+          'accessor': accessor,
+        },
+        {
+          'variable': 'y',
+          'category': 'telemetry',
+          'csc': 'ESS',
+          'salindex': salindex,
+          'topic': topic,
+          'item': item,
+          'accessor': accessor,
+        }
+      ]
+    };
+    return config;
+  }
 
   componentDidMount() {
     this.props.subscribeToStreams();
+    const positions = [];
+    const option =  this.props.option ?? 'temperature';
+    for(let i = 0; i < this.props[option].length; i++) {
+      positions.push({
+        x: this.props[option][i].xPosition,
+        y: this.props[option][i].yPosition,
+        z: this.props[option][i].zPosition,
+      });
+    }
+
+    const referenceIds = Array.from({length: this.props[option].length}).fill(0).map((_, index) => index + 1);
+    const louversIds = Array.from({length: 34}).fill(0).map((_, index) => index + 1);
+
+    this.setState({
+      positions: positions,
+      referenceIds: referenceIds,
+      louversIds: louversIds,
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const option =  this.props.option ?? 'temperature';
+
+    if (
+      !isEqual(prevProps[option], this.props[option])
+    ) {
+      const referenceIds = Array.from({length: this.props[option].length}).fill(0).map((_, index) => index + 1);
+      const positions = [];
+      for(let i = 0; i < this.props[option].length; i++) {
+        positions.push({
+          x: this.props[option][i].xPosition,
+          y: this.props[option][i].yPosition,
+          z: this.props[option][i].zPosition,
+        });
+      }
+      this.setState({
+        positions: positions,
+        referenceIds: referenceIds,
+      });
+    }
+
+    if (prevState.selectedSensor !== this.state.selectedSensor ||
+        prevProps.option !== this.props.option
+    ) {
+      const telemetry = this.state.selectedSensorData?.telemetry ?? 'telemetry-ESS-1-temperature';
+      const indexArr = this.state.selectedSensorData?.indexArr !== undefined ? this.state.selectedSensorData?.indexArr : undefined;
+      const [_category, _csc, salindex, _topic] = telemetry.split('-');
+
+      const topic = this.props.option;
+      const item = this.items[option] ?? option;
+      const inputsPlot = this.getBasePlot(salindex, topic, item, indexArr);
+      this.setState({plot: inputsPlot});
+    }
   }
 
   componentWillUnmount() {
     this.props.unsubscribeToStreams();
   }
 
-  render() {
-    return (
-      <>
-        <div>AuxTel</div>
-      </>
-    );
+  getGradiantColorX = (value) => {
+    const { minGradiantLimit, maxGradiantLimit } = this.props;
+    const colorInterpolate = d3
+      .scaleLinear()
+      .domain(d3.extent([minGradiantLimit, maxGradiantLimit]))
+      .range([0, 1]);
+    return Gradiant.COLOR_SCALE(1 - colorInterpolate(value));
+  };
+
+  setSensor( sensorId ) {
+    const { referenceIds } = this.state;
+    const option =  this.props.option ?? 'temperature';
+    const index = referenceIds.indexOf(sensorId);
+
+    const selectedSensorData = {
+      sensorId: sensorId,
+      telemetry: this.props[option][index].telemetry,
+      sensorName: this.props[option][index].sensorName,
+      numChannels: this.props[option][index].numChannels,
+      indexArr: this.props[option][index].indexArr,
+      value: this.props[option][index].value,
+      speed: this.props[option][index].speed,
+      direction: this.props[option][index].direction,
+      location: this.props[option][index].location,
+      position: {
+        x: this.props[option][index].xPosition,
+        y: this.props[option][index].yPosition,
+        z: this.props[option][index].zPosition,
+      },
+    };
+    this.setState({
+      selectedSensor: sensorId,
+      selectedSensorData: selectedSensorData,
+    });
   }
+
+  render() {
+    const { selectedSensor, selectedSensorData, positions, referenceIds, louversIds } = this.state;
+    const { minGradiantLimit, maxGradiantLimit, option, percentOpenLouvers, percentOpenShutter } = this.props;
+    const sensors = this.props[option] ?? [];
+    const values = sensors.map((sensor) => sensor.value ?? 0) ?? [];
+    const speeds = sensors.map((sensor) => sensor.speed ?? undefined) ?? [];
+    const directions = sensors.map((sensor) => sensor.direction ?? undefined) ?? [];
+    
+    return (
+      <div className={styles.sceneAndInfoPlotsContainer}>
+
+        <div className={styles.sceneContainer}>
+          <Scene
+            positions={positions}
+            selectedSensor={selectedSensor}
+            setSensor={(id) => this.setSensor(id)}
+            values={values}
+            speeds={speeds}
+            directions={directions}
+            getGradiantColorX={this.getGradiantColorX}
+            louversIds={louversIds}
+            percentOpenLouvers={percentOpenLouvers}
+            percentOpenShutter={percentOpenShutter}
+          />
+        </div>
+
+        <div className={styles.infoAndPlotsContainer}>
+          <div className={styles.infoContainer}>
+            <Info 
+              sensor={selectedSensorData}
+            /> 
+          </div>
+
+          <div className={styles.tempContainer}>
+            <Gradiant
+              sensorReferenceId={referenceIds}
+              selectedId={selectedSensor}
+              setpoint={selectedSensorData?.value}
+              absoluteGradiant={values}
+              minGradiantLimit={minGradiantLimit}
+              maxGradiantLimit={maxGradiantLimit}
+              option={option}
+            />
+          </div>
+
+          <div className={styles.plotsContainer}>
+            <div className={styles.title}>Plot</div>
+            <div className={styles.plots} ref={this.plotRef} >
+              <PlotContainer
+                containerNode={this.plotRef?.current}
+                xAxisTitle="Time"
+                legendPosition="bottom"
+                inputs={this.state.plot}
+              />
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    );
+  } 
 }
---
+++ love/src/components/ESS/AuxTel/Scene/Scene.jsx
@@ -0,0 +1,124 @@
+import React, { Suspense, useRef, useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Canvas, useThree } from '@react-three/fiber';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import * as THREE from "three";
+import { Sensors } from '../../Common/Sensors';
+import { Dome } from './Dome';
+import { isEqual } from 'lodash';
+import { Shutter } from './Shutter';
+
+const INITIAL_CAMERA_POSITION = [14.8, 24.5, 12];
+const INITIAL_TARGET = [0, 10, 0];
+
+function CameraController() {
+  const { camera, gl } = useThree();
+  useEffect(
+     () => {
+        const controls = new OrbitControls(camera, gl.domElement);
+        controls.minDistance = 3;
+        controls.maxDistance = 35;
+        controls.target = new THREE.Vector3(INITIAL_TARGET[0], INITIAL_TARGET[1], INITIAL_TARGET[2]);
+        controls.update();
+        return () => {
+          controls.dispose();
+        };
+     },
+     [camera, gl]
+  );
+  return null;
+};
+
+const Scene = (props) => {
+
+  const {
+    positions,
+    selectedSensor,
+    setSensor,
+    values,
+    speeds,
+    directions,
+    getGradiantColorX,
+    percentOpenShutter
+  } = props;
+
+  return (
+    <>
+    <Canvas
+      camera={{
+        position: INITIAL_CAMERA_POSITION,
+      }}
+    >
+      <Suspense fallback={<div>loading</div>}>
+      <CameraController />
+      <ambientLight intensity={0.8} />
+      <directionalLight position={[-20, 20, 0]} intensity={1} />
+      <axesHelper args={[25]} />
+      <gridHelper args={[17*2, 17]}/>
+
+      <Dome />
+
+      <Shutter
+        name={'shutter 0'}
+        position={{x: 0, y: -3.3, z: 7}}
+        openPercent={percentOpenShutter[0] ?? 100}
+      />
+
+      <Shutter
+        name={'shutter 1'}
+        position={{x: 0, y: 3.3, z: 7}}
+        openPercent={percentOpenShutter[1] ?? 100}
+      />
+
+      <Sensors 
+        selectedSensor={selectedSensor}
+        setSensor={setSensor}
+        positions={positions}
+        values={values}
+        speeds={speeds}
+        directions={directions}
+        getGradiantColorX={getGradiantColorX}
+      />
+
+      </Suspense>
+    </Canvas>
+    </>
+  );
+}
+
+Scene.propTypes = {
+  selectedSensor: PropTypes.number,
+  positions: PropTypes.arrayOf(PropTypes.shape({
+    x: PropTypes.number,
+    y: PropTypes.number,
+    z: PropTypes.number,
+  })),
+  setSensor: PropTypes.func,
+  values: PropTypes.arrayOf(PropTypes.number),
+  speeds: PropTypes.oneOf([undefined, PropTypes.shape({
+    x: PropTypes.number,
+    y: PropTypes.number,
+    z: PropTypes.number,
+  })]),
+  directions: PropTypes.arrayOf(PropTypes.number),
+  getGradiantColorX: PropTypes.func,
+  louversIds: PropTypes.arrayOf(PropTypes.number),
+  percentOpen: PropTypes.arrayOf(PropTypes.number),
+};
+
+Scene.defaultProps = {
+  selectedSensor: 0,
+  positions: [],
+  percentOpen: [],
+  setSensor: () => {console.log('scene default setSensor')},
+}
+
+const comparator = (prevProps, nextProps) => {
+  return (
+    prevProps.selectedSensor === nextProps.selectedSensor &&
+    isEqual(prevProps.percentOpen, nextProps.percentOpen ) &&
+    isEqual(prevProps.positions, nextProps.positions )
+  )
+}
+
+export default React.memo(Scene, comparator);
