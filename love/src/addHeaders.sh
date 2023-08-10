27824f53aa4f5c47df70a5ac286fa138a1cf8251
Astrid Reyes <astridreyes@MacBook-Pro-de-Astrid-1103.local>
2023-08-10 18:19:54 -0400

    Replace licence header

--- love/src/addHeaders.sh
+++ love/src/addHeaders.sh
@@ -4,5 +4,6 @@ FILES=$(<files.txt)
 for f in $FILES
 do
     echo "Processing $f file..."
+    sed -i '' '1,19d' $f
     (cat header-frontend.txt; echo -e "\n\n"; cat $f) > tmpfile && mv tmpfile $f
 done
