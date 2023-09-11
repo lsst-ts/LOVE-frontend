9c3716657967f234612d7e918c373c73ca7629ee
Astrid Reyes <astridreyes@MacBook-Pro-de-Astrid-1103.local>
2023-09-11 15:34:42 -0300

    Added license header for missing files

--- love/styleguide.config.js
+++ love/styleguide.config.js
@@ -1,3 +1,22 @@
+/** 
+This file is part of LOVE-frontend.
+
+Copyright (c) 2023 Inria Chile.
+
+Developed by Inria Chile.
+
+This program is free software: you can redistribute it and/or modify it under 
+the terms of the GNU General Public License as published by the Free Software 
+Foundation, either version 3 of the License, or at your option) any later version.
+
+This program is distributed in the hope that it will be useful,but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+ A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 
+this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 const path = require('path');
 const webpack = require('webpack');
 
@@ -129,21 +148,21 @@ module.exports = {
   ],
   dangerouslyUpdateWebpackConfig(config) {
     config.module.rules.push({
-       test: /.\.md$/,
-       type: "javascript/auto"
-     });
-     config.plugins.push(
-       new webpack.NormalModuleReplacementPlugin(
-         /react-styleguidist\/lib\/loaders\/utils\/client\/requireInRuntime$/,
-         "react-styleguidist/lib/loaders/utils/client/requireInRuntime"
-       )
-     );
-     config.plugins.push(
-       new webpack.NormalModuleReplacementPlugin(
-         /react-styleguidist\/lib\/loaders\/utils\/client\/evalInContext$/,
-         "react-styleguidist/lib/loaders/utils/client/evalInContext"
-       )
-     );
-     return config;
-   },
+      test: /.\.md$/,
+      type: 'javascript/auto',
+    });
+    config.plugins.push(
+      new webpack.NormalModuleReplacementPlugin(
+        /react-styleguidist\/lib\/loaders\/utils\/client\/requireInRuntime$/,
+        'react-styleguidist/lib/loaders/utils/client/requireInRuntime',
+      ),
+    );
+    config.plugins.push(
+      new webpack.NormalModuleReplacementPlugin(
+        /react-styleguidist\/lib\/loaders\/utils\/client\/evalInContext$/,
+        'react-styleguidist/lib/loaders/utils/client/evalInContext',
+      ),
+    );
+    return config;
+  },
 };
