528d11abad894883301a59471dc2b7fa4092df8b
Astrid Reyes <astridreyes@MacBook-Pro-de-Astrid-1103.local>
2023-09-26 11:54:54 -0300

    Add missing import removed after rebase

--- love/src/components/GeneralPurpose/Input/Input.jsx
+++ love/src/components/GeneralPurpose/Input/Input.jsx
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License along with
 this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import React from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './Input.module.css';
