18cd30fab525e98a0948ee7f9022ce5f6d8c885a
Sebastian Aranda Sanchez <aranda.sebastian@gmail.com>
2023-10-19 13:41:28 -0300

    Add tests for `htmlToJiraMarkdown`

--- love/src/Utils.test.js
+++ love/src/Utils.test.js
@@ -1,4 +1,26 @@
-import { jiraMarkdownToHtml } from './Utils';
+import { htmlToJiraMarkdown, jiraMarkdownToHtml } from './Utils';
+
+describe('htmlToJiraMarkdown', () => {
+  it('should handle links', () => {
+    const input = '<p>This is a <a href="https://example.com" rel="noopener noreferrer" target="_blank">link</a>.</p>';
+    const expectedOutput = 'This is a [link|https://example.com].\r\n';
+    expect(htmlToJiraMarkdown(input)).toEqual(expectedOutput);
+  });
+
+  it('should handle headings', () => {
+    const input = '<h1>This is a heading.</h1>';
+    const expectedOutput = 'h1. This is a heading.\r\n';
+    expect(htmlToJiraMarkdown(input)).toEqual(expectedOutput);
+  });
+
+  it('should convert HTML to Jira markdown', () => {
+    const input =
+      '<p>This is a string with mixed content</p><h1>This is a heading.</h1><p>This is a <a href="http://google.com/" rel="noopener noreferrer" target="_blank">link</a>.</p>';
+    const expectedOutput =
+      'This is a string with mixed content\r\nh1. This is a heading.\r\nThis is a [link|http://google.com/].\r\n';
+    expect(htmlToJiraMarkdown(input)).toEqual(expectedOutput);
+  });
+});
 
 describe('jiraMarkdownToHtml', () => {
   it('should handle links', () => {
