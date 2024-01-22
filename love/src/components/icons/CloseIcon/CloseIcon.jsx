cbabe5f9fdc5456444c68e272262c9e08b0cb70b
Sebastian Aranda Sanchez <aranda.sebastian@gmail.com>
2024-01-22 12:36:50 -0300

    Add title props to CloseIcon

--- love/src/components/icons/CloseIcon/CloseIcon.jsx
+++ love/src/components/icons/CloseIcon/CloseIcon.jsx
@@ -23,7 +23,7 @@ import styles from './CloseIcon.module.css';
 function CloseIcon(props) {
   return (
     <svg viewBox="0 0 24 24" {...props}>
-      <title>{'Close'}</title>
+      <title>{props.title ?? 'Close'}</title>
       <path
         className={styles.path}
         d="M3.33 22.19A1 1 0 012 20.82l8.26-8.26L2 4.31a1 1 0 010-1.38 1 1 0 011.37 0l8.25 8.25 8.26-8.26a1 1 0 011.37 1.38L13 12.56l8.25 8.26a1 1 0 010 1.37 1 1 0 01-1.38 0l-8.26-8.26z"
