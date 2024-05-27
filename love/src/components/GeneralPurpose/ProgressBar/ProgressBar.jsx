2ba8ffabb70784086a3d9ee928826f5d682b405c
Sebastian Aranda Sanchez <aranda.sebastian@gmail.com>
2024-05-27 15:46:32 -0400

    Refactor `ProgressBar` component:
    Size calculation handling and rounding of completion percentage
    
    Add a `title` props for custom labeling of progress
    
    Add props PropTypes
    
    Add LSST Project to the developers section of the license header

--- love/src/components/GeneralPurpose/ProgressBar/ProgressBar.jsx
+++ love/src/components/GeneralPurpose/ProgressBar/ProgressBar.jsx
@@ -3,7 +3,7 @@ This file is part of LOVE-frontend.
 
 Copyright (c) 2023 Inria Chile.
 
-Developed by Inria Chile.
+Developed by Inria Chile and the LSST Project (https://www.lsst.org).
 
 This program is free software: you can redistribute it and/or modify it under 
 the terms of the GNU General Public License as published by the Free Software 
@@ -17,60 +17,24 @@ You should have received a copy of the GNU General Public License along with
 this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import React, { useRef } from 'react';
+import React, { useState, useCallback } from 'react';
+import PropTypes from 'prop-types';
 import styles from './ProgressBar.module.css';
 
-const ProgressBar = (props) => {
-  const ref = useRef(null);
-  const { targetValue, completed, hideCompleted = false, height = 20 } = props;
+const ProgressBar = ({ targetValue, completed, title, hideCompleted = false, height = 20 }) => {
+  const [width, setWidth] = useState(0);
+
+  const measuredRef = useCallback((node) => {
+    if (node !== null) {
+      setWidth(node.getBoundingClientRect().width);
+    }
+  }, []);
+
   const padding = 4;
-
-  const parentDiv = {
-    display: 'flex',
-  };
-
-  const containerStyles = {
-    height: 20,
-    width: '100%',
-    backgroundColor: '#455a64',
-    marginBottom: '5px',
-    float: 'rigth',
-    position: 'relative',
-    padding: `${padding}px`,
-  };
-
-  const fillerStyles = {
-    height: '100%',
-    width: `${completed}%`,
-    backgroundColor: 'var(--second-tertiary-background-color)',
-    textAlign: 'right',
-  };
-
-  const labelStyles = {
-    padding: 5,
-    color: 'white',
-    fontWeight: 'bold',
-  };
-
-  const targetValueLine = {
-    stroke: 'white',
-    strokeWidth: 2,
-    strokeDasharray: 1.8,
-    strokeOpacity: 0.5,
-  };
-
-  const progressCommandedLine = {
-    position: 'absolute',
-    top: padding,
-    right: padding,
-    bottom: padding,
-    left: padding,
-    overflow: 'visible',
-  };
-
-  const width = ref.current?.clientWidth ?? 0;
-  const targetValuePixels = ((width - 2 * padding) * targetValue) / 100;
+  const targetValuePixels = Math.ceil(((width - 2 * padding) * targetValue) / 100);
   const completedValue = completed.toString().padStart(3, '0');
+
+  const titleHtml = title ?? `${Math.floor(completed)}/100`;
 
   return (
     <div className={styles.parentDiv}>
@@ -81,8 +45,9 @@ const ProgressBar = (props) => {
       ) : (
         <></>
       )}
-      <div ref={ref} className={styles.containerStyles} style={{ height: `${height}px` }}>
+      <div ref={measuredRef} className={styles.containerStyles} style={{ height: `${height}px` }}>
         <svg width={width > 0 ? width - 2 * padding : 0} height={height} className={styles.progressCommandedLine}>
+          <title>{titleHtml}</title>
           {targetValue ? (
             <line
               className={styles.targetValueLine}
@@ -99,6 +64,19 @@ const ProgressBar = (props) => {
       </div>
     </div>
   );
+};
+
+ProgressBar.propTypes = {
+  /** Expected value to reach */
+  targetValue: PropTypes.number.isRequired,
+  /** Value already completed */
+  completed: PropTypes.number.isRequired,
+  /** Title of the progress bar */
+  title: PropTypes.string,
+  /** Whether to hide the completed value label */
+  hideCompleted: PropTypes.bool,
+  /** Height of the progress bar */
+  height: PropTypes.number,
 };
 
 export default ProgressBar;
