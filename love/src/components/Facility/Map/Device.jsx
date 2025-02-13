17078fe7cb26099eaa49d64e9c059e9c02634288
Sebastian Aranda <aranda.sebastian@gmail.com>
2025-02-13 12:23:56 -0300

    Clean Facility/Map/Device component code.

--- love/src/components/Facility/Map/Device.jsx
+++ love/src/components/Facility/Map/Device.jsx
@@ -18,7 +18,7 @@ this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 import React, { Component } from 'react';
-import { fixedFloat } from 'Utils';
+import { defaultNumberFormatter } from 'Utils';
 import lodash from 'lodash';
 import PropTypes from 'prop-types';
 import styles from './Device.module.css';
@@ -88,8 +88,9 @@ export default class Device extends Component {
     }));
   }
 
-  renderStates(ctx) {
-    switch (this.props.states[ctx]) {
+  stateToStyle(state) {
+    const { states } = this.props;
+    switch (states[state]) {
       case true:
         return `${styles.statusOk}`;
       case false:
@@ -99,83 +100,63 @@ export default class Device extends Component {
     }
   }
 
-  statusToStyle(ctx) {
-    switch (ctx) {
+  statusToBackgroundStyle(status) {
+    switch (status) {
       case 'true':
-        return `${styles.bgStatusOk}`;
-      case 'false':
-        return `${styles.bgStatusAlert}`;
-      case 'null':
-        return `${styles.bgStatusNull}`;
       case 'Initialized':
-        return `${styles.bgStatusOk}`;
-      case 'Shutting Down':
-        return `${styles.bgStatusWarning}`;
-      case 'Powering On':
-        return `${styles.bgStatusWarning}`;
       case 'Powered On':
-        return `${styles.bgStatusOk}`;
-      case 'Powering Off':
-        return `${styles.bgStatusWarning}`;
-      case 'Powered Off':
-        return `${styles.bgStatusAlert}`;
-      case 'Warning':
-        return `${styles.bgStatusWarning}`;
-      case 'Alarm':
-        return `${styles.bgStatusAlert}`;
-      case 'Shut Off':
-        return `${styles.bgStatusDisabled}`;
-      case 'Max Speed':
-        return `${styles.bgStatusWarning}`;
-      case 'Min Speed':
-        return `${styles.bgStatusWarning}`;
       case 'Nominal':
         return `${styles.bgStatusOk}`;
+      case 'false':
+      case 'Alarm':
+      case 'Powered Off':
+        return `${styles.bgStatusAlert}`;
+      case 'Shutting Down':
+      case 'Powering On':
+      case 'Powering Off':
+      case 'Warning':
+      case 'Max Speed':
+      case 'Min Speed':
+        return `${styles.bgStatusWarning}`;
+      case 'Shut Off':
+        return `${styles.bgStatusDisabled}`;
       default:
         return `${styles.bgStatusNull}`;
     }
   }
 
-  statusTextToStyle(ctx) {
-    switch (ctx) {
+  statusToTextStyle(status) {
+    switch (status) {
       case 'true':
-        return `${styles.statusOk}`;
-      case 'false':
-        return `${styles.statusAlert}`;
       case 'Initialized':
-        return `${styles.statusOk}`;
-      case 'Shutting Down':
-        return `${styles.statusWarning}`;
-      case 'Powering On':
-        return `${styles.statusWarning}`;
       case 'Powered On':
-        return `${styles.statusOk}`;
-      case 'Powering Off':
-        return `${styles.statusWarning}`;
-      case 'Powered Off':
-        return `${styles.statusAlert}`;
-      case 'Warning':
-        return `${styles.statusWarning}`;
-      case 'Alarm':
-        return `${styles.statusAlert}`;
-      case 'Shut Off':
-        return `${styles.statusDisabled}`;
-      case 'Max Speed':
-        return `${styles.statusWarning}`;
-      case 'Min Speed':
-        return `${styles.statusWarning}`;
       case 'Nominal':
         return `${styles.statusOk}`;
+      case 'false':
+      case 'Alarm':
+      case 'Powered Off':
+        return `${styles.statusAlert}`;
+      case 'Shutting Down':
+      case 'Powering On':
+      case 'Powering Off':
+      case 'Warning':
+      case 'Max Speed':
+      case 'Min Speed':
+        return `${styles.statusWarning}`;
+      case 'Shut Off':
+        return `${styles.statusDisabled}`;
       default:
         return `${styles.statusNull}`;
     }
   }
 
   renderArrow() {
+    const { width } = this.props;
+    const { hidden } = this.state;
     return (
       <g
-        id={this.id}
-        transform={'translate(' + (this.props.width - 14) + ' 4)'}
+        // id={this.id}
+        transform={'translate(' + (width - 14) + ' 4)'}
         pointerEvents="all"
         onClick={(e) => this.toggle()}
       >
@@ -183,17 +164,16 @@ export default class Device extends Component {
         <polyline
           className={styles.arrow}
           points=".35 .35 4.35 4.35 8.35 .35"
-          transform={'translate(0.5 3) rotate(' + (this.state.hidden ? -90 : 0) + ')'}
+          transform={'translate(0.5 3) rotate(' + (hidden ? -90 : 0) + ')'}
           transform-origin="4 2"
         />
       </g>
     );
   }
 
-  // This function renders the badge style alarm norifications on devices.
   renderAlarms() {
     const { height, width, alarms } = this.props;
-    const hidden = this.state.hidden;
+    const { hidden } = this.state;
 
     const alarmsKeys = Object.keys(alarms);
     const rowLength = Math.round(width / 42 - 1);
@@ -228,7 +208,6 @@ export default class Device extends Component {
     });
   }
 
-  //This function renders the Decive parameters. This is the detailed information that is collapsible
   renderParams() {
     const { width, parameters } = this.props;
     const parametersKeys = Object.keys(parameters);
@@ -236,7 +215,7 @@ export default class Device extends Component {
     const gutter = 4;
     const rowHeight = 10;
 
-    let returnContent, groupBox, groupData, length, transX;
+    let groupBox, groupData, length, transX;
 
     //Vars i and j are used to translate parameters down each time a new parameter is rendered.
     let i = 0;
@@ -273,8 +252,9 @@ export default class Device extends Component {
           </g>
         );
       }
+
       //Status Parameters are styled as Summary States.
-      else if (parameters[x].type === 'status') {
+      if (parameters[x].type === 'status') {
         i = j;
         j = j + rowHeight;
         return (
@@ -283,7 +263,7 @@ export default class Device extends Component {
               <tspan>{parameters[x].name}</tspan>
             </text>
             <rect
-              className={this.statusToStyle(String(parameters[x].value))}
+              className={this.statusToBackgroundStyle(String(parameters[x].value))}
               width={30}
               height={8}
               rx={5}
@@ -292,7 +272,7 @@ export default class Device extends Component {
             />
 
             <text
-              className={this.statusTextToStyle(String(parameters[x].value))}
+              className={this.statusToTextStyle(String(parameters[x].value))}
               transform={'translate(' + (width - 19) + ' 0)'}
               textAnchor="middle"
             >
@@ -303,7 +283,7 @@ export default class Device extends Component {
       }
 
       //Single Parameters are those displayed as a single row. They have a name and a value which may have a unit.
-      else if (parameters[x].type === 'single') {
+      if (parameters[x].type === 'single') {
         i = j;
         j = j + rowHeight;
         return (
@@ -312,11 +292,15 @@ export default class Device extends Component {
               <tspan>{parameters[x].name}</tspan>
             </text>
             <text className={styles.paramVal} transform={'translate(' + (width - 19) + ' 0)'} textAnchor="middle">
-              <tspan>{fixedFloat(parameters[x].value, 2) + (parameters[x].unit ? parameters[x].unit : '')}</tspan>
+              <tspan>
+                {defaultNumberFormatter(parameters[x].value, 2) + (parameters[x].unit ? parameters[x].unit : '')}
+              </tspan>
             </text>
           </g>
         );
-      } else if (parameters[x].type === 'text') {
+      }
+
+      if (parameters[x].type === 'text') {
         i = j;
         j = j + rowHeight;
         return (
@@ -333,100 +317,103 @@ export default class Device extends Component {
 
       //Group Parameters are those that are grouped under a specific Title.
       //This title might have a value and units as well.
-      else {
-        i = j;
-        j = j + rowHeight * 3;
+      i = j;
+      j = j + rowHeight * 3;
 
-        //This will go through every parameter inside the parameter group
-        groupBox = Object.keys(parameters[x].params).map((ctx, k) => {
-          //length calculates the distance from the edge to the first element
-          length = (width - 8) / (Object.keys(parameters[x].params).length * 2);
+      //This will go through every parameter inside the parameter group
+      groupBox = Object.keys(parameters[x].params).map((ctx, k) => {
+        //length calculates the distance from the edge to the first element
+        length = (width - 8) / (Object.keys(parameters[x].params).length * 2);
 
-          //trans x calulates the distance between elements
-          transX = 2 * length * k + length;
+        //trans x calulates the distance between elements
+        transX = 2 * length * k + length;
 
-          //Badge type group parameter
-          if (parameters[x].params[ctx].type === 'badge') {
-            return (
-              <React.Fragment>
-                <rect
-                  className={styles.badge}
-                  width={40}
-                  height={10}
-                  rx={5}
-                  ry={5}
-                  transform={'translate(' + (transX - 20) + ' 3)'}
-                />
-                <text className={styles.paramVal} transform={'translate(' + transX + ' 9)'} textAnchor="middle">
-                  <tspan>{fixedFloat(parameters[x].params[ctx].value, 2) + parameters[x].params[ctx].unit}</tspan>
-                </text>
-              </React.Fragment>
-            );
-          }
+        //Badge type group parameter
+        if (parameters[x].params[ctx].type === 'badge') {
+          return (
+            <>
+              <rect
+                className={styles.badge}
+                width={40}
+                height={10}
+                rx={5}
+                ry={5}
+                transform={'translate(' + (transX - 20) + ' 3)'}
+              />
+              <text className={styles.paramVal} transform={'translate(' + transX + ' 9)'} textAnchor="middle">
+                <tspan>
+                  {defaultNumberFormatter(parameters[x].params[ctx].value, 2) + parameters[x].params[ctx].unit}
+                </tspan>
+              </text>
+            </>
+          );
+        }
 
-          //Box type group parameter
-          else if (parameters[x].params[ctx].type === 'box') {
-            return (
-              <React.Fragment>
-                <rect
-                  className={this.statusTextToStyle(String(parameters[x].params[ctx].state))}
-                  width={10}
-                  height={10}
-                  transform={'translate(' + (transX - 5) + ' 0)'}
-                />
-                <text className={styles.paramVal} transform={'translate(' + transX + ' 6)'} textAnchor="middle">
-                  <tspan>{parameters[x].params[ctx].name}</tspan>
-                </text>
-                <text className={styles.paramVal} transform={'translate(' + transX + ' 15)'} textAnchor="middle">
-                  <tspan>
-                    {parameters[x].params[ctx].value
-                      ? fixedFloat(parameters[x].params[ctx].value, 2) + ' ' + parameters[x].params[ctx].unit
-                      : ''}
-                  </tspan>
-                </text>
-              </React.Fragment>
-            );
-          }
+        //Box type group parameter
+        else if (parameters[x].params[ctx].type === 'box') {
+          return (
+            <>
+              <rect
+                className={this.statusToTextStyle(String(parameters[x].params[ctx].state))}
+                width={10}
+                height={10}
+                transform={'translate(' + (transX - 5) + ' 0)'}
+              />
+              <text className={styles.paramVal} transform={'translate(' + transX + ' 6)'} textAnchor="middle">
+                <tspan>{parameters[x].params[ctx].name}</tspan>
+              </text>
+              <text className={styles.paramVal} transform={'translate(' + transX + ' 15)'} textAnchor="middle">
+                <tspan>
+                  {parameters[x].params[ctx].value
+                    ? defaultNumberFormatter(parameters[x].params[ctx].value, 2) + ' ' + parameters[x].params[ctx].unit
+                    : ''}
+                </tspan>
+              </text>
+            </>
+          );
+        }
 
-          //noBox type group parameter
-          else {
-            return (
-              <React.Fragment>
-                <text className={styles.paramVal} transform={'translate(' + transX + ' 7)'} textAnchor="middle">
-                  <tspan>{parameters[x].params[ctx].name}</tspan>
-                </text>
-                <text className={styles.paramVal} transform={'translate(' + transX + ' 13)'} textAnchor="middle">
-                  <tspan>{fixedFloat(parameters[x].params[ctx].value, 2) + parameters[x].params[ctx].unit}</tspan>
-                </text>
-              </React.Fragment>
-            );
-          }
-        });
+        //noBox type group parameter
+        else {
+          return (
+            <>
+              <text className={styles.paramVal} transform={'translate(' + transX + ' 7)'} textAnchor="middle">
+                <tspan>{parameters[x].params[ctx].name}</tspan>
+              </text>
+              <text className={styles.paramVal} transform={'translate(' + transX + ' 13)'} textAnchor="middle">
+                <tspan>
+                  {defaultNumberFormatter(parameters[x].params[ctx].value, 2) + parameters[x].params[ctx].unit}
+                </tspan>
+              </text>
+            </>
+          );
+        }
+      });
 
-        return (
-          <g transform={'translate(0 ' + i + ')'}>
-            <text className={styles.paramGroup} transform={'translate(4 0)'} textAnchor="start">
-              <tspan>{parameters[x].name}</tspan>
-            </text>
-            <text className={styles.paramVal} transform={'translate(' + (width - 4) + ' 0)'} textAnchor="end">
-              <tspan>
-                {(parameters[x].value ? fixedFloat(parameters[x].value, 2) : '') +
-                  (parameters[x].unit ? parameters[x].unit : '')}
-              </tspan>
-            </text>
-            <g transform={'translate(4 4)'}>{groupBox}</g>
-            <g transform={'translate(4 10)'}>{groupData}</g>
-          </g>
-        );
-      }
+      return (
+        <g transform={'translate(0 ' + i + ')'}>
+          <text className={styles.paramGroup} transform={'translate(4 0)'} textAnchor="start">
+            <tspan>{parameters[x].name}</tspan>
+          </text>
+          <text className={styles.paramVal} transform={'translate(' + (width - 4) + ' 0)'} textAnchor="end">
+            <tspan>
+              {(parameters[x].value ? defaultNumberFormatter(parameters[x].value, 2) : '') +
+                (parameters[x].unit ? parameters[x].unit : '')}
+            </tspan>
+          </text>
+          <g transform={'translate(4 4)'}>{groupBox}</g>
+          <g transform={'translate(4 10)'}>{groupData}</g>
+        </g>
+      );
     });
   }
 
   render() {
-    const { title, height, temp, width, scale, posX, posY, collapsible } = this.props;
-    const hidden = this.state.hidden;
-    const isAlarmed = Object.keys(this.props.alarms).some((a) => {
-      return this.props.alarms[a].state;
+    const { title, height, temp, width, scale, posX, posY, collapsible, alarms } = this.props;
+    const { hidden } = this.state;
+
+    const isAlarmed = Object.keys(alarms).some((alarm) => {
+      return alarms[alarm].state;
     });
 
     return (
@@ -436,11 +423,11 @@ export default class Device extends Component {
         transform-origin={width / 2 + ' ' + (height / 2 + 20)}
         transform={'translate(' + posX + ' ' + posY + ') scale(' + scale + ')'}
       >
-        <clipPath id={this.id + 'Mask'}>
+        <clipPath id={this.id}>
           <rect width={width} height={height} transform={'translate(0 30)'} />
         </clipPath>
 
-        <g clipPath={'url(#' + this.id + 'Mask)'}>
+        <g clipPath={'url(#' + this.id + ')'}>
           <g className={styles.insideContent} transform={hidden ? 'translate(0 -' + height + ')' : 'translate(0 0)'}>
             <rect className={styles.container} width={width} height={height} transform={'translate(0 30)'} />
 
@@ -455,16 +442,16 @@ export default class Device extends Component {
               <tspan>{title}</tspan>
             </text>
             <text className={styles.title} transform={'translate(' + (width - 6) + ' 8)'} textAnchor="end">
-              <tspan>{temp ? fixedFloat(temp, 2) + 'ºC' : ''}</tspan>
+              <tspan>{temp ? defaultNumberFormatter(temp, 2) + '°C' : ''}</tspan>
             </text>
           </g>
           <g transform={'translate(0 12)'}>
             <rect className={isAlarmed ? styles.alarmRow2 : styles.titleRow2} width={width} height={18} />
             <g transform={'translate(4 4)'}>
-              <rect className={this.renderStates('command')} width={10} height={10} />
-              <rect className={this.renderStates('working')} width={10} height={10} transform={'translate(12 0)'} />
-              <rect className={this.renderStates('unit')} width={10} height={10} transform={'translate(24 0)'} />
-              <rect className={this.renderStates('switch')} width={10} height={10} transform={'translate(36 0)'} />
+              <rect className={this.stateToStyle('command')} width={10} height={10} />
+              <rect className={this.stateToStyle('working')} width={10} height={10} transform={'translate(12 0)'} />
+              <rect className={this.stateToStyle('unit')} width={10} height={10} transform={'translate(24 0)'} />
+              <rect className={this.stateToStyle('switch')} width={10} height={10} transform={'translate(36 0)'} />
             </g>
 
             {collapsible ? this.renderArrow() : ''}
