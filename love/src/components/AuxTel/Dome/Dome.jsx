5f1ae6f537c744a07d3e3e194b7615ca334be543
Sebastian Aranda <aranda.sebastian@gmail.com>
2025-07-29 18:29:16 -0400

    Remove controls for historical data on the Auxtel/Dome component as now that is being handled directly by the Plot component.

--- love/src/components/AuxTel/Dome/Dome.jsx
+++ love/src/components/AuxTel/Dome/Dome.jsx
@@ -3,7 +3,9 @@ This file is part of LOVE-frontend.
 
 Copyright (c) 2023 Inria Chile.
 
-Developed by Inria Chile.
+Developed by Inria Chile and the Telescope and Site Software team.
+
+Developed for the Vera C. Rubin Observatory Telescope and Site Systems.
 
 This program is free software: you can redistribute it and/or modify it under 
 the terms of the GNU General Public License as published by the Free Software 
@@ -19,9 +21,8 @@ this program. If not, see <http://www.gnu.org/licenses/>.
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import ManagerInterface, { fixedFloat, parseCommanderData, degreesToHMS, degreesToDMS } from 'Utils';
+import { fixedFloat } from 'Utils';
 import PlotContainer from 'components/GeneralPurpose/Plot/Plot.container';
-import TimeSeriesControls from 'components/GeneralPurpose/Plot/TimeSeriesControls/TimeSeriesControls';
 import Elevation from 'components/GeneralPurpose/Elevation/Elevation';
 import Azimuth from 'components/GeneralPurpose/Azimuth/Azimuth';
 import WindRose from 'components/icons/WindRose/WindRose';
@@ -122,14 +123,6 @@ export default class Dome extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      az: 0,
-      el: 0,
-      timeWindow: 60,
-      isLive: true,
-      historicalData: [],
-    };
-
     this.azimuthPlotRef = React.createRef();
     this.elevationPlotRef = React.createRef();
   }
@@ -154,6 +147,8 @@ export default class Dome extends Component {
           salindex: '0',
           topic: 'mount_AzEl_Encoders',
           item: 'elevationCalculatedAngle',
+          isArray: true,
+          arrayIndex: 0,
           accessor: '(x) => x[0]',
         },
       ],
@@ -221,6 +216,8 @@ export default class Dome extends Component {
           salindex: '0',
           topic: 'mount_AzEl_Encoders',
           item: 'azimuthCalculatedAngle',
+          isArray: true,
+          arrayIndex: 0,
           accessor: '(x) => (x[0] < 0 ? x[0] + 360 : x[0])',
         },
       ],
@@ -241,29 +238,6 @@ export default class Dome extends Component {
         },
       ],
     },
-  };
-
-  setHistoricalData = (startDate, timeWindow) => {
-    const cscs = {
-      ATDome: {
-        0: {
-          position: ['azimuthPosition'],
-          azimuthCommandedState: ['azimuth'],
-        },
-      },
-      ATMCS: {
-        0: {
-          mount_AzEl_Encoders: ['azimuthCalculatedAngle', 'elevationCalculatedAngle'],
-          target: ['azimuth', 'elevation'],
-        },
-      },
-    };
-    const parsedDate = startDate.format('YYYY-MM-DDTHH:mm:ss');
-    // historicalData
-    ManagerInterface.getEFDTimeseries(parsedDate, timeWindow, cscs, '1min').then((data) => {
-      const parsedPlotData = parseCommanderData(data, 'x', 'y');
-      this.setState({ historicalData: parsedPlotData });
-    });
   };
 
   render() {
@@ -310,8 +284,6 @@ export default class Dome extends Component {
       raDecHourFormat,
     } = this.props;
 
-    const { timeWindow, isLive, historicalData } = this.state;
-
     const isProjected = true;
     let azDiff = Math.abs(azimuthPosition - currentPointingAz);
     if (azDiff > 180) azDiff = azDiff - 360;
@@ -326,12 +298,6 @@ export default class Dome extends Component {
     const elScaleDomain = {
       domainMax: 90,
       domainMin: 0,
-    };
-
-    const timeSeriesControlsProps = {
-      timeWindow: timeWindow,
-      isLive: isLive,
-      historicalData: historicalData,
     };
 
     return (
@@ -451,17 +417,6 @@ export default class Dome extends Component {
             raDecHourFormat={raDecHourFormat}
           />
         </div>
-        {this.props.controls && (
-          <div>
-            <TimeSeriesControls
-              setTimeWindow={(timeWindow) => this.setState({ timeWindow })}
-              timeWindow={this.state.timeWindow}
-              setLiveMode={(isLive) => this.setState({ isLive })}
-              isLive={this.state.isLive}
-              setHistoricalData={this.setHistoricalData}
-            />
-          </div>
-        )}
         <div className={styles.telemetryTable}>
           <div className={styles.azimuthSection}>
             <h2>Azimuth</h2>
@@ -470,12 +425,10 @@ export default class Dome extends Component {
                 inputs={this.azimuthPlotInputs}
                 containerNode={this.azimuthPlotRef}
                 xAxisTitle="Time"
-                timeSeriesControlsProps={timeSeriesControlsProps}
                 scaleDomain={azScaleDomain}
               />
             </div>
           </div>
-
           <div className={styles.elevationSection}>
             <h2>Elevation</h2>
             <div ref={this.elevationPlotRef} className={styles.elevationPlot}>
@@ -484,7 +437,6 @@ export default class Dome extends Component {
                 containerNode={this.elevationPlotRef}
                 xAxisTitle="Time"
                 yAxisTitle="Elevation"
-                timeSeriesControlsProps={timeSeriesControlsProps}
                 scaleDomain={elScaleDomain}
               />
             </div>
