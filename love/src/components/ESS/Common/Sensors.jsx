5db5c5ad997dce73b65f2e75395238c78ff76bbe
Sebastian Aranda Sanchez <aranda.sebastian@gmail.com>
2026-01-06 15:39:09 -0300

    Fix function return in ESS/Common/Sensors component.

--- love/src/components/ESS/Common/Sensors.jsx
+++ love/src/components/ESS/Common/Sensors.jsx
@@ -3,7 +3,9 @@ This file is part of LOVE-frontend.
 
 Copyright (c) 2023 Inria Chile.
 
-Developed by Inria Chile.
+Developed by Inria Chile and the Telescope and Site Software team.
+
+Developed for the Vera C. Rubin Observatory Telescope and Site Systems.
 
 This program is free software: you can redistribute it and/or modify it under 
 the terms of the GNU General Public License as published by the Free Software 
@@ -130,7 +132,5 @@ Sensors.defaultProps = {
   values: [],
   speeds: [],
   directions: [],
-  getGradiantColorX: () => {
-    0xffff00;
-  },
+  getGradiantColorX: () => 0xffff00,
 };
