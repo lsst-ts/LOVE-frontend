68c02b4c3072405314aed569f7658b695c92f419
Sebastian Aranda <aranda.sebastian@gmail.com>
2026-02-19 12:39:01 -0300

    Refactor ScriptQueue/Scripts/ContextMenu component to get props using object destructuring.

--- love/src/components/ScriptQueue/Scripts/ContextMenu/ContextMenu.jsx
+++ love/src/components/ScriptQueue/Scripts/ContextMenu/ContextMenu.jsx
@@ -3,7 +3,9 @@ This file is part of LOVE-frontend.
 
 Copyright (c) 2023 Inria Chile.
 
-Developed by Inria Chile.
+Developed by Inria Chile and the Telescope and Site Software team.
+
+Developed for the Vera C. Rubin Observatory Telescope and Site Systems.
 
 This program is free software: you can redistribute it and/or modify it under 
 the terms of the GNU General Public License as published by the Free Software 
@@ -19,7 +21,6 @@ this program. If not, see <http://www.gnu.org/licenses/>.
 
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-
 import styles from './ContextMenu.module.css';
 
 export default class ContextMenu extends PureComponent {
@@ -66,12 +67,12 @@ export default class ContextMenu extends PureComponent {
   };
 
   render() {
-    const { target, children } = this.props;
+    const { isOpen, options, target, children } = this.props;
     const { offset } = this.state;
     const targetBoundingRect = target ? target.getBoundingClientRect() : { right: 0, bottom: 0 };
 
     return (
-      this.props.isOpen && (
+      isOpen && (
         <div
           className={styles.container}
           style={{
@@ -83,7 +84,7 @@ export default class ContextMenu extends PureComponent {
           onMouseDown={(e) => e.stopPropagation()}
         >
           <div className={styles.options}>
-            {this.props.options.map((child, index) => {
+            {options.map((child, index) => {
               return (
                 <div
                   className={[styles.row, child.disabled ? '' : styles.enabled].join(' ')}
