f713176df29665a15f759e5a92596b7dd1deb99b
Sebastian Aranda <aranda.sebastian@gmail.com>
2026-02-23 14:40:11 -0300

    Add estlint-plugin-react.

--- love/eslint.config.mjs
+++ love/eslint.config.mjs
@@ -1,5 +1,6 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import vitest from 'eslint-plugin-vitest';
@@ -22,12 +23,14 @@ export default [
       },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      'react/jsx-uses-vars': 'error',
       'no-unused-vars': ['error', { varsIgnorePattern: '^_', argsIgnorePattern: '^_$' }],
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
