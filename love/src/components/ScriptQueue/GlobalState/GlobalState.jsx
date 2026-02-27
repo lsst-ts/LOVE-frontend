f815b0cf949346427cfb364cd49cf924c5ea570e
Sebastian Aranda <aranda.sebastian@gmail.com>
2026-02-27 11:22:35 -0300

    Extend observatory statuses display to show full name for important statuses.

--- love/src/components/ScriptQueue/GlobalState/GlobalState.jsx
+++ love/src/components/ScriptQueue/GlobalState/GlobalState.jsx
@@ -47,6 +47,8 @@ const ALLOWED_COMMANDS = {
   STANDBY: ['start'],
 };
 
+const FULL_NAME_OBSERVATORY_STATES = ['OPERATIONAL', 'FAULT'];
+
 const OBSERVATORY_STATE_DETAIL = {
   0: {
     name: 'UNKNOWN',
@@ -86,23 +88,25 @@ function getActiveObservatoryStates(decimalValue) {
   return activeStatuses;
 }
 
-function renderObservatoryState(state, statusClass) {
+function renderObservatoryState(state, statusClass, acronymize = true) {
   return (
     <StatusText title={state} status={statusClass} small>
-      {acronymizeString(state)}
+      {acronymize ? acronymizeString(state) : state}
     </StatusText>
   );
 }
 
 const observatoryStateTooltip =
   'Current state of the observatory. ' +
-  'Only active statuses are color-coded and shown, ' +
-  'each following letter represents a different status:' +
-  '\n(D)aytime (green): daytime when on, nighttime when off, automatically set by the Scheduler CSC.' +
-  '\n(O)perational (green): set when the observatory is operating in normal state. Note this status can only be set during nighttime.' +
-  '\n(F)ault (red): set when a fault is detected in any subsystem. Automatically set by the Scheduler CSC, but can also be changed manually.' +
-  '\n(W)eather (red): set when weather conditions are not suitable for observations.' +
-  '\n(D)owntime (yellow): set during scheduled maintenance or upgrades.' +
+  'Only active statuses are color-coded and shown. ' +
+  'Important states like ' +
+  FULL_NAME_OBSERVATORY_STATES.join(' and ') +
+  ' are displayed with their full name, others just with their acronym format:' +
+  '\n· Daytime (green): daytime when on, nighttime when off, automatically set by the Scheduler CSC.' +
+  '\n· Operational (green): set when the observatory is operating in normal state. Note this status can only be set during nighttime.' +
+  '\n· Fault (red): set when a fault is detected in any subsystem. Automatically set by the Scheduler CSC, but can also be changed manually.' +
+  '\n· Weather (red): set when weather conditions are not suitable for observations.' +
+  '\n· Downtime (yellow): set during scheduled maintenance or upgrades.' +
   "\n\nHover over each status to see its full name. Additionally, 'UNKNOWN' status will be set and shown " +
   ' when transitioning from daytime to nighttime. ' +
   'It is responsibility of the observers to set it to operational when in nighttime. This status can be also shown when ' +
@@ -289,6 +293,9 @@ const GlobalState = ({
         {activeObservatoryStates.map((stateDetail) => {
           if (!stateDetail) {
             return renderObservatoryState(state, 'invalid');
+          }
+          if (FULL_NAME_OBSERVATORY_STATES.includes(stateDetail.name)) {
+            return renderObservatoryState(stateDetail.name, stateDetail.statusText, false);
           }
           return renderObservatoryState(stateDetail.name, stateDetail.statusText);
         })}
